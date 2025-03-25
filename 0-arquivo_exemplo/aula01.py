f5fe9bad0b73a2e4b053df965a112e2e6c15a4e5
yuripereira02 <yuyupg0204@gmail.com>
2025-03-25 17:16:54 -0300

    exercicio-2

--- 0-arquivo_exemplo/aula01.py
+++ 0-arquivo_exemplo/aula01.py
@@ -1,2 +1,4 @@
-nome=input('Qual é o seu nome?')
-print('Olá',nome,'! Prazer em te conhecer !')
+print("Hello world")
+nome =input('Qual o seu nome?')
+idade =input('Qual sua idade?')
+print (nome,idade)
