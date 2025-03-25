bf52b11a539298954bb6fdfc303751d996d4f776
João Pedro <joao.pedro@youxlab.com.br>
2025-03-25 17:33:57 -0300

    Terminando o desafio 06

--- Aula-07/desafio06.py
+++ Aula-07/desafio06.py
@@ -1,4 +1,8 @@
 num = int(input("Qual é o número? "))
-numDobro = num*2
-numTriplo = num*3
-numRaiz = num
+numDobro = num*2 #dobro
+numTriplo = num*3 #triplo
+numRaiz = num ** (1/2) #raiz
+
+print("O dobre de {} vale {}.".format(num, numDobro))
+print("O triplo de {} vale {}.".format(num, numTriplo))
+print("A raiz de {} vale {:.f}.".format(num, numRaiz)) 
