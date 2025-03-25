feb6cfc67ca665fadbef474de4d0fa69058410a0
Keverson Teodoro <keverson.teodoro@youxlab.com.br>
2025-03-25 17:32:31 -0300

    exercicio do numeric

---
+++ aula06/desafiomid.py
@@ -0,0 +1,6 @@
+#n1 = int(input('digite um número'))
+#n2 = int(input('digite um número'))
+#s = n1 + n2
+#print('a soma dos valores entre {} e {} é {}'.format(n1, n2, s))
+n = input('digite um valor ')
+print(n.isnumeric())
