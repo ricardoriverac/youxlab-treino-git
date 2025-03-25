c88e76842fd568eceec2257c934e80e9ca66258c
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-03-25 17:46:51 -0300

    fechando

---
+++ 04-ola_mundo/aula.py
@@ -0,0 +1,13 @@
+print ('olá, mundo!')
+print (7+4)
+print ('7'+'4')
+'7'+'4'
+nome = 'Vinícius'
+idade = 17
+peso = 82.2
+print (nome,idade,peso)
+nome = input('qual é o seu nome?')
+idade = input('quantos anos você tem?')
+peso = input('quanto você pesa?')
+print (nome,idade,peso)
+
