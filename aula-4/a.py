f133272884162534cc62d08c7371a9edd4e45e8a
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-03-25 17:40:03 -0300

    alice-python

--- aula-4/a.py
+++ aula-4/a.py
@@ -5,3 +5,7 @@ nome ='Alice'
 idade = 17
 altura = 1.60
 print(nome,idade,altura)
+nome = input ('Qual é o seu nome')
+idade = input ('Quantos anos você tem?')
+altura = input ('Qual é a sua altura?')
+print (nome, idade, peso)
