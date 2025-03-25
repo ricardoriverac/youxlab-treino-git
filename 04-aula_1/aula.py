e3b84122727db8f0ecfd2d46b7501860878c7de0
Maria Luiza <maria.silva@youxlab.com.br>
2025-03-25 17:35:01 -0300

    curso de python

---
+++ 04-aula_1/aula.py
@@ -0,0 +1,34 @@
+print ('Olá, mundo!')
+
+print (7+4)
+
+print ('7' + '4')
+
+print ('Olá' , 5)
+
+nome = 'maria luiza'
+idade = 16
+peso = 50.1
+print (nome, idade, peso)
+
+nome = input ('Qual é o seu nome?')
+
+idade = input ('Quantos anos vc tem?')
+
+peso = input ('Qual é o seu peso?')
+
+print (nome, idade, peso)
+
+nome = input ('Qual é o seu nome?')
+print ('Olá' + nome + '! Pazer em te conhecer!')
+
+dia = input ('Que dia você nasceu?')
+mês = input ('Que mês você nasceu?')
+ano = input ('Que ano você nasceu?')
+print ('Você nasceu no dia' +  dia  + ' de' +  mês  + ' de' +  ano + '. Correto?')
+
+número1 = int(input ('Primeiro número'))
+número2 = int(input ('Segundo número'))
+print ('A soma é' , número1 + número2)
+
+ 
