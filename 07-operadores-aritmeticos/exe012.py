d3513a3f48d6cc0eebee597033c81a0b876d92a9
juliocesar <juliogoiabinha060@gmail.com>
2025-03-25 19:59:15 -0300

    Arrumando erros

--- 07-operadores-aritmeticos/exe012.py
+++ 07-operadores-aritmeticos/exe012.py
@@ -1,4 +1,4 @@
 preco = float(input('Digite um valor de um preço R$: '))
-np = preco - (preco * 30/100)
+np = preco - (preco * 5/100)
 #calculo de porcentagem
 print(f'O preço que custuva {preco} agora com o desconto de 5% será de {np:.2f} ')
---
+++ 07-operadores-aritmeticos/exe013.py
@@ -0,0 +1,4 @@
+#ns novo salarío
+s = float(input('Digite um salarío: '))
+ns = s + (s * 15/100)
+print(f'O salario que antes era de {s} com aumento de 15% receberá {ns:.2f}')
