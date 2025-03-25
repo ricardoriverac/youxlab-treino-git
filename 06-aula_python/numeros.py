1610582b109c1abf6cfcccb4fdccea9e0baea962
GustavoValacio <gustavoseabra2007@gmail.com>
2025-03-25 17:48:03 -0300

    Numeros importantes e soma

---
+++ 06-aula_python/numeros.py
@@ -0,0 +1,18 @@
+n0 = input('Digite um valor:')
+print(type(n0))
+
+
+n1 = int(input('Digite um valor'))
+n2 = int(input('Digite um número'))
+s = n1 + n2
+print('A soma entre {0} e {1} vale {2}'.format(n1,n2, s))
+
+n3 = int(input('Digite um valor'))
+print(type(n3))
+
+
+n4 = float(input('Digite um número'))
+print(type(n4))
+
+n = bool(input('Digite um valor'))
+print(n)
