592f96b575338a03681fff874e518eb0bee80c17
fabricioyoux <fabricio.genesi@youxlab.com.br>
2025-03-25 18:06:35 -0300

    fiz a atividade 2 do Guanabara

--- 04-ola_mundo/ex002.py
+++ 04-ola_mundo/ex002.py
@@ -3,4 +3,4 @@ mes = 'Dezembro'
 ano = 2007
 
 print(f"Voce nasceu no dia {dia} de  {mes} de {ano} , Correto?")
-#print('Você nasceu dia {} do mês {} e ano {}'.format(dia, mes, ano))
+print('Você nasceu dia {} do mês {} e ano {}'.format(dia, mes, ano))
