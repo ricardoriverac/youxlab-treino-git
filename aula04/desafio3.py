8072e14a8c1ca52b22d4c86533f12e17b3bf23b6
Keverson Teodoro <keverson.teodoro@youxlab.com.br>
2025-03-25 17:15:51 -0300

    alteração no ex 3

--- aula04/desafio3.py
+++ aula04/desafio3.py
@@ -1,3 +1,9 @@
-n1 = int (input('digite um número'))
-n2 = int (input('digite outro número'))
-print(n1 + n2)
+n1 = int(input('digite um número:' ))
+n2 = int(input('digite outro número:' ))
+s = n1 + n2
+# print('a soma entre', n1, 'e', n2, 'vale' , s)
+print('a soma entre {} e {} vale {}'.format(n1, n2 , s))
+
+#n1 = int (input('digite um número'))
+#n2 = int (input('digite outro número'))
+#print(n1 + n2)
