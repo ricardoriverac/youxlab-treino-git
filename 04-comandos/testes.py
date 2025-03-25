04fad2a3ec4cfd171590b23e410f247e8c2333e5
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-03-25 15:27:39 -0300

    aprendendo comandos

---
+++ 04-comandos/testes.py
@@ -0,0 +1,11 @@
+print("Olá, mundo! ")
+print (7+4)
+print ("7"+"4")
+nome = 'kawanne'
+idade = 18 
+peso = 55.00
+print (nome,idade,peso)
+nome = input ('Qual é o seu nome ? ')
+idade = input ('Qual é a sua idade? ')
+peso = input ('Qual é o seu peso ? ')
+print (nome,idade,peso)
