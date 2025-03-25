c5340b06f1e17099edcb48ff31884c92685a2f5c
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-03-25 17:47:26 -0300

    Primeiro desafio

--- python-tayla/aula04.py
+++ python-tayla/aula04.py
@@ -1,7 +1,16 @@
+#aprendendo print
 print('Olá, Mundo!')
 print(7+4)
 print('7'+'4')
+
+#aprendendo variaves
 nome = 'Tayla'
 idade = '15'
 altura = '1.65m'
-print(nome,idade,altura) 
+print(nome,idade,altura)
+
+#aprendendo input
+nome = input('Qual é seu nome? ')
+idade = input('Quantos anos você tem? ')
+altura = input('Qual a sua altura? ')
+print(nome,idade,altura)
