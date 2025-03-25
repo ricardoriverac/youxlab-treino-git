2bd205ffc90fc12066e0d9b310bd416cdb4e7324
Marcos <marcos.garcia@youxlab.com.br>
2025-03-25 18:14:06 -0300

    Fiz novas atividades

--- aulas_python/atividade04.py
+++ aulas_python/atividade04.py
@@ -1,6 +1,6 @@
 print('Digite numeros para serem somados')
 
-n1=intinput('Numero 1:')
-n2=input('Numero 2:')
+n1=int(input('Numero 1:'))
+n2=int(input('Numero 2:'))
 s=n1+n2
 print('A soma dos dois é', s)
