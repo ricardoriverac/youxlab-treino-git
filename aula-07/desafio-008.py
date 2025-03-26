a480305e44e5e298766f499ad867165588cd3408
Keverson Teodoro <keverson.teodoro@youxlab.com.br>
2025-03-26 15:20:33 -0300

    desafio008

---
+++ aula-07/desafio-008.py
@@ -0,0 +1,6 @@
+vm = float(input('Valor em centimetro:'))
+cm = vm * 100
+mm =  vm * 1000
+
+print('O valor {} convertido para centimetro é {}'.format(vm , cm))
+print('o valor {} convertido para milimetro é {}'.format(vm , mm))
