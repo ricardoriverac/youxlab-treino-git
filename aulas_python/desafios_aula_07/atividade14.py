df1dff019dcb53bdaa6550f64d75ca9422a32b87
Marcos <marcos.garcia@youxlab.com.br>
2025-03-26 17:38:05 -0300

    Fiz um codigo que aumenta a % de um valor especifico

--- aulas_python/desafios_aula_07/atividade14.py
+++ aulas_python/desafios_aula_07/atividade14.py
@@ -1,8 +1,7 @@
-print('---Descontarei 5% do valor que colocar aqui---')
-v= float ( input('Qual o valor? '))
+print('---Aumentarei a % escolhida e o numero especifico---')
+v= float ( input('Qual o valor? (Sem desconto) '))
+p= float (input('Qual a porcentagem? (Para ser aumentada) '))
 
-p=5/100*v
-
-s=(v-p)
-
-print(s)
+e=p/100*v
+r=v+p
+print('O resultado é R$',r)
