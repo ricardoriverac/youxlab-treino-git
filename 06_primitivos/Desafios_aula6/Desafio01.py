f67e9a849a3cb8732adc53a7d684d6f56e725c24
VictorGabrielBarros <victor.barros@youxlab.com.br>
2025-03-26 16:55:21 -0300

    concluindo aula 06

---
+++ 06_primitivos/Desafios_aula6/Desafio01.py
@@ -0,0 +1,4 @@
+n1= int(input("Escreva um numero"))
+n2= int(input("Escreva outro numero"))
+s= n1 + n2
+print("A soma de {} com {} e {}".format(n1, n2, s))
---
+++ 06_primitivos/desafioextra.py
@@ -0,0 +1,5 @@
+n1= int(input("Digite um valor "))
+n2= int(input("Digite outro valor"))
+s = n1 + n2 
+#print("a soma de ", n1, "+", n2, "e igual a", s)
+print("A  soma entre {} + {} e igual a {}".format(n1, n2, s ))
