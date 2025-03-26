f709bdfffd9e0e86a7738ebf7ad1b54d59332442
Marcos <marcos.garcia@youxlab.com.br>
2025-03-26 16:03:07 -0300

    Fiz um tabuador automatico

---
+++ aulas_python/desafios_aula_07/atividade11.py
@@ -0,0 +1,23 @@
+print('---Tabuador automatic---')
+n1 = int(input('Qual numero você quer ver a tabuada? '))
+
+s = n1*1
+print('X1: {}'.format (s))
+s = n1*2
+print('X2: {}'.format (s))
+s = n1*3
+print('X3: {}'.format (s))
+s = n1*4
+print('X4: {}'.format (s))
+s = n1*5
+print('X5: {}'.format (s))
+s = n1*6
+print('X6: {}'.format (s))
+s = n1*7
+print('X7: {}'.format (s))
+s = n1*8
+print('X8: {}'.format (s))
+s = n1*9
+print('X9: {}'.format (s))
+s = n1*10
+print('X10: {}'.format(s))
