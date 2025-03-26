fbd59797fc01a95a65f8827459d9a7a046602928
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-03-26 14:35:17 -0300

    fiz os desafios do vídeo 06 de python

--- 04-curso_python/aula1.py
+++ 04-curso_python/aula1.py
@@ -15,7 +15,3 @@ nome = input ('Qual o seu nome?')
 idade = input ('Quantos anos você tem?')
 peso = input ('Quanto você pesa?')
 print  (nome , idade , peso)
-
-print ('DESAFIO 1')
-nome = input ('Qual seu nome?')
-print ('Olá' + nome  + '! Seja bem vindo!')
---
+++ teste/teste.py
@@ -0,0 +1,11 @@
+n1 = int(input ('Escolha um número:'))
+n2 = int(input ('Escolha outro número:'))
+s = n1+n2
+#print ('A soma entre ', n1 , ' e ' , n2 , ' vale ' , s)
+print ('a soma entre {} e {} vale {}'.format(n1, n2, s))
+
+n1d = float(input ('Escolha um número com ponto:'))
+n2d = float(input('Escolha outro:'))
+s = n1d+n2d
+print ('A soma:' , s)
+print (type(n1d))
