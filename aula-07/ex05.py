567b06e598b54cc37205a8eaa42193d123b1ca15
yuripereira02 <yuyupg0204@gmail.com>
2025-03-26 17:39:36 -0300

    dobro, triplo e raiz

---
+++ aula-07/ex05.py
@@ -0,0 +1,2 @@
+n1=int(input('Um valor '))
+print('O sucessor deste número é:', n1+1,'.' ' E o antecessor é', n1-1 )
---
+++ aula-07/ex06.py
@@ -0,0 +1,2 @@
+n1=int(input('Um valor '))
+print('Seu dobro é',n1*2,' seu triplo é', n1*3,' e sua raiz é',n1**(1/2))
