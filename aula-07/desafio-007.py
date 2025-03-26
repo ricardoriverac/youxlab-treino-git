600b1a9fb37187d5a1ff8e3b5829511375da1883
Keverson Teodoro <keverson.teodoro@youxlab.com.br>
2025-03-26 14:56:54 -0300

    desafio007

---
+++ aula-07/desafio-007.py
@@ -0,0 +1,4 @@
+nota = float(input('Digite sua nota'))
+nota2 = float(input('Digite sua outra nota'))
+m = (nota + nota2) / 2
+print('A média entre {: .1f} e {: .1f} é {: .1f}'.format(nota , nota2 , m))
