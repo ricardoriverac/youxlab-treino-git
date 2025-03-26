0b0367e371b3295d444e1c2a7e6b63e8a6706676
Marcos <marcos.garcia@youxlab.com.br>
2025-03-26 15:01:45 -0300

    Fiz um codigo que soma a media de duas notas ou numeros

---
+++ aulas_python/desafios_aula_07/atividade09.py
@@ -0,0 +1,6 @@
+print('----Calculador de media de notas de alunos----') 
+n1 = float ( input('Primeira nota: '))
+n2 = float ( input('Segunda nota: '))
+
+s = (n1+n2) /2
+print('A media desses numeros: {} {} é: {}'.format(n1, n2, s ))
