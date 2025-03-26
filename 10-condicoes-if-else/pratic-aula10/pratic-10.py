a880a449e7eea25d243f56fd405ee3de07fe7c17
juliocesar <juliogoiabinha060@gmail.com>
2025-03-26 20:45:33 -0300

    Pratica da aula10

---
+++ 10-condicoes-if-else/pratic-aula10/pratic-10.py
@@ -0,0 +1,9 @@
+#if significa "se" 
+#else significa "senão"
+#exemplo
+for c in range(0,5):
+    if c == 3 :
+        print('computador gerou um 3')
+    else:
+        print(c)
+print('FIM')            
