ae62b209453f37bfbfe9e57644ffdf7d9b40aa9c
João Pedro <joao.pedro@youxlab.com.br>
2025-03-26 17:06:53 -0300

    Terminando o exe14

---
+++ Aula-07/desafio14.py
@@ -0,0 +1,7 @@
+#escreva um programa que converte uma temperatura digitada em celsius para fahrenheit
+#OBS, não consegui fazer sozinho. olhei da aula do guanabara como fazia
+
+c = float(input("Informe a temperatura em Celsius: "))
+f = ((9 * c) / 5) + 32
+
+print("A temperatura de {} Celsius corresponde a {} Fahrenheit!".format(c, f)) 
