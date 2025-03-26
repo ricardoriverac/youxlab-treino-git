b2d40ee1ee8f8dd5b67df4ee22836edfd7c70b27
João Pedro <joao.pedro@youxlab.com.br>
2025-03-26 14:12:39 -0300

    Corrigindo o desafio 06, da aula 07

--- Aula-07/desafio06.py
+++ Aula-07/desafio06.py
@@ -1,10 +1,10 @@
 #Crie um algoritmo que leia um numero e mostre o seu dobro, triplo e raiz quadrada
-
 num = int(input("Qual é o número? "))
-numDobro = num*2 #dobro
-numTriplo = num*3 #triplo
-numRaiz = num ** (1/2) #raiz
 
-print("O dobre de {} vale {}.".format(num, numDobro))
+numDobro = num * 2  # Dobro
+numTriplo = num * 3  # Triplo
+numRaiz = num ** (1/2)  # Raiz quadrada
+
+print("O dobro de {} vale {}.".format(num, numDobro))
 print("O triplo de {} vale {}.".format(num, numTriplo))
-print("A raiz de {} vale {:.f}.".format(num, numRaiz)) 
+print("A raiz quadrada de {} vale {:.2f}.".format(num, numRaiz))
---
+++ Aula-07/desafio10.py
@@ -0,0 +1,3 @@
+#Crie um programa que leia quanto uma pessoa tem na carteira e mostre quantos dolares ela pode comprar
+#Considere US$1,00 = R$5,70
+
