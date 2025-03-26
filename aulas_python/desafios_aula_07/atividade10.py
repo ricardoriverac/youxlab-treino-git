a603c12ab559f11f08d829e80d27eb82415d400d
Marcos <marcos.garcia@youxlab.com.br>
2025-03-26 15:25:38 -0300

    Novo codigo criado, converte Metros em cm e mm

---
+++ aulas_python/desafios_aula_07/atividade10.py
@@ -0,0 +1,8 @@
+print('---Convertor de metros --> CM e MM---') 
+n1 = float ( input('Quantos metros?: '))
+
+s = n1*100
+print('{} Metros são: {} centimetros'.format(n1,s))
+
+s = n1*1000
+print('{} Metros são: {} milimetros'.format(n1,s))
