aa221f4418220e0faef9edd47912c7be04fd83fb
Marcos <marcos.garcia@youxlab.com.br>
2025-03-26 14:28:45 -0300

    Fiz um codigo que mostra o dobro, o triplo e a raiz

---
+++ aulas_python/desafios_aula_07/atividade08.py
@@ -0,0 +1,13 @@
+# dobro triplo e raiz 
+n1= int(input('Digite um numero inteiro: '))
+
+s = n1*2
+print('O dobro de: {} é: {}'.format(n1,s))
+
+s = n1*3 
+print('O triplo de: {} é: {}'.format(n1,s))
+
+s = n1** (1/2)
+print('A raiz quadrada de: {} é: {}'.format(n1,s))
+
+
