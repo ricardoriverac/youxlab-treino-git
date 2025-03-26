fcb5c4f60f3d14eda3c080ef805001bbeba4d293
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-03-26 16:56:53 -0300

    pratica aula 06

---
+++ python-tayla/aula-6/aula06.py
@@ -0,0 +1,9 @@
+#codigo pra mostrar o tipo do numero
+#n1 = (input('DIgite um valor: '))
+#print(type(n1))
+
+#resolução do desafio 3 de uma forma diferente
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro: '))
+s = n1 + n2
+print(f'A soma entre {n1} e {n2} vale', s)
