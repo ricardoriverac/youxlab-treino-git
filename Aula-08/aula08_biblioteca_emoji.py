61ce6be51c03ad4d0e158e094753753c60e8cb80
João Pedro <joao.pedro@youxlab.com.br>
2025-03-26 21:23:08 -0300

    Aprendendo a ultlizar a biblioteca emoji, na aula 08

---
+++ Aula-08/aula08_biblioteca_emoji.py
@@ -0,0 +1,5 @@
+#import emoji
+#print(emoji.emojize("Olá mundo :moneybag:", use_aliases=True))
+
+#Não funciona, porque não tenho a biblioteca instalada, e acredito que nao vou ultilizar isso
+#Então, não irei instalar
