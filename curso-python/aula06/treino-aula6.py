c49c775169596a12f9495bddf02774d8668b9388
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-03-27 14:51:19 -0300

    atualizando testes

---
+++ curso-python/aula06/treino-aula6.py
@@ -0,0 +1,17 @@
+# Exemplo de como fazer o sistema somar os números (alternativo)
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro: '))
+s = n1 + n2
+print ('A soma entre' ,n1, 'e' ,n2, ' vale' ,s)
+
+# Exemplo de como fazer o sistema somar os números (normal)
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro: '))
+s = n1 + n2
+print ('A soma vale' , s)
+
+# Exemplo de como fazer o sistema somar os números (alternativo)
+n1 = (input('Digite um valor: '))
+n2 = (input('Digite outro: '))
+s = n1 + n2
+print ('A soma vale' , s)
