663a0b3ecc85b3642da9dc8b8066f15dc2dfae42
VictorGabrielBarros <victor.barros@youxlab.com.br>
2025-03-27 14:29:21 -0300

    Atualizando Desafios

---
+++ 07_Aritméticos/Desafios/Desafio05.py
@@ -0,0 +1,4 @@
+n1= int(input("Insira um numero: "))
+ant= n1 -1
+suc= n1 + 1
+print("O numero {}, seu antecessor e {}, e seu sucessor e {}".format(n1,ant,suc,))
---
+++ 07_Aritméticos/Desafios/Desafio06.py
@@ -0,0 +1,6 @@
+n1=float(input("Insira um numero: "))
+dob= n1 * 2
+tri= n1 * 3
+raiz= n1 **(1/2)
+print(f"O dobro {n1} e  {dob}, o triplo {tri} e a raiz e {raiz}")
+
