cad947d6a796d6f2e4edbf76a5a8eb1b0ec47db8
Marcos <marcos.garcia@youxlab.com.br>
2025-03-27 18:04:05 -0300

    so para nao perder nada

--- aulas_python/desafios_aula_09/atividade23.py
+++ aulas_python/desafios_aula_09/atividade23.py
@@ -1,1 +1,7 @@
-n9 = int(input('Digite um numero de 0 a 9999: '))
+n9 = float(input('Digite um numero de 0 a 9999: '))
+
+w = n9//1000
+print('Seu milenio é {:.3f} '.format(w))
+
+e = n9//100
+print('Sua centena é {:.1f}'.format (e))
