0938944adb65f684910aaf411f4759aa5c6e6b25
marianne0614 <marianne.lima@youxlab.com.br>
2025-03-27 17:09:42 -0300

    exercicio2 da aula 7

---
+++ 04-estudo_logica_python/aula 7/ex002.py
@@ -0,0 +1,9 @@
+n1= int(input('digite um valor '))
+n2= int(input('outro valor '))
+soma=n1+ n2
+multiplicaçao=n1*n2
+divisao=n1/n2
+divisaointeira =n1//n2
+potencia= n1**n2
+print('a soma é {},o produto é {} e a divisao é {:.3f} '.format(soma, multiplicaçao, divisao))
+print('divisao inteira {} e potencia {} '.format(divisaointeira, potencia))
