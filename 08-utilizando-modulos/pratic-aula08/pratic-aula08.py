708008d6a56df96e55166018079092d4f849569e
juliocesar <juliogoiabinha060@gmail.com>
2025-03-27 01:42:28 -0300

    Pratica da aula08

---
+++ 08-utilizando-modulos/pratic-aula08/pratic-aula08.py
@@ -0,0 +1,10 @@
+import math 
+n = float(input('Digite um número: '))
+#arrendonda para baixo
+print(n.__ceil__())
+#valor boleano
+print(n.__bool__())
+#arrendonda para cima
+print(n.__float__())
+#vai truncar o número
+print(n.__trunc__())
