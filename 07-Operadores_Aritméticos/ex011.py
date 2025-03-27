24f554c133af450e8de5d0bdaf0536655072c614
fabricioyoux <fabricio.genesi@youxlab.com.br>
2025-03-27 14:17:29 -0300

    atividade 11

---
+++ 07-Operadores_Aritméticos/ex011.py
@@ -0,0 +1,6 @@
+n1 = float(input('Metros de uma parede: '))
+n2 = float(input('Largura de uma parede '))
+
+a = (n1*n2) 
+d = (a / 2)
+print('A área da parede {}, e gastará {} litros de tinta'.format(a, d))
