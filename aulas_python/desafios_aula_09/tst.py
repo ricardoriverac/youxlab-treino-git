2dab4e94d132979c99258aee4a16f79a8b0d2b93
Marcos <marcos.garcia@youxlab.com.br>
2025-03-27 17:20:50 -0300

    Codigo que fala seu nome completo, seu nome em maiusculo, seu nome em minusculo e as letras do seu primeiro nome

---
+++ aulas_python/desafios_aula_09/tst.py
@@ -0,0 +1,13 @@
+
+nomec = str(input('Qual seu nome completo? '))
+
+#nome com todas as letras maiusculas
+
+m = nomec.upper()
+print('Seu nome em maiusculo é: {}'.format(m))
+
+mi = nomec.lower()
+print('Seu nome em minusculo é: {}'.format(mi))
+
+
+print('Seu primeiro nome tem {} letras '.format(nomec.find(' ')))
