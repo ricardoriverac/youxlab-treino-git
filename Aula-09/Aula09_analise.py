865d6f80244b1744b3a20f8c0c1baa3614e762ce
João Pedro <joao.pedro@youxlab.com.br>
2025-03-27 14:41:14 -0300

    Terminando de aprender Análise, na manipulação de textos, aula 09

---
+++ Aula-09/Aula09_analise.py
@@ -0,0 +1,19 @@
+#Manipulando textos
+
+#Análise
+frase = "Aprendendo a programar em python."
+
+#len(frase) #Qual o comprimento da frase
+#len igual a comprimento
+
+#frase.count("a")
+#frase.count("o",0,13)
+#count igual a contar
+
+#frase.find("python") 
+#find igual a encontrar
+#Caso coloque algo que nao existe ele vai retornar -1, porque ele começa na posição 0
+#Então se receber -1, significa que não existe
+
+#"Aprendendo" in frase 
+#vai retornar um valor boleano, true ou false
