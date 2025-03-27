11044f20b5b347fe1cd963613b62e1944a7ad3d4
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-03-27 08:32:15 -0300

    Exercicios aula 6

---
+++ aula6/ex004.py
@@ -0,0 +1,5 @@
+n1= int (input('Me diga um numero:'))
+n2= int (input('O segundo:'))
+n3= int (input('O terceiro:'))
+
+print('Me diga {},{},{} '.format(n2,n3,n1))
---
+++ aula6/ex006.py
@@ -0,0 +1,7 @@
+ale = input('Digite algo: ')
+print(type (ale))
+
+print(ale.isalnum())
+print(ale.find())
+print(ale.encode())
+print(ale.
