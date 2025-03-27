f302efe5c6e2cb0982aa47614f2adfe520051ee3
marianne0614 <marianne.lima@youxlab.com.br>
2025-03-27 16:35:22 -0300

    exercicio da aula 7

---
+++ 04-estudo_logica_python/aula 7/ex001.py
@@ -0,0 +1,11 @@
+n1=int(input('um valor:'))
+n2=int(input('outro valor:'))
+print('a soma vale {}'.format(n1+n2))
+
+n1=int(input('um valor:'))
+n2=int(input('outro valor:'))
+print('a soma vale {}'.format(n1-n2))
+
+n1=int(input('um valor:'))
+n2=int(input('outro valor:'))
+print('a soma vale {}'.format(n1*n2))
