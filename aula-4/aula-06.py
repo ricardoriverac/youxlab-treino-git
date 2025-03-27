08c9ac20967f6b626067f6c490ee884f6b6298f0
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-03-27 16:41:15 -0300

    Testes aula 006

---
+++ aula-4/aula-06.py
@@ -0,0 +1,18 @@
+n = float(input('Digite um valor:'))
+print(n)
+n1 = bool(input('Digite um valor:'))
+print(n1)
+n2 = input('Digite um valor:')
+print(type(n2))
+n3 = input('Digite aqui:')
+print(n3.isnumeric())
+
+a = input('Digita aí:')
+print(a.isalpha())
+
+b = input('Digita aí:')
+print(b.isalnum())
+
+
+
+
