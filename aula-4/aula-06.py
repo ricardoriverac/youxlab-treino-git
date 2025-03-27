e0151b15c1c5b0619068ebce5f7838c64f5207c5
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-03-27 17:12:21 -0300

    Exercício 003

--- aula-4/aula-06.py
+++ aula-4/aula-06.py
@@ -1,15 +1,24 @@
+#printa na tela números reais utilizando o tipo primitivo float
 n = float(input('Digite um valor:'))
 print(n)
+
+#printa na tela um valor lógico true/false (verdadeiro/falso) utilizando o tipo booleano
 n1 = bool(input('Digite um valor:'))
 print(n1)
+
+#printa na tela o tipo primitivo utilizado no comando
 n2 = input('Digite um valor:')
 print(type(n2))
+
+#printa na tela se o que foi digitado é numérico ou não
 n3 = input('Digite aqui:')
 print(n3.isnumeric())
 
+#printa na tela se o que foi digitado é alfabético ou não
 a = input('Digita aí:')
 print(a.isalpha())
 
+#printa na tela se o que foi digitado é alfanumérico ou não
 b = input('Digita aí:')
 print(b.isalnum())
 
--- exercicios/ex003.py
+++ exercicios/ex003.py
@@ -1,5 +1,18 @@
-n1=int(input('Digite um número:'))
-n2=int(input('Digite mais um número:'))
-s=n1+n2
-#rint('A soma entre:',n1, 'e',n2,'vale:',s)
-print('A soma entre {} e {} vale {}'.format(n1,n2,s))
+#n1=int(input('Digite um número'))
+#2=int(input('Digite mais um número:'))
+#s=n1+n2
+#p#rint('A soma entre:',n1, 'e',n2,'vale:',s)
+#print('A soma entre {} e {} vale {}'.format(n1,n2,s))
+
+#exercício 3 da aula 006
+a1 =int(input('Digite um número:'))
+a2 =float(input('Agora um número real:'))
+s = a1+a2
+print('A soma entre {} e {} é igual a {}'.format(a1,a2,s))
+
+#exercício 4 da aula 006
+b1 = (input('Digite qualquer coisa'))
+print(b1.isnumeric())
+
+
+
