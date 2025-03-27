83bcb311535f9a026197a9abe609ca5264f3809f
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-03-27 14:19:39 -0300

    aula-7

---
+++ python-tayla/aula-7/aula07.py
@@ -0,0 +1,7 @@
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+p = n1 ** n2
+print(f'A soma é {s}, o produto é {m}, a divisão é {d} e a potência é {p}')
