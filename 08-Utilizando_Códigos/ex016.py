86f7d93fbb8f04ec6aa88b941433dda13f04f365
fabricioyoux <fabricio.genesi@youxlab.com.br>
2025-03-27 18:03:18 -0300

    atividade 17

--- 08-Utilizando_Códigos/ex016.py
+++ 08-Utilizando_Códigos/ex016.py
@@ -4,4 +4,4 @@ n1 = float(input('Número real qualquer '))
 
 p = math.floor(n1)
 
-print('O número {} tem a parte inteira {}.'.format(n1, p))
+print('O número {} tem a parte inteira {}.'.format(n1,p))
---
+++ 08-Utilizando_Códigos/ex017.py
@@ -0,0 +1,5 @@
+import math
+Co = float(input('Cateto O:'))
+Ca = float(input('Cateto A:'))
+h = math.sqrt(Co**2 + Ca**2)
+print('Os Catetos são {} e {}! A  hipotenusa é {}.  '.format(Co, Ca, h))
