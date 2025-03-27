895e649a84acd57a2225144ece35949a8735e2a3
yuripereira02 <yuyupg0204@gmail.com>
2025-03-27 13:43:36 -0300

    exercicios 05 a 13

--- aula-07/ex13.py
+++ aula-07/ex13.py
@@ -1,2 +1,2 @@
 s=int(input('Qual o seu salário? '))
-print('Seu salário com quinze por cento de aumento ficará {} reais'.format(s/100*115))
+print('Seu salário com quinze por cento de aumento ficará {} reais'.format(s/100*115))  
