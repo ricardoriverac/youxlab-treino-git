705a940a5559199446f19e5c1693bcfc95dea2dc
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-03-27 16:46:13 -0300

    fiz os desafios do vdo 07 e editei um erro no desafio 10

---
+++ exercicios.py/exerc5.py
@@ -0,0 +1,6 @@
+nuint = int(input('Digite um número:'))
+print( nuint )
+an = (nuint-1)
+su = (nuint+1)
+print('O antecessor de {} é {}'.format(nuint, an))
+print('O sucessor de {} é {}'.format(nuint, su))
