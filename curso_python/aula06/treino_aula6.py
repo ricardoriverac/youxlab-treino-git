d80cc1c6ad8a2cd1cd8e0de1a41625f36612e8ad
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-03-27 17:24:52 -0300

    corrigindo erros

--- curso_python/aula06/treino_aula6.py
+++ curso_python/aula06/treino_aula6.py
@@ -7,11 +7,11 @@ print ('A soma entre {} e {} vale {}' .format (n1, n2, soma))
 # Como fazer o sistema somar os números (normal)
 n1 = int(input('Digite um valor: '))
 n2 = int(input('Digite outro: '))
-s = n1 + n2
+soma = n1 + n2
 print ('A soma vale' , soma)
 
 # Como fazer o sistema só juntar os números
 n1 = (input('Digite um valor: '))
 n2 = (input('Digite outro: '))
-s = n1 + n2
+soma = n1 + n2
 print ('A soma vale' , soma)
