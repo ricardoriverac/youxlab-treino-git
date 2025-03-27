4eee6ed52492f55a54fd255811f49351f456826c
GustavoFreireGomes <gustavo.gomes@youxlab.com.br>
2025-03-27 17:30:43 -0300

    desafio1,2

---
+++ 08-Aula_Modulos/Desafio1.py
@@ -0,0 +1,4 @@
+from math import trunc
+num=float(input('digite um valor '))
+print('o valor de inteiro e {} e a porçaõ e {}'.format(num, trunc(num)))
+
---
+++ 08-Aula_Modulos/Pratica.py
@@ -0,0 +1,5 @@
+import math
+num=float(input('Digite um valor:  '))
+raiz = math.sqrt(num)
+print('a raiz e {}'.format(raiz))
+
