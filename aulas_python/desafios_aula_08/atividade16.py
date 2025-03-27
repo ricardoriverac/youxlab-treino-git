176ff4455f1a9ffa229f139a2b410605da4fe451
Marcos <marcos.garcia@youxlab.com.br>
2025-03-26 22:58:12 -0300

    Fiz um conversor de numeros reais pra porçoes inteiras

---
+++ aulas_python/desafios_aula_08/atividade16.py
@@ -0,0 +1,7 @@
+import math
+
+print('---Transformar números reais em porções inteiras---')
+
+n1 = float( input('Digite um valor: '))
+
+print('O valor digitado é {} e sua porção inteira é {}'.format(n1, math.trunc(n1)))
