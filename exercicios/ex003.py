b5ee041c771da3f4efcbd63bf05dae8326417af2
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-03-27 17:15:05 -0300

    Exercício 003

--- exercicios/ex003.py
+++ exercicios/ex003.py
@@ -13,6 +13,8 @@ print('A soma entre {} e {} é igual a {}'.format(a1,a2,s))
 #exercício 4 da aula 006
 b1 = (input('Digite qualquer coisa'))
 print(b1.isnumeric())
+print(b1.isalpha())
+print(b1.isalnum())
 
 
 
