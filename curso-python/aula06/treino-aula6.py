1df9cbd0eb1314d2aed244463458ea1148cfb5a7
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-03-27 16:06:18 -0300

    corrigindo ortografia

--- curso-python/aula06/treino-aula6.py
+++ curso-python/aula06/treino-aula6.py
@@ -2,7 +2,7 @@
 n1 = int(input('Digite um valor: '))
 n2 = int(input('Digite outro: '))
 s = n1 + n2
-print ('A soma entre {} e {} vale {}' .format (n1, n2, s)
+print ('A soma entre {} e {} vale {}' .format (n1, n2, s))
 
 # Como fazer o sistema somar os números (normal)
 n1 = int(input('Digite um valor: '))
