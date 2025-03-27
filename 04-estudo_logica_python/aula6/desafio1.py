0f75fb4fa16093cb3e5f53c0abb43386f02d1fca
marianne0614 <marianne.lima@youxlab.com.br>
2025-03-27 13:32:29 -0300

    desafio 4

--- 04-estudo_logica_python/aula6/desafio1.py
+++ 04-estudo_logica_python/aula6/desafio1.py
@@ -1,4 +1,4 @@
-n1=int (input('dite um numero:'))
-n2=int(input('digite outro numero:'))
+n1=float (input('dite um numero:'))
+n2=float(input('digite outro numero:'))
 s=n1+n2
-print('a soma entre {} e {} vale {}'. str(n1, n2, s))
+print('a soma entre {} e {} vale {}'. format(n1, n2, s))
