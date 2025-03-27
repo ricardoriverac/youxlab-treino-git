8e2e8105dd07968e313b2b2145b777ef70506d2e
juliocesar <juliogoiabinha060@gmail.com>
2025-03-27 00:11:18 -0300

    Adicionado a pasta exercicio 034

--- 10-condicoes-if-else/exercicios-aula10/exe034.py
+++ 10-condicoes-if-else/exercicios-aula10/exe034.py
@@ -3,4 +3,4 @@ if sl >= 1250.00 :
     aum = sl + (sl * 10/100)
 else:
     aum = sl + (sl * 15/100)
-print(f'Quem ganhava R${sl} passar a ganhar R${aum:.2f} agora')        
+print(f'Quem ganhava R${sl} passar a ganhar R${aum:.2f} agora')
