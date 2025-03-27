0e9d75da51f2e94aac879dcf8cac43e8264f232f
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-03-27 16:20:31 -0300

    alterando linhas

--- curso-python/aula04/desafio 3 aula04.py
+++ curso-python/aula04/desafio 3 aula04.py
@@ -2,4 +2,3 @@ n1 = int (input ('Digite um número:'))
 n2 = int (input ('Digite mais um número:'))
 s = n1 + n2
 print ('A soma vale',s)
-print ('A soma vale{}'.format(s))
---
+++ curso-python/aula06/treino2-aula6.py
@@ -0,0 +1,7 @@
+# qualquer número inteiro, vira número com . (4 vira 4.0)
+n = float(input('Digite um valor: '))
+print (n)
+
+# se digitar qualquer número/tecla, ele dirá 'True', se não, dirá 'False'
+n = bool(input('Digite um valor: '))
+print (n)
