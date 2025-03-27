63cae5d012d0992a348e192332dfaaf000840a1f
yuripereira02 <yuyupg0204@gmail.com>
2025-03-27 10:44:17 -0300

    exercicios 5 a 13

--- aula-07/ex09.py
+++ aula-07/ex09.py
@@ -1,2 +1,2 @@
 n=int(input('Me fale um valor '))
-print('A tabuada desse valor é',n*1,n*2,n*3,n*4,n*5,n*6,n*7,n*8,n*9,n*10)
+print('A tabuada desse valor é, 1 x {} = {}, 2 x {} = {}, 3 x {} = {}, 4 x {} = {}, 5 x {} = {}, 6 x {} = {}, 7 x {} = {}, 8 x {} = {}, 9 x {} = {}, 10 x {} = {}'.format(n,n*1,n,n*2,n,n*3,n,n*4,n,n*5,n,n*6,n,n*7,n,n*8,n,n*9,n,n*10))
