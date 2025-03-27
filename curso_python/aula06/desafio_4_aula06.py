fc83f655476c334ce8c8720f3b5a5cdf37fe3b9a
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-03-27 17:41:49 -0300

    salvando desafio 4

--- curso_python/aula06/desafio_4_aula06.py
+++ curso_python/aula06/desafio_4_aula06.py
@@ -1,6 +1,7 @@
 # Para verificar se o valor é númerico; 
-n = input('Digite algo: ')
-print (n.isnumeric())
-print (n.isalpha())
-print (n.isupper())
-print (n.islower())
+n = str (input('Digite algo: '))
+print(n.isnumeric())
+print(n.isalpha())
+print(n.isupper())
+print(n.islower())
+print(n.isdecimal())
