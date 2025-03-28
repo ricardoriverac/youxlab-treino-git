2efd71e6d0646dda993dbceb9ea9ae62ca0dba5d
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-03-28 13:01:17 -0300

    ex009

---
+++ aula007/ex009.py
@@ -0,0 +1,19 @@
+n =int (input('Me diga um numero: '))
+
+print ('O numero {} X {} = {}'.format(n,2,n*2))
+
+print('O numero {} X {} = {} '.format(n,3,n*3))
+
+print('O numero {} X {} = {} '.format(n,4,n*4))
+
+print('O numero {} X {} = {} '.format(n,5,n*5))
+
+print('O numero {} X {} = {} '.format(n,6,n*6))
+
+print('O numero {} X {} = {} '.format(n,7,n*7))
+
+print('O numero {} X {} = {} '.format(n,8,n*8))
+
+print('O numero {} X {} = {} '.format(n,9,n*9))
+
+print('O numero {} X {} = {} '.format(n,10,n*10))
