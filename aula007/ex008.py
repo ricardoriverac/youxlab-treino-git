6b8e79e284f76313318ce8433736a585363a0795
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-03-28 12:59:54 -0300

    ex008

---
+++ aula007/ex008.py
@@ -0,0 +1,15 @@
+metros = int(input('Me diga a distancia em metros :'))
+
+km = metros/1000
+
+hm = metros/100
+
+dam = metros/10
+
+dm = metros*10
+
+cm = metros*100
+
+mm = metros*1000
+
+print ('O valor em km e {} em hm {} em dm {} em cm {}\ em mm {}'.format(km,hm,dam,dm,cm,mm))
