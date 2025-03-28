2d95c5738907c795b2786561318433b65ae70c3b
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-03-28 15:03:37 -0300

    aula08.py

---
+++ python-tayla/aula-8/aula08.py
@@ -0,0 +1,12 @@
+#pratica dos modulos
+
+#raiz quadrada
+import math
+numero = int(input('Digite um número: '))
+raiz = math.sqrt(numero)
+print(f'A raiz de {numero} é igual a {raiz}')
+
+#gerando numeros aleatorios
+import random
+num = random.randint(1,10)
+print(num)
