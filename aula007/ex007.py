78e1b4759dddec26a3b7572d2cb69b75185d31a4
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-03-28 12:57:58 -0300

    ex007

---
+++ aula007/ex007.py
@@ -0,0 +1,7 @@
+n1 = int (input('Digite a primeira nota: '))
+
+n2 = int (input('A segunda nota: '))
+
+media = (n1+n2)/2
+
+print ('A primeira nota foi {} a segunda foi {}, entao a media foi de {}'.format(n1,n2,media))
