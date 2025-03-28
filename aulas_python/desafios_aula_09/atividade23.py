176e67754cbfafb124be698961746ca9aefab912
Marcos <marcos.garcia@youxlab.com.br>
2025-03-27 21:48:15 -0300

    Fiz a atividade 23 do guanabara

--- aulas_python/desafios_aula_09/atividade23.py
+++ aulas_python/desafios_aula_09/atividade23.py
@@ -1,7 +1,8 @@
-n9 = float(input('Digite um numero de 0 a 9999: '))
+num = int(input('Informe um numero: '))
+n = str(num)
 
-w = n9//1000
-print('Seu milenio é {:.3f} '.format(w))
-
-e = n9//100
-print('Sua centena é {:.1f}'.format (e))
+print('Analisando o número {}'.format(num))
+print('Unidade: {}'.format(n[3]))
+print('Dezena: {}'.format(n[2]))
+print('Centena: {}'.format(n[1]))
+print('Milhar: {}'.format(n[0]))
---
+++ aulas_python/desafios_aula_09/teste.py
@@ -0,0 +1,11 @@
+n9 = int(input('Digite um numero de 0 a 9999: '))
+
+m = n9//1000
+print('Seu milenio é {} '.format(m))
+
+c = n9//100
+print('Sua centena é {}'.format (c))
+
+d = n9//10
+print('Sua dezena é {:5f}'.format (d))
+
