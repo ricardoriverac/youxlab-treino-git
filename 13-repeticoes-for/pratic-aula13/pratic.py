1f916a44df23a15adb2d121dff4818b07aa32a6b
juliocesar <juliogoiabinha060@gmail.com>
2025-03-28 14:37:54 -0300

    Repeticoes for

--- 13-repeticoes-for/pratic-aula13/pratic.py
+++ 13-repeticoes-for/pratic-aula13/pratic.py
@@ -1,5 +1,17 @@
-for c in range(0,6):
+#for c in range(0,6):
+    #print(c)
+    #if c == 3 :
+        #print('\033[31mDestaquei o "3"\033[m')
+#print('\033[32mFIM\033[m')
+#........................................
+n = int(input('Digite um número:'))
+f = int(input('Fim'))
+p = int(input('Passo'))
+for c in range(n,f+1,p):
     print(c)
-    if c == 3 :
-        print('\033[31mDestaquei o "3"\033[m')
-print('\033[32mFIM\033[m')
+#
+s = 0
+for c in range(0,4):
+    n = int(input('Digite um número'))
+    s += n
+print(f'A soma dos valores digitados foi {s}')
