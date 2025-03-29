649625674130aff2943535d395302027561050bd
juliocesar <juliogoiabinha060@gmail.com>
2025-03-28 23:34:34 -0300

    Repeticoes for

--- 13-repeticoes-for/exercicios-aula13/exe052.py
+++ 13-repeticoes-for/exercicios-aula13/exe052.py
@@ -1,7 +1,14 @@
 num = int(input('Digite o número: '))
+tot = 0 
 for c in range(1 , num +1):
     if num  % c == 0:
+        tot += 1
         print('\033[32m', end=" ")
     else:
         print('\033[31m', end=" ")
-    print(f'{c}', end=" ")
+    print(f'{c}', end=' ')
+print(f'\n\033[30mO número {num} foi divisivél {tot} vezes')
+if tot == 2 :
+    print('É por isso ele é número primo')
+else:
+    print('É por isso ele não é número primo')
