15ca47fb3f59e3d9b3599990a6140e7b6542fb2d
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-03-29 18:37:59 -0300

    catetos

---
+++ aula008/ex017.py
@@ -0,0 +1,4 @@
+from math import hypot
+catoposto =float(input('O tamanho do cateto oposto: '))
+catadjacente =float(input('O tamanho do cateto adjacente: '))
+print('O valor da hipotenusa e igual a {:.2f} '.format(hypot(catoposto,catadjacente)))
