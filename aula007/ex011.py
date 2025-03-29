1bede337eabfa041c0dea61dfad687a2e5f10428
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-03-29 14:23:07 -0300

    Calculando tinta

---
+++ aula007/ex011.py
@@ -0,0 +1,3 @@
+alt = int(input('Me diga a aultura'))
+largura = int(input('Me diga qual a largura: '))
+print('A area a ser pintada e {} então serão necessarios {} de tinta '.format(alt*largura,alt*largura/2))
