5fa18c9867cd9e88db050d416320d73d52c8be39
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-03-29 11:43:35 -0300

    aula-9

---
+++ python-tayla/aula-9/aula09.py
@@ -0,0 +1,23 @@
+#fatiando a frase
+#frase = 'Curso em Vídeo Python'
+#print(frase[0:15:2])
+
+#lendo quantas vezes aparece a letra
+#frase = 'Curso em Vídeo Python'
+#print(frase.count('0'))
+
+#frase = 'Curso em Vídeo Python'
+#print(frase.upper().count('O'))
+
+#vendo o tamanho da frase
+#frase = 'Curso em Vídeo Python'
+#print(len(frase))
+
+#trocando as palavras
+#frase = 'Curso em Vídeo Python'
+#frase = frase.replace('Python', 'Android')
+#print(frase)
+
+#usando split
+frase = 'Curso em Vídeo Python'
+print(frase.split())
