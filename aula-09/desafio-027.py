03786fe0c9f051202525997b777fe23538ac7785
Keverson Teodoro <keverson.teodoro@youxlab.com.br>
2025-03-29 00:41:43 -0300

    desafio 27

---
+++ aula-09/desafio-027.py
@@ -0,0 +1,4 @@
+nome = str(input('Digite seu nome:')).split()
+print('Prazer em te conhecer, {}!'.format(nome))
+print('Seu primeiro nome é {}'.format(nome[0]))
+print('seu último nome é {}'.format(nome[len(nome)-1]))
