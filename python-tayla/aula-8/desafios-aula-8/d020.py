59071f9b90d7582a302125529b66e1580d6db556
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-03-29 10:14:29 -0300

    desafio 20

--- python-tayla/aula-8/desafios-aula-8/d020.py
+++ python-tayla/aula-8/desafios-aula-8/d020.py
@@ -1,8 +1,8 @@
 import random
-primeiro = input('Nome do primeiro aluno: ')
-segundo = input('Nome do segundo aluno: ')
-terceiro = input('Nome do terceiro aluno: ')
-quarto = input('Nome do quarto aluno: ')
+primeiro = input('Nome do primeiro aluno(a): ')
+segundo = input('Nome do segundo aluno(a): ')
+terceiro = input('Nome do terceiro aluno(a): ')
+quarto = input('Nome do quarto aluno(a): ')
 lista = (primeiro,segundo,terceiro,quarto)
 sorteio = random.choice(lista)
 print(f'O aluno escolhido é {sorteio}')
