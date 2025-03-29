5f1bb45f270045c776c02f6a2521ede7a7bf3b2e
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-03-29 11:44:15 -0300

    aula-9

--- python-tayla/aula-9/aula09.py
+++ python-tayla/aula-9/aula09.py
@@ -1,22 +1,22 @@
 #fatiando a frase
-#frase = 'Curso em Vídeo Python'
-#print(frase[0:15:2])
+frase = 'Curso em Vídeo Python'
+print(frase[0:15:2])
 
 #lendo quantas vezes aparece a letra
-#frase = 'Curso em Vídeo Python'
-#print(frase.count('0'))
+frase = 'Curso em Vídeo Python'
+print(frase.count('0'))
 
-#frase = 'Curso em Vídeo Python'
-#print(frase.upper().count('O'))
+frase = 'Curso em Vídeo Python'
+print(frase.upper().count('O'))
 
 #vendo o tamanho da frase
-#frase = 'Curso em Vídeo Python'
-#print(len(frase))
+frase = 'Curso em Vídeo Python'
+print(len(frase))
 
 #trocando as palavras
-#frase = 'Curso em Vídeo Python'
-#frase = frase.replace('Python', 'Android')
-#print(frase)
+frase = 'Curso em Vídeo Python'
+frase = frase.replace('Python', 'Android')
+print(frase)
 
 #usando split
 frase = 'Curso em Vídeo Python'
