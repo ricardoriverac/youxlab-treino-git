4072e4eec615de999226a96dc4803daa499acb80
Marcos <marcos.garcia@youxlab.com.br>
2025-03-30 00:06:57 -0300

    Atualização

--- aulas_python/desafios_aula_10/atividade28.py
+++ aulas_python/desafios_aula_10/atividade28.py
@@ -1,5 +1,5 @@
 import random
-print('---Tente adivinhar o numero que o computador está pensando---')
+print('---Tente adivinhar o numero que o computador está pensando (De 0 a 10)---')
 
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
