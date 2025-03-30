f8e60039d6e5d3502f361a286c21b32ffa44b62f
Marcos <marcos.garcia@youxlab.com.br>
2025-03-30 00:05:30 -0300

    Fiz um codigo que você tem que tentar acertar um numero de 1 a 10

---
+++ aulas_python/desafios_aula_10/atividade28.py
@@ -0,0 +1,14 @@
+import random
+print('---Tente adivinhar o numero que o computador está pensando---')
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+e = random.choice(numeros)
+
+y = int(input('Qual o número que ele esta pensando? '))
+
+if y == (e): 
+    print('Você acertou, parabens!')
+
+else:
+    print('Você errou, tente novamente..')
