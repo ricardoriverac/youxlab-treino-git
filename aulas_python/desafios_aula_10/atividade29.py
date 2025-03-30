3a9154e31d7f3c2ae3313f25daf19a176ac8ae78
Marcos <marcos.garcia@youxlab.com.br>
2025-03-30 00:43:38 -0300

    atualizações ;)

---
+++ aulas_python/desafios_aula_10/atividade29.py
@@ -0,0 +1,21 @@
+v = float(input('Qual a velocidade que seu carro estava? '))
+
+if v >= 80:
+    print('Você foi multado')
+    
+    
+else:
+    print('Você não foi multado')
+    
+m =  v >= 80
+
+m2 = m*7
+
+
+if v :
+    
+    
+    print('sua multa é de {}'.format(m2) )
+    
+else: 
+    print('Não teve multa ;)')
