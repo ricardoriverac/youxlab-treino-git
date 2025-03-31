e2ef6941c95cf5c34b92fce37fd35cac69dc1a5e
Marcos <marcos.garcia@youxlab.com.br>
2025-03-31 16:31:50 -0300

    Testes :)

---
+++ aulas_python/desafios_aula_11/teste4.py
@@ -0,0 +1,7 @@
+nome = 'Garcia'
+cores = {'limpa':'\033[m',
+         'azul' :'\033[34m',
+         'amarelo':'\033[33m',
+         'pretoebranco':'\033[7;30m'}
+
+print('Olá! Muito prazer em te conhecer, {}{}{}!!'.format(cores['amarelo'], nome, (cores['limpa'], nome, cores['limpa'])))
