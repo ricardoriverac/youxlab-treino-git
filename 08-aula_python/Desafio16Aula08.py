8d54ae617df1db891235e20d070d6f2866f74b0f
GustavoValacio <gustavoseabra2007@gmail.com>
2025-03-31 14:04:37 -0300

    Desafio16 porçao inteira

---
+++ 08-aula_python/Desafio16Aula08.py
@@ -0,0 +1,6 @@
+''' from math import trunc
+num = float(input('Digite seu valor'))
+print('O valor que você digitou fou {} e a sua porção inteira é {}'. format (num, trunc(num))) '''
+
+num = float(input('Digite um valor'))
+print('O valor que você digitou foi {} e a sua porcção inteira é {}'. format (num, int(num)))
