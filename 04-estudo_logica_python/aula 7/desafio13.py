7b33aabf794410cef6161da2d4703c6a2ac05dff
marianne0614 <marianne.lima@youxlab.com.br>
2025-03-31 16:57:09 -0300

    desafio 13

---
+++ 04-estudo_logica_python/aula 7/desafio13.py
@@ -0,0 +1,4 @@
+salario=float(input(' Digite o valor:'))
+porcentagem= 15/100*salario
+valor=(salario +porcentagem )
+print(' analisando salario{}, o aumento é {}, e valor total é {};'.format(salario, porcentagem, valor))
---
+++ desafio8.py
@@ -0,0 +1,4 @@
+metro= int(input('Digite um numero metros :'))
+centimetro= metro *100
+milimetro= metro *1000
+print('analisando seu valor em {} m,em centimetros é {}, e em milimetro {}'. format(metro, centimetro, milimetro))
