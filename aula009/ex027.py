8cc973d9cfc95345e79c1f3bd929005e9b7b17b1
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-03-31 14:42:50 -0300

    Ultimo e primeiro nome

---
+++ aula009/ex027.py
@@ -0,0 +1,5 @@
+nome =str (input('Me ediga um nome: ')).strip()
+total = nome.split()
+print('Seu primeiro nome e: {}'.format(total[0]))
+print ('Seu ultimo nome e: {} '.format(nome[len(nome)-1]))
+
