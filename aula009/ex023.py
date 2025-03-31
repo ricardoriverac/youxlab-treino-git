9d46b1edb8778ccdaddd0f0d9fa8a2a90eefc2b0
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-03-31 13:25:36 -0300

    Analidando um numero

---
+++ aula009/ex023.py
@@ -0,0 +1,11 @@
+num = int(input('Informe um nukmero '))
+u =num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print ('Analisando  o numero ')
+print (' A unidade e {} '.format(u))
+print (' A dezena e {} '.format(d))
+print (' A centena  e {} '.format(c))
+print (' A milhare e {} '.format(m))
+
