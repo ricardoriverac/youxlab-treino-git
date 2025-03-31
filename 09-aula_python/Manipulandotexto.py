4e65076e113930da187138077491f2059984509e
GustavoValacio <gustavoseabra2007@gmail.com>
2025-03-31 16:49:02 -0300

    ManipulandoTextos

---
+++ 09-aula_python/Manipulandotexto.py
@@ -0,0 +1,19 @@
+frase = 'Curso em video Python'
+print(frase[ ::2])
+print('Oi')
+
+print("""opa bão ou nõa tá tudo bom jjedjkbfehbfwekjjsncbhekwbcewhlcbwbecewlibcewlucbjewkcbwucbwjwiebcwjewucbjwebckwbjkjwb""")
+
+frase1 = 'Opa bao'
+print(frase1.upper().count('o'))
+print(len(frase1))
+print (frase.replace('Python', 'Opa'))
+print(frase[0])
+print('Curso' in frase)
+print(frase.find ('Python'))
+print(frase.lower().find ('python'))
+print(frase.split())
+
+frase3 = 'Curso em video python'
+dividido = frase.split()
+print(dividido[2][3])
