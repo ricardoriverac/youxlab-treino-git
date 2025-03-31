1c27d9843357da6dc30952d6bcabf4e5323aa1f7
marianne0614 <marianne.lima@youxlab.com.br>
2025-03-31 16:17:31 -0300

    desafio 11

---
+++ 04-estudo_logica_python/aula 7/desafio11.py
@@ -0,0 +1,5 @@
+largura=float(input('digite a largura da parede : '))
+altura= float(input(' Digitea altura da parede:'))
+area=largura * altura
+tinta= area/2
+print('Analizando a largura {},a altura {}, a area é :{}, e quantidade de tinta é {} '. format(largura, altura, area, tinta))
