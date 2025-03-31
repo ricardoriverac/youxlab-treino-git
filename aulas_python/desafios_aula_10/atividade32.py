a1b978428e7b125d269533e5e5d632390664632c
Marcos <marcos.garcia@youxlab.com.br>
2025-03-31 13:52:11 -0300

    Fiz um codigo que mostra se o ano é bissexto ou não

---
+++ aulas_python/desafios_aula_10/atividade32.py
@@ -0,0 +1,8 @@
+n = int(input('Digite um ano qualquer pra saber se é bissexto ou não: '))
+
+if n %4 == 0:
+    print('Esse ano é bissexto!')
+    
+else:
+    print('Esse ano não é bissexto!')
+    
