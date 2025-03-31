34f740a459a98c470990fdc358862b51365b3248
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-03-31 16:16:47 -0300

    quebrando um número]

--- exercicios/ex16.py
+++ exercicios/ex16.py
@@ -1,6 +1,2 @@
-#from math import trunc
-#num = float (input ("Digite um valor: "))
-#print ("O valor digitado foi {} e a sua porção inteira é {}".format(num,trunc(num))
-
 num = float (input ("Digite um valor : "))
 print ("O valor digitado foi {:.2f} e a sua porção é {:.2f}".format(num,int(num)))
