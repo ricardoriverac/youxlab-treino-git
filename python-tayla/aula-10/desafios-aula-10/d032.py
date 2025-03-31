18bd694344f3c0fe90697110f461dc8856fd2109
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-03-31 16:39:04 -0300

    desafio 32

---
+++ python-tayla/aula-10/desafios-aula-10/d032.py
@@ -0,0 +1,11 @@
+import datetime
+ano = int(input('Que ano quer analisar? Coloque 0 para analisar o ano atual: '))
+data_atual = datetime.datetime.now()
+ano_atual = data_atual.year
+if ano == 0:
+    ano = ano_atual
+    print(f'Estamos no ano {ano}')
+if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0) :
+    print('Esse ano é bissexto')
+else:
+    print('Esse ano não é bissexto')
