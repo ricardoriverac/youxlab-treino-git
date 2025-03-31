f23c56dc965b07029ebb9d66b708204b177ede6c
GustavoFreireGomes <gustavo.gomes@youxlab.com.br>
2025-03-31 14:14:50 -0300

    novos

--- 08-Aula_Modulos/Desafio6.py
+++ 08-Aula_Modulos/Desafio6.py
@@ -1,5 +1,8 @@
 import pygame
+from time import sleep
 pygame.init()
 pygame.mixer.music.load('/home/youx/youxlab-treino-git/Desafio6.mp3')
 pygame.mixer.music.play()
 pygame.event.wait()
+while pygame.mixer.music.get_busy():
+    sleep (1)
