5becbf48ecc8ad8804ff0b2fe6125eeb7ac7daf3
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-03-31 17:37:27 -0300

    desafio 34

--- python-tayla/aula-10/desafios-aula-10/d034.py
+++ python-tayla/aula-10/desafios-aula-10/d034.py
@@ -1,7 +1,7 @@
 salario = float(input('Qual é o seu salário? R$'))
 aumento1 = salario * 0.10
 aumento2 = salario * 0.15
-if salario > 1.250:
+if salario > 1250:
     print(f'Parabéns você recebeu 10% de aumento. Seu novo salário é de R${aumento1 + salario:.3f}')
-if salario < 1.250:
+if salario < 1250:
     print(f'Parabéns você recebeu 15% de aumento. Seu novo salário é de {aumento2 + salario:.3f}')
