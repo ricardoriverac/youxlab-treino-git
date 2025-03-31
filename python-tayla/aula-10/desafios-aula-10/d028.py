076abbc76785812b21e2896ffe77d00149c071e9
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-03-31 14:52:59 -0300

    desafio 28

--- python-tayla/aula-10/desafios-aula-10/d028.py
+++ python-tayla/aula-10/desafios-aula-10/d028.py
@@ -1,7 +1,7 @@
 from random import randint
-numero = randint(0,5)
+computador = randint(0,5)
 usuario = int(input('Escolha um número inteiro entre 0 e 5: '))
-if usuario == numero:
+if usuario == computador:
     print('Você venceu, PARABÉNS!')
 else:
     print('Você perdeu, MELHORE!')
