5c1a0ab214aaebe7c6d270b87b932032f5d550fd
Marcos <marcos.garcia@youxlab.com.br>
2025-03-31 17:48:43 -0300

    Atualização

--- aulas_python/desafios_aula_12/atividade36.py
+++ aulas_python/desafios_aula_12/atividade36.py
@@ -3,8 +3,8 @@ valor = float(input('Qual o valor da casa? R$'))
 salario = float(input('Qual o seu salário? R$'))
 quantosa = float(input('Em quantos anos você vai pagar: '))
 
-anos = quantosa*12
-divisao = valor/anos
+meses = quantosa*12
+divisao = valor/meses
 
 print('Para pagar uma casa de R${} em {} anos a prestação será de R${:.2f} '.format(valor, quantosa, divisao))
 
