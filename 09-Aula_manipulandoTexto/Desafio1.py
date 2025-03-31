ea8345917d58ecbfa758ffe1665e7877022901a2
GustavoFreireGomes <gustavo.gomes@youxlab.com.br>
2025-03-31 16:43:01 -0300

    novos

---
+++ 09-Aula_manipulandoTexto/Desafio1.py
@@ -0,0 +1,8 @@
+nome =str( input('Digite seu nome ')).strip()
+print('Analisando seu nome.......')
+print('seu nome maiusculo e {}'.format(nome.upper ()))
+print('seu nome minicuslo e {}'.format(nome.lower()))
+print('a quantidade de letras {}'.format(len(nome)))
+print('a quantidade de letras que tem no seu primeiro nome e {}'.format(nome.find('')))
+
+
