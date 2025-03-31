5802fe2dd2b488a9fde8a1c16f1dfb6d6a15d2c4
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-03-31 16:58:19 -0300

    seno, cosseno e hipotenusa

---
+++ exercicios/ex18.py
@@ -0,0 +1,10 @@
+from math import radians, sin, cos, tan
+angulo = float (input ("Digite o ângulo que você deseja: "))
+seno = sin(radians(angulo))
+print ("O ângulo de {} tem o SENO {:.2f}".format(angulo,seno))
+cosseno = cos(radians(angulo))
+print ("O ângulo de {} tem o COSSENO {:.2f}".format(angulo,cosseno))
+tangente = tan(radians(angulo))
+print ("O ângulo de {} tem a TANGENTE {:.2f}".format(angulo,tangente))
+
+
