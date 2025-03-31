113f2f88049bcd11509239e8736beeecc0ffb12f
GustavoFreireGomes <gustavo.gomes@youxlab.com.br>
2025-03-31 13:05:30 -0300

    novo

---
+++ 08-Aula_Modulos/Desafio5.py
@@ -0,0 +1,9 @@
+import random
+name=['mateus', 'Ana' , 'Marcos', 'vitor' , 'Paim' , 'Debora']
+sorte= random.choice(name)
+sorte2= random.choice(name)
+sorte3= random.choice(name)
+sorte4= random.choice(name)
+sorte5= random.choice(name)
+sorte6= random.choice(name)
+print(input('sortear {}, {} , {} , {} , {} , {}'.format(sorte , sorte2 , sorte3 , sorte4 ,sorte5 , sorte6)))
