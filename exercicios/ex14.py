a575aaffab1f3f3e73b0fb31a1d88022696ce7a6
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-03-31 15:14:52 -0300

    transformando °C em °F!

---
+++ exercicios/ex14.py
@@ -0,0 +1,3 @@
+temperatura = float (input ("informe a temperatura em °C :  "))
+fahrenheit = (( 9 * temperatura) / 5 ) + 32
+print ("a temperatura de {}°C corresponde a {}°F!".format(temperatura,  fahrenheit))
