76a3c432fb7a86ebaba7596066c18ac40f143b53
GustavoValacio <gustavoseabra2007@gmail.com>
2025-03-31 13:55:16 -0300

    Aula08 Bibliotecas

---
+++ 08-aula_python/Biblioteca.py
@@ -0,0 +1,8 @@
+from math import sqrt, floor
+num = int(input('Digite um número'))
+raiz = sqrt(num)
+print('A raiz quadrada de {}, é {:.2f}'. format(num, floor(raiz)))
+
+#import random
+# num = random.randit(1,10)
+#print ('num')
