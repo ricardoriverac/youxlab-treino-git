280b94015afd3920d30e5f8daf9bfa5c4c661498
annaluiza777 <anna.araujo@youxlab.com.br>
2025-03-31 16:59:22 -0300

    desafio 2 aula 8

---
+++  aula_8_modulos.py/desafio2_aula8.py
@@ -0,0 +1,7 @@
+#co=cateto oposto
+#ca= cateto adjacente
+import math
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hipotenusa = math.hypot(co, ca)
+print('A hipotenusa vai medir {}'.format(hipotenusa))
