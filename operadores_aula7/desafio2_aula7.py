43e5c0a14394726dd155781662f7c0dc46a3a389
annaluiza777 <anna.araujo@youxlab.com.br>
2025-03-31 20:46:01 -0300

    desafio 2 aula 7

---
+++ operadores_aula7/desafio2_aula7.py
@@ -0,0 +1,6 @@
+numero = int(input('Digite um número: '))
+dobro = numero * 2 
+triplo = numero * 3
+raiz = numero ** (1/2)
+print(' O dobro de {} vale {}. '.format(numero, dobro))
+print(' O triplo de {} vale {}. A raiz quadrada de {} é igual a {}.'.format(numero, triplo, numero, raiz))
