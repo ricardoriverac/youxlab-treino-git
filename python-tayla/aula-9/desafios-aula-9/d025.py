9821ee60b46c2b74013265902f21500526470c75
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-03-31 13:18:31 -0300

    desafio 25

--- python-tayla/aula-9/desafios-aula-9/d025.py
+++ python-tayla/aula-9/desafios-aula-9/d025.py
@@ -1,3 +1,3 @@
-nome = input('Qual é o seu nome? ').strip().capitalize()
+nome = input('Qual é o seu nome? ')
 existe = 'Silva' in nome
 print(f'Seu nome é {nome[:]} então é verdade que têm Silva nele? {existe}')
