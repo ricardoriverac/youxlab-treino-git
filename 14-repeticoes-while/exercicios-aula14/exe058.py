c40a92ca91f9432692f22b20c736f1de75baf771
juliocesar <juliogoiabinha060@gmail.com>
2025-03-31 16:07:13 -0300

    Repeticoes while

--- 14-repeticoes-while/exercicios-aula14/exe058.py
+++ 14-repeticoes-while/exercicios-aula14/exe058.py
@@ -1,11 +1,15 @@
 from random import randint
 numerosdeTentativas = 1
-computador = randint(0,1)
+computador = randint(0,10)
 print("""Computador ta pensando em número entre 0 a 10 
 \033[36mTente adivinhar\033[m""")
 jogador = int(input('Digite o número: '))
 while jogador != computador :
     jogador = int(input('\033[31mErrou!.\033[m Tente novamente: '))
+    if jogador > computador :
+        print('Menos... Tente mais uma vez.')
+    else:
+        print('Mais... Tente mais uma vez.')
     numerosdeTentativas += 1
 print(f'O número de tentativas até acertar foi {numerosdeTentativas}')
     
