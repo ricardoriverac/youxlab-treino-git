370ec6989a70a3f54e652edb629f3f96a6ac7309
Marcos <marcos.garcia@youxlab.com.br>
2025-03-31 13:38:52 -0300

    Fiz um codigo que abaixo de 200km você paga por 0.50 a cada km e acima de 200km fica 0.45

---
+++ aulas_python/desafios_aula_10/atividade31.py
@@ -0,0 +1,10 @@
+print('---Bem vindo(a) a nossa impressa de viagens!---')
+km = float(input('Qual a distancia da sua viagem? Acima de 200km terá um desconto! '))
+
+if km <= 200:
+    q = km*0.50
+    print('O preço é: R4{:.2f}'.format(q))
+    
+else:
+    w = km*0.45
+    print('O preço com nosso desconto é: R${:.2f}'.format(w))
