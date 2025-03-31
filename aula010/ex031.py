6b7a405aa8336176461772b17ba243a0d20207f6
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-03-31 17:13:42 -0300

    preço por km

---
+++ aula010/ex031.py
@@ -0,0 +1,9 @@
+km= int (input('Me diga a distância que ira viajar '))
+if km <= 100:
+    total =  5.50*km
+    print('O valor dessa viagem sera de:{} '.format(total))
+else: 
+    total = 3*km
+    print ('O valor dessa viagem sera de: {} '.format(total))
+print('Boa viagem!')
+
