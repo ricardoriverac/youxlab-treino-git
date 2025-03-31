0d906a55718d723488a3ca4931b6c7ec2613e921
João Pedro <joao.pedro@youxlab.com.br>
2025-03-31 13:13:54 -0300

    Terminando o exercicio 27

---
+++ exercicios/exe27.py
@@ -0,0 +1,8 @@
+#Primeiro nome e ultimo nome de uma pessoa
+
+n = input("Digite seu nome completo: ")
+nome = n.split()
+
+print("Muito prazer em te conhecer!")
+print("Seu primeiro nome é {}".format(nome[0]))
+print("Seu ultimo nome é {}".format(nome[len(nome)-1]))
