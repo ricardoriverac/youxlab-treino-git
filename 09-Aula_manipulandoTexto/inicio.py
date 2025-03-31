ef34f7df6f8e132b0fa340c3ac36c702c506625c
GustavoFreireGomes <gustavo.gomes@youxlab.com.br>
2025-03-31 16:29:51 -0300

    novo

---
+++ 09-Aula_manipulandoTexto/inicio.py
@@ -0,0 +1,11 @@
+frase = ('Curso em Video Python')
+print(len(frase))
+
+spider= ('Speder-man 4')
+print(spider.replace('4' , '2'))
+
+frase1=('Bob esponja')
+print('Bob' in frase1)
+print(frase1.find('Bob'))
+print(frase1.lower().find('Bob'))
+print(frase1.split())
