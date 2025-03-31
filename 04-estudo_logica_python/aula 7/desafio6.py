07316a9d3642e285f6558dd09c16d59c413843a5
marianne0614 <marianne.lima@youxlab.com.br>
2025-03-31 13:16:38 -0300

    desafio 6

---
+++ 04-estudo_logica_python/aula 7/desafio6.py
@@ -0,0 +1,5 @@
+n=int(input('digite um numero :'))
+dobro= n*2
+triplo= n*3
+raiz= n ** (1/2)
+print('analisando o valor{}, seu dobro é {}, e seu triplo é {},e a raiz é {}'. format(n, dobro, triplo, raiz)) 
