198d2424aefdaf789b4593495c72e13c8cd69a9a
João Pedro <joao.pedro@youxlab.com.br>
2025-03-31 16:02:11 -0300

    Terminando o exercicio 37

--- exercicios/exe37.py
+++ exercicios/exe37.py
@@ -1,23 +1,11 @@
-#Escreva um programa que leia um numero inteiro qualquer e peça para o usuario
-#escolher qual será a base de conversão:
-
-#1 para binario
-#2 para ocatal
-#3 para hexadecimal
-
 numero = int(input("Digite um número: "))
-escolha = input("Qual voce deseja: 1 - Binario, 2 - Ocatal, 3 - Hexadecimal: ").lower
+escolha = input("Qual você deseja: 1 - Binário, 2 - Octal, 3 - Hexadecimal? ").lower()
 
-if escolha in ["binário" or "binario"] or 1:
-    binario = bin(numero)[2:]
-    print(f"O número {numero} em binário é: {binario}")
-
-elif escolha == "ocatal" or 2:
-    octal = format(numero, 'o')
-    print(f"O número {numero} em octal é: {octal}")
-
-elif escolha == "hexadecimal" or 3:
-    hexadecimal = format(numero, 'x')
-    print(f"O número {numero} em hexadecimal é: {hexadecimal}")
-    
-else: print("Erro")
+if escolha in ["1", "binário", "binario"]:
+    print(f"O número {numero} em binário é: {bin(numero)[2:]}")
+elif escolha in ["2", "octal"]:
+    print(f"O número {numero} em octal é: {oct(numero)[2:]}")
+elif escolha in ["3", "hexadecimal"]:
+    print(f"O número {numero} em hexadecimal é: {hex(numero)[2:]}")
+else:
+    print("Erro: Escolha inválida!")
