43877aaf492ee5b986053683feec22bb7813ed7f
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-03-31 17:48:42 -0300

    analisador de textos

---
+++ exercicios/ex22.py
@@ -0,0 +1,7 @@
+nome = str (input ("digite seu nome completo : ")).strip()
+print ("Analisando seu nome....")
+print ("Seu nome em maiúscula é {}".format(nome.upper()))
+print ("Seu nome em minúscula é {}".format(nome.lower()))
+print ("Seu nome ao todo tem {} letras ".format (len(nome ) - nome.count(' ')))
+separa = nome.split()
+print ("Seu primeiro nome é {} , e ele tem {} letras".format(separa[0], len(separa )))
