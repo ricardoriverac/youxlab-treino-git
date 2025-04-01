d2b4a6b3e8caa1bdca35690b3186d86b62259bde
gustavofreire <gustavo.gomes@youxlab.com.br>
2025-04-01 15:34:21 -0300

    novos

--- 10-Aula_Condicoes/Desafio4.py
+++ 10-Aula_Condicoes/Desafio4.py
@@ -1,8 +1,8 @@
-km =float(input('Que distancia voce percorreu?  '))
-prc= km * 0,50
-lng= km * 0,45
-if km >200:
-    print('A sua viagem custou{}'.format(lng))
-else:
-    print('Sua viagem custou {}'.format(prc))    
+distancia = float(input('Digite a distancia da sua viagem '))
+mn = distancia * 0.50
+m= distancia * 0.45
 
+if distancia > 200:
+    print('O valor da sua viagem é de R${:.2f}'.format(m))
+else:
+    print('O valor da sua viagem é de R${:.2f}'.format(mn))
---
+++ 10-Aula_Condicoes/Desafio5.py
@@ -0,0 +1,5 @@
+ano = int(input('Diite o Ano'))
+if ano % 4 == 0:
+    print('O ano {} BISSXESTO'. format (ano))
+else:
+    print('O ano {} não é BISSXESTO' . format (ano))
