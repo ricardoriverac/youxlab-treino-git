28b80486cf7452db3fa82d0a3a12ff1ed5e99b35
annaluiza777 <anna.araujo@youxlab.com.br>
2025-04-01 13:05:30 -0300

    desafio 7 aula

--- operadores_aula7/desafio7_aula7.py
+++ operadores_aula7/desafio7_aula7.py
@@ -1,4 +1,4 @@
 larg = float(input('Largura da parede '))
 alt = float(input('Altura da parede '))
 area = larg * alt
-print('Sua parede tem a dimensão de {}x{} e sua area é de {}'.format(larg, alt, area))
+print('Sua parede tem a dimensão de {}x{} e sua area é de {}m² '.format(larg, alt, area))
