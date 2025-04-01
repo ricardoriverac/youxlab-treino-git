855c39f1b1d58737055e0bada5235f2451b7fe13
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-04-01 17:40:04 -0300

    assisti a aula 11

---
+++ 11-curso_python/aula.py
@@ -0,0 +1,3 @@
+print('\033[0;30mTeste')
+print('\033[4;33mTeste')
+print('\033[1;35;40mTeste')
---
+++ teste/teste32.py
@@ -0,0 +1,6 @@
+ano = int(input('Digite um ano: '))
+conta = ano % 4 
+if conta ==0 :
+    print('o ano e bissexto')
+else :
+    print('O ano n e bissexto ')
