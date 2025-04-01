b7a567d475a7fd4fa9bef9bdc91333b337d0e24d
gustavofreire <gustavo.gomes@youxlab.com.br>
2025-04-01 17:09:48 -0300

    novos

---
+++ 12-Aula_Condicoes-Alinhadas/Desafio1.py
@@ -0,0 +1,10 @@
+valorcasa=float(input('Qual o valor da casa?  '))
+salario=float(input('Quanto você recebe por mês ?  '))
+anos=int(input('Em quantos anos pretende pagar a casa?  '))
+ani=anos * 12 
+men= salario * 0.3
+vlr= valorcasa / 12
+if men>vlr:
+    print('Aprovado')
+else:
+    print('Negado')    
