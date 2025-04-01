1ef0b2dde88580fd4040738ee5fa1a399e2b85fb
Marcos <marcos.garcia@youxlab.com.br>
2025-03-31 21:00:11 -0300

    Fiz um codigo que converte seu número inteiro pra binario, octal ou hexadecimal

--- aulas_python/desafios_aula_12/atividade37.py
+++ aulas_python/desafios_aula_12/atividade37.py
@@ -8,4 +8,18 @@ print('[3] Converter para HEXADECIMAL')
 opcao = int(input('Sua opção: '))
 
 binario = bin(numero)[2:]
-print(binario)
+
+octal = oct(numero)[2:]
+
+hexa = hex(numero)[2:]
+
+if opcao == 1: 
+    print('O número {} em BINARIO é: {}'.format(numero, binario))
+    
+elif opcao == 2 :
+    print('O número {} em OCTAL é: {}'.format(numero, octal))
+    
+
+elif opcao == 3 :
+    print('O numero {} em HEXADECIMAL é: {}'.format(numero, hexa))
+
