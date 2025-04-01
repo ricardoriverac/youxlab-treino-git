60ba0f9b0e9545555fb8ec77085b038dbf406296
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-01 16:01:33 -0300

    custo da viagem

---
+++ exercicios/ex31.py
@@ -0,0 +1,8 @@
+distancia = float (input ("Qual a distância da viagem em Km  ? "))
+print ("Sua viagem terá {:.2f} KM de distância.".format(distancia))
+passagem = distancia * 0.50
+if distancia <= 200:
+    print ("Você pagará: {:.2f}".format(passagem))
+else:
+    passagem = distancia * 0.40
+    print ("Você pagará: {:.2f}".format(passagem))
