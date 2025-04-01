bee3b9b7e60501b98243352abbbda17616bf85ab
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-04-01 13:24:27 -0300

    fiz o desafio 28

---
+++ 10-curso_python/desafio28.py
@@ -0,0 +1,5 @@
+numero = int(input('Tente advinhar o número que estou pensando, digite um número de 1 a 5: '))
+if numero ==2 :
+    print('Vocẽ acertou, nossa.')
+else :
+    print('MUHAHAHA, vocẽ errou!')
