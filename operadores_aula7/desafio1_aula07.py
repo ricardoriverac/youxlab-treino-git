4eb4b6795f30e6eb8a7e673a1c846148690e6f12
annaluiza777 <anna.araujo@youxlab.com.br>
2025-03-31 21:18:24 -0300

    desafio 1 aula 7

---
+++ operadores_aula7/desafio1_aula07.py
@@ -0,0 +1,4 @@
+numero = int(input('Digite um valor: '))
+antecessor = numero - 1 
+sucessor = numero + 1
+print('Analisando o valor {} seu antecessor é {} e seu sucessor é {}'.format(numero, antecessor, sucessor))
--- operadores_aula7/desafio1_aula7.py
+++
@@ -1,4 +0,0 @@
-n= int(input('digite um número:'))
-antecessor= n-1
-sucessor = n+1 
-print('analisando o valor {}, seu antecessor {}, e o sucessor é {}'. format (n , antecessor, sucessor ))
