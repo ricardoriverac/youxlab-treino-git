f54ff2808cc4dbe71fc62b95b2050bfa95d7b123
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-01 13:36:23 -0300

    Exercício 13

---
+++ exercicios/ex013.py
@@ -0,0 +1,4 @@
+salario= float(input('Informe seu salário: R$ '))
+aumento= salario*(15/100)
+salarioComAumento= salario+aumento
+print('Seu salário é {:.2f} com 15% de aumento'.format(salarioComAumento))
