762abe339ee1ecd4dd40abfd845898403d1165fd
annaluiza777 <anna.araujo@youxlab.com.br>
2025-04-01 17:07:26 -0300

    exercicio 1  aula 9

---
+++ aula_09.py/exercicio1_aula9.py
@@ -0,0 +1,10 @@
+frase  = '   curso em vídeo Python'
+print(len(frase.strip()))
+
+frase = 'Curso em Vídeo Python'
+print(frase.replace('Python', 'Android'))
+print(frase)
+
+frase = 'Curso em video python'
+dividindo = frase.split()
+print(dividindo [2] [4]) 
