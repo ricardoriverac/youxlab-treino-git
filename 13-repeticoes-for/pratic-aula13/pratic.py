6f36027181097cb87c10dffa96d6b6c2b1b72573
juliocesar <juliogoiabinha060@gmail.com>
2025-04-01 14:07:45 -0300

    Subindo exercicios

--- 13-repeticoes-for/pratic-aula13/pratic.py
+++ 13-repeticoes-for/pratic-aula13/pratic.py
@@ -4,11 +4,11 @@
         #print('\033[31mDestaquei o "3"\033[m')
 #print('\033[32mFIM\033[m')
 #........................................
-n = int(input('Digite um número:'))
-f = int(input('Fim'))
-p = int(input('Passo'))
-for c in range(n,f+1,p):
-    print(c)
+#n = int(input('Digite um número:'))
+#f = int(input('Fim'))
+#p = int(input('Passo'))
+#for c in range(n,f+1,p):
+#    print(c)
 #
 s = 0
 for c in range(0,4):
--- 14-repeticoes-while/exercicios-aula14/exe061.py
+++ 14-repeticoes-while/exercicios-aula14/exe061.py
@@ -1,9 +1,9 @@
 primeiro = int(input('Primeiro termo: '))
-razao = int(input('Razão da PA: '))
+razao = int(input('Razão de uma PA: '))
 termo = primeiro
 cont = 1
 while cont <= 10:
-    print(f'{termo} ->', end=' ')
+    print(f'{termo} -> ', end=' ')
     termo += razao
     cont += 1
 print('FIM')
