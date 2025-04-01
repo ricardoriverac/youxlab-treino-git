8f39e63eaaf9d65946e58513efc77625b3646b6c
João Pedro <joao.pedro@youxlab.com.br>
2025-04-01 15:00:58 -0300

    Terminando o exercicio 57

--- exercicios/exe57.py
+++ exercicios/exe57.py
@@ -1,1 +1,10 @@
-#
+#Faça um programa que leia o sexo de uma pessoa, mas so aceite os valores "M" ou "F".
+#Caso esteja errado, peça a digitaçao novamente ate ter um valor correto.
+
+
+nome = input("Qual é o seu nome: ")
+sexo = str(input("Qual é o seu sexo: "))
+
+while sexo != "M" and sexo != "F":
+    print("Entrada inválida! Por favor, digite apenas 'M' ou 'F'.")
+    sexo = str(input("Digite o sexo novamente: "))
