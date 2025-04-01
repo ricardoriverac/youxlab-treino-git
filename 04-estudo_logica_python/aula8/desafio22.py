634af997beabc64e899a25268f90eb3c4330df5d
marianne0614 <marianne.lima@youxlab.com.br>
2025-04-01 17:04:09 -0300

    teoria do video 8

---
+++ 04-estudo_logica_python/aula8/desafio22.py
@@ -0,0 +1,13 @@
+frase= 'curso de video python'
+print(frase.count('o'))
+
+frase= 'curso video python'
+print (len(frase))
+
+frase= 'curso video python'
+print(frase.replace('python', 'android'))
+
+frase= 'curso video python'
+dividindo= frase.split()
+print(dividindo [2] [4])
+
