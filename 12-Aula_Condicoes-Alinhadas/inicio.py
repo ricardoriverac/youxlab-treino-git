4f7b1df828d5ed6f5b267b742a4b5af367c25f27
gustavofreire <gustavo.gomes@youxlab.com.br>
2025-04-01 16:38:56 -0300

    novo

---
+++ 12-Aula_Condicoes-Alinhadas/inicio.py
@@ -0,0 +1,8 @@
+nome =str(input('Qual o seu nome?  '))
+if nome == 'Gustavo':
+    print('Que nome bonito, {} !'.format(nome))
+elif nome == 'Vitoria' or nome == "Vitor" or nome == 'Luciara':
+    print('Seu nome e bem comum, {}!!'.format(nome))
+else:
+    print('Você não e nada para a sociedade...')
+print('Tenha um bom dia !!!')           
