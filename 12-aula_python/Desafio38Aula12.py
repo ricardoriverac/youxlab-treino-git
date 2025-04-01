5bc07d5fa64940527ec5448a42885a179e29e77d
GustavoValacio <gustavoseabra2007@gmail.com>
2025-04-01 17:43:04 -0300

    Desafio38 Valor maior e menor

---
+++ 12-aula_python/Desafio38Aula12.py
@@ -0,0 +1,13 @@
+n1 = int(input('Digite um número inteiro: '))
+n2 = int(input('Digite um segundo número inteiro: '))
+
+if n1 > n2:
+    print('O primeiro valor é maior!')
+
+elif n2 > n1:
+    print('O segundo valor é maior!')
+
+else:
+    print('Os dois valores são iguais')
+
+
