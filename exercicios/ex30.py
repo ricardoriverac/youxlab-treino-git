6e74b07d7d32b38915bec7c7f2dd7e896176efa5
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-01 15:35:55 -0300

    par ou ímpar

---
+++ exercicios/ex30.py
@@ -0,0 +1,8 @@
+numero = int (input ("Digite um número: "))
+resultado = numero % 2
+if resultado == 0:
+    print ("O número digitado foi: {}".format(numero))
+    print ("O número digitado é PAR !! ")
+else:
+    print ("O número digitado foi: {}".format(numero))
+    print ("O número digitado é ÍMPAR !!")
