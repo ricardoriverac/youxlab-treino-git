613273352cb3bfdca698a1d71e8d2eda35851a3f
Maria Luiza <maria.silva@youxlab.com.br>
2025-04-01 16:51:39 -0300

    desafio das retas

--- desafiosaula10/desafio35.py
+++ desafiosaula10/desafio35.py
@@ -3,6 +3,6 @@ primeiraReta = float(input('Reta 1: '))
 segundaReta = float(input('Reta 2: '))
 terceiraReta = float(input('Reta 3: '))
 if primeiraReta + segundaReta > terceiraReta and segundaReta + terceiraReta > primeiraReta and terceiraReta + primeiraReta > segundaReta:
-    print('Com essas retas é possível formar um triângulo! ')
+    print('Com essas retas É POSSÍVEL formar um triângulo! ')
 else:
-    print('Com essas retas não é possível fazer um triângulo! ')
+    print('Com essas retas NÃO É POSSÍVEL fazer um triângulo! ')
