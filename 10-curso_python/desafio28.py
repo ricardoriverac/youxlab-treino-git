0ea51e7526719374ae73829c17e8d171bf1d33d5
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-04-01 13:59:23 -0300

    fiz o desafio 29

--- 10-curso_python/desafio28.py
+++ 10-curso_python/desafio28.py
@@ -1,5 +1,8 @@
 from random import randint
+from time import sleep
 numero = int(input('Tente advinhar o número que estou pensando, digite um número de 0 a 5: '))
+print('PROCESSANDO...')
+sleep(2)
 numeroQueEuQuero = randint(0, 5)
 if numero == numeroQueEuQuero :
     print('Vocẽ acertou, nossa.')
--- exercicios.py/exerc29.py
+++ exercicios.py/exerc29.py
@@ -1,10 +1,9 @@
 from time import sleep
 km = int(input('Digite a velocidade de um carro em Km: '))
 multa = (km - 80)*7.00
-print('VERIFICANDO SE SERÁ MULTADO...')
+print('Verificando se você será multado...')
 sleep(2)
 if km >=80 :
     print('{}Km? Vocẽ foi multado! Sua multa será de: {:.2f} reais! '.format(km, multa))
 else :
     print('Ótimo, {}Km está razoável. Você não receberá multa. '.format(km))
-
