89ba9df22a0d4580a04a8c9011229642ca189abd
Youx <sophia.carvalho@youxlab.com.br>
2025-04-01 14:04:33 -0300

    alteracao

--- aula8/1°teste-aula08.py
+++ aula8/1°teste-aula08.py
@@ -3,3 +3,8 @@ import math
 n=int(input('Digite um número:'))
 raiz=math.sqrt(n)
 print(f'a raiz quadrada de {n} é {raiz}!')
+#Pode ser feito assim também se quiser apenas uma função da biblioteca
+from math import sqrt
+n=int(input('Digite um número:'))
+raiz= sqrt(n)   # não precisa do comando math. pois a função já tinha sido selecionada a cima 
+print(f'a raiz quadrada de {n} é {raiz}!')
