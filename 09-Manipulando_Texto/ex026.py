4b23876130901f2dbb83de2c1ed2b049daedd9a2
fabricioyoux <fabricio.genesi@youxlab.com.br>
2025-04-01 14:50:13 -0300

    atividade 26

---
+++ 09-Manipulando_Texto/ex026.py
@@ -0,0 +1,10 @@
+frase = str(input('Uma frase: ')) 
+
+y= frase.count('a')
+
+i = frase.find('a')
+
+o = frase.rfind('a')
+
+print(f'Aparece a letra a {y} vezes, na posição {i},ela aparece a ultima vez no {o}.')
+
