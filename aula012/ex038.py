1a3b97c59820cc9d827b2c18b55ca840a04dbb28
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-04-01 16:13:24 -0300

    ex038.py

---
+++ aula012/ex038.py
@@ -0,0 +1,10 @@
+n1=float(input('Me diga o primeiro numero: '))
+n2=float(input('Me diga agora o segundo: '))
+
+if n1 >n2:
+    print('O maior numero e: {}'.format(n1 ))
+elif n2 > n1:
+    print('O maior numero e: {} '.format(n2))
+else:
+    print('Não existe numero maior (>:')
+    
