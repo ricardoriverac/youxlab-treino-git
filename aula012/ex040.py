35b31fcc7e67aff06801ee49cf273cc88726c52b
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-04-01 16:43:20 -0300

    Media notas

--- aula012/ex040.py
+++ aula012/ex040.py
@@ -2,6 +2,7 @@ nota1=float(input('Me diga sua nota do primeiro Bimestre: '))
 nota2=float(input('Me diga a nota do segundo Bimestre: '))
 media=(nota1 + nota2)/2
 
+print('Sua primeira nota foi {:.1f} sua segunda nota foi {:.1f} e sua media foi {:.1f} '.format(nota1,nota2,media))
 if media <= 14:
     print('Você não ira passar de ano ')
 elif media ==15:
