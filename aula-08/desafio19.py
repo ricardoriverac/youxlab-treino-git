683a181ad127f4e858dc762850c7506f62e0cf03
alexia-assis <alexiaassis.youxlab.com.br>
2025-04-01 13:52:51 -0300

    Fim de dasafio 19

---
+++ aula-08/desafio19.py
@@ -0,0 +1,9 @@
+import random
+print('Sorteando alunos:')
+a1 = str(input('Digite o primeiro aluno:'))
+a2 = str(input('Digite o segundo aluno:'))
+a3 = str(input('Digite o terceiro aluno:'))
+a4 = str(input('Digite o quarto aluno:'))
+sorteio = (a1)
+escolhido = random.choice(sorteio)
+print('Parabens {}, você foi o escolhido'.format(sorteio))
