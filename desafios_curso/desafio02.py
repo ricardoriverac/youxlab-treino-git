a9255b6ece94933d19eec44aa4a3bbce03d0f5bd
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-01 17:09:32 -0300

    desafio 02

---
+++ desafios_curso/desafio02.py
@@ -0,0 +1,4 @@
+dia = int (input ("Digite o dia do seu aniversário: "))
+mes = str (input ("Digite o mês do seu aniversário: "))
+ano = int (input ("Digite o ano do seu aniversário: "))
+print ("Você nasceu no dia {} de {} de {} !!".format(dia,mes,ano))
