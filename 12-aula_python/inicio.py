9ef525f2bcf31072f7d76228013b19f0735555e0
GustavoValacio <gustavoseabra2007@gmail.com>
2025-04-01 16:44:12 -0300

    Aula12 elif

---
+++ 12-aula_python/inicio.py
@@ -0,0 +1,7 @@
+nome = str(input('Digite seu nome!!'))
+if nome =='Gustavo':
+    print('Seu nome é pereito')
+elif nome == 'Pedro' or nome == 'Ana' or nome == 'Clara':
+    print('Que nome lindo!!!')
+else:
+    print('Que nome paia!!')
