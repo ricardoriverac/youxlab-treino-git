b4c38aa51dac7b18e242ab01d4c7a4345fe24857
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-03-31 21:40:20 -0300

    Achando o menor numero

---
+++ aula010/ex033.py
@@ -0,0 +1,13 @@
+n1=(input('Digite o primeiro valor: '))
+n2=(input('Digite o segundo valor: '))
+n3=(input('Digite o terceiro valor: '))
+
+if n1 < n2 or n1 < n3:
+    print('O menor numero e:{} '.format(n1)) 
+else:
+    n2 < n3 or n2 < n1
+    print('O menor numero e:{} '.format(n2))
+if n3 < n1 or n3 < n2:
+    print('O menor numero e:{} '.format (n3))
+else:
+    print('Não existe numero menor...')
