bad540e2383dfda5e17f149b26821558dc913aad
juliocesar <juliogoiabinha060@gmail.com>
2025-04-01 14:02:11 -0300

    repeticoes while true

--- 15-repeticoes-while-true/exercicios-aula15/exe066.py
+++ 15-repeticoes-while-true/exercicios-aula15/exe066.py
@@ -1,9 +1,9 @@
-s = 0
+soma = 0
 cont = 0
 while True:
     n = int(input('Digite o número \033[31m[999]\033[m para parar: '))
     if n == 999:
         break
-    s += n
+    soma += n
     cont += 1
-print(f'Você digitou {cont} é a soma deles foi {s}')
+print(f'Você digitou {cont} números é a soma deles foi {soma}')
