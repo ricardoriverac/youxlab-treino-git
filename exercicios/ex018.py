b8074affee9acdd832d4b5068f245369826f559e
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-01 16:44:41 -0300

    Exercício 18

---
+++ exercicios/ex018.py
@@ -0,0 +1,6 @@
+import math
+valor= int(input('Digite o ângulo: ''°'))
+sen= math.sin(valor)
+cos= math.cos (valor)
+tan= math.tan(valor)
+print('O valor do seno é {} \n Do cosseno é {} \n E da tangente é {}'.format(sen,cos,tan))
