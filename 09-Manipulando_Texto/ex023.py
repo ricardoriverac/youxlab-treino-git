1c5fbde27b49009fd9f628a0c43eb5680a684465
fabricioyoux <fabricio.genesi@youxlab.com.br>
2025-04-01 13:34:50 -0300

    atividade 23

---
+++ 09-Manipulando_Texto/ex023.py
@@ -0,0 +1,8 @@
+números = int(input('Escoolha um número: '))
+número = str(números)
+
+print('Analisando o número{}'.format(números))
+print(' Unidade: {}'.format(número[3]))
+print(' Dezena: {}'.format (número[2]))
+print(' Centena: {}' .format (número[1]))
+print('Milhar: {}'.format (número[0]))
