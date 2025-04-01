0e4838a0139963fcfb8284cabb6e4b50c92262de
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-04-01 16:54:24 -0300

    Avaliando atletas

---
+++ aula012/ex041.py
@@ -0,0 +1,14 @@
+anoNasc=int(input('Digite o ano que nasceu: '))
+anoAtual=int(input('Digite o ano atual: '))
+idade =anoAtual - anoNasc
+
+if idade <= 10:
+    print('Você e um atleta Mirin ')
+elif idade <= 17:
+    print('Você e um atleta Junior ')
+elif idade <=21: 
+    print('Você e um atleta jovem  ')
+else:
+    print('Você e um atleta sênior')
+
+
