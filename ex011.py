9f73279a75f63843865d850ec6c467acf2f50271
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-03-31 22:05:48 -0300

    Edição do exercício 11

--- ex011.py
+++ ex011.py
@@ -2,5 +2,5 @@ width= float(input('Largura: '))
 height= float (input('Altura: '))
 a= width*height
 litros= a/2
-print('A quantidade de tinta necessária para pintar {}m² é {} litros'.format(a,litros))
+print('A quantidade de tinta necessária para pintar {:.2f}m² é {:.2f} litros'.format(a,litros))
 
