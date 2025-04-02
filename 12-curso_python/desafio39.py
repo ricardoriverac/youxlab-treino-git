9bfaa22400e83a7acab48327faf56ba59a76638f
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-04-02 15:00:52 -0300

    fiz o exercício 39

---
+++ 12-curso_python/desafio39.py
@@ -0,0 +1,9 @@
+ano = int(input('Qual o seu ano de nascimento? '))
+anoAlistar = 2025-ano
+if anoAlistar ==18 :
+    print('Já está na hora de se alistar ao serviço militar.')
+elif anoAlistar <18 :
+    print('Você ainda irá se alistar ao serviço militar.')
+elif anoAlistar >18 :
+    print('Já passou da hora de você se alistar ao serviço militar!!!')
+    
