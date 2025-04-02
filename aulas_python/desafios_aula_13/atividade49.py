51b3fde8260245a972530d0422f4af68c79aae1d
Marcos <marcos.garcia@youxlab.com.br>
2025-04-02 17:58:39 -0300

    Fiz um Tabuador V2

--- aulas_python/desafios_aula_13/atividade49.py
+++ aulas_python/desafios_aula_13/atividade49.py
@@ -1,4 +1,8 @@
 print('\033[1;34m-=-=-=-=-= Bem vindo(a)! =-=-=-=-=-\033[m')
-numero = int(input('Qual número você gostaria de ver a tabuada? '))
 
-for c in range()
+n = int(input('Qual número você gostaria de ver a tabuada? '))
+for c in range(0, 11,):
+    m = c*n
+    print('{} x {} = {}'.format(n, c, m))
+
+print('\033[1;34m-=-=-=-=-= FIM! =-=-=-=-=-\033[m')
