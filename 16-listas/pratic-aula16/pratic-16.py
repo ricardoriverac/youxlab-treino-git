ba0b7367b580b2921d3f00c6a4e1728e65d0957f
juliocesar <juliogoiabinha060@gmail.com>
2025-04-02 14:35:34 -0300

    Variaveis compostas tupla

---
+++ 16-listas/pratic-aula16/pratic-16.py
@@ -0,0 +1,28 @@
+#lanche = ('Hamburguêr' , 'suco' , 'banana')
+#for cont in range(0, len(lanche)):
+   # print(f'eu vou comer {lanche[cont]} na posicao {cont+1}')
+
+#tuplas sao imutaveis
+#for comida in lanche :
+#print(f'Eu vou comer {comida}')
+#print('comi pra caramba')
+
+#sorted = deixa a tupla em ordem em alfabetica
+
+#a = (2 , 5 , 4)
+#b = (5 , 8 , 1 , 2 )
+#_________
+#c = a + b
+
+# junta as variaveis
+
+#print(c)
+#print(c.index(5 , 1))
+
+pessoas = {'Gustavo' , 39 , 'M' , 99.88}
+#___________
+del(pessoas)
+
+# pode apagar variaveis
+
+print(pessoas)
