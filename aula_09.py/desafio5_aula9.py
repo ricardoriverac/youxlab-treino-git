440da55f2569537bc78ab2a245aba037954468e7
annaluiza777 <anna.araujo@youxlab.com.br>
2025-04-02 15:36:45 -0300

    desafio 5  aula 9

--- aula_09.py/desafio5_aula9.py
+++ aula_09.py/desafio5_aula9.py
@@ -1,4 +1,4 @@
-frase = str(input('Digite uma frase: '))
-print('A letra A aparece {} vezes '.format(frase.upper().count('A')))
-print('A letra A aparece na posição {} '.format(frase.find('a') +1))
-print('A ultima posição que a letra A aparece é {}'.format(frase.rfind('a')+1))
+frase = str(input('Digite uma frase: ')).strip().upper()
+print(f'A letra "A" aparece {frase.count("A")} vezes')
+print(f'A letra A aparece pela primeira vez na posição {frase.find("A") + 1 }')
+print(f'A letra A aparece pela última vez na posição {frase.rfind("A") + 1}')
