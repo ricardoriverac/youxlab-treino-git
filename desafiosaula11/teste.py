163ea6b00b9964332cc7adcb504fed20fddd3fd9
Maria Luiza <maria.silva@youxlab.com.br>
2025-04-02 13:35:14 -0300

    teste cores no terminal

--- desafiosaula11/teste.py
+++ desafiosaula11/teste.py
@@ -1,1 +1,8 @@
 print ('\033[1;31;43mOlá, mundo!\033[m')
+a = 3
+b = 4
+print ('Os valores são \033[35m{}\033[m e \033[32m{}\033[m!!!'.format(a,b))
+
+nome = input('Qual seu nome?: ')
+print('Olá! muito prazer em te conhecer, {}{}{}!!!'.format('\033[32m', nome, '\033[m'))
+
