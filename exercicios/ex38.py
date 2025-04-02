fa2534197b1f04b8f26dd60a94abf5d833a4f2e6
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-02 17:02:00 -0300

    Comparando números

---
+++ exercicios/ex38.py
@@ -0,0 +1,8 @@
+numero1 = int (input ("Digite o primeiro valor : "))
+numero2 = int (input ("Digite o segundo valor : "))
+if numero1 > numero2:
+    print ("O primeiro valor é maior!! ")
+elif numero1 < numero2:
+    print ("O segundo valor é maior!! ")
+elif numero1 == numero2:
+    print ("os dois valores são iguais!! ")
