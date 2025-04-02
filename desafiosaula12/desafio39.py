8d23c3d6d03507e6f14425d8b2b470f8f6b5c39a
Maria Luiza <maria.silva@youxlab.com.br>
2025-04-02 16:49:26 -0300

    desafio do alistamento militar

---
+++ desafiosaula12/desafio39.py
@@ -0,0 +1,10 @@
+idade = int(input('Que ano você nasceu?: '))
+ano = idade - 2007
+if idade == 2007:
+    print('Você tem que se alistar ao serviço militar ')
+elif idade>2007:
+    print('Você ainda vai ter que se alistar ao serviço militar daqui a {} ano '.format(ano))
+elif idade<2007:
+    print('Já passou do tempo do alistamento tem {} anos '.format(ano))
+
+
