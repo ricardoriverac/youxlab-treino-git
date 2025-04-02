85103774ba6ce30a08d203397fcc074f54c0c3ed
annaluiza777 <anna.araujo@youxlab.com.br>
2025-04-02 17:58:15 -0300

    exercicios aula 10

---
+++ aula_10.py/exercicios_aula10.py
@@ -0,0 +1,16 @@
+n1= float(input('Digite a primeira nota: '))
+n2= float(input('Digite a segunda nota: '))
+m = (n1 + n2)/2
+print('A sua media foi {}'.format(m))
+print('PARABENS' if m >=6 else 'ESTUDE MAIS!')
+
+
+
+
+
+nome =str(input('Qual é seu nome? '))
+if nome == 'Gustavo':
+    print('Que nome lindo você tem!')
+else:
+    print('Seu nome é tão normal!!')
+print('Bom dia, {}! '.format(nome))
