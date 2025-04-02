851b7d4d9ef67f53ed5dc0d93df042fbffa42b49
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-04-02 16:30:33 -0300

    fiz o desafio 41

---
+++ exercicios.py/exerc41.py
@@ -0,0 +1,19 @@
+from time import sleep
+print('Olá, seja bem vindo a aula de natação!')
+sleep(1)
+print('Para sabermos qual será sua categoria, devemos saber sua idade.')
+sleep(1)
+print ('PROCESSANDO...')
+sleep(2)
+ano = int(input('Qual seu ano de nascimento? '))
+idade = 2025-ano
+if idade <=9 :
+    print('Sua categoria será mirim.')
+elif idade >9 and idade <=14 :
+    print('Sua categoria será infantil.')
+elif idade >14 and idade <=19 :
+    print('Sua categoria será junior.')
+elif idade ==20 :
+    print('Sua categoria será sênior.')
+elif idade >20 :
+    print('Sua categoria será master')
