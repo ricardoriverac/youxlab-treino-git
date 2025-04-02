f38e7daf4190246e866098d2e95d678808904569
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-04-02 20:42:41 -0300

    desafio 47

--- python-tayla/aula-13/desafios-aula-13/d047.py
+++ python-tayla/aula-13/desafios-aula-13/d047.py
@@ -1,4 +1,4 @@
-print('Esses são os pares entre 0 e 50')
+print('Esses são os pares entre 0 e 50: ')
 for c in range(0 , 51):
     if c % 2 == 0:
-        print(f' {c} ->' , end='')
+        print(f' {c} \033[1;31m->\033[m' , end='')
