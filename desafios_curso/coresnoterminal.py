6f18b5573814ef8e879d16da816b672b70cf343c
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-02 14:11:49 -0300

    cores no terminal

---
+++ desafios_curso/coresnoterminal.py
@@ -0,0 +1,5 @@
+print ("\033[31mOlá mundoo![m") #letra na cor vermelha 
+print ("\033[31;43mOlá mundoo!")  #letra na cor vermelha e fundo amarelo
+print ("\033[1;31;43mOlá mundoo!")  #letra na cor vermelha, fundo amarelo e negrito
+print ("\033[1;31;43mOlá mundoo!\033[m")  #letra na cor vermelha, fundo amarelo, negrito e sem barra até o final
+print ("\033[4;32;45mOlá mundoo!") #letra sublinhada, fundo lilás, letra branca
