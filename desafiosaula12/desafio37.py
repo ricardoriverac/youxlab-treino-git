68bbbd05ae3afb7b0e3bd603ac4c27312bf19b40
Maria Luiza <maria.silva@youxlab.com.br>
2025-04-02 15:32:31 -0300

    desafio de número binário octal e hexadecimal

---
+++ desafiosaula12/desafio37.py
@@ -0,0 +1,15 @@
+numero = int(input('Digite um valor: '))
+print('[1] binário')
+print('[2] octal')
+print('[3] hexadecimal')
+escolha = int(input('Qual opção você escolhe?: '))
+binario = bin(numero) [2:] #esses '[2:] serve pra começar a resposta depois dos 2 primeiros números
+octal = oct (numero) [2:]
+hexadecimal = hex(numero) [2:]
+if escolha == 1:
+    print('O número que você escolheu convertido para {}BINÁRIO {}é {}'.format('\033[35m','\033[m', binario))
+elif escolha == 2:
+    print('O número que você escolheu convertido para {}OCTAL {}é {}'.format('\033[35m', '\033[m', octal))
+elif escolha == 3:
+    print('O número que você escolheu convertido para {}HEXADECIMAL {}é {}'.format('\033[35m', '\033[m', hexadecimal))
+    
