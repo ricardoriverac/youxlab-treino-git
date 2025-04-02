dc14fc4918363634d68693a42e1398b19817f74b
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-04-02 15:06:10 -0300

    terminei o desafio 39

--- exercicios.py/exerc39.py
+++ exercicios.py/exerc39.py
@@ -1,9 +1,10 @@
 ano = int(input('Qual o seu ano de nascimento? '))
 anoAlistar = 2025-ano
+tempo = 18 - anoAlistar
 if anoAlistar ==18 :
     print('Já está na hora de se alistar ao serviço militar.')
 elif anoAlistar <18 :
-    print('Você ainda irá se alistar ao serviço militar.')
+    print('Você ainda irá se alistar ao serviço militar. Faltam {} anos.'.format(tempo))
 elif anoAlistar >18 :
-    print('Já passou da hora de você se alistar ao serviço militar!!!')
+    print('Já passou da hora de você se alistar ao serviço militar!!! Você está atrasado por {} anos!'.format(tempo))
     
