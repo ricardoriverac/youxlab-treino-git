76a4367fd156a166bf47f55d699c81b875d268e0
Maria Luiza <maria.silva@youxlab.com.br>
2025-04-02 12:52:12 -0300

    desafio da tabuada

---
+++ exercicioscursopython/exercicio9.py
@@ -0,0 +1,21 @@
+n = int(input('Escolha um número: '))
+n1=n*2
+n2=n*3
+n3=n*4
+n4=n*5
+n5=n*6
+n6=n*7
+n7=n*8
+n8=n*9
+n9=n*10 
+print('A tabuada desse número é:')
+print('{} x 2 = {}'.format(n,n1))
+print('{} x 3 = {}'.format(n,n2))
+print('{} x 4 = {}'.format(n,n3))
+print('{} x 5 = {}'.format(n,n4))
+print('{} x 6 = {}'.format(n,n5))
+print('{} x 7 = {}'.format(n,n6))
+print('{} x 8 = {}'.format(n,n7))
+print('{} x 9 = {}'.format(n,n8))
+print('{} x 10 = {}'.format(n,n9))
+
