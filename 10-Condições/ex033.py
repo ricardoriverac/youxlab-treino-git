c94f8474f2f87b0fb42bda2af32733731a2fb292
fabricioyoux <fabricio.genesi@youxlab.com.br>
2025-04-02 15:20:09 -0300

    Update ex033.py

--- 10-Condições/ex033.py
+++ 10-Condições/ex033.py
@@ -1,1 +1,14 @@
+a = int(input('Primeiro Valor: '))
 
+b = int(input('Segundo Valor '))
+
+c = int(input('Tercero Valor:'))
+
+menor = a
+if b<a and b<c:
+    menor = b
+if c<a and c<b:
+  menor = c     
+
+  print('O menor número digitado foi {}'.format(menor))
+  
