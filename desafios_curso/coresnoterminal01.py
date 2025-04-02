a8b3681fc7240731abf2d13657d327c9fa30e9e9
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-02 14:20:57 -0300

    cores no terminal

---
+++ desafios_curso/coresnoterminal01.py
@@ -0,0 +1,8 @@
+#código normal.
+a = 3
+b = 5
+print ("O primeiro valor digitado foi {} e o segundo {}".format(a,b))
+#código alterado com cores amarela e vermelha.
+a = 3
+b = 5
+print ("O primeiro valor digitado foi \033[33m{}\033[m e o segundo \033[31m{}".format(a,b))
