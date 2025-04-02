97fc3cd95f21ec1211a8e9f13cea41056e505e27
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-04-02 14:57:11 -0300

    desafio 45

--- python-tayla/aula-12/desafios-aula-12/d045.py
+++ python-tayla/aula-12/desafios-aula-12/d045.py
@@ -2,10 +2,10 @@ from random import randint
 inicio = print('Vamos jogar jokenpô?')
 jogadas = ('Pedra', 'Papel', 'Tesoura')
 print("""Suas opções:
-[1] pedra
-[2] papel
-[3] tesoura""")
-computador = randint(1,3)
+[0] pedra
+[1] papel
+[2] tesoura""")
+computador = randint(0,2)
 jogou = jogadas[computador]
 jogador = int(input('Qual é a sua jogada? '))
 print('\033[1;36m-=\033[m' * 12 )
@@ -13,26 +13,26 @@ print(f'O computador jogou {jogou}')
 print(f'Jogador jogou {jogadas[jogador]}')
 print('\033[1;36m-=\033[m' * 12)
 
+if computador == 0:
+    if jogador == 1:
+        print('Você venceu!')
+    elif jogador == 2:
+        print('Você perdeu!')
+    else:
+        print('Deu empate')
+
 if computador == 1:
     if jogador == 2:
         print('Você venceu!')
-    elif jogador == 3:
+    elif jogador == 0:
         print('Você perdeu!')
     else:
         print('Deu empate')
 
 if computador == 2:
-    if jogador == 2:
+    if jogador == 0:
         print('Você venceu!')
     elif jogador == 1:
-        print('Você perdeu!')
-    else:
-        print('Deu empate')
-
-if computador == 3:
-    if jogador == 1:
-        print('Você venceu!')
-    elif jogador == 2:
         print('Você perdeu!')
     else:
         print('Deu empate')
