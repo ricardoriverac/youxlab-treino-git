4079874365623dd1f77ab52c00c4df26b9c696a7
marianne0614 <marianne.lima@youxlab.com.br>
2025-04-02 15:02:42 -0300

    desafio 25

---
+++ 04-estudo_logica_python/aula 9/desafio25.py
@@ -0,0 +1,9 @@
+nome=str(input('Qual é seu nome?')).strip()
+print('SILVA'in nome .upper())  
+
+
+#nome=str(input(' Digite seu nome:')).split ()
+#print('Seu nome tem Silva {}'.format('SILVA'in nome.upper()))
+#nome =input('digite seu nome:')
+#f 'silva'in nome
+#print('o nome contem silva {}'.format(in nome))
