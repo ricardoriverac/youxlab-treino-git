bb7b323a790f56d73c1e66cad984d494de059f7f
marianne0614 <marianne.lima@youxlab.com.br>
2025-04-03 16:30:50 -0300

    desafio 32

---
+++ 04-estudo_logica_python/aula 10/desafio32.py
@@ -0,0 +1,6 @@
+ano=int(input(' digite um ano qualquer,para saber se é bissexto: '))
+if ano % 4 == 0:
+   print('O ano {} bissexto '. format(ano))
+else:
+   print('O ano {} nao é bissexto'.format(ano))
+   
