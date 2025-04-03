04164ad2c7fd73cef3f2d9edf1bb46d7d80f1186
annaluiza777 <anna.araujo@youxlab.com.br>
2025-04-03 17:01:45 -0300

    desafio 7  aula 10

---
+++ aula_10.py/desafio7_aula10.py
@@ -0,0 +1,9 @@
+salario = float(input('Digite o seu salario: '))
+
+if salario > 1250:
+    superior = salario + (salario * 10 / 100)
+    print(f'Você teve um aumento de 10%. Novo Salario R$ {superior:.2f}')
+
+elif salario <= 1250:
+    inferior = salario + (salario * 15 / 100)
+    print(f'Você teve um aumento de 15%. Novo Salario R$ {inferior:.2f}')
