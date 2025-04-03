a362ffbf5279253fa433ccd98fd9a4643ac26a52
davisales07 <davisales260810@gmail.com>
2025-04-03 16:59:07 -0300

    exercícios

---
+++ aula_012/exercício41.py
@@ -0,0 +1,18 @@
+from datetime import date
+
+at = date.today().year
+ns = int(input('ano de nascimento: '))
+idade = at - ns
+print(' o atleta tem {} anos. '.format (idade))
+if idade <= 9:
+    print('classificação mirim')
+elif idade <= 14:
+    print('infantil')
+elif idade <= 19:
+    print('junior. ')
+elif idade <= 25:
+    print('senior')
+elif idade <= 130:
+    print('master')
+else:
+    print('ta morto já')
