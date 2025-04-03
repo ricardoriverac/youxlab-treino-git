f167b8f57a600595f2ccf4e7042010a0e0c70e27
Marcos <marcos.garcia@youxlab.com.br>
2025-04-02 22:14:48 -0300

    Fiz um código de Progressão Aritmética (P.A.)

---
+++ aulas_python/desafios_aula_13/atividade51.py
@@ -0,0 +1,15 @@
+import time
+
+print('\033[1;34m-=-=-=-=-= Bem vindo(a)! =-=-=-=-=-\033[m')
+time.sleep(1)
+
+
+print('\n\033[1;34m-=-=-=-=-= 10 TERMOS DE P.A. =-=-=-=-=-\033[m')
+a1  = int(input('Primeiro termo: '))
+pos = int(input('Termo final: '))
+raz = int(input('Razão: '))
+
+for c in range(a1, pos, raz):
+    print(F'✦{c}✦')
+    
+print('\033[1;34m-=-=-=-=-= FIM! =-=-=-=-=-\033[m')
