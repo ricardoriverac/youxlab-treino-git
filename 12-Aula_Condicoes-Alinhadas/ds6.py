c5c5f399ce126216c8e02a9754fc2689e18ee8e4
gustavofreire <gustavo.gomes@youxlab.com.br>
2025-04-03 13:47:10 -0300

    ds41

---
+++ 12-Aula_Condicoes-Alinhadas/ds6.py
@@ -0,0 +1,12 @@
+ano =int(input('Qual ano você nasceu ?  '))
+id = 2025 - ano
+if id <=9 :
+    print('você e mirim, muito comum')
+elif id <14:
+    print('Infantil, ta legal')    
+elif id <19:
+    print('Junior, ta bom..')    
+elif id <20:
+    print('VOCÊ E SENIOR QUE LEGAAAAALLL') 
+elif id >20:
+    print('CARACAAA VOCÊ E MASTEEEEERRR')    
