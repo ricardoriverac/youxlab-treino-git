f883526703ed276be5363f00e28ac359889a3550
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-04-03 13:34:10 -0300

    Jo Ken Po

--- aula012/ex045.py
+++ aula012/ex045.py
@@ -1,7 +1,7 @@
 from random import choice
 import random
 print('Vamos jogar Jo Ken Po!!!')
-print('<=>'*20)
+print('<=>'*15)
 suaEscolha=input(str('Escolha pedra,papel ou tesoura: '))
 opcoes=('pedra','papel','tesoura')
 computador=random.choice(opcoes)
@@ -9,6 +9,9 @@ print('O seu adversário escolheu: {}'.format(computador))
 
 if suaEscolha == 'pedra' and computador == 'tesoura' or suaEscolha == 'tesoura' and computador == 'papel' or suaEscolha == 'papel' and computador == 'pedra':
     print('Parabéns você ganhou!!! ')
+
+elif suaEscolha == computador:
+    print('Temos um empate!!! ')
 
 else:
     print('A máquina ganhou!!! ')
