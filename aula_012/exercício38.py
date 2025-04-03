15dcabf88771571f373d14c6ff1e46e4900cd33f
davisales07 <davisales260810@gmail.com>
2025-04-03 16:22:27 -0300

    exercícios

---
+++ aula_012/exercício38.py
@@ -0,0 +1,10 @@
+num1 = int(input( 'diga o primeiro valor  inteiro.' ))
+num2 = int(input( 'diga o segundo valor inteiro. '))
+if num1 > num2:
+    print(' o primeiro valor é maior. ')
+elif num2 > num1:
+    print(' o segundo valor é maior. ')
+else:
+    print('os dois valores são iguais. ')
+    
+
