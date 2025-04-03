a00c04baa6a64864b8d48f4e2491bca5304b7d01
João Pedro <joao.pedro@youxlab.com.br>
2025-04-03 13:14:28 -0300

    Terminando o exercicio 72

--- Exercicios/exe72.py
+++ Exercicios/exe72.py
@@ -3,4 +3,13 @@
 
 #seu programa devera ler um numero pelo teclado (entre 0 e 20) e mostra-lo por exetenso
 
-tuplaDeNumeros = ("um", "dois", "")
+cont = ("zero", "um", "dois", "tres", "quatro", "cinco", "seis",
+        "sete", "oito", "nove", "dez", "onze", "doze", "treze", "quatorze",
+        "quinze", "dezesseis", "dezessete", "dezoito", "dezenove", "vinte")
+
+while True:
+    num = int(input("Digite um número entre 0 a 20: "))
+    if 0 <= num <= 20:
+        break
+    print("Tente novamente.", end="")
+print(f"Voce digitou o número {cont[num]}")
