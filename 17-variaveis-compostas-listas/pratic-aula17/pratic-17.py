2aa89ec3182e3af2ce775fc8b46241a8ef32b5e0
juliocesar <juliogoiabinha060@gmail.com>
2025-04-03 14:31:04 -0300

    Variaveis compostas listas

---
+++ 17-variaveis-compostas-listas/pratic-aula17/pratic-17.py
@@ -0,0 +1,33 @@
+# Primeira pratica.
+ 
+# num = [2 , 5 , 9 , 1]
+# nomes = ['Julio' , 'César' , 'dos']
+# num.extend(nomes)
+# print(num)
+# num[2] = 3
+# num.append(7)
+# num.insert(3,9)
+# # num.append(4)
+# num.pop(2)
+# print(num)
+# if 4 in num:
+#     num.remove(4)
+# else:
+#     print('Não a número na lista 4 ')
+# print(f'Essa lista tem {len(num)} elementos')
+
+
+#Segunda pratica
+# valores = []
+# for cont in range(0,5):
+#     valores.append(int(input('Digite um valor')))
+
+# for posicao , valor in enumerate(valores):
+#     print(f' na posiçao {posicao} , encotrei o valor {valor}...')
+
+#Terceira pratica 
+# a = [2 , 3 , 4 , 7]
+# b = a[:]
+# b[2] = 8
+# print(f'Lista A: ê {a}')
+# print(f'Lista B: {b}')
