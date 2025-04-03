3b0504f25b834a543038a9c833075c19c2ca612b
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-03 17:03:04 -0300

    Exercício 23

---
+++ exercicios/ex023.py
@@ -0,0 +1,8 @@
+numero= int(input('Insira um número de 0 a 9999: '))
+digitos= str(numero)
+print('Analisando o número {}'.format(numero))
+print('Unidade: {}'.format(digitos[3]))
+print('Dezena: {}'.format(digitos[2]))
+print('Centena: {}'.format(digitos[1]))
+print('Milhar: {}'.format(digitos[0]))
+ 
