0a1ef4d0f8d8b96de504ed794e35d315ca5ded26
gustavofreire <gustavo.gomes@youxlab.com.br>
2025-04-03 12:35:48 -0300

    ds4

--- 12-Aula_Condicoes-Alinhadas/Desafio3.py
+++ 12-Aula_Condicoes-Alinhadas/Desafio3.py
@@ -2,11 +2,11 @@ num1=int(input('[Valor 1]:  '))
 num2=int(input('[Valor 2]:  '))
 if num1>num2:
     print('-=-'* 20)
-    print('o valor 1: {} e maior que o valor 2: {}'.format(num1, num2))
+    print('o valor 1: {} é maior que o valor 2: {}'.format(num1, num2))
     print('-=-'* 20)
 elif num1<num2:
     print('-=-' *20)
-    print('o valor 1: {} e menor que o valor 2: {}'.format(num1, num2))
+    print('o valor 1: {} é menor que o valor 2: {}'.format(num1, num2))
     print('-=-' *20)
 else:
     print('-=-'* 20)
