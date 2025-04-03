7d3d1d532a65608ee92558de75f4974fb6482a9c
GustavoValacio <gustavoseabra2007@gmail.com>
2025-04-03 13:34:21 -0300

    Desafio40 Media, Aprovado, Reprovado

---
+++ 12-aula_python/Desafio40Aula12.py
@@ -0,0 +1,16 @@
+n1 = float(input('Digite sua nota '))
+n2 = float(input('Digite sua segunda nota '))
+
+media = (n1 + n2) / 2
+
+if media < 5:
+    print('REPROVADO INCOMPETENTE!!!!')
+
+elif media >= 7:
+    print('APROVADO LENDA!!')
+
+elif media >= 5 < 6.9:
+    print('RECUPERAÇÂO')
+
+
+
