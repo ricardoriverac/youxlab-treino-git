b11ec74d4b9787f80c204a0e4585f49b8ec0532c
Maria Luiza <maria.silva@youxlab.com.br>
2025-04-03 13:28:55 -0300

    desafio pra ver se o triângulo é equilatero, escaleno e isósceles

--- desafiosaula12/desafio42.py
+++ desafiosaula12/desafio42.py
@@ -1,11 +1,18 @@
-print('ME DIGA O COMPRIMENTO DE TRÊS RETAS')
-primeiroLado = float(input('Reta 1: '))
-segundoLado = float(input('Reta 2: '))
-terceiroLado = float(input('Reta 3: '))
-if primeiroLado == segundoLado == terceiroLado:
-    print('O triângulo é {}EQUILÁTERO{}, pois todos os lados são iguais '.format('\033[35m', '\033[m'))
-elif primeiroLado == segundoLado or segundoLado == terceiroLado or primeiroLado == terceiroLado:
-    print('O triângulo é {}ISÓSCELES{}, pois dois lados são iguais '.format('\033[35m', '\033[m'))    
-elif primeiroLado != segundoLado != terceiroLado:
-    print('O triângulo é {}ESCALENO{}, pois todos os lados são diferentes '.format('\033[35m', '\033[m'))
-    
+print ('ME DIGA O COMPRIMENTO DE 3 RETAS')
+primeiraReta = float(input('Reta 1: '))
+segundaReta = float(input('Reta 2: '))
+terceiraReta = float(input('Reta 3: '))
+if primeiraReta + segundaReta > terceiraReta and segundaReta + terceiraReta > primeiraReta and terceiraReta + primeiraReta > segundaReta:
+    print('Com essas retas É POSSÍVEL formar um triângulo! ')
+else:
+    print('Com essas retas NÃO É POSSÍVEL formar um trângulo! ')
+
+if primeiraReta == segundaReta == terceiraReta:
+        print('O triângulo é {}EQUILÁTERO{}, pois todos os lados são iguais '.format('\033[35m', '\033[m'))   
+
+elif primeiraReta != segundaReta != terceiraReta !=primeiraReta:
+        print('O triângulo é {}ESCALENO{}, pois todos os lados são diferentes '.format('\033[35m', '\033[m'))
+        
+else:
+        print('O triângulo é {}ISÓSCELES{}, pois dois lados são iguais '.format('\033[35m', '\033[m')) 
+        
