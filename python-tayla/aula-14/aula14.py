39a2287fbf4d3973beb9b5f878f4002670da2816
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-04-04 19:51:17 -0300

    aula14.py

--- python-tayla/aula-14/aula14.py
+++ python-tayla/aula-14/aula14.py
@@ -1,20 +1,20 @@
 #testando while
-# c = 1
-# while c < 10:
-#     print(c)
-#     c = c + 1
-# print('Fim')
+c = 1
+while c < 10:
+    print(c)
+    c = c + 1
+print('Fim')
 
-# n = 1
-# while n != 0:
-#     n = int(input('Digite um valor: '))
-# print('Fim')
+n = 1
+while n != 0:
+    n = int(input('Digite um valor: '))
+print('Fim')
 
-# r = 'S'
-# while r == 'S':
-#     n = int(input('Digite um valor: '))
-#     r = input('Quer continuar? [S/N] ').upper()
-# print('Fim')
+r = 'S'
+while r == 'S':
+    n = int(input('Digite um valor: '))
+    r = input('Quer continuar? [S/N] ').upper()
+print('Fim')
 
 #vendo numero par e impar sem contar o 0
 n = 1
