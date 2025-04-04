73b7ff302c61145e20f4571f8bca9566c1153639
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-04-04 19:50:12 -0300

    aula14.py

---
+++ python-tayla/aula-14/aula14.py
@@ -0,0 +1,29 @@
+#testando while
+# c = 1
+# while c < 10:
+#     print(c)
+#     c = c + 1
+# print('Fim')
+
+# n = 1
+# while n != 0:
+#     n = int(input('Digite um valor: '))
+# print('Fim')
+
+# r = 'S'
+# while r == 'S':
+#     n = int(input('Digite um valor: '))
+#     r = input('Quer continuar? [S/N] ').upper()
+# print('Fim')
+
+#vendo numero par e impar sem contar o 0
+n = 1
+par = impar = 0
+while n != 0:
+    n = int(input('Digite um valor: '))
+    if n != 0:
+        if n % 2 == 0:
+            par += 1
+        else:
+            impar += 1
+print(f'Você digitou {par} números pares e {impar} números ímpares')
