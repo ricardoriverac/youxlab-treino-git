66281a005fc24d5affe21c2c36a7f52f4c8c4b12
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-04-04 17:11:54 -0300

    Soma idades 2.0

---
+++ aula013/ex56.py
@@ -0,0 +1,37 @@
+soma = 0
+maisVelho = 0
+nomeVelho = ''
+mulheres=0
+
+for c in range(1, 4):
+    print(f'---- {c} Pessoa ----')
+    nome = input('Nome: ')
+    idade = int(input('Idade: '))
+    sexo = input('[M/F]: ').strip().upper()
+    
+    soma += idade
+    
+    if sexo == 'M':
+        if idade > maisVelho: 
+            maisVelho = idade
+            nomeVelho = nome
+
+    elif sexo =='F' and idade < 20:
+            mulheres += 1
+
+media = soma/3
+
+print('O homem mais velho tem {} anos e seu nome é {}.'.format(maisVelho,nomeVelho))
+print('A média de idade do grupo é {}. '.format(media))
+print('Ao todo são {} mulheres '.format(mulheres))
+    
+
+
+    
+
+    
+
+
+
+
+
