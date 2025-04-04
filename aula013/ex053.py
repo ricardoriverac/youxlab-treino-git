c77b1888ed453df1838a320a496e4e2792ae3156
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-04-04 13:55:37 -0300

    Menor ou maior de idade

---
+++ aula013/ex053.py
@@ -0,0 +1,12 @@
+frase=str(input('Digite uma frase: ')).strip().upper()
+palavras=frase.split()
+junto = ''.join(palavras)
+print('Você digitou a frase {} '.format(junto))
+inverso=junto[::-1]
+#for letra in range(len(junto)-1,-1,-1):
+    #inverso += junto[letra]
+if inverso == junto:
+    print('Temos um palíndromo! ')
+else:
+    print('A frase digitada não é um palíndromo! ')
+    
--- aula013/ex054.py
+++ aula013/ex054.py
@@ -1,2 +1,19 @@
 from datetime import date
+menor=0
+maior=0
 anoAtual=date.today().year
+for pes in range(1,4):
+    ano=int(input('Em que ano você nasceu ? '))
+    idade = anoAtual - ano
+
+    if idade <=17:
+       menor +=1
+    else:
+        maior +=1
+
+print('Ao todo tivemos {} pessoas maiores.E também tivemos {} pessoas menores de idade '.format(maior,menor))
+
+
+
+
+    
