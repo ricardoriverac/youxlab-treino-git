bc6d66f763bee05c274b608fccae858cd06a0572
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-04-05 20:44:41 -0300

    aula 15

---
+++ python-tayla/aula-15/aula15.py
@@ -0,0 +1,21 @@
+# cont = 1
+# while cont <= 10:
+#     print(cont, '--> ', end=' ')
+#     cont += 1
+# print('Acabou')
+
+#usando o true - deixa em loop infinito
+# cont = 1
+# while True:
+#     print(cont, '--> ', end=' ')
+#     cont += 1
+# print('Acabou')
+
+#usando o break pra parar o loop e somando os valores
+n = s = 0
+while True:
+    n = int(input('Digite um número: '))
+    if n == 999:
+        break
+    s += n
+print(f'A soma vale {s}')
