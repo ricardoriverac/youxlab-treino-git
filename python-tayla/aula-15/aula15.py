84eca0baf417f59aafe7a7341afc3d91d4917297
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-04-05 20:45:07 -0300

    aula 15

--- python-tayla/aula-15/aula15.py
+++ python-tayla/aula-15/aula15.py
@@ -1,15 +1,15 @@
 # cont = 1
-# while cont <= 10:
-#     print(cont, '--> ', end=' ')
-#     cont += 1
-# print('Acabou')
+while cont <= 10:
+    print(cont, '--> ', end=' ')
+    cont += 1
+print('Acabou')
 
 #usando o true - deixa em loop infinito
-# cont = 1
-# while True:
-#     print(cont, '--> ', end=' ')
-#     cont += 1
-# print('Acabou')
+cont = 1
+while True:
+    print(cont, '--> ', end=' ')
+    cont += 1
+print('Acabou')
 
 #usando o break pra parar o loop e somando os valores
 n = s = 0
