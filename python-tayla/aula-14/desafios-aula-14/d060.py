d2646818e40c1ee5b0e7f9476e4160f741af0bab
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-04-05 11:22:12 -0300

    desafio 60

--- python-tayla/aula-14/desafios-aula-14/d060.py
+++ python-tayla/aula-14/desafios-aula-14/d060.py
@@ -4,4 +4,4 @@ resultado = numero
 while fatorial != 1:
     fatorial = fatorial - 1
     resultado = fatorial * resultado
-print(f'O fatorial do número {numero} é {resultado}')
+print(f'O fatorial do número {numero} é {resultado} ')
