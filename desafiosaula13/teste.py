4f1a2c9406c9372095db1c532f9eab335c0d7a0c
Maria Luiza <maria.silva@youxlab.com.br>
2025-04-06 15:21:39 -0300

    teste da aula 13

--- desafiosaula13/teste.py
+++ desafiosaula13/teste.py
@@ -1,2 +1,20 @@
 for c in range(0 , 6):
     print('Oi')
+
+for c in range(0 , 7, 2):
+    print(c)
+
+# n = int(input('Digite um número inteiro: '))
+i = int(input('Ínicio: '))
+f = int(input('Final: '))
+p = int(input('Passo: '))
+for c in range (i, f+1, p):
+    print(c)
+
+s = 0
+for c in range(0, 4):
+    n = int(input('Digite um valor: '))
+    s += n 
+print('O somatório de todos valores é {} '.format(s))
+
+
