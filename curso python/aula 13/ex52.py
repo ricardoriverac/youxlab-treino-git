c6fd0098a275309ec246c5c2d486d796c1177fc4
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-07 17:46:39 -0300

    organizando pastas

--- curso python/aula 13/ex52.py
+++ curso python/aula 13/ex52.py
@@ -1,14 +1,14 @@
-numero = int(input('Digite um número: '))
-tot = 0
-for c in range(1, num +1):
-    if num % c == 0:
-        print('\033[33m', end=' ')
-        tot += 1
+numero = int(input("Digite um número: "))
+contador = 0
+for c in range(1, numero +1):
+    if numero % c == 0:
+        print("\033[32m", end=" ")
+        contador = contador + 1
     else:
-        print('\033[31m', end=' ')
-    print('{}'.format(c), end=' ')
-print('\n\033[mO número {} foi divisivel {} vezes' .format(num, tot))
-if tot == 2:
-    print('E por isso ele é PRIMO!')
+        print("\033[34m", end=" ")
+    print('{}'.format(c), end=" ")
+print("\n\033[mO número {} foi divisivel {} vezes".format(numero, contador))
+if contador == 2:
+    print("Ele é PRIMO!")
 else:
-    print('E por isso ele NÃO É PRIMO!')
+    print("não é PRIMO!") 
