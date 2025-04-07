6b6961d58b99b10ceb0351514b0c82dd3954f288
Youx <sophia.carvalho@youxlab.com.br>
2025-04-07 15:23:06 -0300

    aula10

---
+++ aula10/4°desafio-aula10.py
@@ -0,0 +1,7 @@
+km=int(input('Sua viajem tem qunantos km?:'))
+if km<=200:
+    valor1= km*0.50
+    print('O valor da sua viajem é de {:.2f}'.format(valor1))
+else:
+    valor2= km*0.45
+    print('O valor da sua viajem é de {:.2f}'.format(valor2))
