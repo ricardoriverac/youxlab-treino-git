9ec4495a045387b7adee27cae7ccc07a04b70b5d
Marcos <marcos.garcia@youxlab.com.br>
2025-04-07 17:43:05 -0300

    Fiz uma P.A. v2.0

---
+++ aulas_python/desafios_aula_14/atividade61.py
@@ -0,0 +1,16 @@
+import time
+print('\033[1;34m-=-=-=-=-= P.A. v2.0 =-=-=-=-=-\033[m')
+time.sleep(1)
+
+print('\n\033[1;34m-=-=-=-=-= 10 TERMOS DE P.A. =-=-=-=-=-\033[m')
+a1  = int(input('Primeiro termo: '))
+raz = int(input('Razão: '))
+contador = 0
+
+while contador < 10:
+    print(a1, end= ' ')
+    a1 += raz
+    contador += 1
+print('FIM')
+
+    
