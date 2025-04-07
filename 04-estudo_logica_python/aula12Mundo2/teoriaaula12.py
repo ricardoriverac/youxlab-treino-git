a73d66b9e642a35d710c7cf1907703476e5e63fa
marianne0614 <marianne.lima@youxlab.com.br>
2025-04-07 14:02:34 -0300

    teoria aula 12

---
+++ 04-estudo_logica_python/aula12Mundo2/teoriaaula12.py
@@ -0,0 +1,10 @@
+nome=str(input('Qual é seu nome?'))
+if nome==' Gustavo':
+    print('Que nome bonito!')
+elif nome == 'Pedro' or nome =='Maria' or nome =='Paulo':
+     print(' Seu nome é popular no Brasil')
+elif nome in 'Ana, claudia, jessica, julia ':
+    print('Belo nome feminino!')
+else:
+    print('Seu nome é normal')
+print('Tenha um bom dia {}!'. format(nome))
