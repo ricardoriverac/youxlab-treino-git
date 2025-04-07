c127d723b515a9200c2c3c271c482fdd22be228f
gustavofreire <gustavo.gomes@youxlab.com.br>
2025-04-07 13:58:21 -0300

    ds62

---
+++ 14-Aula _EstruturaDeRepetição/ds62.py
@@ -0,0 +1,15 @@
+prm=int(input('Primeiro termo: '))
+raz=int(input('Razão:  '))
+termo=prm
+cont= 1
+mais=10
+total=0
+while mais != 0:
+    total=total + mais
+    while cont <= total:
+        print('{} ->'.format(termo), end =' ')
+        termo +=raz
+        cont += 1
+        print('PAUSA') 
+        mais=int(input('Quantos numeros quer mostrar a mais ?  '))
+print('FIM')
