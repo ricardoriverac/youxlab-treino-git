1336580828426822733ddf4755fd674042e7c339
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-07 16:15:08 -0300

    atividades aula 08

---
+++ aula 08/aula08-atividade.py
@@ -0,0 +1,9 @@
+from math import sqrt , floor #biblioteca de matématica
+num = int (input("Digite um número: "))
+raiz = sqrt(num)
+print ("A ríz de {} é igual a {:.2f} ".format(num, floor(raiz)))
+#raiz = math. #todas as funcionalidades da biblioteca math
+
+import random
+num = random.randint(1,10) 
+print (num)
