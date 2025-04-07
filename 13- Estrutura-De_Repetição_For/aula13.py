047cba7a6030160f2276701864db5c774a3333f2
fabricioyoux <fabricio.genesi@youxlab.com.br>
2025-04-07 16:51:49 -0300

    atividade 46

---
+++ 13- Estrutura-De_Repetição_For/aula13.py
@@ -0,0 +1,52 @@
+#é a mesma coisa só traduzindo para o portugues na primeira
+
+#laço c no intervalo(0,3):        for c in range(0,3):
+# se (moeda)                      if (moeda):
+#     pega                                  pega
+#    passo                               passo
+#   pula                                 pula
+# passo                                 passo
+# pega                                  pega
+
+#-fazer uma palavra repetir tantas vezes
+#for c in range(0, 6):
+#    print('Oi')
+#print('FIM')
+
+#-contagem de 1 a 5, porque o ultimo numero é ignorado
+#for c in range(1, 6):
+#    print(c)
+#print('FIM')
+
+#-----------------------------------------------------
+#-fazer os números contarem pra tras
+#for c in range(6, 0, -1):
+#    print(c)
+#print('FIM')
+#---------------------------------------------------
+#n = int(input('Digite um número:'))
+#for c in range(0, n+1):
+#    print(c)
+#print("FIM")
+
+#---------------------------------------------------
+#i = int(input('Inicio:'))
+
+#f = int(input('Fim:'))
+
+#p = int(input('Passo:'))
+
+#for c in range (i,f+1, p):
+#    print(c)   
+#print('FIM')
+
+#----------------------------------------------------
+#for c in range(0, 5):
+#    n = int(input('Digite um valor: '))
+#print('Fim')
+
+s = 0
+for c in range(0, 4):
+    n = int(input('Digite um valor: '))
+    s += n
+print(f'O somatório de todos os valores foi {s}. ')
