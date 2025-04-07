5b7682d0de49425f8ff9c47d4ac7a4e7de6de899
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-07 17:47:09 -0300

    Exercício 33

---
+++ ex033.py
@@ -0,0 +1,7 @@
+numero1= int(input('Digite um número: '))
+numero2= int(input('Digite outro número: '))
+numero3= int(input('Digite mais um: '))
+if numero1 > numero2 and numero3:
+    print('{} é o maior número'.format(numero1))
+if numero2 > numero1 and numero3:
+    print({} 'é o maior número'.format())
