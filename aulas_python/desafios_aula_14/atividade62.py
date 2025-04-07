29d855dd8ae2e3be5eba9846dabb03102101ed87
Marcos <marcos.garcia@youxlab.com.br>
2025-04-07 18:03:44 -0300

    Pra deixar salvo

---
+++ aulas_python/desafios_aula_14/atividade62.py
@@ -0,0 +1,19 @@
+import time
+print('\033[1;34m-=-=-=-=-= P.A. v2.0 =-=-=-=-=-\033[m')
+time.sleep(1)
+
+print('\n\033[1;34m-=-=-=-=-= 10 TERMOS DE P.A. =-=-=-=-=-\033[m')
+a1  = int(input('Primeiro termo: '))
+raz = int(input('Razão: '))
+contador = 0
+
+while contador < 10:
+    print(a1, end= ' ')
+    a1 += raz
+    contador += 1
+
+termos = int(input('\nQuantos termos você quer mostrar a mais '))
+while contador < termos:
+    print(a1, end= ' ')
+    a1 += raz
+    contador += 1
