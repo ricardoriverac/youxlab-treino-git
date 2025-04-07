f1dd28885403f6e84d83db710ec2002b1f9921d9
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-07 13:46:39 -0300

    soma dos pares

--- exercicios/ex50.py
+++ exercicios/ex50.py
@@ -4,6 +4,6 @@ for c in range (0,6):
    num = int (input ("Digite um número: "))
    if num % 2 == 0:
         cont += num
-print ("A soma dos números pares são: {} ".format(cont))
+print ("A soma dos números pares é: \033[31m{}\033[m".format(cont))
  
        
