36ef52195d9091658ae0129986a2918f0621effb
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-07 16:25:08 -0300

    atividades aula 09

---
+++ aula 09/aula09-atividade.py
@@ -0,0 +1,10 @@
+frase = "Curso em vídeo python"# fatear o inicio
+print (frase[3:13])
+frase = "Curso em vídeo python"#fatear o inicio
+print (frase[13:3])
+
+print ("olá")
+
+print (""""Basicamente, eu leio a letra e depois o número. Como eu leio o número dígito a dígito, vou atualizando o seu valor. """)
+#escrever várias coisas sem print
+
