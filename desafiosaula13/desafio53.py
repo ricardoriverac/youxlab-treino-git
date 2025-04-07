9dbee4c2ac97548dc0a985f4d5b26c1944a90372
Maria Luiza <maria.silva@youxlab.com.br>
2025-04-07 16:10:16 -0300

    desafio da frase palíndromo

--- desafiosaula13/desafio53.py
+++ desafiosaula13/desafio53.py
@@ -3,9 +3,9 @@ from time import sleep
 frase = str(input('Digite uma frase: ').strip())
 fraseLimpa = re.sub(r'[^a-zA-Z0-9]', '', frase.lower())
 print('{}PROCESSANDO...{}'.format('\033[36m', '\033[m'))
+print('A frase {} ao contrário é {} '.format(frase, fraseLimpa))
 while fraseLimpa != fraseLimpa[::-1]:
     print('Essa frase NÃO é um {}PALÍNDROMO{} \n {}Tente de novo{}'.format('\033[35m', '\033[m', '\033[33m','\033[m'))
     break
 else:
     print('Essa frase é um {}PALÍNDROMO{}'.format('\033[35m', '\033[m'))
-
