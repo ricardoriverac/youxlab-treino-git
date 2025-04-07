166201d0ec8457149e7db953484c8379759f3615
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-07 15:16:39 -0300

    atividades aula 06

---
+++ aula 06/aula06-atividade.py
@@ -0,0 +1,18 @@
+n1= input ("Digite um valor: ")  #entrada de um valor
+print (type(n1)) #tipo primitivo
+n1=  int (input("Digite um valor: "))  #entrada de um valor inteiro
+print (type(n1)) #tipo primitivo
+n1 = int (input("Digite um número: "))  #entrada do primeiro número
+n2 = int (input("Digite outro número: "))  #entrada do segundo número
+s = n1 + n2 #soma dos valores digitados
+print ("escreva a soma dos números : {}",s)
+#print ("A soma entre", n1, "e", n2, "vale", s,)
+print ("A soma entre {} e {} vale {}".format(n1,n2,s))  
+n= float (input ("Digite um valor: ")) #número com virgula
+print (n)
+#print (n.isnumeric()) string verdadeiro ou faslo
+#print (n.isaplha()) se é letra
+#print (n.isupper()) se é letra minúscula
+#print (n.type) tipo primitivo
+#print (n.isupper) letra maiúscula
+
