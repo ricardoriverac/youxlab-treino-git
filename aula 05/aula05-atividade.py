4652f0fbac294f78336f7984c4fa9e6af2cd8caf
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-07 14:55:15 -0300

    atividades aula 05

---
+++ aula 05/aula05-atividade.py
@@ -0,0 +1,3 @@
+print ("Olá mundo!!") #saída de uma mensagem
+nome = input ("Qual é o seu nome ? ") #saída de uma pergunta + variavél
+print ("É um prazer te conhecer!!", nome) #saída da mensagem com o uso da varavél 
