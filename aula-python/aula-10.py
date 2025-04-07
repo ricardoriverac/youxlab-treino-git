1f0b3f59f49d6e7811684b2558a6c203fcdd80bf
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-07 14:34:43 -0300

    Aula 10

---
+++ aula-python/aula-10.py
@@ -0,0 +1,11 @@
+#condições
+#if- se (estrutura simples)
+#else- senão (if/else estrutura composta)
+#bloco True e False nunca poderão ser realizados juntos
+
+tempo= int(input('Quantos anos tem seu carro? '))
+if tempo<=3:
+    print('Carro novo')
+else: 
+  print('Carro velho')
+print('--FIM--')
