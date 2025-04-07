c3dd27b953208fbb7c12f2681b7f0207ff88f12f
marianne0614 <marianne.lima@youxlab.com.br>
2025-04-07 16:59:30 -0300

    desafio 38

---
+++ 04-estudo_logica_python/aula12Mundo2/desafio38.py
@@ -0,0 +1,8 @@
+numero1= int(input( 'Digite um numero :'))
+numero2=int(input('Digite outro numero:'))
+if numero1 > numero2 :
+   print('O valor {} é maior do que o {}'. format(numero1, numero1))
+elif numero1 < numero2 :
+    print(f'o numero {numero2} é maior que o numero {numero1}' ) 
+else:
+    print(f'Os dois numeros sao iguais. Valores digitados {numero1}')
