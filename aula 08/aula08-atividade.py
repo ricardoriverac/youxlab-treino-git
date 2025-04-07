f05a43cf4f05db5b7443f7e6d20263697d6a8588
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-07 16:17:36 -0300

    atividades aula 08

--- aula 08/aula08-atividade.py
+++ aula 08/aula08-atividade.py
@@ -1,7 +1,7 @@
 from math import sqrt , floor #biblioteca de matématica
 num = int (input("Digite um número: "))
 raiz = sqrt(num)
-print ("A ríz de {} é igual a {:.2f} ".format(num, floor(raiz)))
+print ("A raíz de {} é igual a {:.2f} ".format(num, floor(raiz)))
 #raiz = math. #todas as funcionalidades da biblioteca math
 
 import random
