1c4fc684f3546016fce41996e73d8e9d259df612
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-04-07 14:07:03 -0300

    refiz o desafio 52

---
+++ 13-curso_python/desafio55.py
@@ -0,0 +1,2 @@
+for peso in range (1, 6):
+    kg = float(input('Digite o peso da {} pessoa: '.format(peso)))
--- exercicios.py/exerc52.py
+++ exercicios.py/exerc52.py
@@ -1,6 +1,11 @@
 numero = int(input('Digite um número: '))
 primo = numero%2
-if primo ==0 :
-    print('Esse número não é primo.')
-else :
-    print('Esse número é primo.')
+tot = 0
+for n in range(1, numero +1):
+    if numero % n ==0:
+      print('\033[35m', end=' ')
+      tot += 1
+    else :
+       print('\033[31m', end=' ')
+    print('{}'.format(n), end = ' ')
+print('O número {} foi divisível {} vezes.'.format(numero, tot)) 
