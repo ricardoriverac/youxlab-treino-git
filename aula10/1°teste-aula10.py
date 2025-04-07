1bd582b47f31866be38509e8dde3cc7fe89f81f1
Youx <sophia.carvalho@youxlab.com.br>
2025-04-07 14:02:38 -0300

    aula10

---
+++ aula10/1°teste-aula10.py
@@ -0,0 +1,7 @@
+#Condições
+nome=str(input('Qual seu nome:'))
+if nome == 'Sophia':
+    print('Oi,Sophia!') 
+else:
+    print(f'Bom dia {nome}!')
+    print('Cadê a Sophia? ')
--- aula9/6°desafio-aula09.py
+++ aula9/6°desafio-aula09.py
@@ -1,3 +1,4 @@
+#Primeiro e último nome da pessoa
 nome=str(input('Digite seu nome:' )).strip()
 nomes=nome.split()#.split()--> comando que fatia palavras separando as as palavras 
 print('Seu primeiro nome é: {}'.format(nomes[0]))
