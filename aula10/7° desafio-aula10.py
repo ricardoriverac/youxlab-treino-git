b57cb63140b6062af70788ece41ec453d9836664
Youx <sophia.carvalho@youxlab.com.br>
2025-04-07 16:52:17 -0300

    aula10

---
+++ aula10/7° desafio-aula10.py
@@ -0,0 +1,7 @@
+salario=float(input('Qual o seu sálario?: '))
+if salario>1250.00:
+    aumento1= salario*1.10
+    print('Seu aumento foi de 10%, então seu sálario é:{:.2f}'.format(aumento1))
+if salario<1250.00:
+    aumento2= salario*1.15
+    print('Seu aumento foi de 15%, então seu sálario é:{:.2f}'.format(aumento2))
