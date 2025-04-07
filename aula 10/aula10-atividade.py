692e2a059e4de20b449bdcc28d1d778a3220c887
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-07 16:34:50 -0300

    atividades aula 10

---
+++ aula 10/aula10-atividade.py
@@ -0,0 +1,15 @@
+nome = str (input ("QUal seu nome ? "))
+if nome == gustavo: #estrutura SE
+    print ("Que nome lido você tem, !") #vai ler essa esse comando
+else: #Estrutura SENÃO
+    print ("Bom dia, {}".format(nome))
+    print ("Seu nome é tão normal!")
+
+n1 = float (("Digite o primeiro número: "))
+n2 = float (("Digite outro número: "))
+m = (n1 + n2)/2
+print ("A sua média foi {:.1f}".format(m))
+if media >= 6:
+    print ("Sua média foi boa, PARABÉNS!")
+else:
+    print ("Sua média foi ruim, ESTUDE MAIS!")
