607ca7696efc9b5b9498d1e9a18525ffd269f30d
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-04-08 14:01:44 -0300

    salvando desafio 8 da aula 7

---
+++ curso_python/aula07/desafio_8_aula07.py
@@ -0,0 +1,4 @@
+valorMetros = int(input('Digite um valor em metros: '))
+centimetros = valorMetros / 100
+milimetros = valorMetros / 1000
+print (f'Este valor tem {centimetros} centímetros e {milimetros} milímetros ')
