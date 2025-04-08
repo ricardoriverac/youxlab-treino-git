2df2dd829f3ff696e1d4bda50bcb642baeb159cc
marianne0614 <marianne.lima@youxlab.com.br>
2025-04-08 17:21:32 -0300

    teoria da aula 13

---
+++ 04-estudo_logica_python/aula 13/teoria.py
@@ -0,0 +1,13 @@
+i=int(input(' inicio:'))
+f= int(input('fim:'))
+p= int(input('passo:'))
+for c in range( i, f+1, p):
+    print(c)
+print(' fim')    
+
+
+s=0
+for c in range(0, 4):
+    n=int(input( 'Digite um numero:'))
+    s += n
+print= ('o somatorio de todos os valores foi {}'.format(s))    
