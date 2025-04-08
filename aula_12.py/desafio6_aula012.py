a7db91c49fe340d4bf2c6159e70d016aa1d5c8b5
annaluiza777 <anna.araujo@youxlab.com.br>
2025-04-08 13:33:45 -0300

    desafio 6  aula 12

---
+++ aula_12.py/desafio6_aula012.py
@@ -0,0 +1,9 @@
+idade = int(input('Digite a sua idade '))
+if idade <= 9:
+    print('Categoria: MIRIM')
+elif idade <= 14:
+    print('Categoria:') 
+elif idade <= 25:
+    print('Categoria: SENIOR')
+elif idade > 25:
+    print('Categoria: MASTER')
