da0a96a26dd3d0ee316f19bea55fc061386ffe40
Youx <sophia.carvalho@youxlab.com.br>
2025-04-08 14:52:38 -0300

    mundo2

---
+++ aula12-Mundo2/3°desafio-aula12.py
@@ -0,0 +1,10 @@
+#Verificando números 
+numero1=int(input('Digite um númro: '))
+numero2=int(input('Digite outro númro: '))
+if numero1>numero2:
+    print('1° número maior')
+elif numero2>numero1:
+    print('2° número maior')
+elif numero1==numero2:
+    print('Os números são iguais')
+    
