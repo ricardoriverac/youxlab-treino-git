420995139fadce1a4521a7d47a78e610b002814a
João Pedro <joao.pedro@youxlab.com.br>
2025-04-08 17:19:34 -0300

    Corrigindo o exercicio 77

--- Exercicios/exe77.py
+++ Exercicios/exe77.py
@@ -1,10 +1,11 @@
 #Crie um programa que tenha uma tupla com varias palavras (não usar acentos).
 #Depois disso, voce devera mostrar, para cada palavra, quais são as suas vogais.
 
-palavras = ("escola", "casa", "abaixo", "assento", "esquadrilha", "")
+palavras = ("escola", "casa", "abaixo", "assento", "esquadrilha", "Ovelha")
 
 for p in palavras:
     print(f"\nNa palavra {p.upper()} temos:", end="")
     for letra in p:
         if letra.lower() in "aeiou":
-            print(letra, end=" ")
+            print(letra.lower(), end=" ")
+
