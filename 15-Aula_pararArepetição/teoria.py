229e53fd7e52be09750c9de3e3eb98643d70b680
gustavofreire <gustavo.gomes@youxlab.com.br>
2025-04-08 12:41:38 -0300

    ds66

---
+++ 15-Aula_pararArepetição/teoria.py
@@ -0,0 +1,15 @@
+n=s=0
+while True:
+    n =int(input('bla'))
+    if n == 999:
+        break
+    s += n
+    n - s 
+    b=100
+    if n == 1:
+        break
+    elif n == 3:
+        print('chega')
+        break
+#print('a soma e {}'.format(s))
+print(f'a soma vale {s:.2f} , {b} , {s}')
