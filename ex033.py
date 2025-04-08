9dfd8ccd6760044288abfd91bb49cd1db8aa2d9f
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-08 13:20:08 -0300

    Exercício 33

--- ex033.py
+++ ex033.py
@@ -4,4 +4,14 @@ numero3= int(input('Digite mais um: '))
 if numero1 > numero2 and numero3:
     print('{} é o maior número'.format(numero1))
 if numero2 > numero1 and numero3:
-    print({} 'é o maior número'.format())
+    print('{} é o maior número'.format(numero2))
+if numero3 > numero1 and numero2:
+    print('{} é o maior número'.format(numero3))
+if numero1 < numero2 and numero3:
+    print('{} é o menor número'.format(numero1))
+if numero2 < numero1 and numero3:
+    print('{} é o menor número'.format(numero2))
+if numero3 < numero1 and numero2:
+    print('{} é o menor número'.format(numero3))
+     
+
