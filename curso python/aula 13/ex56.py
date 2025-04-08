ae9f393186c6ad1b37ebe361a98f0f6569dc04d4
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-08 17:18:09 -0300

    idade, nome,sexo, média, homem mais velho, quantidade de mulheres com idade abaixo de 20

--- curso python/aula 13/ex56.py
+++ curso python/aula 13/ex56.py
@@ -1,14 +1,23 @@
-contadorHomemMaisVelho = 0
+NomeHomemMaisVelho = ''
+maioridade = 0
+somaIdade = 0
+mediaIdade = 0
 contadorMulherabaixoDe20Anos= 0
 for c in range (1,5):
     nome = str (input ("Qual é o seu nome ? "))
     idade = int (input ("Qual é a sua idade ? "))
     sexo = str (input ("Qual é o seu sexo ? "))
-    media = (c + idade) / 2
-print ("a média foi: {}".format(media))
-if sexo == 'masculino' or sexo == 'm' or sexo == 'M':
-         contadorHomemMaisVelho = c < idade-1
-         print ("{} é o nome do homem mais velho!".format(nome))
-elif sexo == 'feminino' or sexo == 'f' or sexo == 'F':
-         contadorMulherabaixoDe20Anos = idade < 20 + 1
-         print ("{} quantidade de mulheres com idade menor que 20.".format(contadorMulherabaixoDe20Anos))
+    somaIdade += idade
+    if c == 1 and sexo in "Mm":
+        maioridade = idade
+        NomeHomemMaisVelho = nome
+    if sexo in "Mm" and idade > maioridade:
+        maioridade = idade
+        NomeHomemMaisVelho = nome
+    if sexo in "Ff" and idade < 20:
+        contadorMulherabaixoDe20Anos += 1
+mediaIdade = somaIdade / 4
+print ("A média da idade do grupo é  \033[33m{}\033[m.".format(mediaIdade))
+print ("A idade do homem mais velho é  \033[31m{}\033[m anos, seu nome é  \033[32m{}\033[m.".format(maioridade,NomeHomemMaisVelho))
+print ("\033[36m{}\033[m é a quantidade de mulheres com idade inferior a 20. ".format(contadorMulherabaixoDe20Anos))  
+  
