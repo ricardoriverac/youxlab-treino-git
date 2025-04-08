44fa5372f70de8f5098c8f39f2c4733709f4eb73
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-04-08 11:03:43 -0300

    Pedindo mais valores

---
+++ aula014/ex064.py
@@ -0,0 +1,18 @@
+valor=0
+soma=0
+c= 0
+valor=int(input('Me diga um valor: '))
+while  valor != 999:
+    
+    soma += valor
+
+    valor=int(input('Me diga um valor: '))
+    
+    c += 1
+
+   
+print('Ao todo a soma e {} e temos ao todo {} números'.format(soma,c))
+
+
+
+
