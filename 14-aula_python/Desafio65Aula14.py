e7882f62ad8784f118030e30aef4460505b55067
GustavoValacio <gustavoseabra2007@gmail.com>
2025-04-08 12:37:54 -0300

    Desafio65 Maior e menor número

---
+++ 14-aula_python/Desafio65Aula14.py
@@ -0,0 +1,17 @@
+res='S'
+soma =quant =media =maior =menor =0
+while res in 'Ss':
+    num=int(input('Digite um número:  '))
+    soma += num
+    quant += 1
+    if quant == 1:
+        maior =menor= num
+    else:
+        if num>maior:
+            maior =num
+        if num<menor:
+            menor=num
+    res=str(input('Quer continuar [S/N]?  ')).upper().strip()[0]
+media=soma/quant
+print('Você digitou {} números e a media foi {} '.format(quant, media))
+print('O maior valor foi {} e o menor foi {}'.format(maior,menor))
