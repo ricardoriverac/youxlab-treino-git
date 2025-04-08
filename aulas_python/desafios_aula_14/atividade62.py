4363571ead96b61b7d09746589f703131abaf2cd
Marcos <marcos.garcia@youxlab.com.br>
2025-04-08 14:59:41 -0300

    Fiz um código de P.A. v2.0

--- aulas_python/desafios_aula_14/atividade62.py
+++ aulas_python/desafios_aula_14/atividade62.py
@@ -3,17 +3,22 @@ print('\033[1;34m-=-=-=-=-= P.A. v2.0 =-=-=-=-=-\033[m')
 time.sleep(1)
 
 print('\n\033[1;34m-=-=-=-=-= 10 TERMOS DE P.A. =-=-=-=-=-\033[m')
-a1  = int(input('Primeiro termo: '))
+termo1  = int(input('Primeiro termo: '))
 raz = int(input('Razão: '))
 contador = 0
+quantos = 10
 
-while contador < 10:
-    print(a1, end= ' ')
-    a1 += raz
+while contador < quantos:
+    print(termo1, end= ' ')
+    termo1 += raz
     contador += 1
 
-termos = int(input('\nQuantos termos você quer mostrar a mais '))
-while contador < termos:
-    print(a1, end= ' ')
-    a1 += raz
-    contador += 1
+    if contador == quantos:
+    
+        mais = str(input('\nVocê quer ver mais termos? S/N '))
+        if mais in 'sS' :
+            quantos = int(input('Mais quantos termos? '))
+            contador = 0 
+     
+        elif mais in 'Nn':
+            print('Programa encerrado :( ')
