e47904cf6faa4584b1b3b0bf3649c121b826a940
fabricioyoux <fabricio.genesi@youxlab.com.br>
2025-04-08 16:58:55 -0300

    atividade

--- 13- Estrutura-De_Repetição_For/teste54.py
+++ 13- Estrutura-De_Repetição_For/teste54.py
@@ -2,13 +2,13 @@ contador = 0
 contador2 = 0
 for c in range(1, 8):
     ano = int(input('digite sua data de nascimento: '))
-    cou = 2025 - ano
-    if cou >= 18:    
-        n1 = cou - cou + 1    
-        contador += n1    
+    c = 2025 - ano
+    if c >= 18:    
+        priumero = c - c + 1    
+        contador += priumero 
     elif cou <= 18:
-        n2 = cou - cou + 1 
-        contador2 += n2 
+        segnNumero = c - c + 1 
+        contador2 += segnNumero
 print('{} JÁ SAO MAIORES DE IDADE'.format(contador))
 print('{} AINDA SAO MENORES!'.format(contador2))
     
