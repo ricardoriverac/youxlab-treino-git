61e5c859330ab9bf80f8007eec066fff90b6a6fe
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-08 17:30:56 -0300

    Exercício 38

---
+++ ex038.py
@@ -0,0 +1,11 @@
+numero1= int(input('Digite um número: '))
+numero2= int(input('Digite um novo número: '))
+if numero1 > numero2:
+    print('O número {} é o primeiro maior número'.format(numero1))
+elif numero1 < numero2:
+    print('O número {} é o segundo maior número'.format(numero1))
+elif numero2 < numero1:
+    print('O número {} é o segundo maior número'.format(numero2))
+else:
+    numero1 == numero2
+    print('Não existe valor maior, os dois são iguais')
