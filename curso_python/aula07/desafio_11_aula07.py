aaccba4deb685a9350e23918414b1283e588a623
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-04-08 16:21:57 -0300

    salvando desafio 11 da aula 7

---
+++ curso_python/aula07/desafio_11_aula07.py
@@ -0,0 +1,5 @@
+altura = int(input('Qual é a altura da parede? '))
+largura = int(input('Qual é a largura da parede? '))
+area = altura * largura 
+tinta = area / 2
+print (f'A quantidade de tinta que será usada é de {tinta} litros! ')
