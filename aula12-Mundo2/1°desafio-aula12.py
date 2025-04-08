3103950efb31ac36cb8570f0e556193de89b8c90
Youx <sophia.carvalho@youxlab.com.br>
2025-04-08 13:59:28 -0300

    mundo2

---
+++ aula12-Mundo2/1°desafio-aula12.py
@@ -0,0 +1,9 @@
+valor=float(input('Qual o preço da sua casa?:'))
+salario=float(input('Quanto é o seu sálario?:'))
+pagamento=int(input('Em quantos anos você pretende financiar?:'))
+prestacao= valor/(pagamento*12)
+if prestacao>(0.3*salario):
+    print('Empréstimo negado!')
+else:
+    print('Empréstimo concedido!')
+    print(f'Para uma casa de {valor} com financiamento de {pagamento} anos o valor das parcelas será de: {prestacao}')
---
+++ aula12-Mundo2/1°teste-aula12.py
@@ -0,0 +1,10 @@
+nome=str(input('Digite seu nome:'))
+if nome=="Sophia":
+    print('Que nome bonito!')
+elif nome=="Sarah" or nome=="Andressa":
+    print('O seu nome é legal.')
+elif nome in "Ana Alice Julia":
+    print('Seu nome é um belo nome feminino!')
+else:
+    print('Que nome feio!')
+print(f'Bom dia, {nome}!!')
