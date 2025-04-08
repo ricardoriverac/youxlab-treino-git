b32e0de7021077340872b0b1a250d59288de1b91
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-04-08 14:22:37 -0300

    Somando

---
+++ aula015/ex066.py
@@ -0,0 +1,14 @@
+numero=0
+soma=0
+c =0
+while True:
+    numero =int(input('Me diga um número: '))
+
+    if numero ==125:
+
+        break
+    c += 1
+    soma += numero
+    
+print('A soma e {} a quantidade de números e:{} '.format(soma,c))
+    
