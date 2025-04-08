3cdd7195b6109e339f27fade1a055001d5a91c56
annaluiza777 <anna.araujo@youxlab.com.br>
2025-04-08 17:31:37 -0300

    exercicios  aula 13

---
+++ aula13.py/exercicios._aula13.py
@@ -0,0 +1,16 @@
+i = int(input('Inicio: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+for c in range(i, f+1,p):
+    print(c)
+print('FIM')
+
+
+
+
+s = 0
+for c in range(0,4):
+    n = int(input('Digite um valor: '))
+    s +=n
+print('O somatório de todos os numeros foi {}'.format(s))
+
