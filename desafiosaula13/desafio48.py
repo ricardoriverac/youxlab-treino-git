df52a5427b325c084c4a1e62e9ad73f2972b4765
Maria Luiza <maria.silva@youxlab.com.br>
2025-04-08 17:00:35 -0300

    teste da aula 14

--- desafiosaula13/desafio48.py
+++ desafiosaula13/desafio48.py
@@ -5,3 +5,4 @@ for c in range(1, 501, 2):
         contador = contador + 1
         soma = soma + c
 print('A soma de todos os {} números solicitados é {}'.format(contador,soma))
+
---
+++ desafiosaula14/teste.py
@@ -0,0 +1,21 @@
+# c = 1
+# while c < 10:
+#     print(c)
+#     c += 1
+
+# n=1
+# r = 'S'
+# while r == 'S':
+#    n = int(input('Digite um valor: '))
+#    r = input('Gostaria de continuar?[S/N]: ').upper()
+
+n=1
+par = impar = 0
+while n != 0:
+    n = int(input('Digite um valor: '))
+    if n!=0:
+        if n % 2 == 0:
+            par+=1
+        else:
+            impar+=1
+print('Você digitou {} números pares e {} números ímpares '.format(par, impar))
