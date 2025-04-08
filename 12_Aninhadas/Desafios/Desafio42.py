48424f31433f99354c49d9956ba4ef8732a4414f
VictorGabrielBarros <victor.barros@youxlab.com.br>
2025-04-08 13:55:27 -0300

    Desafio42

--- 12_Aninhadas/Desafios/Desafio42.py
+++ 12_Aninhadas/Desafios/Desafio42.py
@@ -1,8 +1,11 @@
-n1= int(input("Insira um comprimento: "))
-n2= int(input("Insira outro comprimento: "))
-n3= int(input("Insira o terceiro comprimento "))
-if n1 < n2 + n3 and n2 < n1 + n3 and n3 < n1 + n2:
+comprimento1= int(input("Insira um comprimento: "))
+comprimento2= int(input("Insira outro comprimento: "))
+comprimento3= int(input("Insira o terceiro comprimento "))
+if comprimento1 < comprimento2 + comprimento3 and comprimento2 < comprimento1 + comprimento3 and comprimento3 < comprimento1 + comprimento2:
     print("Os segmentos pode formar um triangulo")
-else:
-    print("Os segmentos nao podem formar um triangulo")
-#obs nao terminei ainda#
+    if comprimento1 == comprimento2 and comprimento2 == comprimento3:
+        print("Equilatero")
+    elif comprimento1 != comprimento2 != comprimento3 != comprimento1:
+            print("Escaleno")
+    else:
+         print("Isosceles")
