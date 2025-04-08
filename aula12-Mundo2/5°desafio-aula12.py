8b93cc4a9da01a932306d802445e4577adad4dbf
Youx <sophia.carvalho@youxlab.com.br>
2025-04-08 16:25:36 -0300

    mundo2

---
+++ aula12-Mundo2/5°desafio-aula12.py
@@ -0,0 +1,10 @@
+nota1=float(input('Sua nota do 1° semestre:'))
+nota2=float(input('Sua nota do 2° semestre:'))
+media=(nota1+nota2)/2
+if media>=7.0:
+    print(f'Média mínima é 6.0\nSua média é de {media}. APROVADO! ')
+elif media>=5.0 and media<7.0:
+    print(f'Média mínima é 6.0\nSua média é de {media}. RECUPERAÇÃO! ')
+elif media<5.0:
+    print(f'Média mínima é 6.0\nSua média é de {media}. REPROVADO! ')
+
