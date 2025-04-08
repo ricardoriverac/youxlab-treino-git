60a029960e86ae82df87d64bfba47457439f0d32
gustavofreire <gustavo.gomes@youxlab.com.br>
2025-04-08 17:21:41 -0300

    ds73

---
+++ 16-Aula_Tuplas/ds73.py
@@ -0,0 +1,6 @@
+times= ('Flamengo', 'palmeiras', 'corinthias ', 'sp' ,'Argentina', 'ZIca', 'r' , 'we')
+print(f'lista brasileirão{times}')
+print(f'5 primeiros {times[0:5]}')
+print(f'4 ultimos {times[-4]}')
+print(f'em ordem alfabetica {sorted(times)}')
+print(f'chapecoense{times.index("sp")+1}posição')
