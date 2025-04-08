e0211bd6ac353b67913a6ef66d7e22f1a3d683ab
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-04-08 14:33:22 -0300

    Tabuada

--- aula015/ex067.py
+++ aula015/ex067.py
@@ -1,16 +1,16 @@
 c =1
-
-while True:
+isRodando = True
+while isRodando:
 
     numero=int(input('Digite um número para ver a tabuada: '))
     print('<->'*10)
 
-    if numero < 0:
-            break
-
-    for c in range(1,11): 
-    
-        print('{} X {} = {}'.format(numero,c,numero*c))
+    if numero <= 0:
+        isRodando= False
+    else:
+        for c in range(1,11): 
+        
+            print('{} X {} = {}'.format(numero,c,numero*c))
 
        
 
