6b34e830000dd2f6656e2ea54e83c63f0067fdfa
alexia-assis <alexiaassis.youxlab.com.br>
2025-04-08 14:21:56 -0300

    Fim do desafio 70

---
+++ aula-15/desafio70.py
@@ -0,0 +1,15 @@
+listaprod = []
+listapreco = []
+while True:
+    produto = str(input('Nome do produto: ')).strip()
+    preco = float(input('Preço: R$'))
+    listaprod += [produto]
+    listapreco += [preco]
+    continuar = ' '
+    while continuar not in 'SN':
+        continuar = str(input('Quer continuar? [S/N]: ')).upper().strip()[0]
+    if continuar == 'N':
+        break
+print(f'O total da compra foi R${sum(listapreco):.2f}')
+print(f'Temos {len([i for i in listapreco if i > 1000])} produto(s) custando mais que R$1.000,00')
+print(f'O produto mais barato foi {listaprod[listapreco.index(min(listapreco))]} e custou R${min(listapreco)}')
