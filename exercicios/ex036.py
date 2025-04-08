8c278189cacdcb78250f6e0d616e432247e992b5
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-08 16:54:29 -0300

    Exercício 36

---
+++ exercicios/ex036.py
@@ -0,0 +1,12 @@
+casa= float(input('Valor da casa: '))
+salario= float(input('Valor do salário: '))
+anos= int(input('Anos de pagamento: '))
+prestacao= casa / (anos * 12)
+mensalidade= salario % 30
+if prestacao > mensalidade:
+  print(mensalidade)
+  print(f'EMPRÉSTIMO RECUSADO! Valor da prestação mensal: R${prestacao :.2f}')
+else:
+  print(prestacao)
+  print(mensalidade)
+  print(f'EMPRÉSTIMO AUTORIZADO! Prestação mensal: R${prestacao :.2f}')
