1ffd729be9919b6015ea2f0c34de6b826af87900
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-09 16:51:13 -0300

    razão,termo, sainda do laço ultilizando 0

--- curso python/aula 14/ex62.py
+++ curso python/aula 14/ex62.py
@@ -5,9 +5,15 @@ print ("\t-----" * 4)
 contador = 1
 numeros = primeiroTermo
 mais = 10
+total = 0
 while mais !=0:
-  while contador <= 10:
+   total = total + mais
+   while contador <= total: 
       print ("\033[32m{}\033[m".format(numeros), end= " - ")
       numeros = numeros + razao
       contador = contador + 1
+   print ("...")
+   print ("Para encerrar digite 0...")
+   mais = int (input("Você quer mostrar mais algum termo ? "))
+print (("Progressão finalizada com \033[36m{}\033[m termos mostrados.".format(total)))
 print ("\033[35mACABOUU!!\033[m")
