64d54fea04c864098f6326ede770eafdbcc91d0b
Maria Luiza <maria.silva@youxlab.com.br>
2025-04-09 13:45:23 -0300

    desafio de adivinhar o número que o computador escolheu

--- desafiosaula14/desafio58.py
+++ desafiosaula14/desafio58.py
@@ -2,8 +2,10 @@ from random import randint
 from time import sleep
 computador = randint(0, 10) #Faz o computador "PENSAR" 
 numero = 0
+palpites = 0
 
 while numero != computador:
     numeroAposta = int(input('Tente descobrir qual número escolhi, digite um número de a 0 a 10: '))
+    palpites+=1
     numero=numeroAposta
-print('{}PARABÉNS!{} Você acertou!!! '.format('\033[35m', '\033[m'))
+print('{}PARABÉNS!{} Você acertou depois de {} tentativas!!! '.format('\033[35m', '\033[m', palpites))
