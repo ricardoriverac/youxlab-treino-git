1d6f58dd29484fdbb7c27dd7c92b39ef9cafd395
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-04-09 15:02:38 -0300

    fiz o desafio 62

--- 14-curso_python/aula.py
+++ 14-curso_python/aula.py
@@ -20,12 +20,15 @@ print ('FIM')
 n = 1
 par = 0
 impar = 0
-while n != 0:
-    n = int(input('Digite um valor: '))
-    if n % 2 ==0:
-     par +=1
-    else :
-     impar +=1
+isRodando = True
+while isRodando: 
+    while n != 0:
+     n = int(input('Digite um valor: '))
+     if n % 2 ==0:
+       par +=1
+     else :
+         impar +=1
+     isRodando = False
 print ('Você digitou {} números pares e {} números ímpares.'.format(par, impar))
 
 
