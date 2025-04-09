626b8ecb56161de7898832ea4969b86e1c872d47
VictorGabrielBarros <victor.barros@youxlab.com.br>
2025-04-09 17:06:05 -0300

    Desafio50

---
+++ 13_Estrutura/Desafios/Desafio50.py
@@ -0,0 +1,12 @@
+somaa = 0
+conta = 0
+for c in range(1,7):
+    numero=int(input("Digite o {} valor: ".format(c)))
+    somaa =somaa + numero
+    conta = conta + 1
+print("Voce informou {} numero e a soma foi {}".format(conta,somaa))
+
+
+
+
+
