b25d54c0019e7f66189987d3764ce6535b155908
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-09 19:43:56 -0300

    Exerccio 41

---
+++ exercicios/ex041.py
@@ -0,0 +1,11 @@
+idade= int(input('Sua idade: '))
+if idade <= 9:
+    print('CATEGORIA MIRIM')
+elif idade <= 14:
+    print('CATEGORIA INFANTIL')
+elif idade <= 19:
+    print('CATEGORIA JUNIOR')
+elif idade <= 20:
+    print('CATEGORIA SÊNIOR')
+else:
+    print('CATEGORIA MASTER')
