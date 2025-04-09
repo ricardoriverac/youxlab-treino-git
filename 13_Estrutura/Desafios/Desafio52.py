4d118edd6ac115ba31d0949453950627c817729e
VictorGabrielBarros <victor.barros@youxlab.com.br>
2025-04-09 19:01:10 -0300

    Desafio52

--- 13_Estrutura/Desafios/Desafio52.py
+++ 13_Estrutura/Desafios/Desafio52.py
@@ -1,5 +1,6 @@
 numero=int(input("Digite um numero: "))
 for c  in range(1,numero +1):
     if numero % c == 0:
-        print(f"{c}")
-    
+        print(f"{[c]}")
+    elif numero % c == 2:
+        print(f"{(c)}")
