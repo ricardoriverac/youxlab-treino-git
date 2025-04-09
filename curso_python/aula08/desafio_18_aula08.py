7bc2a6e6d8f43126eea2fe7b4dbbeb7a561d1d28
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-04-09 17:42:55 -0300

    salvando desafio 18 da aula 8

--- curso_python/aula08/desafio_18_aula08.py
+++ curso_python/aula08/desafio_18_aula08.py
@@ -1,2 +1,6 @@
 import math
-angulo = int(input('Qual é o ângulo? '))
+angulo = int(input('Qual é o valor do ângulo em graus? '))
+rad = math.radians(angulo)
+print (f'O seno de {angulo}° é {math.sin(rad):.2f}')
+print (f'O cosseno de {angulo}° é {math.cos(rad):.2f}')
+print (f'O tangente de {angulo}° é {math.tan(rad):.2f}')
