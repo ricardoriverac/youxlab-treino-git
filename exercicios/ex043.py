cd23b55761f24892e0cad8111fdae6bd75686898
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-09 20:59:13 -0300

    Consertar

--- exercicios/ex043.py
+++ exercicios/ex043.py
@@ -5,7 +5,7 @@ if imc < 18.5:
     print('ABAIXO DO PESO! {:.2f}'.format(imc))
 elif imc >= 18.5 and 25:
     print('PESO IDEAL! {:.2f}'.format(imc))
-elif imc >= 25 and 30:
+elif imc > 25 and 30:
     print('SOBREPESO! {:.2f}'.format(imc))
 elif imc >= 30 and 40:
     print('OBESIDADE! {:.2f}'.format(imc))
