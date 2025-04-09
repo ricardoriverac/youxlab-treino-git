69d6788138b69e55bcea3f6a8fde01293b8006e0
João Pedro <joao.pedro@youxlab.com.br>
2025-04-09 16:47:01 -0300

    Finalizando o exercicio 96

--- Exercicios/exe96.py
+++ Exercicios/exe96.py
@@ -9,13 +9,13 @@ def cabecalho(texto):
     lin()
     print(texto.center(0))
     lin()
-    cabecalho("Programa de calcular área de um terreno.")
 
 def area(largura, comprimento):
     calculo = comprimento * largura
     print(f"A área de um terreno {largura} x {comprimento} é de {calculo} m².")
 
 #Codigo abaixo
+cabecalho("Programa de calcular área de um terreno.") #Titulo do cabeçalho
 comprimento = float(input("\nDigite um comprimento: "))
 largura = float(input("Digite a largura: "))
 area(largura, comprimento)
