d3cb96e083d4c3646d872131827fbb430f6611d4
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-09 20:32:46 -0300

    Consertar

---
+++ exercicios/ex043.py
@@ -0,0 +1,13 @@
+peso= float(input('Informe seu peso: '))
+altura= float(input('Informe sua altura: '))
+imc= peso / (altura ** 2)
+if imc < 18.5:
+    print('ABAIXO DO PESO! {:.2f}'.format(imc))
+elif imc >= 18.5 and 25:
+    print('PESO IDEAL! {:.2f}'.format(imc))
+elif imc >= 25 and 30:
+    print('SOBREPESO! {:.2f}'.format(imc))
+elif imc >= 30 and 40:
+    print('OBESIDADE! {:.2f}'.format(imc))
+elif imc > 40:
+    print('OBESIDADE MÓRBIDA! {:.2f}'.format(imc))
