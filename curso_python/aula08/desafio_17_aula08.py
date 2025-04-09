5196ec4ce2e5a77888bab95074d0e2f12b40690a
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-04-09 16:38:00 -0300

    salvando desafio 17 da aula 8

---
+++ curso_python/aula08/desafio_17_aula08.py
@@ -0,0 +1,6 @@
+import math
+oposto = int(input('Qual o valor do cateto oposto? '))
+adjacente = int(input('Qual o valor do cateto adjacente? '))
+hipotenusa = adjacente**2 + oposto**2
+hipotenusa2 = hipotenusa**(1/2)
+print (hipotenusa2)
