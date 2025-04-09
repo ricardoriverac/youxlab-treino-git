27b8492cbc89b0373b9f931cbca9445354242105
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-04-09 15:02:29 -0300

    salvando desafio 16 da aula 8

---
+++ curso_python/aula08/desafio_16_aula08.py
@@ -0,0 +1,4 @@
+from math import trunc
+numero = float(input('Digite um número qualquer: '))
+porcaointeira = trunc(numero)
+print ('A porção inteira de {} é igual a {}'.format(numero, porcaointeira ))
