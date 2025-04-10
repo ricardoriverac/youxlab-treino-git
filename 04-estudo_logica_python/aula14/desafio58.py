be5585f53d423b14b53f17fcfaa636a202b5b833
marianne0614 <marianne.lima@youxlab.com.br>
2025-04-10 16:39:42 -0300

    desfio . 58

--- 04-estudo_logica_python/aula14/desafio58.py
+++ 04-estudo_logica_python/aula14/desafio58.py
@@ -1,15 +1,14 @@
 from random import randint
-from time import sleep
-palpites = 0
+palpites =0
 acertou = False
+computador = randint(0, 10)
 print('Estou pensando num número... consegue adivinhar?')
 while not acertou:
-    comp = randint(0, 10)
     jogador = int(input('Dê seu papite entre 0 e 10: '))
     palpites += 1
-    if jogador == comp:
-        print('Parabéns o computador pensou em {} e você pensou em {}.'.format(comp, jogador))
+    if jogador == computador:
+        print('Parabéns o computador pensou em {} e você pensou em {}.'.format(computador, jogador))
         acertou = True
 else:
-        print('Quase.. pensei em {}.'.format(comp))
+        print('Eu escolhi  {}.'.format(computador))
 print('Foram necessárias {} tentativas para acertar.'.format(palpites))
