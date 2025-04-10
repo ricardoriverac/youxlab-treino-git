94fa0b08e0fcda159641d3355dda2417b51c9354
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-10 15:02:15 -0300

    Exercício 46

--- exercicios/ex045.py
+++ exercicios/ex045.py
@@ -1,6 +1,5 @@
 import random
 jokenpo= int(input('Escolha um número de 1 a 3: \n 1 para pedra \n 2 para papel \n 3 para tesoura \n Digite aqui: '))
-if jokenpo == 1:
-  lista= [1,2,3]
-  jogo1= jokenpo == random.shuffle(lista)
-  print(jogo1)
+lista= [1, 2, 3]
+jokenpo2= random.shuffle(lista)
+print(jokenpo, jokenpo2)
