db2801d9449d69216deeb8d8067fe623fa572311
marianne0614 <marianne.lima@youxlab.com.br>
2025-04-10 13:36:29 -0300

    desafio 56

--- 04-estudo_logica_python/aula 13/desafio56.py
+++ 04-estudo_logica_python/aula 13/desafio56.py
@@ -1,25 +1,29 @@
 soma=0
 mulheres=0
-homem=0
+homemvelho=0
 media=0
+nomehomem=''
 for i in range(1,5):
 
     nome = input('Digite o seu nome: ')
-    sexo = input('Masculino ou Feminino ? [m/f]').upper()[0]
+    sexo = input('Masculino ou Feminino ? [m/f]').upper()
     idade = int(input('Digite a sua idade : '))
+    print('-'*40)
+    soma += idade 
+    media = soma / 4
+    if i == 1 and sexo in 'Mm':
+       homemvelho =idade
+       nomehomem= nome
 
-    soma = soma + idade 
-    media = soma / i 
+    if sexo in 'Mm' and idade > homemvelho:
+        homemvelho= idade 
+        nomehomem=nome
 
-    if sexo == ['m'] and idade > maior:
-        maior = idade 
-        homem = nome
-
-    if sexo == ['f'] and idade < 20:
-        i_mulheres += 1
+    if sexo in 'Ff' and idade < 20:
+        mulheres += 1
 
 print('Media das idades é de {:.2f} '.format(media))
-print('Home do mais velho é {}'.format(homem))
+print('Home do mais velho é {}'.format(nomehomem))
 
 if mulheres ==  1:
     print('Não temos mulheres no grupo !')
