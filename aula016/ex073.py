23c9b48d88a2dbf3ab337ca648a18bff544d0e3e
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-04-10 15:57:54 -0300

    Times e sua posição

---
+++ aula016/ex073.py
@@ -0,0 +1,40 @@
+lista =('Flamengo','Cruzeiro','Bota fogo','Alemanha ','Brasil','EUA', 'Canada','Vasco','Japão','São Paulo')
+print('<>'*54)
+
+print(f'Lista de times: {lista}') 
+
+print('<>'*30)
+
+print(f'Os primeiros são {lista[0:4]}')
+
+print('<>'*30)
+
+print(f'Os ultimos são {lista[-3:]}')
+
+print('<>'*30)
+
+print(f'Em ordem alfabética: {(sorted(lista))}')
+
+print('<>'*30)
+
+print(f'O Brasil está na {lista.index("Brasil")} posição')
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
