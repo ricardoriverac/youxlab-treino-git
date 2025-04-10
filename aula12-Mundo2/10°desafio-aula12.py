674d1830be4fd36dd2170b7ccd1ffbd566d6c84e
Youx <sophia.carvalho@youxlab.com.br>
2025-04-10 14:46:33 -0300

    mundo2

---
+++ aula12-Mundo2/10°desafio-aula12.py
@@ -0,0 +1,34 @@
+from random import randint
+opcoes=('Pedra','Papel','Tesoura')
+computador=randint(0,2)
+print('VAMOS JOGAR JOKENPÔ?')
+print('Escolha uma dessas opções:\n' \
+'0-PEDRA\n' \
+'1-PAPEL\n' \
+'2-TESOURA')
+escolha=int(input('Qual opção você quer?:'))
+print('Você jogou:{}'.format(opcoes[escolha]))
+print('Computador jogou:{}'.format(opcoes[computador]))
+if computador==0:
+ if escolha==0:
+  print('EMPATE!')
+ if escolha==1:
+  print('VOCÊ GANHOU!')
+ if escolha==2:
+  print('VOCÊ PERDEU!')
+if computador==1:
+ if escolha==1:
+  print('EMPATE!')
+ if escolha==2:
+  print('VOCÊ GANHOU!')
+ if escolha==0:
+  print('VOCÊ PERDEU!')
+if computador==2:
+ if escolha==2:
+  print('EMPATE!')
+ if escolha==1:
+  print('VOCÊ GANHOU!')
+ if escolha==0:
+  print('VOCÊ PERDEU!')
+  
+  
