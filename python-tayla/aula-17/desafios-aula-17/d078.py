4ee2f21a85b5219bb8686d23a32c14a9716aaec7
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-04-10 13:39:20 -0300

    desafio 78

--- python-tayla/aula-17/desafios-aula-17/d078.py
+++ python-tayla/aula-17/desafios-aula-17/d078.py
@@ -13,13 +13,13 @@ for indice in range(5):
             menor = numero
 
 print('=-' * 30)
-print(f'Você digitou os valores {valores}')
-print(f'O maior valor digitado foi {maior} nas posições: ', end='')
+print(f'Você digitou os valores \033[1;35m{valores}\033[m')
+print(f'O maior valor digitado foi \033[1;36m{maior}\033[m nas posições: ', end='')
 for indice, valor in enumerate(valores):
     if valor == maior:
-        print(f'{indice} ', end='')
+        print(f'\033[1;31m{indice}\033[m ', end='')
 
-print(f'\nO menor valor digitado foi {menor} nas posições: ', end='')
+print(f'\nO menor valor digitado foi \033[1;33m{menor}\033[m nas posições: ', end='')
 for indice, valor in enumerate(valores):
     if valor == menor:
-        print(f'{indice} ', end='')
+        print(f'\033[1;31m{indice}\033[m ' '... ' , end='')
