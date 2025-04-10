18db85d8e3b97b67363604b84e9623ab5fa9fefa
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-10 16:40:13 -0300

    Exercício 46

--- exercicios/ex045.py
+++ exercicios/ex045.py
@@ -1,5 +1,6 @@
-import random
-jokenpo= int(input('Escolha um número de 1 a 3: \n 1 para pedra \n 2 para papel \n 3 para tesoura \n Digite aqui: '))
-lista= [1, 2, 3]
-jokenpo2= random.shuffle(lista)
-print(jokenpo, jokenpo2)
+from random import randint
+jokenpo= int ('Escolha um número de 1 a 3: \n 1 para pedra \n 2 para papel \n 3 para tesoura \n ')
+computador= randint(1 , 3)
+print('O computador escolheu {}'.format(computador))
+if jokenpo == 1 and computador == 2 or 3:
+    print('')
