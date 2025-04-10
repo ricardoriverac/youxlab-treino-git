db33fcadf6b1b47ddd3bd2165d0749f8f8804f53
VictorGabrielBarros <victor.barros@youxlab.com.br>
2025-04-10 14:15:56 -0300

    Desafio54

--- 13_Estrutura/Desafios/Desafio54.py
+++ 13_Estrutura/Desafios/Desafio54.py
@@ -1,20 +1,23 @@
-from datetime import date 
+from datetime import datetime
 maior= 0
 menor= 0
 
-atual = date.today().year
-for anos in range(1,8):
-    nascimento=int(input("Digite ano do nascimento: "))
-    idade= atual - nascimento
+for anos in range(0,3):
+    print("\n")
+    dia=int(input(f"Digite o dia do nascimento, da {anos+1} pessoa: "))
+    mes=int(input(f"Digite o mes do nascimento, da {anos+1} pessoa: "))
+    ano=int(input(f"Digite ano do nascimento, da {anos+1} pessoa: "))
 
+    datanasc= datetime(ano,mes,dia)
+    hoje= datetime.now()
+    idade = hoje.year - datanasc.year
+    if (hoje.month,hoje.day) < (datanasc.month, datanasc.day):
+        idade -= 1
     if idade >= 18:
-        maior += 1
-
-    elif idade <= 0:
-        print("\nEssa pessoa ainda nao nasceu.")
+        maior += 1    
     else:
         menor += 1
-print(f"Pessoas maiores de idade: {maior}")
+print(f"\nPessoas maiores de idade: {maior}")
 print(f"Pessoas menores de idade: {menor}\n")
 
 
