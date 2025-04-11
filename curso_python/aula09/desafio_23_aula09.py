900342554530b55760af25d91563bbdb1ad2eacc
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-04-11 17:49:50 -0300

    salvando desafio 23 da aula 9

---
+++ curso_python/aula09/desafio_23_aula09.py
@@ -0,0 +1,9 @@
+numero = str (input('Digite um número de 0 a 9999: '))
+unidade = numero[3]
+dezena = numero[2]
+centena = numero[1]
+milhar = numero[0]
+print (f'A unidade é {unidade}')
+print (f'A dezena é {dezena}')
+print (f'A centena é {centena}')
+print (f'E a milhar é {milhar}!')
