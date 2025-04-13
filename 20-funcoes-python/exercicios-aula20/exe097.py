fded66515703850b06f640ed360aaf317d618b9d
juliocesar <juliogoiabinha060@gmail.com>
2025-04-13 15:32:47 -0300

    Funcoes em py

---
+++ 20-funcoes-python/exercicios-aula20/exe097.py
@@ -0,0 +1,13 @@
+def escreva(palavra):
+    tamanhodalinha = len(palavra) + 4
+    print('~' * tamanhodalinha)
+    print(f'  {palavra}')
+    print('~'* tamanhodalinha)
+
+
+
+# programa principal
+escreva('Ola Mundo!')
+escreva('Cesar Seja Bem Vindo!')
+escreva('Hello Wordl')
+escreva('Top Demais')
