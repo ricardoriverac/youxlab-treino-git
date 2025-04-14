6146c26fed11e40750a1e91adfdd6df0d8836f4d
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-14 17:19:02 -0300

    Exercício 51

--- exercicios/ex051.py
+++ exercicios/ex051.py
@@ -1,3 +1,5 @@
-for pa in range(2, 48, 2):
-    progressao= pa - 1
-print(progressao)
+primeiroTermo= int(input('Primeiro termo: '))
+razao= int(input('Razão: '))
+ultimoTermo= primeiroTermo + (10 - 1) * razao
+for a in range(primeiroTermo, ultimoTermo+1, razao):
+    print('{}° > '.format(a), end='')
