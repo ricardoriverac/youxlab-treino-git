91dad174298d3f1cc0cf97365d9324d4e1c45669
GustavoValacio <gustavoseabra2007@gmail.com>
2025-04-14 16:24:53 -0300

    Desafio79 Biblioteca

---
+++ 17-aula_python/Desafio79Aula17.py
@@ -0,0 +1,14 @@
+numeros=list()
+while True:
+    n =int(input('Digite um valor:  '))
+    if n not  in  numeros:
+        numeros.append(n)
+        print('Valor adicionado com sucesso...')
+    else:
+        print('Valor duplicado!não vou adicionar')
+    r=str(input('Quer continuar? [S/N]')).upper()
+    if r in 'N':
+        break
+print('=-'*10)
+numeros.sort()
+print(f'Você digitou os valores  {numeros}')
