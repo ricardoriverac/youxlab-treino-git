e39c7b09bb60deb89d8cbcdfb277dff9b19813d7
juliocesar <juliogoiabinha060@gmail.com>
2025-04-14 16:28:25 -0300

    Funcoes em py

--- 20-funcoes-python/exercicios-aula20/exe099.py
+++ 20-funcoes-python/exercicios-aula20/exe099.py
@@ -1,14 +1,11 @@
 def maior(*num):
     maior = 0
     for numero in num:
-        if numero == 0:
-            maior = numero 
-        else:
-            if numero > maior:
-                maior = numero
+        if numero > maior:
+            maior = numero
     print(f'Os valores {num}')
     print(f'O maior valor é {maior}')
 
-maior(1,2,5,6,7)
+maior(1,2,7,0,5)
 maior(3,1,2)
 maior(9,1,2,4,5,10)
