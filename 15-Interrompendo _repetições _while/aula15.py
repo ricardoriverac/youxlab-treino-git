c6f9875c4b9f82cdeebdcb614d42f655cb275f28
fabricioyoux <fabricio.genesi@youxlab.com.br>
2025-04-14 17:45:07 -0300

    aula15

---
+++ 15-Interrompendo _repetições _while/aula15.py
@@ -0,0 +1,9 @@
+#cont = 1 
+#while cont <= 10:
+#    print(cont, '->', end='')
+#    cont += 1
+
+cont = 1 
+while True:
+    print(cont, '->', end='')
+    cont += 1
---
+++ 15-Interrompendo _repetições _while/ex69.py
@@ -0,0 +1,10 @@
+while True:
+    pcao = 'M'
+
+    cont = 0
+
+    soma = 0
+    
+    idade = int(input('Idade de uma pessoa'))
+    opcao = str(input('Qual seu sexo? [F/M]:')).upper()
+    i = str(input('Quer continuar? [S/N]')).upper()
