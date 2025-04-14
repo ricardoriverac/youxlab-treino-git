6aeab98e953f5559647b8845ead1df63449f12e1
VictorGabrielBarros <victor.barros@youxlab.com.br>
2025-04-14 16:29:50 -0300

    Desafio65

---
+++ 14_While/Desafios/Desafio65.py
@@ -0,0 +1,17 @@
+resp = "S"
+soma = quant = media = maior = menor=0
+while resp in "Ss":
+    num = int(input("Digite um numero: "))
+    soma += num
+    quant += 1
+    if quant == 1:
+        maior =menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num<  menor:
+            menor= num
+    resp = str(input("Quer continuar [S/N]: ")).upper().strip()[0]
+media= soma / quant
+print(f"Voce digitou {quant} numeros e a media e {media}")
+print(f"O maior valor foi {maior} e o menor foi {menor}")
