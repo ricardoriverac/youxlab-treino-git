4319d04622c5f2c39b65348ef05de55c650a2f6f
marianne0614 <marianne.lima@youxlab.com.br>
2025-04-14 17:23:56 -0300

    desafio 64

---
+++ 04-estudo_logica_python/aula14/desafio64.py
@@ -0,0 +1,12 @@
+flag = False
+somador = 0
+contador = 0
+while flag == False:
+    numero = int(input('Digite um número... [Digite 999 para quebrar a soma]: \n'))
+    if numero ==999:
+        flag = True
+    else:
+        somador += numero
+        contador += 1
+        print('soma: {}.'.format(somador))
+        print('numeros digitados: {}.'.format(contador))
