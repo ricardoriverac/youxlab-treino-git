b67d0f2c320847cf109789179cd727eb5bc1e2f8
GustavoValacio <gustavoseabra2007@gmail.com>
2025-04-14 13:19:45 -0300

    Desafio76 lista com tuplas

---
+++ 16-aula-python/Desafio76Aula16.py
@@ -0,0 +1,16 @@
+lista=( 'Bolo' , 1.50,
+        'Café' , 90.0,
+        'Doce' , 0.50,
+        'Coca' , 8.0,
+        'Wisky' , 0.1,
+        'pepsi' , 98.0,
+        'Gomes' , 30.0)
+print('__' *12)
+print('|LISTAGEM|DOS|PRODUTOS|')
+print('¨' *24)
+for pos in  range (0, len(lista)):
+    if pos % 2 == 0:
+        print(f'{lista[pos]:.<30}', end ='')
+    else:
+        print(f'R$ {lista[pos]:>7.2f}')
+print('-' * 41)
