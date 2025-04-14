a56780bae7cba77087a003db7acc0f350e53d3e7
Youx <sophia.carvalho@youxlab.com.br>
2025-04-14 13:30:49 -0300

    mundo2

--- aula13-Mundo2/4°desafio-aula13.py
+++ aula13-Mundo2/4°desafio-aula13.py
@@ -1,3 +1,4 @@
+#TABUADA COM LAÇO DE REPETIÇÃO 
 numero=int(input('Digitr um númeo para ver sua tabuada:'))
 for contador in range (1,11):
     print('{}x{}={}'.format(numero,contador,numero*contador))
---
+++ aula13-Mundo2/5°desafio-aula13.py
@@ -0,0 +1,8 @@
+soma=0
+cont=0
+for contador in range(1,7):
+    numero=int(input('Digite o {}° número:'.format(contador)))
+    if numero % 2 == 0:
+        soma+=numero
+        cont+=1
+print(f'Você adicionou {cont} números PARES , a soma deles é igual:{soma}')
