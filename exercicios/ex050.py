d80e43276abfd32114cd0dc268fb184378de8ba9
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-14 17:04:58 -0300

    Exercício 50

--- exercicios/ex050.py
+++ exercicios/ex050.py
@@ -1,9 +1,8 @@
 soma= 0
-for i in range(0, 7, 2):
-    numero= int(input('Digite um valor: '))
-if numero % 2 != 0:
-    numero == 'Inválido'
-elif numero % 2 == 0:
-    soma += numero
-    print(i)
-    
+contador= 0
+for a in range(1, 7):
+    numero= int(input('Digite o {}° valor: '.format(a)))
+    if numero % 2 == 0: 
+        soma += numero 
+        contador += 1
+print('Você informou {} números PARES e a soma foi {}'.format(contador, soma))
