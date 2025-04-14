f3545bb67bf7feb65b0f90d27c4e33d7f81021af
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-14 14:54:00 -0300

    Exercício 45

--- exercicios/ex045.py
+++ exercicios/ex045.py
@@ -1,11 +1,38 @@
 #fazer
 from random import randint
-itens= ('Pedra, Papel, Tesoura')
 print('''Suas opções: 
       [1] PEDRA
       [2] PAPEL
       [3] TESOURA ''')
 computador= randint(1 , 3)
 jogador= int(input('Qual é sua jogada? '))
-print('Computador jogou {}'.itens[computador])
-print('Jogador jogou {}'.itens[jogador])
+if computador == 1 and jogador == 3:      
+      print('Computador: {}\n Jogador: {}'.format(computador, jogador))
+      print('Você perdeu!')
+
+elif computador == 2 and jogador == 1:
+      print('Computador: {}\n Jogador: {}'.format(computador, jogador))
+      print('Você perdeu!')
+
+elif computador == 3 and jogador == 2:    
+     print('Computador: {}\n Jogador: {}'.format(computador, jogador))      
+     print('Você perdeu!')
+
+elif jogador == 1 and computador == 3:
+      print('Computador: {}\n Jogador: {}'.format(computador, jogador))
+      print('Você venceu!')
+
+elif jogador == 2 and computador == 1:     
+      print('Computador: {}\n Jogador: {}'.format(computador, jogador))
+      print('Você venceu!')
+
+elif jogador == 3 and computador == 2: 
+      print('Computador: {}\n Jogador: {}'.format(computador, jogador))
+      print('Você venceu!')
+
+elif computador == jogador:
+      print('Tente mais uma vez!')
+
+
+
+
