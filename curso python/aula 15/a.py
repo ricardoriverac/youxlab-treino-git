05f3a726dd2248c2d213407c3f047053d3aea817
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-14 15:07:49 -0300

    jogo par ou ímpar

---
+++ curso python/aula 15/a.py
@@ -0,0 +1,8 @@
+dados = dict()
+
+dados['nome'] = input('nome: ')
+dados['idade'] = int(input('idade: '))
+
+print(dados['nome'])
+
+dicionario = {'nome': 'Kawannne', 'idade': 30}
--- curso python/aula 15/ex68.py
+++ curso python/aula 15/ex68.py
@@ -1,10 +1,35 @@
 from random import randint
-itens = ['impar', 'par']
 print ("\t_=_" * 20)
 print ("VAMOS JOGAR PAR OU ÍMPAR... ")
 print ("\t_=_" * 20)
-while True : 
+contadorAcertos = 0
+while True:
     computador = randint (0,10)
-    jogador = str (input ("Digite um número: "))
-    print ("Compuador jogou: \033[33m{}\033[m ".format(itens[computador]))
-    print ("Jogador jogou: \033[34m{}\033[m ".format(itens[jogador]))
+    jogador = int (input ("Digite um número: "))
+    soma = jogador + computador
+    escolha = str (input("Você escolhe par ou impar ? [P ou I]")).strip().upper()
+    while escolha not in "PI":
+        escolha = str (input("Você escolhe par ou impar ? [P ou I]")).strip().upper()
+    if soma % 2 == 0:
+        print ("PAR")
+    else:
+        print ("ìmpar")
+    print (f"Você jogou {jogador} e o computador jogou {computador}, resultando em {soma}")
+    if escolha == "P":
+        if soma % 2 == 0:
+            print ("Número par, VOCẼ VENCEU!!")
+            contadorAcertos += 1
+        else:
+            print ("VOCÊ PERDEU!!")
+            break
+        print ("Vamos jogar novamente...")
+    elif escolha == "I":
+        if soma % 2 == 1:
+            print ("Número impar, VOCẼ VENCEU!!")
+            contadorAcertos += 1
+        else:
+            print ("VOCÊ PERDEU!!")
+            break
+        print ("Vamos jogar novamente...")
+print ("FIM DE JOGOOO!!!")
+print (f"Quantidade de vitórias: {contadorAcertos}.") 
