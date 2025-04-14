f6cf00734287154a194d56ab535ca954f1e8c9d2
GustavoValacio <gustavoseabra2007@gmail.com>
2025-04-14 17:17:38 -0300

    Desafio81 Valores ordem

---
+++ 17-aula_python/Desafio81Aula17.py
@@ -0,0 +1,20 @@
+valores = []
+while True:
+    numeros = int(input('Digite um valor: '))
+    valores.append(numeros)
+    valores. sort(reverse=True)
+    responda = ' '
+    while responda not in 'SN':
+        responda = input('Você quer continuar? [S/N] ').strip().upper()[0]
+
+    if responda == 'N':
+        break
+
+if 5 in valores:
+    print('O número 5 está na lista!')
+else:
+    print('O número 5 não está na lista! ')
+
+print(f'Foram digitados {len(valores)}, números')
+print(f'Sua lista em ordem decrescente {valores}')
+    
