5e9bb1d165de3d189a3fbc15d5a742a3c657a376
annaluiza777 <anna.araujo@youxlab.com.br>
2025-04-14 17:05:13 -0300

    exercicios  aula 15

---
+++ aula_15.py/exercicios_aula15.py
@@ -0,0 +1,42 @@
+cont = 1 
+while cont <= 10:
+    print(cont, '->', end= '')
+    cont += 1 
+print('Acabou')
+
+n = 0
+while n !=999:
+    n = int(input('Digite um número: '))
+    cont += 1
+
+
+n = cont = 0
+while cont < 3:
+    n = int(input('Digite um número: '))
+    cont += 1
+
+
+
+    n = cont = 0
+while cont < 5:
+     n = int(input('Digite um número: '))
+     cont += 1
+
+
+
+n = 0
+while n !=999:
+    n = int(input('Digite um número: '))
+
+
+n = 0
+while n !=0:
+    n = int(input('Digite um número: '))
+
+
+
+n = s =  0
+while n !=999:
+    n = int(input('Digite um número: '))
+    s += n 
+print('A soma vale {}'.format(s))
