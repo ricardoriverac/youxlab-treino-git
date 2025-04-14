5c49af28659f8ecb04770ac91ee1cc189c8e0868
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-04-14 12:29:07 -0300

    Ache as vogais

---
+++ aula016/ex077.py
@@ -0,0 +1,16 @@
+palavras= ('Nicolas','aprendendo','dominando','paython','nomes','cirurgia','conhecimento')
+
+for p in palavras:
+        print( )
+        print(f'\n A palavras {p} possui: ',end=' ')
+        print( )
+    
+        for letra in p:
+            if letra in 'aeiou':
+
+                print(letra,end=' ')
+                #print(f'letra{letra} posição:  {p.index(letra)} ',end=' ')
+
+                
+
+
