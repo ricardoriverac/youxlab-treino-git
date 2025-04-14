e902d03fea0f7d47d8f633a28bd17eb325b61dd1
GustavoValacio <gustavoseabra2007@gmail.com>
2025-04-14 14:15:16 -0300

    Aula 17, Inicio

---
+++ 17-aula_python/Inicio.py
@@ -0,0 +1,12 @@
+num = [2, 6, 9, 1]
+num [2] = 3
+num.append(7)
+num. sort(reverse=True)
+num. insert(2,2)
+if 4 in num:
+    num.remove(4)
+else:
+    print('Não encontrei o número 4')
+#num. pop (3)
+print (num)
+print(f'Esta lista tem {len(num)} elementos')
