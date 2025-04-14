92bb17d934243b36e200302a491a53290b5850f9
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-04-14 17:16:45 -0300

    salvando desafio 27 da aula 9

---
+++ curso_python/aula09/desafio_27_aula09.py
@@ -0,0 +1,4 @@
+nome = (input('Digite o seu nome completo: ')).strip()
+primeiroNome = nome.split()[0]
+ultimoNome = nome.split()[-1]
+print (f'O seu primeiro nome é {primeiroNome} e o seu último nome é {ultimoNome} ')
