3e91cd9987e305bed5d2dad7df0b0070f3c1af3a
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-04-15 14:04:03 -0300

    salvando desafio 29 da aula 10

--- curso_python/aula10/desafio_28_aula10.py
+++ curso_python/aula10/desafio_28_aula10.py
@@ -1,1 +1,8 @@
 import random
+numeroComputador = random.randint(0, 5)
+pergunta = int(input('Tente adivinhar o número que o computador escolheu entre 0 e 5 : '))
+if pergunta == numeroComputador:
+    print ('Você venceu! ')
+else:
+    print ('Você perdeu! ')    
+print (f'O computador escolheu {numeroComputador} ') 
