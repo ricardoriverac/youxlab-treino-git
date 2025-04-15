465f4f360b241ef951b68eaf57ce34b6c86e3883
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-15 16:52:10 -0300

    loja

--- curso python/aula 15/ex70.py
+++ curso python/aula 15/ex70.py
@@ -1,8 +1,9 @@
 continuar = 'S'
 soma = 0
 contadorProdutoMaiorQue1000 = 0
-maior = 0
-list = [ ]
+menor = 0
+nomeMenor = ''
+list = []
 while continuar == "S":
     nomeProduto = str (input ("Qual o nome do produto ? "))
     preco = int (input ("Qual é o preço do produto ? "))
@@ -12,13 +13,13 @@ while continuar == "S":
     list.append(preco)
     if preco > 1000:
         contadorProdutoMaiorQue1000 += 1
-    if len (nomeProduto) == 0:
-        MaisBarato = preco
-        preco = list[1]
-    else:
-        if preco < MaisBarato:
-            MaisBarato == preco
-            list[1] == MaisBarato 
-print (f"a soma TOTAL de todos os produtos {soma}")
-print (f"Quantidade dos produto com maiores preços {contadorProdutoMaiorQue1000}")
-print (f"Produto mais barato {MaisBarato}")
+    if menor == 0:
+        menor = preco
+        nomeMenor = nomeProduto
+    elif menor > preco:
+        menor = preco
+        nomeMenor = nomeProduto
+
+print (f"a soma TOTAL de todos os produtos: \033[36m{soma}\033[m")
+print (f"Quantidade dos produto com preços maiores que 1000: \033[36m{contadorProdutoMaiorQue1000}\033[m")
+print (f"Produto mais barato: \033[36m{nomeMenor}\033[m")
