b8f8f175365b505e3ce2d17729ef2f951b44debd
Maria Luiza <maria.silva@youxlab.com.br>
2025-04-16 17:09:18 -0300

    desafio do Campeonato Brasileiro de Futebol

---
+++ desafiosaula16/desafio73.py
@@ -0,0 +1,31 @@
+from time import sleep
+tabela = ('Flamengo', 'Palmeiras', 'Ceará SC', 'Juventude', 'Fluminense', 'Vasco da Gama', 'Internacional', 'Fortaleza', 'Corinthias', 'Bota fogo', 'Bragantino', 'Cruzeiro', 'Grêmio', 'Bahia', 'São Paulo', 'Mirassol', 'Atlético-MG', 'Santos', 'EC Vitória', 'Sport Recife')
+primeirosColocados = tabela[0:5]
+ultimosColocados = tabela[16:]
+ordemAlfabética = sorted(tabela)
+posiçãoCruzeiro = tabela.index('Cruzeiro')
+
+print('-='*20)
+print('{:^30}'.format('\033[32mCAMPEONATO\033[m \033[33mBRASILEIRO\033[m \033[36mDE FUTEBOL\033[m'))
+print('-='*20)
+sleep(1)
+
+print(f'''Os \033[36m5 PRIMEIROS\033[m colocados são: 
+      {primeirosColocados} ''')
+print('')
+sleep(1)
+
+print(f'''Os \033[35m4 ÚLTIMOS\033[m colocados na tabela são:
+      {ultimosColocados}''')
+print('')
+sleep(1)
+
+print(f'''A \033[32mORDEM ALFABÉTICA\033[m dos times da tabela é:
+      {ordemAlfabética}''')
+print('')
+sleep (1)
+
+print(f'''O \033[34mCRUZEIRO\033[m está na posição 
+      {posiçãoCruzeiro+1}''')
+
+
