f1d47e668465ab137f345175292d4d29a061d374
VictorGabrielBarros <victor.barros@youxlab.com.br>
2025-04-16 13:09:29 -0300

    Desafio66

---
+++ 15_InterrompendoWhile/Desafios/Desafio66.py
@@ -0,0 +1,5 @@
+numero = soma = 0
+while True:
+    numero = int(input("Digite um numero:"))
+    if numero == 999:
+        break
--- 15_InterrompendoWhile/teste/Teste.py
+++ 15_InterrompendoWhile/teste/Teste.py
@@ -3,13 +3,24 @@
 #     print(cont, " -> ", end="")
 #     cont += 1
 # print("Acabou")
+
+
 # numero = 0
 # while numero != 999:
 #     numero = int(input("Digite um numero: ")) 
+
 
 
 # numero = contador = 0
 # while contador < 10:
 #     num = int(input("Digite um numero: "))
 #     contador += 1
-    
+
+numero = soma = 0
+while True:
+    numero= int(input("Digite um numero"))
+    if numero == 999:
+       break
+    soma += numero
+print(f"[A soma  vale {soma}]")
+        
