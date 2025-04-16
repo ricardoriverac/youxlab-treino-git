7dcb467c0c913efd149f1bd3ccfd746acc052196
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-04-16 17:59:33 -0300

    fiz o desafio 79

---
+++ exercicios.py/exerc79.py
@@ -0,0 +1,21 @@
+numeros = []
+
+while True:
+    n = int(input('Digite um número: '))
+    if n not in numeros:
+        numeros.append(n)
+        print('Número adicionado com sucesso.')
+    else:
+        print('Número repetido não será adicionado.')
+
+    while True:
+        resposta = input('Quer continuar? [S/N] ').strip().upper()
+        if resposta in 'SN':
+            break  
+        print('Ocorreu um erro, digite de novo.')
+
+    if resposta == 'N':
+        print('FINALIZANDO...')
+        break  
+print('-' * 40)
+print(f'A lista ficou: {numeros}')
