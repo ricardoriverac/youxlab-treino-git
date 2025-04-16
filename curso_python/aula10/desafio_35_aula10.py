857ea7d1bbdc34bfd3e43dc2b553420c30f27de1
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-04-16 15:19:34 -0300

    salvando desafio 35 da aula 10

--- curso_python/aula10/desafio_35_aula10.py
+++ curso_python/aula10/desafio_35_aula10.py
@@ -1,3 +1,7 @@
-reta1 = (input('Digite o tamanho da primeira reta: '))
-reta2 = (input('Digite o tamanho da segunda reta: '))
-reta3 = (input('Digite o tamanho da terceira reta: '))
+reta1 = float(input('Digite o tamanho da primeira reta: '))
+reta2 = float(input('Digite o tamanho da segunda reta: '))
+reta3 = float(input('Digite o tamanho da terceira reta: '))
+if reta1 < reta2 + reta3 and reta2 < reta1 + reta3 and reta3 < reta2 + reta1:
+    print ('É um triângulo! ')
+else:
+    print ('Não é um triângulo! ')
