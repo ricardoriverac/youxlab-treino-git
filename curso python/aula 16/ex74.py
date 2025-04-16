241655bcee71123ca9af7d4b6362e3724acf04ce
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-16 16:52:41 -0300

    sorteando 5 números, imprimindo o maior e menor número

---
+++ curso python/aula 16/ex74.py
@@ -0,0 +1,6 @@
+from random import randint
+numerosAleatorios = randint (0,10),randint (0,10),randint (0,10),randint (0,10),randint (0,10)
+print(f"Os números aleatórios foram: {numerosAleatorios}")  
+print (f"O maior número sorteado foi: {max(numerosAleatorios)}")
+print (f"O menor número sorteado foi: {min(numerosAleatorios)}")
+
