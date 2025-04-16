525350651f616803384d4b32a0ec5b885886c9db
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-04-16 17:46:28 -0300

    salvando treino da aula 11

---
+++ curso_python/aula11/teste_aula11.py
@@ -0,0 +1,16 @@
+#\033[0;30;41m
+#\033[4;33;44m
+#\033[1;35;43m
+#\033[30;42m
+#\033[m
+#\033[7;30m
+
+print ('\033[4;30;45mOlá, Mundo!\033[m')
+print ('\033[0;33;44mOlá, Mundo!\033[m')
+
+a = 3
+b = 5
+print (f'Os valores são \033[32m{a}\033[m e \033[31m{b}\033[m!!!')
+
+#nome = 'Vinícius'
+#print (f'Olá! Muito prazer em te conhecer, {'\033[m'} {nome} {'\033[m'} !!! )
