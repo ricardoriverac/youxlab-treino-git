0e1136945cd9346d857c4c968ca272e3da09a278
juliocesar <juliogoiabinha060@gmail.com>
2025-04-17 17:11:05 -0300

    Arrumando erros

--- 22-modularizacao/exercicios-aula22/exe107.py
+++ 22-modularizacao/exercicios-aula22/exe107.py
@@ -1,4 +1,4 @@
-from utilizaveis import moeda
+from modulos import moeda
 p = float(input('Digite o valor: '))
 print(f'A metade de {p} é {moeda.metade(p)}')
 print(f'O dobro de {p} é {moeda.dobro(p)}')
