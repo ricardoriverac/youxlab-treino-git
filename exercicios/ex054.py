188ec7090142c717a3a37a083aededccdb285d25
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-16 22:30:14 -0300

    Exercício 54

--- exercicios/ex054.py
+++ exercicios/ex054.py
@@ -3,12 +3,12 @@ numMaiorIdade= 0
 numMenorIdade= 0
 for pessoa in range(1, 7+1):
     pessoa= (input('Seu nome: '))
-    idade= int(input('Sua idade: '))
+    ano= int(input('Sua data de nascimento: '))
     numMaiorIdade= pessoa(1, 8)
     numMenorIdade= pessoa(1, 8)
 
-    if idade >= 18:
+    if ano >= 18:
         print('{} atingiu a maior idade'.format(numMaiorIdade))
-    elif idade < 18: 
+    elif ano < 18: 
         print('{} não atingiu a maior idade'.format(numMenorIdade))
     
