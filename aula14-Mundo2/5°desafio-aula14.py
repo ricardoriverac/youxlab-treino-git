752f0adf5bbbc74af400c870fc60c4ad5fa4be7b
Youx <sophia.carvalho@youxlab.com.br>
2025-04-17 16:35:46 -0300

    mundo2

---
+++ aula14-Mundo2/5°desafio-aula14.py
@@ -0,0 +1,12 @@
+#P.A COM WHILE
+termo1=int(input('Digite o 1° termo:'))
+razao=int(input('Digite sua razão:'))
+termo=termo1
+contador=1
+while contador<=10:
+    print(f'{termo} --> ',end='')
+    termo+=razao
+    contador+=1
+print('FIM')
+
+
