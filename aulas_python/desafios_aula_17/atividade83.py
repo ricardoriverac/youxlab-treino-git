9d6da90e2f921280cba0f7329ae4fe1dff1dbb76
Marcos <marcos.garcia@youxlab.com.br>
2025-04-17 17:49:51 -0300

    Com listas agora Paim!

--- aulas_python/desafios_aula_17/atividade83.py
+++ aulas_python/desafios_aula_17/atividade83.py
@@ -1,14 +1,32 @@
-exp = str(input('Digite sua expressão: '))
+exp = str(input('Digite sua expressão: ')).split()
+lista = []
+lista.append(exp)
 cont = 0 
 
-for c in exp:
-    if c[:] == '(':
+for c in lista:
+    if c == '(':
         cont += 1
-    if c[:] == ')':
-        cont += 1
+    elif c == ')':
+        cont -= 1
         
 if cont %2 == 0:
     print('É uma expressão válida!')
     
 else:
     print('É uma expressão inválida!!')
+
+
+# exp = str(input('Digite sua expressão: ')).split()
+# cont = 0 
+
+# for c in exp:
+#     if c == '(':
+#         cont += 1
+#     elif c == ')':
+#         cont -= 1
+        
+# if cont %2 == 0:
+#     print('É uma expressão válida!')
+    
+# else:
+#     print('É uma expressão inválida!!')
