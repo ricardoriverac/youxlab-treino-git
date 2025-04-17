c7b37087b64d89f389ab32d2846d0b1912cc8a68
Marcos <marcos.garcia@youxlab.com.br>
2025-04-17 17:54:21 -0300

    Atualização

--- aulas_python/desafios_aula_17/atividade83.py
+++ aulas_python/desafios_aula_17/atividade83.py
@@ -1,32 +1,32 @@
-exp = str(input('Digite sua expressão: ')).split()
-lista = []
-lista.append(exp)
-cont = 0 
-
-for c in lista:
-    if c == '(':
-        cont += 1
-    elif c == ')':
-        cont -= 1
-        
-if cont %2 == 0:
-    print('É uma expressão válida!')
-    
-else:
-    print('É uma expressão inválida!!')
-
-
-# exp = str(input('Digite sua expressão: ')).split()
+# exp = str(input('Digite sua expressão: '))
+# lista = []
+# lista.append(exp)
 # cont = 0 
 
-# for c in exp:
+# for c in lista:
 #     if c == '(':
 #         cont += 1
 #     elif c == ')':
-#         cont -= 1
+#         cont += 1
         
 # if cont %2 == 0:
 #     print('É uma expressão válida!')
     
 # else:
 #     print('É uma expressão inválida!!')
+
+
+exp = str(input('Digite sua expressão: '))
+cont = 0 
+
+for c in exp:
+    if c == '(':
+        cont += 1
+    elif c == ')':
+        cont += 1
+        
+if cont %2 == 0:
+    print('É uma expressão válida!')
+    
+else:
+    print('É uma expressão inválida!!')
