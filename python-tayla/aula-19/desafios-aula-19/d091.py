47b642a04ae76c58cf38321ec8e9e61ee6f1ac4e
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-04-17 16:49:48 -0300

    desafio 89

---
+++ python-tayla/aula-19/desafios-aula-19/d091.py
@@ -0,0 +1,20 @@
+from random import randint
+from time import sleep
+
+jogadores = {'jogador1': randint(1, 6), 'jogador2': randint(1, 6), 'jogador3': randint(1, 6), 'jogador4': randint(1, 6)}
+cont = 1
+
+print('Valores sorteados:')
+
+for k, v in jogadores.items():
+    print(f'O {k} tirou {v} no dado.')
+    sleep(1)
+
+print('-=' * 20)
+
+print('  == RANKING DOS JOGADORES ==')
+
+for i in sorted(jogadores, key = jogadores.get, reverse = True):
+    print(f'    {cont}ª lugar: {i} com {jogadores[i]}')
+    cont += 1
+    sleep(1)
