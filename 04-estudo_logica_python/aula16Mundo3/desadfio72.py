98eb78cabb3e796721305e6eeff6a873caf5b5d5
marianne0614 <marianne.lima@youxlab.com.br>
2025-04-17 16:09:19 -0300

    desafio 72

---
+++ 04-estudo_logica_python/aula16Mundo3/desadfio72.py
@@ -0,0 +1,5 @@
+extenso=(' zero', 'um', 'dois', 'três', 'quatro','cinco', 'seis', 'sete', 'oito', 'nove','dez',
+         'onze','doze','treze', 'quatorze','quinze','dezesseis', 'dezessete','dezoito','dezenove','vinte')
+while True:
+    numero=int(input(' Digite um numero de 0 a 20:'))
+    print(extenso[numero])
