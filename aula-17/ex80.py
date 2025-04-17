37e5061a8f6fc3ed4151f30f92fea91f8171770a
yuripereira02 <yuyupg0204@gmail.com>
2025-04-17 13:45:31 -0300

    exercicios 78 a 83

--- aula-17/ex80.py
+++ aula-17/ex80.py
@@ -2,7 +2,7 @@ lista = []
 for i in range(5):
     num = int(input(f'Digite um número para a posição {i + 1}ª: '))
     posicao_inserir = 0 
-    while posicao_inserir < lista.count('(' , ')') and lista[posicao_inserir] < num:
+    while posicao_inserir < len(lista) and lista[posicao_inserir] < num:
         posicao_inserir += 1
     lista.insert(posicao_inserir, num)
 print(lista)
