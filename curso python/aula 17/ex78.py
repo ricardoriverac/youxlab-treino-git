c1cdc4bc43eb3386381c0d28c6c7087694f76144
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-17 16:02:30 -0300

    maior e menor número da lista

--- curso python/aula 17/ex78.py
+++ curso python/aula 17/ex78.py
@@ -1,2 +1,11 @@
-valores = list (range(0,5))
-print(valores)
+valores = []
+for i in range(1, 6):
+    numero = int(input(f"Digite o {i}° número: "))
+    valores.append(numero)
+maior = max(valores)
+menor = min(valores)
+posicaoMaior = valores.index(maior)
+posicaoMenor = valores.index(menor)
+print(f"Os números digitados foram: {valores}")
+print(f"O maior número digitado foi: {maior}, na posição {posicaoMaior}")
+print(f"O menor número digitado foi: {menor}, na posição {posicaoMenor}")
