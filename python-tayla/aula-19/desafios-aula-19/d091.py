837703962642ce3ec7962b07ebfc034caf761562
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-04-17 16:50:21 -0300

    desafio 91

--- python-tayla/aula-19/desafios-aula-19/d091.py
+++ python-tayla/aula-19/desafios-aula-19/d091.py
@@ -4,7 +4,7 @@ from time import sleep
 jogadores = {'jogador1': randint(1, 6), 'jogador2': randint(1, 6), 'jogador3': randint(1, 6), 'jogador4': randint(1, 6)}
 cont = 1
 
-print('Valores sorteados:')
+print('Valores sorteados: ')
 
 for k, v in jogadores.items():
     print(f'O {k} tirou {v} no dado.')
