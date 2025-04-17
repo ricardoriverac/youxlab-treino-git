b10953e94cc435c888dae517bc2d2e067c7c5915
Marcos <marcos.garcia@youxlab.com.br>
2025-04-17 12:06:29 -0300

    Fiz um código de maior e menor valor na lista, guanabara ajudou

--- aulas_python/desafios_aula_16/atividade78.py
+++ aulas_python/desafios_aula_16/atividade78.py
@@ -1,13 +1,43 @@
-numero = (str(input('Digite um número para a posição 0 : ')),
-           str(input('Digite um número para a posição 1 : ')),
-           str(input('Digite um número para a posição 2 : ')),
-           str(input('Digite um número para a posição 3 : ')),
-           str(input('Digite um número para a posição 4 : ')))
+#GUANABARA
+listanum = []
+maior = 0
+menor = 0 
+for c in range(0, 5):
+    listanum.append(int(input(f'Digite um valor para a posição {c}: ')))
+    if c == 0:
+        maior = menor = listanum[c]
+    else:
+        if listanum[c] > maior:
+            maior = listanum[c]
+        if listanum[c] < menor:
+            menor = listanum[c]
+print('=-'*30)
+print(F'Você digitou os valores: {listanum}')
+print(F'O maior número digitado foi {maior} nas posições: ', end='')
+for i, v in enumerate(listanum):
+    if v == maior:
+        print(F'{i}... ', end='')
+print()
+print(f'O menor valor digitado foi {menor} nas posições: ', end='')
+for i, v in enumerate(listanum):
+    if v == menor:
+        print(f'{i}...', end='')
+print()
 
-maximo = max(numero)
-minimo = min(numero)
 
 
-print(F'Você digitou os valores: {numero}')
-print(F'O maior valor digitado foi {maximo} na posição : {len(maximo)}')
-print(F'O menor valor digitado foi {minimo} na posição : {len(minimo)}')
+
+#CODIGO ABAIXO FUNCIONA POREM SELECIONA APENAS UM NÚMERO MAIOR E UM MENOR
+# valores = list()
+# for numeros in range(0, 5):
+#     valores.append(int(input(f'Digite um valor para a posição {numeros}: ')))
+
+
+# maximo = max(valores)
+# minimo = min(valores)
+
+
+
+# print(F'Você digitou os valores: {valores}')
+# print(F'O maior valor digitado foi {maximo} na posição : {valores.index(maximo)}')
+# print(F'O menor valor digitado foi {minimo} na posição : {valores.index(minimo)}')
