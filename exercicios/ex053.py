7ccf39437f0eb8dab333b4d0844051b2379650bb
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-16 22:12:20 -0300

    Exercício 53

--- exercicios/ex053.py
+++ exercicios/ex053.py
@@ -1,5 +1,9 @@
-#terminar de fazer:
-frase= str(input('Digite uma frase: '))
-invertida= frase.split()
-juntar= "".join(frase)
-print(juntar)
+print('=====ANALISADOR DE PALÍNDROMO=====')
+frase= str(input('Escreva uma frase: ')).strip().upper()
+palavras= frase.split()
+juntar= ''.join(palavras)
+print('Você digitou a frase {}'.format(juntar))
+inverso= ''
+for letra in range(len(juntar - 1, -1, -1)):
+    inverso += juntar[letra]
+    print(juntar, inverso)
