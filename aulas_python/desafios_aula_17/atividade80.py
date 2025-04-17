d7b2f5f16a3246c88e5dfe9317460b8f2de1e3f3
Marcos <marcos.garcia@youxlab.com.br>
2025-04-17 16:04:22 -0300

    Fiz o código de Lista ordenada sem repetições (ajuda do google

--- aulas_python/desafios_aula_17/atividade80.py
+++ aulas_python/desafios_aula_17/atividade80.py
@@ -1,2 +1,14 @@
-while True:
+lista = []
+for c in range(0, 5):
     numero = int(input('Digite um número: '))
+    if c == 0 or numero > lista[-1]:
+        lista.append(numero)
+        
+    else:
+        pos = 0
+        while pos < len(lista):
+            if numero <= lista[pos]:
+                lista.insert(pos, numero)
+                break
+            
+print(lista)
