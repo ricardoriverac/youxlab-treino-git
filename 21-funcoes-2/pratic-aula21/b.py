17520ffb994d81ae9eacb8c95779205bea87b656
juliocesar <juliogoiabinha060@gmail.com>
2025-04-17 02:04:34 -0300

    Aula pratica da 21

---
+++ 21-funcoes-2/pratic-aula21/b.py
@@ -0,0 +1,15 @@
+# lista = [1 , 2 , 3 ,4 ,5 , 6,7,8,9,9]
+
+# def soma():
+#     somatorio = 2 + 10
+
+#     somatorio2 = somatorio + 2
+
+#     return somatorio2
+
+# teste = soma()
+
+# soma2 = 10 + teste
+# print(soma2)
+
+help(print)
