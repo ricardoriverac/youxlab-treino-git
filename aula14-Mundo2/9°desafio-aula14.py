ff2c29ecd701f98712781ce71acc593b2f7387d4
Youx <sophia.carvalho@youxlab.com.br>
2025-04-18 10:09:51 -0300

    mundo2

---
+++ aula14-Mundo2/9°desafio-aula14.py
@@ -0,0 +1,18 @@
+
+resposta='S'
+soma= quantidade= media=maior= menor=0
+while resposta in 'Ss':
+    numero=int(input('Digite um n°: '))
+    soma+=numero
+    quantidade+=1
+    resposta=str(input('Quer continuar? [s/n]: ')).upper().strip()[0] #maiúscula,removeu os espaços e sera considerado a 1° letra
+    if quantidade==1:
+        maior=menor=numero
+    else:
+        if numero>maior:
+            maior=numero
+        if numero<menor:
+            menor=menor
+media=soma/quantidade
+print('VocÊ digitou {} números e a média deles é de {:.2f}'.format(quantidade,media))
+print(f'O maior número foi {maior} e o menor foi {menor}')
