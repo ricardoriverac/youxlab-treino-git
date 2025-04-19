5ca8bd66861cf5408632a44fbd85b72a8cfe24bf
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-04-19 13:02:56 -0300

    fiz o desafio 85

--- 18-curso_python/desafio85.py
+++ 18-curso_python/desafio85.py
@@ -10,5 +10,7 @@ for valores in range(1,8):
     
 lista = listaPares + listaImpares
 ordem = sorted(lista)
+print(f'A lista dos valores pares é: {listaPares}')
+print(f'A lista dos valores impares é: {listaImpares}')
 print(f'A lista separando os números pares e ímpares é: \033[1;35{lista}\033[m')
 print(f'A lista em ordem crescente é: \033[1;33{ordem}\033[m')
