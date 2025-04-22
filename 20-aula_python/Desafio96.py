8602c049b72317fd7b69a1658127c80512b1f595
GustavoValacio <gustavoseabra2007@gmail.com>
2025-04-22 15:13:14 -0300

    Desafio 96 area

---
+++ 20-aula_python/Desafio96.py
@@ -0,0 +1,7 @@
+def area():
+    largura = float(input('Qual a largura em metros ? '))
+    comprimento = float(input('Qual o comprimento em metros ?'))
+    m = largura * comprimento
+    print(f'A área de um terreno de {largura} x {comprimento} é {m}')
+area()
+
