89f16fdc8c10cc17cd1b6b91d7f62039770ffae8
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-22 14:21:43 -0300

    Exercício 56

--- exercicios/ex056.py
+++ exercicios/ex056.py
@@ -1,1 +1,7 @@
-#fazer
+#orgulhosa de mim por ter chegado aqui!!! 
+idadeMedia= 0
+for a in range(1, 5):
+    nome= input('Informe o seu nome: ')
+    idade= int(input('Agora a sua idade: '))
+    sexo= input('E por último, seu sexo: ')
+
