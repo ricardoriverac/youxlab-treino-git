060ca1b92c08a72f8787c85333e15600616989d1
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-04-22 11:46:31 -0300

    Notas na média

---
+++ aula019/ex090.py
@@ -0,0 +1,23 @@
+dados = {}
+
+dados['nome'] = str(input('Digite seu nome: '))
+dados['media'] = float(input('Média: '))
+
+
+# print(f'nome: {dados["nome"]}')
+
+# print(f'média: {dados["media"]}')
+
+
+
+if dados['media'] <= 6:
+    print('Recuperação')
+    #dados[situacao] ='Aprovado'
+
+
+else:
+    print('Passou de ano')
+    #dados[situacao] = 'Não passou'
+
+#print(dados)
+
