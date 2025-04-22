004e369e60a4b1eabb8c24cfd572cd2fe074fb8a
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-22 13:51:13 -0300

    Exercício 55

--- exercicios/ex055.py
+++ exercicios/ex055.py
@@ -1,1 +1,18 @@
 #fazer em casa: 
+maior= 0
+menor= 0
+for a in range(1, 6): 
+    peso= float(input('Informe seu peso em kg: '))
+    if a == 1:
+        maior= peso
+    else:
+        if peso > maior:
+            maior= peso
+    if a == 1:
+        menor= peso
+    else:
+        if peso < menor:
+            menor= peso
+                
+print(f'O maior peso lido foi {maior}')
+print(f'O menor peso foi {menor}')
