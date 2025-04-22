c4afd85cda41c39ed09373ec3f2d7e545bc81f0a
Marcos <marcos.garcia@youxlab.com.br>
2025-04-22 18:03:54 -0300

    Atualizações

--- aulas_python/desafios_aula_19/atividade91.py
+++ aulas_python/desafios_aula_19/atividade91.py
@@ -1,5 +1,14 @@
 import random
+Dic = dict()
+listaN = []
+listaJ = []
+print()
+for c in range (1,5):
+    escolha = random.randint(0,6)
+    listaN.append(escolha)
+    print(f'Jogador {c} tirou {escolha} no dado')
+Dic['Jogador '] = listaJ
+Dic['Números'] = listaN
 
-escolha = random.randint(0,6)
-for c in range (1,4):
-    print(f'Jogador {c} tirou {}')
+print('\033[33m=-=\033[m'*30)
+print(Dic)
