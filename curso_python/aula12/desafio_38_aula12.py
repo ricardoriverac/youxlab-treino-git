00e02dc85e565b3cf69a778e2d643422533d505d
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-04-22 13:14:37 -0300

    salvando desafio 38  da aula 12

---
+++ curso_python/aula12/desafio_38_aula12.py
@@ -0,0 +1,8 @@
+valor1 = int(input('Digite um valor: '))
+valor2 = int(input('Digite outro valor: '))
+if valor1 > valor2:
+    print ('O primeiro valor é maior. ')
+elif valor2 > valor1:
+    print ('O segundo valor é maior. ')
+else:
+    print ('Não existe valor maior, os dois são iguais. ')
