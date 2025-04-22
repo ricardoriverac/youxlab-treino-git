87500887cba8342c92ac5207c85ac07672beb5c2
juliocesar <juliogoiabinha060@gmail.com>
2025-04-21 22:20:00 -0300

    modulos e pacotes

--- 22-modularizacao/ex112/utilidadescev/dados.py
+++ 22-modularizacao/ex112/utilidadescev/dados.py
@@ -1,12 +1,12 @@
 def leiadinheiro(msg=''):
-    leia = str(input(msg)).replace(',','.')
+    leia = str(input(msg)).replace(',','.').strip()
     if '.' in leia or leia.isnumeric():
         leia = float(leia)
         return leia
     else:
         while True:
             print('\033[31mERRO! TENTE NOVAMENTE!\033[m')
-            leia = str(input(msg)).replace(',','.')
-            if '.' in leia or leia.isnumeric():
+            leia = str(input(msg)).replace(',','.').strip()
+            if '.' in leia or leia.isnumeric() :
                 leia = float(leia)
                 return leia
