166a4f1279bd2e58bda51c026c43908bda62dfdf
annaluiza777 <anna.araujo@youxlab.com.br>
2025-04-22 13:06:06 -0300

    desafio 74 aula 16

---
+++ aula_16.py/desafio074_aula16.py
@@ -0,0 +1,5 @@
+from random import randint
+numero=randint(0,10),randint(0,10),randint(0,10), randint(0,10),randint(0,10)
+print(f' os numeros gerados sao:{numero}')
+print(f' o menor numero é:{min(numero)}')
+print(f'o numero maior é: {max(numero)}')
