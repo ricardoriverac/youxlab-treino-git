a656723cc0db707a96d4171dc1a7da0cc6dc8e97
gustavofreire <gustavo.gomes@youxlab.com.br>
2025-04-22 14:07:26 -0300

    ds96

---
+++ 20-Aula_Funções/ds96.py
@@ -0,0 +1,15 @@
+from time import sleep
+
+def area():
+    c = float(input('Largura: '))
+    l = float(input('Comprimento: '))
+    total = c * l
+    print(f'A área de um terreno {c}x{l} é de {total}')
+
+
+
+
+print('>>>>>>>>>AREA<<<<<<<<<<<')
+print('-='*30)
+sleep(1)
+area()
