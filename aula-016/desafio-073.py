7b14c69b066a5aa5f4663dfc56f5210dc7677dc2
Keverson Teodoro <keverson.teodoro@youxlab.com.br>
2025-04-22 18:35:26 -0300

    pastas 16 a 21

--- aula-016/desafio-073.py
+++ aula-016/desafio-073.py
@@ -24,15 +24,25 @@ clubes_albafeto = [
     'Sport Recife',
     'Vasco'
 ]
-escolha = str(input("Você quer ver informaçôes sobre o brasileirão? S/N")).upper
-while escolha != 'S':
-    break
+
+escolha = str(input("Você quer ver informaçôes sobre o brasileirão? \033[33m[S/N]:\033[m ")).upper()
+while escolha == 'S':
+    
         
-posicao = brasileirao.index('Bragantino')
-print(f'\n Os 5 primeiro colocados são: {brasileirao[:5]}')
+    posicao = brasileirao.index('Bragantino')
+    print(f'\n Os 5 primeiro colocados são: {brasileirao[:5]}')
 
-print(f'\n Os 4 Ultimos colocados são: {brasileirao[16:]}')
+    print(f'\n Os 4 Ultimos colocados são: {brasileirao[16:]}')
 
-print(f'\n Os clubes em ordem alfabética: {clubes_albafeto}')
+    print(f'\n Os clubes em ordem alfabética: {clubes_albafeto}')
 
-print(f'\n O Bragantino esta na posição: {posicao}')
+    print(f'\n O Bragantino esta na posição: {posicao}')
+
+    if escolha =='N':
+        break
+print('-=' * 30)
+print('OBRIGADO, VOLTE SEMPRE')
+print('-=' * 30)
+
+
+
