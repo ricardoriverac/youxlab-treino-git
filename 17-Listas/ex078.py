1430067519b96cbf70236f77b56c537618481a9b
fabricioyoux <fabricio.genesi@youxlab.com.br>
2025-04-22 15:19:57 -0300

    atividade 78

--- 17-Listas/ex078.py
+++ 17-Listas/ex078.py
@@ -1,11 +1,9 @@
 valores = list()
 for cont in range (0, 5):
     valores.append(int(input('Digite um valor:')))
-
-for c, v in enumerate(valores): 
     
-    print(f'Você digitou os valores: {valores}')
+print(f'Você digitou os valores: {valores}')
 
-    print(f'O maio número sorteado foi {max(valores)}')
+print(f'O maio número sorteado foi {max(valores)} na posição {valores.index(max(valores))}')
 
-    print(f'O menor número sorteado foi {min(valores)}')
+print(f'O menor número sorteado foi {min(valores)} na posição {valores.index(min(valores))}')
