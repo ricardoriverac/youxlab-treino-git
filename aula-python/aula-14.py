614a70b0cc79dc365ae8a4711fb73d289afe4611
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-22 17:45:57 -0300

    Aula 14

--- aula-python/aula-14.py
+++ aula-python/aula-14.py
@@ -1,1 +1,7 @@
 #preparando território: ESTRUTURA DE REPETIÇÃO WHILE
+# resposta= 'S'
+# while resposta == 'S':
+#     n= int(input('Digite um valor: '))
+#     resposta= str(input('Quer continuar?: ')).upper().capitalize()
+# print('Fim')
+
