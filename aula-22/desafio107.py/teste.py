25bb9c5ddf4665e01b45880cc0d7ead943b2ab9c
alexia-assis <alexiaassis.youxlab.com.br>
2025-04-22 13:54:19 -0300

    Fim do desafio de modulo da moeda

---
+++ aula-22/desafio107.py/teste.py
@@ -0,0 +1,7 @@
+import moeda
+
+p = float(input('Digite um preço: R$ '))
+print(f'A metade de {p} é {moeda.metade(p)}')
+print(f'O dobro de {p} é {moeda.dobro(p)}')
+print(f'Aumentando 10% de {p} é {moeda.aumentar(p, 10)}')
+print(f'Diminuindo 10% de {p} é {moeda.diminuir(p, 10)}')
