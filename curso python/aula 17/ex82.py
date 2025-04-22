3754b1493fdd6ed54d8fc4ee1b7ec08b41dcf529
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-22 14:04:55 -0300

    listagem, lista com número par e ímpar

---
+++ curso python/aula 17/ex82.py
@@ -0,0 +1,16 @@
+continuacao = 'S'
+lista = []
+listapar = []
+listaimpar = []
+while continuacao == "S":
+    numero = int (input ("Digite um número \033[34m\033[m : "))
+    lista.append(numero)
+    continuacao = str (input ("Quer continuar [S/N] ? ")).strip().upper()
+    if numero % 2 == 0:
+        listapar.append(numero)
+    else:
+        listaimpar.append(numero)
+print (f"\tA lista completa é {lista}")
+print (f"\tA lista de números pares é {listapar}")
+print (f"\tA lista de números ímpares é {listaimpar}")
+
