7f28bb58f8be7c5ec0fdcc8f7d5cea3eae76ab2a
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-04-22 15:09:36 -0300

    desafio 97

---
+++ python-tayla/aula-20/desafios-aula-20/d097.py
@@ -0,0 +1,9 @@
+def escreva(texto):
+    tamanho = len(texto)
+    print('~' * tamanho)
+    print(texto)
+    print('~' * tamanho)
+
+escreva('   Tayla Lisboa    ')
+escreva('   Careca de saber   ')
+escreva('   Oi   ')
