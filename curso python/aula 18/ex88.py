ec42096e9a63116565194bdbd87f283b2c797c85
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-23 16:52:43 -0300

    números mega sena

---
+++ curso python/aula 18/ex88.py
@@ -0,0 +1,27 @@
+from random import randint
+lista = list()
+jogos = list()
+print ('-=' * 30)
+print ('      \033[35mMEGA SENA\033[m     ')
+print ('-=' * 30)
+quantidade = int (input('\033[34mQuantos jogos você quer que eu sorteei ?\033[m '))
+total = 1
+while total<= quantidade:
+    contador = 0
+    while True:
+        numero = randint (1,60)
+        if numero not in lista:
+            lista.append(numero)
+            contador += 1
+        if contador >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    total += 1
+print ('-=' * 4, f'sorteando {quantidade} jogos','-=' * 4 )
+print (' \033[34mOs números sorteados foram\033[m')
+for indice , lista in enumerate(jogos):
+    print (f'Jogo {indice+1}: {lista}')
+
+
