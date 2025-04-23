de31a2602071a6bcd20d7415d0b64bef18f38dc3
annaluiza777 <anna.araujo@youxlab.com.br>
2025-04-23 16:08:13 -0300

    exercicios  aula 18

---
+++ aulas_18.py/exercicios_aula18.py
@@ -0,0 +1,33 @@
+teste = list()
+teste.append('Gustavo')
+teste.append(40)
+galera = list()
+galera.append(teste[:])
+teste [0] = 'Maria'
+teste[1]= 22
+galera.append(teste[:])
+print(galera)
+
+
+
+dado = list()
+
+totmai = totmen = 0
+
+for c in range(0, 3):
+
+  dado.append(str(input('Nome:')))
+
+dado.append(int(input('Idade: ')))
+
+galera.append(dado[:])
+
+dado.clear()
+for p in galera:
+    if p[1]>=21:
+        print(f'{p[0]} é maior de idade')
+        totmai += 1
+    else:  
+        print(f'{p[0]} e menor de idade')
+        totmai+=1
+print(f'Temos (totmai maiores e (totmen) menores de idade.')
