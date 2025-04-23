7bbcad847588498cf852f8aaff8cd41af8a4b5c1
Keverson Teodoro <keverson.teodoro@youxlab.com.br>
2025-04-23 15:09:59 -0300

    desafio-102

--- aula-021_funcoespt2/desafio-102.py
+++ aula-021_funcoespt2/desafio-102.py
@@ -1,18 +1,24 @@
-def fatorial(num, show=0):
+def fatorial(num, show):
     f =1
     for c in range(num, 0, +1):
         f *= c
     if show == True:
         for i in range(num, 0, -1):
             print(f'x {i} ', end='')
+        return f
     else:
         if show == False:
             print('FIM')
-
-    return f
+            return f
             
 
 
 num = int(input('Digite um valor: '))
-show = bool(input('Quer mostrar o calculo do fatoria? (True ou False)'))
-print(f'{fatorial(num, show,),}')
+show = str(input('Quer mostrar o calculo do fatoria? (True ou False)')).upper()
+if show == 'T':
+    show = bool(True)
+else:
+    if show == 'N':
+        show = bool(False)
+
+print(f'{fatorial(num, show,)}')
