752ff49e96b8692127dd175a30a236bab1f86fe6
Maria Luiza <maria.silva@youxlab.com.br>
2025-04-23 17:04:39 -0300

    desafio do cadastro de jogador de futebol

---
+++ desafiosaula19/desafio93.py
@@ -0,0 +1,29 @@
+from time import sleep
+jogador = {}
+lista = []
+soma = 0
+jogador['nome'] = input('Nome: ')
+jogador['partidas'] = int(input('Quantas partidas você jogou?: '))
+for c in range(0,jogador['partidas']):
+    gol = int(input(f'Quantos gols você fez na partida {c+1}?: '))
+    soma+=gol
+    lista.append(gol)
+    jogador['gol'] = lista
+    jogador['total'] = soma
+print('-='*20)
+for k, v in jogador.items():
+    print(f'O campo \033[1;35m{k}\033[m tem o valor {v} ')
+    sleep(1)
+print('-='*20)
+sleep(1)
+partida = jogador['partidas']
+nome = jogador['nome']
+print('O \033[36mjogador {}\033[m jogou \033[34m{} partidas\033[m'.format(nome, partida))
+for i, v in enumerate(jogador['gol']):
+    print(f'   → Na partida {i}, fez {v} gols ')
+    sleep(1)
+print(f'Foram um total de \033[1;36m{soma} GOLS!\033[m ')
+
+
+    
+
