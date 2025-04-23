ed9d5e92daeb415ff7f31491d341a4912246b1ba
marianne0614 <marianne.lima@youxlab.com.br>
2025-04-23 17:13:17 -0300

    desafio 84 modificada

--- 04-estudo_logica_python/aula18/desafio84.py
+++ 04-estudo_logica_python/aula18/desafio84.py
@@ -1,10 +1,10 @@
-lista=[]
-dados=[]
-maior = 0
-menor = 0
+lista = []
+dados = []
+maior = menor = 0
 while True:
     dados.append(str(input("\033[38;5;213mNome:\033[0m ")).title().strip())
-    dados.append(float(input("\033[35;5;213mPeso:\033[0m" )))
+    dados.append(float(input("\033[35;5;213mPeso:\033[0m ")))
+
     if len(lista) == 0:
         menor = maior = dados[1]
     else:
@@ -12,18 +12,26 @@ while True:
             maior = dados[1]
         if dados[1] < menor:
             menor = dados[1]
+
     lista.append(dados[:])
     dados.clear()
-    r = str(input('deseja continuar? [S/N]')).upper()
-    if r in 'N':
+
+    r = input('Deseja continuar? [S/N] ').strip().upper()
+    if r == 'N':
         break
-print(f'ao todo vc cadastrou {len(lista)} pessoas.')
-print(f'Maior peso foi {maior} . Peso de ', end='')
+print(f'\nAo todo foram cadastradas {len(lista)} pessoas.')
+
+print(f'\033[32mMaior peso foi {maior} Kg. Peso de\033[0m ', end='')
 for p in lista:
     if p[1] == maior:
-        print(p[0], end=' ')
+        print(f'{p[0]}', end=' ')
 print()
-print(f'Menor peso foi {menor}. Peso de ', end='')
+print(f'\033[34mMenor peso foi {menor} Kg. Peso de\033[0m ', end='')
 for p in lista:
     if p[1] == menor:
-        print(p[0], end=' ')
+        print(f'{p[0]}', end=' ')
+print()
+ordenada = sorted(lista, key=lambda x: x[1])
+print('\n\033[36mPessoas da mais leve para a mais pesada:\033[0m')
+for pessoa in ordenada:
+    print(f'{pessoa[0]} - {pessoa[1]} Kg')
