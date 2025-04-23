4e26ceb78b076812eca79a39eccac822fee50dd4
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-23 19:53:11 -0300

    Aula 14

--- aula-python/aula-14.py
+++ aula-python/aula-14.py
@@ -5,3 +5,13 @@
 #     resposta= str(input('Quer continuar?: ')).upper().capitalize()
 # print('Fim')
 
+numero= 1
+par=impar= 0
+while numero != 0:
+        numero= int(input('Digite um valor: '))
+if numero != 0:
+        if numero % 2== 0:
+         par += 1
+        else:
+            impar +=1
+print(f'Você digitou {par} números pares e {impar} números ímpares!')
