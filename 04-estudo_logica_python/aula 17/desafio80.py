45a4b4ecb082651394ec7600b05f1c1263a66d54
marianne0614 <marianne.lima@youxlab.com.br>
2025-04-23 14:12:07 -0300

    desafio 80 MODIFICADO

--- 04-estudo_logica_python/aula 17/desafio80.py
+++ 04-estudo_logica_python/aula 17/desafio80.py
@@ -1,17 +1,53 @@
+
 lista=[]
+
+ 
+
 for i in range(0,5):
+ 
+
     numero=int(input('Digite um valor:'))
+ 
+
     if i == 0 or numero> lista[-1]:
+ 
+
         lista.append(numero)
-        print('ADICIONADO NO FINAOL DA LISTA')
+ 
+
+        print('ADICIONADO NA LISTA')
+ 
+
     else:
+ 
+
         posiçao=0
+ 
+
         while posiçao < len(lista):
-            if numero<= lista[posiçao]:
+ 
+
+           if numero<= lista[posiçao]:
+ 
+
               lista.insert(posiçao, numero)   
-              print(f'foi adicionado na posiçao {posiçao}')
+ 
+
+              #print(f'foi adicionado na posiçao {posiçao}')
+ 
+
               break
-            posiçao+= 1
+ 
+
+           posiçao+= 1
+ 
+
+
+ 
 
 print(f' os valores digitados em ordem sao{lista}')            
+ 
+
         
+
+  
