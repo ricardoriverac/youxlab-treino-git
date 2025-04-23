aa13d131ac855b472a3466bd3e4c8e5845b88b8a
Marcos <marcos.garcia@youxlab.com.br>
2025-04-23 15:04:53 -0300

    Fiz o código dos dados (91)

--- aulas_python/desafios_aula_19/atividade91.py
+++ aulas_python/desafios_aula_19/atividade91.py
@@ -1,14 +1,29 @@
-import random
-Dic = dict()
-listaN = []
-listaJ = []
-print()
-for c in range (1,5):
-    escolha = random.randint(0,6)
-    listaN.append(escolha)
-    print(f'Jogador {c} tirou {escolha} no dado')
-Dic['Jogador '] = listaJ
-Dic['Números'] = listaN
+from random import randint
+from time import sleep
+from operator import itemgetter
 
+Jogo = {'Jogador 1': randint(1, 6),
+        'Jogador 2': randint(1, 6),
+        'Jogador 3': randint(1, 6),
+        'Jogador 4': randint(1, 6)}
+ranked = dict()
+
+print('\033[33m=-=\033[m'*30,'\nValores sorteados:')
+
+for jogador, valor in Jogo.items():
+    print(f'O {jogador} tirou {valor} no DADO')
+    sleep(1)
 print('\033[33m=-=\033[m'*30)
-print(Dic)
+ranked = sorted(Jogo.items(), key=itemgetter(1), reverse=True)
+
+print('==========>>> Ranked dos Jogadores <<<==========')
+for i, v in enumerate(ranked):
+    print(f'{i+1}º lugar: {v[0]} com {v[1]}.')
+    sleep(1)    
+print()
+
+
+
+
+
+
