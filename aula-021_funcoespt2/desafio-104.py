2250eeaa6ba21f0dc2e965f61f6ae7ce335011c3
Keverson Teodoro <keverson.teodoro@youxlab.com.br>
2025-04-23 15:11:17 -0300

    desafio-104

---
+++ aula-021_funcoespt2/desafio-104.py
@@ -0,0 +1,15 @@
+def leiaInt(num):
+    if num.isnumeric():
+        num = int(num)
+    if isinstance(num, str):
+        raise ValueError (f'o valor valor "{num}" não é válido, apenas valores inteiros')
+    else:
+        return print(f'Você digitou o valor {num}')
+
+
+
+try:
+   n = leiaInt(input('Digite um valor: '))
+
+except ValueError as erro:
+    print(f'Erro {erro}')
