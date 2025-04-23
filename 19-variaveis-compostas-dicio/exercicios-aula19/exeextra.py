332e80d27b8a9d24f5707c7941640559a2330815
juliocesar <juliogoiabinha060@gmail.com>
2025-04-23 15:24:30 -0300

    exercicio proposto pelo rodrigo

--- 19-variaveis-compostas-dicio/exercicios-aula19/exeextra.py
+++ 19-variaveis-compostas-dicio/exercicios-aula19/exeextra.py
@@ -2,19 +2,23 @@
 from datetime import date
 atual = date.today().year
 trabalhador = {
-    'nome':  str(input('NOME: ')),
+    'nome':  str(input('Nome: ')),
+    'sexo': str(input('Sexo [M/F] ')).upper(),
     'idade': int(input('Ano de nascimento: ')),
     'ctps': int(input('Carteira de trabalho (0 não tem): ')) 
 }
+print(trabalhador)
 if trabalhador['ctps'] == 0:
-    print(trabalhador)
     for k , v in trabalhador.items():
         print(f'{k} tem o valor {v}')
 else:
-    trabalhador['contratação'] = int(input('Ano da contratação: '))
-    trabalhador['salario'] = float(input('Salarío: '))
-    idades = atual - trabalhador['idade']
-    tempotrabalho = atual - trabalhador['contratação']
-    if idades >= 65 and tempotrabalho >= 20:
-        print('Voce esta apto a aposentar')
-    print('Voce nao esta apto a aposentar!')
+        trabalhador['salario'] = float(input('Salarío: '))
+        trabalhador['contratação'] = int(input('Ano da contratação: '))
+        idades = atual - trabalhador['idade']
+        tempotrabalho = atual - trabalhador['contratação']
+        if idades >= 65 and tempotrabalho >= 20 and trabalhador['sexo'] == 'M':
+            print('Você Homem já pode se aposentar')
+        elif idades >= 62 and tempotrabalho >= 15 and trabalhador['sexo'] == 'F':
+            print('Você mulher já pode se aposentar')
+        else:
+            print('Voce nao esta apto a aposentar!')
