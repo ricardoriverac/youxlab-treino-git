7c3f4fa796c3bc8b2843f57281da8aad0bed3910
Maria Luiza <maria.silva@youxlab.com.br>
2025-04-23 14:51:13 -0300

    desafio dos sorteios de dados

---
+++ desafiosaula19/desafio91.py
@@ -0,0 +1,25 @@
+from random import randint
+from time import sleep
+from operator import itemgetter 
+jogo = {'Jogador 1': randint(1, 6),
+        'Jogador 2': randint(1, 6), 
+        'Jogador 3': randint(1, 6),
+        'Jogador 4': randint(1, 6)}
+ranking = []
+
+print('-='*20)
+print('{:^40}'.format('\033[1;35mVALORES SORTEADOS:\033[m '))
+print('-='*20)
+for k, v in jogo.items():
+        print(f'{k} tirou {v} no dado')
+        sleep(1)
+        
+print('-='*20)
+print('{:^40}'.format('\033[1;34mRANKING DOS JOGADORES\033[m'))
+print('-='*20)        
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+for i, v in enumerate(ranking):
+        print(f'Em {i+1}º lugar: {v[0]} com {v[1]}')
+        sleep(1)
+
+        
