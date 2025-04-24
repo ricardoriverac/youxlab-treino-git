0876771de783bca389b55e88e4978d982954caa5
annaluiza777 <anna.araujo@youxlab.com.br>
2025-04-24 13:11:06 -0300

    desafio 86 aula 18

---
+++ aulas_18.py/desafio086_aula18.py
@@ -0,0 +1,10 @@
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input(f'Digite o valor de [{l},{c}]: '))
+print('-=' * 30)
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}]', end='')
+    print()
+print('\033[38;5;198m✩' * 1)
