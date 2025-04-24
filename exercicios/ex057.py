7808b9802eb5f384a281c497c69191e3141e769d
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-23 21:45:00 -0300

    Exercício 57

---
+++ exercicios/ex057.py
@@ -0,0 +1,19 @@
+m= 'M'
+f= 'F'
+sexo= ''
+while sexo != m:
+    sexo= str(input('Informe seu sexo: ')).upper() .capitalize()
+    if sexo == m:
+       print('Seu sexo é masculino!')
+    else:
+        sexo == f
+
+if sexo != f:  
+    if sexo == f:
+        print('Seu sexo é feminino!')
+    else:
+        sexo != f
+        sexo != m
+        print('INVÁLIDO!')
+                
+
