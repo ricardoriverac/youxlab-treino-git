e8e12e46d713b192387908f9360e0ed14cb78b48
VictorGabrielBarros <victor.barros@youxlab.com.br>
2025-04-24 14:13:06 -0300

    Desafio80

---
+++ 17_lista/Desafios/Desafio80.py
@@ -0,0 +1,16 @@
+lista = []
+for c in range(0,5):
+    n = int(input("Digite um valor: "))
+    if c == 0 or n > lista[-1]:
+        lista.append(n)
+        print("Adicionado ao final da lista")
+    else:
+        pos = 0
+        while pos < len(lista):
+            if n <= lista[pos]:
+                lista.insert(pos,n)
+                prnt(f"Adicionado na posiçao {pos}")
+                break
+            pos = + 1
+print("======"*8)
+print(f"os valores digitados em ordem foram {lista}")
