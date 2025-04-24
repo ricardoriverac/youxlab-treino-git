2f84d4023e5c20520724ddc8f65b47c6770ede38
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-04-24 16:35:22 -0300

    desafio 103

---
+++ python-tayla/aula-21/desafios-aula-21/d103.py
@@ -0,0 +1,15 @@
+def ficha(nome = 'desconhecido', gol = 0 ):
+    print(f'O jogador {nome} fez {gol} gol(s) no campeonato')
+
+jogador = str(input('Nome do Jogador: '))
+gols = str(input('Número de Gols: '))
+
+if gols.isnumeric():
+    gols = int(gols)
+else:
+    gols = 0
+    
+if jogador.strip() == '':
+    ficha(gol = gols)
+else:
+    ficha(jogador, gols)
