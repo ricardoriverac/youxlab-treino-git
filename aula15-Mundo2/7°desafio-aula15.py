05ed185ad8c00605dfcc0eeea0225b2d434f5b28
Youx <sophia.carvalho@youxlab.com.br>
2025-04-24 13:32:53 -0300

    Mundo3

---
+++ aula15-Mundo2/7°desafio-aula15.py
@@ -0,0 +1,6 @@
+#MAIOR E MENOR NÚMERO
+from random import randint
+numeros= (randint(1,10),randint(1,10),randint(1,10),randint(1,10),randint(1,10),)
+print(f'Os números sorteados foram: {numeros}')
+print(f'O maior número é o {max(numeros)}')
+print(f'O menor número é o {min(numeros)}')
