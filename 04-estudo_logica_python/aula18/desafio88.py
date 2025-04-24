3607648cf19a7035b56c7bb3757b887ce9bbff99
marianne0614 <marianne.lima@youxlab.com.br>
2025-04-24 14:30:08 -0300

    desafio 88

---
+++ 04-estudo_logica_python/aula18/desafio88.py
@@ -0,0 +1,22 @@
+from random import randint
+from time import sleep
+mega=[]
+numerosjogo = int(input('Quantos jogos vc deseja fazer:'))
+for valores in range (numerosjogo):
+    jogo=[]
+    for valores1 in range(6):
+        sorteio= randint
+        sorteio = randint(1, 60)
+
+        if sorteio in jogo:
+            sorteio = randint(1, 60)
+            jogo.append(sorteio)
+        else:
+            jogo.append(sorteio)
+
+    jogo.sort()
+    mega.append(jogo[:])
+    print(f'Jogo {valores + 1}: {mega[valores]}')
+    sleep(0.5)
+print()
+print(" ----------------BOA SORTE--------------  ")
