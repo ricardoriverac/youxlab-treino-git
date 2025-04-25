59ae409ed3f952eb01f8282eef51c94e3f6e98e3
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-25 19:31:21 -0300

    atividades aula 20

---
+++ curso python/aula 20/aula20-atividade.py
@@ -0,0 +1,25 @@
+#       DEF
+# def mostralinha(): # ele não executa essa linha, aqui eu jogo a variavél pra dentro do def para usar depois
+#    print('-------------')
+# mostralinha() # print ('-' * 30)
+# print ('    sistema de alunos   ')
+#def lin():
+ #   print ('-' * 30)
+
+# programa principal
+'''''lin()
+print ('    CURSO EM VÍDEO  ')
+lin()
+print ('    APRENDA PYTHON  ')
+lin()
+print ('    GUSTAVO GUANABARA   ')
+lin()'''''
+
+# segunda parte
+''''def titulo(txt):
+    print('-' * 30)
+    titulo(txt)
+    print('-' * 30)
+titulo ('   CURSO EM VÍDEO  ')''''
+
+
