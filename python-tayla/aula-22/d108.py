6dc3eee50dd7585686483808ef1ade0845029b37
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-04-25 17:17:43 -0300

    desafio 108

--- python-tayla/aula-22/d108.py
+++ python-tayla/aula-22/d108.py
@@ -2,6 +2,7 @@ from ex108 import moeda
 
 pergunta = float(input('Digite o preço: R$'))
 
+moeda.linha()
 print(f'A metade de {moeda.moeda(pergunta)} é {moeda.moeda(moeda.metade(pergunta))}')
 print(f'O dobro de {moeda.moeda(pergunta)} é {moeda.moeda(moeda.dobro(pergunta))}')
 print(f'Aumentando 10% temos {moeda.moeda(moeda.aumentar(pergunta, 10))}')
--- python-tayla/aula-22/ex108/moeda.py
+++ python-tayla/aula-22/ex108/moeda.py
@@ -1,16 +1,17 @@
-def aumentar(numero, x):
+def aumentar(numero = 0, x = 0):
     numero += (numero * (x / 100))
     return numero
 
-def metade(numero):
+def metade(numero = 0):
     numero /= 2
     return numero
 
-def dobro(numero):
+def dobro(numero = 0):
     numero *= 2
     return numero
 
 def linha():
     print('-=' * 15)
 
-def moeda(numero):
+def moeda(numero = 0, moeda = 'R$'):
+    return f'{moeda}{numero:.2f}'.replace('.', ',')
