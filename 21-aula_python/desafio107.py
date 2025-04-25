b630d95b5bed29f22e4a348e54ecf61cbbadc030
GustavoValacio <gustavoseabra2007@gmail.com>
2025-04-25 12:21:10 -0300

    Desafio 107 importação

---
+++ 21-aula_python/desafio107.py
@@ -0,0 +1,10 @@
+import Desafio107
+
+preco = float(input('Digite o preço: R$ ' ))
+print(f'A metade de {preco} é {Desafio107.metade(preco)}')
+
+print(f'O dobro de {preco} é {Desafio107.dobro(preco)}')
+
+print(f'Com o aumento de 10% fica {Desafio107.aumentar(preco,10)}')
+
+print(f'')
