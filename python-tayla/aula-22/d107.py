bee20bb0a00644e864beadb8b61e3e88e5a3fcf7
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-04-25 17:06:50 -0300

    desafio 107

--- python-tayla/aula-22/d107.py
+++ python-tayla/aula-22/d107.py
@@ -3,7 +3,7 @@ from ex107 import moeda
 pergunta = float(input('Digite um preço: R$'))
 
 moeda.linha()
-print(f'A metade de {pergunta} é {moeda.metade(pergunta)}')
-print(f'O dobro de {pergunta} é {moeda.dobro(pergunta)}')
+print(f'A metade de R${pergunta} é R${moeda.metade(pergunta)}')
+print(f'O dobro de R${pergunta} é R${moeda.dobro(pergunta)}')
 print(f'Aumentando 30% temos {moeda.aumentar(pergunta, 30)}')
 print(f'Reduzindo 20% temos {moeda.diminuir(pergunta, 20)}')
---
+++ python-tayla/aula-22/ex108/moeda.py
@@ -0,0 +1,16 @@
+def aumentar(numero, x):
+    numero += (numero * (x / 100))
+    return numero
+
+def metade(numero):
+    numero /= 2
+    return numero
+
+def dobro(numero):
+    numero *= 2
+    return numero
+
+def linha():
+    print('-=' * 15)
+
+def moeda(numero):
