58d908216b1774881bad7e0c45bb30ee7d2c8bc7
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-26 14:05:49 -0300

    moeda

---
+++ curso python/aula 22/exercicio 107/moeda.py
@@ -0,0 +1,11 @@
+def aumentar(preco, porcentagem):
+    return preco + (preco * porcentagem / 100)
+
+def diminuir(preco, porcentagem):
+    return preco - (preco * porcentagem / 100)
+
+def dobro(preco):
+    return preco * 2
+
+def metade(preco):
+    return preco / 2
