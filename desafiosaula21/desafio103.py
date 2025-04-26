ad6b1ed6cbf6bf88d44760af1ce3689a9359eee4
Maria Luiza <maria.silva@youxlab.com.br>
2025-04-26 20:44:46 -0300

    desafio validando ficha de jogador

--- desafiosaula21/desafio103.py
+++ desafiosaula21/desafio103.py
@@ -14,5 +14,25 @@ if nome.strip() == '':
 else:
     ficha(nome, gols)
 
+
+
+
+
+
+
+def ficha(jogador='DESCONHECIDO', gol=0):
+    print(f'O jogador {jogador} fez {gol} gol(s) ')
     
     
+nome = input('Nome: ')
+gols = input('Quantos gols fez?: ')
+
+if gols.isnumeric():
+    int(gols)
+else:
+    gols = 0
+    
+if nome.strip() == '':
+    ficha(gol = gols)
+else:
+    ficha(nome, gols)
