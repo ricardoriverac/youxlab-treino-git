bbf83ed21976d66477a04891421ead901e176aff
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-04-27 20:33:42 -0300

    Pratica da aula

---
+++ aula016/Teste.py
@@ -0,0 +1,26 @@
+lanche=('Arroz','feijao','Morango','Abacaxi')
+
+#for cont in range(1,len(lanche)):
+   # print(lanche[cont])
+
+for posi,cont in enumerate(lanche):
+    print(f'{cont} na posição {posi}')
+
+#for cont in lanche:
+    #print(f'{cont}')
+
+#print(sorted(lanche))
+#print(lanche)
+
+# a =(1,2,9,4,5)
+# b=(6,7,8,9,10)
+# c = b + a
+
+# #print(c.count(9))
+# print(c)
+# print(c.index(9))
+
+
+# pessoa=('Nicolas',17,'M',88)
+# del(pessoa)
+# print(pessoa)
