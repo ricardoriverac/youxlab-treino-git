1c20ac3c485a4b122eb4ad2ccdc5d2e34aa25557
GustavoValacio <gustavoseabra2007@gmail.com>
2025-04-27 19:52:33 -0300

    Desafio 114 e correção de nome do ex 113

--- 22-aula_python/Desafio113.py
+++ 22-aula_python/Desafio113.py
@@ -1,11 +1,30 @@
-import urllib
-import urllib.error
-import urllib.request
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except(ValueError, TypeError):
+            print('\033[0;31mERRO! Digite um número inteiro válido\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\n\33[0;31mUsuário preferiu não digitar esse número.\33[m')
+            return 0
+        else:
+            return n
 
 
-try:
-    site = urllib.request.urlopen('http://www.pudim.com.br')
-except urllib.error.URLError:
-    print('Nõa funcionou, site não esta acessivel.')
-else:
-    print('Deu certo, o site está acessive.l')
+def leiaFloat(msg):
+    while True:
+        try:
+            n = str(input(msg))
+            n = float(n.replace(',', '.'))
+        except(ValueError, TypeError):
+            print('\33[0;31mERRO! Digite um número real válido\33[m')
+            continue
+       
+        else:
+            return n
+
+
+n1 = leiaInt('Digite um número inteiro: ')
+n2 = leiaFloat('Digite um número real: ')
+print(f'O valor inteiro digitado foi {n1} e o real foi {n2}')
