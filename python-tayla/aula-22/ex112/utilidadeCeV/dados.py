6dbbfea7f22d63ff06b73efff9550edf3d5c4d6e
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-04-27 15:44:41 -0300

    pacote desafio 112

--- python-tayla/aula-22/ex112/utilidadeCeV/dados.py
+++ python-tayla/aula-22/ex112/utilidadeCeV/dados.py
@@ -4,7 +4,7 @@ def leiaDinheiro(mensagem):
     if '.' in leiaValor or leiaValor.isnumeric():
         leiaValor = float(leiaValor)
         return leiaValor
-    else:
+    else: 
         while True:
             print(f'\033[1;31mERRO! {leiaValor} é um preço inválido\033[m')
             leiaValor = str(input(mensagem)).replace(',', '.')
