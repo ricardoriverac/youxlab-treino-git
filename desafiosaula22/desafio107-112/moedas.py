dc10475b51b6bf96692feaaf56e9f7da512ea985
Maria Luiza <maria.silva@youxlab.com.br>
2025-04-27 19:10:17 -0300

    desafio formatando moedas em python

--- desafiosaula22/desafio107-112/moedas.py
+++ desafiosaula22/desafio107-112/moedas.py
@@ -18,3 +18,14 @@ def metade(preço=0, formato = False):
 def moeda(preço=0, moeda='R$'):
     True
     return f'{moeda}{preço:.2f}'.replace('.', ',')
+
+def resumo(preço, aumento, redução):
+    print('-'*40)
+    print('\033[1;35mRESUMO DO VALOR\033[m'.center(40))
+    print('-'*40)
+    print(f'Preço analisado: \t{moeda(preço)}')
+    print(f'Dobro do preço: \t{dobro(preço, True)}')
+    print(f'Metade do preço: \t{metade(preço, True)}')
+    print(f'{aumento}% de aumento: \t{aumentar(preço, aumento, True)}')
+    print(f'{redução}% de redução: \t{diminuir(preço, redução, True)}')
+    print('-'*40)
