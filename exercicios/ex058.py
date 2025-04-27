4b84e6c35f442c403a7ea40fcc432b785387172c
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-04-26 21:17:54 -0300

    Exercício 58

--- exercicios/ex058.py
+++ exercicios/ex058.py
@@ -1,12 +1,17 @@
-import random
-
-computador= random.randint(1, 10)
-numero= int(input('Palpite, um número de 1 a 10: '))
-
-while numero != computador:
-    if computador == numero:
-        print('Você venceu!')
-        if computador != numero:
-            print('Você perdeu!')
-        
-    
+from random import randint
+computador= randint(0, 10)
+print('Olá sou seu computador... pensei em um número entre 0 e 10.')
+print('Consegue adivinhar?')
+acertou= False
+palpites= 0
+while not acertou:
+    jogador= int(input('Qual é o seu palpite?: '))
+    palpites += 1
+    if jogador == computador:
+        acertou = True 
+    else:
+        if jogador < computador:
+            print('Um pouco mais...Tente novamente.')
+        elif jogador > computador:
+            print('Um pouco menos...tente novamente.')
+print(f'Acertou com {palpites} tentativas!')
