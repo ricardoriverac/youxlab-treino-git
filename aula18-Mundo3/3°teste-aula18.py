3570ab9900ab92b11fae1d374704e9ab04d0f03b
Youx <sophia.carvalho@youxlab.com.br>
2025-04-26 23:13:17 -0300

    Mundo3

---
+++ aula18-Mundo3/3°teste-aula18.py
@@ -0,0 +1,10 @@
+#MATRIZ
+matriz=[[0,0,0],[0,0,0],[0,0,0]]
+for linha in range(0,3):
+    for coluna in range(0,3):
+        matriz[linha][coluna]=int(input(f'Digite um valor para {[linha]}, {[coluna]}: '))
+print('Sua matriz:')
+for linha in range(0,3):
+    for coluna in range(0,3):
+        print(f'[{matriz[linha][coluna]}]', end=' ')
+    print()
