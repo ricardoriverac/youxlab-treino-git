9b4eb6d7853b7a70320631d9d01deba4b6e64fb4
Marcos <marcos.garcia@youxlab.com.br>
2025-04-27 12:34:27 -0300

    Fiz o código 107

---
+++ aulas_python/desafios_aula_22/moeda.py
@@ -0,0 +1,16 @@
+def metade(n):
+    return n /2
+
+def dobro(n):
+    return n * 2
+
+def aumentar(preco, porcentagem):
+    aumento = preco * (porcentagem / 100)
+    novo_preco = preco + aumento
+    return novo_preco
+
+def diminuir(preco, porcentagem):
+    diminuicao = preco * (porcentagem / 100)
+    novo_precoS = preco - diminuicao
+    return novo_precoS
+
