1311a910b0cb8f2b9ad3b59071f39da2fdac3b78
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-04-27 15:43:46 -0300

    desafio 112

---
+++ python-tayla/aula-22/d112.py
@@ -0,0 +1,4 @@
+from ex112.utilidadeCeV import dados, moeda
+
+valor = dados.leiaDinheiro('Digite um valor: R$')
+moeda.resumo(valor, 50, 23)
---
+++ python-tayla/aula-22/ex112/utilidadeCeV/dados.py
@@ -0,0 +1,13 @@
+def leiaDinheiro(mensagem):
+    leiaValor = str(input(mensagem)).replace(',', '.')
+
+    if '.' in leiaValor or leiaValor.isnumeric():
+        leiaValor = float(leiaValor)
+        return leiaValor
+    else:
+        while True:
+            print(f'\033[1;31mERRO! {leiaValor} é um preço inválido\033[m')
+            leiaValor = str(input(mensagem)).replace(',', '.')
+            if '.' in leiaValor or leiaValor.isnumeric():
+                leiaValor = float(leiaValor)
+                return leiaValor
