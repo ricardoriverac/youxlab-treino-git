2e64cd71b7bdf6ee172ac72a7d646cd9d1f86c97
fabricioyoux <fabricio.genesi@youxlab.com.br>
2025-04-27 13:51:30 -0300

    atividade 85

---
+++ 18-Listas_parte(2)/ex085.py
@@ -0,0 +1,18 @@
+numero = list()
+
+for n in range(0, 7):
+    numero.append(float(input('Digite um númuro')))
+
+if list % 2 == 0:
+   print(f'Esses números são pares: ')
+else:
+    print(f'Esses são números ímpares')
+
+
+# while n != 0:
+#     n = int(input('Digite um valor: '))    
+#     if  2 == 0:
+#         par = +1
+#     else:  
+#         impar += +1
+# print(f'Vo ce digitou {par} números pares e {impar} numeros impares!')
