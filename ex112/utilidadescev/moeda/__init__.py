2176fbc98a02b7c907c218ae8f6978751ea184b0
GustavoValacio <gustavoseabra2007@gmail.com>
2025-04-27 19:13:51 -0300

    Desafio112

---
+++ ex112/utilidadescev/moeda/__init__.py
@@ -0,0 +1,31 @@
+def aumentar(preço = 0, taxa = 0, formato=False):
+    res = preço + (preço * taxa/100)
+    return res if formato is False else moeda(res)
+
+
+def diminuir(preço = 0, taxa = 0, formato=False):
+    res = preço - (preço * taxa/100)
+
+
+def dobro(preço = 0, formato=False):
+    res = preço * 2
+    return res if formato is False else moeda(res)
+
+
+def metade(preço = 0, formato=False):
+    res = preço / 2
+    return res if formato is False else moeda(res)
+
+def moeda(preço = 0,moeda = 'R$'):
+    return f'{moeda}{preço:>2.2f}'.replace('.', ',')
+
+def resumo(preço=0, taxaa=10, taxar=5):
+    print('='*34)
+    print('Resumo do valor'.center(30))
+    print('='*34)
+    print(f'O preço foi analisado:   \t{moeda(preço)}')
+    print(f'Preço Dobrado:           \t{dobro(preço, True)}')
+    print(f'A metade do preço:       \t{metade(preço, True)}')
+    print(f'Com {taxaa}% de aumento:        \t{aumentar(preço, taxaa, True)}')
+    print(f'{taxar}% de redução:            \t{diminuir(preço, taxar, True)}')
+    print('='*34)
---
+++ teste112.py
@@ -0,0 +1,7 @@
+from ex112.utilidadescev import moeda
+from ex112.utilidadescev import dado
+
+preco = dado.leiagrana('Coloque o valor: R$')
+moeda.resumo(preco, 20, 12)
+
+
