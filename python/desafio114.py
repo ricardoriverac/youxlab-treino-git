7d9afb25464c380d1397f3a1d4cba467e3462147
alexia-assis <alexiaassis.youxlab.com.br>
2025-04-27 12:04:13 -0300

    Fim dos ex python

---
+++ python/desafio114.py
@@ -0,0 +1,9 @@
+import urllib.request
+import urllib.error
+
+try:
+    site = urllib.request.urlopen('http://pudim.com.br')
+except urllib.error.URLError:
+    print('O site Pudim não está acessível no momento.')
+else:
+    print('Consegui acessar o site Pudim com sucesso!')
