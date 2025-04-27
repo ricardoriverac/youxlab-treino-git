aa4b2bcb118bdd914da85674c700506d678f7a4a
VictorGabrielBarros <victor.barros@youxlab.com.br>
2025-04-27 18:26:22 -0300

    Desafio88

---
+++ 18_lista2/Desafio/Desafio88.py
@@ -0,0 +1,12 @@
+from random import randint
+from random import shuffle
+megasena = list(range(0,61))
+print('-' * 30)
+print(f'{"JOGO DA MEGA SENA":^30}')
+print('-' * 30)
+pergunta = int(input('Quantos números quer que eu sorteie? '))
+for aposta in range(1, pergunta+1):
+    print(f'Jogo {aposta}: ', end='')
+    shuffle(megasena)
+    print(megasena[:6])
+    del(megasena[:6])
