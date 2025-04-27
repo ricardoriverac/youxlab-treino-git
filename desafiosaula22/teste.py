7121121cdf448fde8091b3ba248acadb68220c24
Maria Luiza <maria.silva@youxlab.com.br>
2025-04-27 17:33:55 -0300

    teste aula 22

---
+++ desafiosaula22/teste.py
@@ -0,0 +1,7 @@
+import uteis
+
+num = int(input('Digite um valor: '))
+fat = uteis.fatorial(num)
+print(f'O fatorial do número {num} é {fat}')
+print(f'O dobro do número {num} é {uteis.dobro(num)}')
+print(f'O triplo do número {num} é {uteis.triplo(num)}')
