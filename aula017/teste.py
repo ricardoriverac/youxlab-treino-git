d1255de07efb47f369ad34859bbf74b8c8f774f5
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-04-27 20:36:01 -0300

    Praticas da aula

---
+++ aula017/teste.py
@@ -0,0 +1,52 @@
+##comidas=['arroz','feijão','tomate','amendoim']
+
+#comidas.insert(2,'Picanha')
+
+#comidas.append('Abacaxi')
+
+#del comidas[2]
+
+#comidas.pop()
+
+#comidas.remove('tomate')
+
+
+#print(comidas)
+
+##print(comidas)
+
+##valores =list(range(4,11))
+
+#print(valores)
+
+##valores = [1,4,7,9,4,5,2]
+#print(sorted(valores))
+
+#print(sorted(valores,reverse=True))
+
+#valores[3] = 7
+#print(valores)
+
+# #valores =[]
+# for cont in range(1,5):
+#     valores.append(int(input('Digite um valor: ')))
+
+# for c,v in enumerate(valores):
+#      print(f'Na posição {c} encontrei o valor {v}! ')
+# print('Cheguei ao final:')
+
+lista1=[3,4,7,9]
+#lista2 =lista1          #Ligação
+
+lista2=lista1[:]         #Uma copia dos valores
+lista2[2]=1
+
+print(lista1)
+print(lista2)
+
+
+
+
+
+
+
