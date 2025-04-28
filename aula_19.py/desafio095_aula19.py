8b62b7068b577a3c8cf5ff7bae067936a8b82450
annaluiza777 <anna.araujo@youxlab.com.br>
2025-04-28 15:28:38 -0300

    desafio 95 aula 19

---
+++ aula_19.py/desafio095_aula19.py
@@ -0,0 +1,42 @@
+gols = []
+jogador = {}
+time = []
+soma = ''
+while True:
+    jogador['Nome'] = str(input('Qual o nome do Jogador? ')).title().strip()
+    partidas = int(input(f'Quantas partidas {jogador["Nome"]} jogou? '))
+    for c in range(partidas):
+        gols.append(int(input(f'Quantos gols na Partida {c+1} ? ')))
+    jogador['gols'] = gols[:]
+    jogador['total'] = sum(gols)
+    gols.clear()
+    time.append(jogador.copy())
+    while True:
+        perg = str(input('Quer continuar? (S/N) ')).upper()
+        if perg in 'SN':
+            break
+        print('Erro! digite apenas N ou S.')
+    if perg == 'N':
+        break
+print()
+print(f'{"cod ":<4}', end='')
+for i in jogador.keys():
+    print(f'{i:<15}', end='')
+print()
+print()
+for k, v in enumerate(time):
+    print(f'{k:<3} ', end='')
+    for d in v.values():
+        print(f'{str(d):<15}', end='')
+    print()
+while True:
+    resp = int(input('Qual jogador quer saber as informações? [999 para sair]: '))
+    if resp == 999:
+        break
+    elif resp >= len(time):
+        print(f'Nao existe jopgador com número {resp}.')
+    else:
+        print('carregando...')
+        print(f'O Jogador {time[resp]["Nome"]} fez {time[resp]["total"]} gols.')
+        for k, v in enumerate(time[resp]['gols']):
+            print(f'Na partida {k+1}, fez {v} gols.')
