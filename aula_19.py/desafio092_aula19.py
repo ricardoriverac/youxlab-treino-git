5d86ca742646aae158e40dc8496fef0ad2ba8841
annaluiza777 <anna.araujo@youxlab.com.br>
2025-04-28 13:49:53 -0300

    desafio 92 aula 19

---
+++ aula_19.py/desafio092_aula19.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+lista = []
+dados = {}
+dados['Nome'] = str(input('nome: ')).title().strip()
+ano = int(input('Digite o ano de nascimento: '))
+dados['Idade'] = datetime.now().year - ano
+ctps = int(input('Digite a carteira de trabalho (0 não tem): '))
+if ctps != '0':
+    dados['Ctps'] = ctps
+    dados['Ano de Contratação'] = int(input('Digite o ano de contratação: '))
+    dados['Salário'] = float(input('Digite o Salário atual: '))
+    dados['aposentadoria'] = dados['Idade'] + dados['Ano de Contratação'] + 35 - datetime.now().year
+lista.append(dados.copy())
+print()
+for k, v in dados.items():
+    print(f'- {k} é {v}.')
