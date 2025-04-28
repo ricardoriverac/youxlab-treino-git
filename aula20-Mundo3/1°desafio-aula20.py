9ce7814c1e168e1aecf9e3bf2775103d0cea6579
Youx <sophia.carvalho@youxlab.com.br>
2025-04-28 08:35:06 -0300

    Mundo3

---
+++ aula20-Mundo3/1°desafio-aula20.py
@@ -0,0 +1,9 @@
+print('    CONTROLE DE TERRENOS    ')
+def valores(largura, comprimento):
+    area=largura*comprimento
+    print(f'A área do terreno {largura}x{comprimento} é de {area}m²')
+
+largura=float(input('Largura (m): '))
+comprimento=float(input('Comprimento (m): '))
+
+valores(largura, comprimento)
