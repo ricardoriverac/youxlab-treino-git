73719f9560a4df19b7b054f864a64eadc7b27f73
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-04-28 13:37:40 -0300

    salvando desafio 47 da aula 13

--- curso_python/aula07/desafio_5_aula07.py
+++ curso_python/aula07/desafio_5_aula07.py
@@ -1,2 +1,2 @@
 n = int(input('Digite um valor: '))
-print(f'O antecessor de {n} é {n-1} e o sucessor é {n+1}')
+print (f'O antecessor de {n} é {n-1} e o sucessor é {n+1}') 
---
+++ curso_python/aula13/desafio_47_aula13.py
@@ -0,0 +1,4 @@
+print ('Os números pares entre 1 e 50 são: ')
+for numero in range (0, 51):
+    if numero % 2 == 0:
+        print (f'{numero} -> ', end='')
