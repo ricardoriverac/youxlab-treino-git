1d85480f336ff926b365ed2f0e44e53ade67c5f2
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-04-27 21:38:09 -0300

    Deixar bonito com len

---
+++ aula020/ex097.py
@@ -0,0 +1,15 @@
+def escreva(mensagem):
+    tamanho = len(mensagem)
+
+    print('^'*tamanho)
+    print(mensagem)
+    print('^'*tamanho)
+    
+
+
+
+escreva('Nicolas Gabriel Fernandes Maia Moreira ')
+
+escreva('Eu estou bem ')
+
+escreva('Estamos no mes conhecido como abril ')
