6be0af9b7123736b3d29f60148db08069535a7bb
VictorGabrielBarros <victor.barros@youxlab.com.br>
2025-04-27 22:26:44 -0300

    Desafio90

---
+++ 19_Dicionarios/Desafios/Desafio.py
@@ -0,0 +1,5 @@
+aluno = {'nome': str(input('Nome: '))}
+aluno['média'] = float(input(f'Digite a média de {aluno["nome"]}: '))
+aluno['situação'] = 'aprovado' if aluno['média'] >= 7 else 'reprovado' if aluno['média'] <6 else 'recuperação'
+for key, value in aluno.items():
+	print(f'{key}:', value)
