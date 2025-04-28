11bf779cdefbd8740ec32170046a18a62841b209
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-04-28 13:09:26 -0300

    número inteiro e real

--- curso python/aula 22/ex13.py
+++ curso python/aula 22/ex13.py
@@ -5,7 +5,6 @@ def leiaInt(msg):
             return numero
         except ValueError:
             print('\033[0;31mERRO! Por favor, digite um número inteiro válido.\033[m')
-
 def leiaFloat(msg):
     while True:
         try:
@@ -13,7 +12,6 @@ def leiaFloat(msg):
             return numero
         except ValueError:
             print('\033[0;31mERRO! Por favor, digite um número real válido.\033[m')
-
 inteiro = leiaInt('Digite um número inteiro: ')
 real = leiaFloat('Digite um número real: ')
 print(f'\nVocê digitou o número inteiro {inteiro} e o número real {real:.2f}')
