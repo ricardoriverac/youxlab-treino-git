c4c580337599a8bfc94ec0fef43846f391f080ae
annaluiza777 <anna.araujo@youxlab.com.br>
2025-04-28 13:21:50 -0300

    desafio 90 aula 19 atualizado

--- aula_19.py/desafio090_aula19.py
+++ aula_19.py/desafio090_aula19.py
@@ -1,1 +1,12 @@
-lista={}
+pessoa= {}
+pessoa['nome'] = str(input('Digite o Nome: ')).strip().title()
+pessoa['media'] = int(input('Digite a média: '))
+if pessoa['media'] >= 6:
+    pessoa['situação'] = 'Aprovado'
+elif 5 < pessoa['media'] < 6:
+    pessoa['situação'] = 'Recuperação'
+else:
+    pessoa['situação'] = 'Reprovado'
+print(pessoa)
+for k, v in pessoa.items():
+    print(f'O {k} é igual a {v}')
