85a0fd9384e440751b0be748f906c211bcc49080
Nicolas G.F <nicolasgabrielfernandesmm@gmail.com>
2025-04-27 21:36:54 -0300

    Area do terreno

---
+++ aula020/ex096.py
@@ -0,0 +1,10 @@
+def area(largura,comprimento):
+    areaTotal = largura * comprimento
+    print(f'A area de um terreno {largura} X {comprimento} é de {areaTotal}m*' )
+
+
+l =float(input('largura (m): '))
+c = float(input('Comprimento (m): '))
+area(l,c)
+
+
