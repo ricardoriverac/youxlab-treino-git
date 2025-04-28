f9119b81dc661ab07f3175fd0698c67865301e8f
annaluiza777 <anna.araujo@youxlab.com.br>
2025-04-28 13:29:40 -0300

    desafio 91 aula 19 atualizado

--- aula_19.py/desafio090_aula19.py
+++ aula_19.py/desafio090_aula19.py
@@ -1,12 +1,16 @@
-pessoa= {}
-pessoa['nome'] = str(input('Digite o Nome: ')).strip().title()
-pessoa['media'] = int(input('Digite a média: '))
-if pessoa['media'] >= 6:
-    pessoa['situação'] = 'Aprovado'
-elif 5 < pessoa['media'] < 6:
-    pessoa['situação'] = 'Recuperação'
-else:
-    pessoa['situação'] = 'Reprovado'
-print(pessoa)
-for k, v in pessoa.items():
-    print(f'O {k} é igual a {v}')
+from random import randint
+from time import sleep
+from operator import itemgetter
+jogo = {'jogador 1': randint(1, 6),
+        'jogador 2': randint(1, 6),
+        'jogador 3': randint(1, 6),
+        'jogador 4': randint(1, 6)}
+print('valores')
+for k, v in jogo.items():
+    print(f'O {k}, tirou {v} no dado.')
+    sleep(0.5)
+print('ganhadores')
+ranking = sorted(jogo.items(), reverse=True, key=itemgetter(1))
+for k, v in enumerate(ranking):
+    print(f'{k+1}º lugar: {v[0]} com {v[1]}.')
+    sleep(0.5)
