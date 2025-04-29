5bfe1fc789ff1de023d71e61a31f1e0b6818d037
fabricioyoux <fabricio.genesi@youxlab.com.br>
2025-04-29 14:59:44 -0300

    Atividade 95

---
+++ 20-Funções/aula20.py
@@ -0,0 +1,15 @@
+#def soma(a, b):...
+
+#Programa Principal
+#soma(4, 5)
+
+#def contador(* num):
+#    for valor in num:
+#        print(f'{valor} ', end='')
+#contador( 5,4,2 )
+    
+def contador (* num):
+    tam= len(num)
+    print(f'Recebi os valores {num} e são ao todo {tam} números.')
+
+contador(1, 2, 7, 9, 10)
