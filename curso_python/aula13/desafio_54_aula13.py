0a2263d11246114207fe73c77a966491908ac905
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-04-29 17:45:39 -0300

    salvando desafio 54 da aula 13

--- curso_python/aula13/desafio_54_aula13.py
+++ curso_python/aula13/desafio_54_aula13.py
@@ -1,1 +1,15 @@
-nascimento = int(input('Digite o ano de nascimento: '))
+import datetime
+conteMenorDeIdade = 0
+conteMaiorDeIdade =
+for pessoas in range(1, 8):
+    nascimento = int(input('Digite o ano de nascimento: '))
+    anoAtual = datetime.datetime.now().year
+    idade = anoAtual - nascimento
+    if idade < 21:
+        conteMenorDeIdade += 1
+        print (f'Você tem {idade} anos de idade. Você é menor de idade!')
+    elif idade <= 110:
+        conteMenorDeIdade += 1
+        print (f'Você tem {idade} anos de idade. Você é maior de idade!')
+    if idade > 110:
+        print (f'Você tem {idade} anos de idade. Esta pessoa provavelmente já morreu. ')
