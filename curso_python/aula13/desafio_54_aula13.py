ddb6544aba06e4d187b4dcc7ec3a4c2c47b9e6ac
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-04-30 13:46:05 -0300

    salvando desafio 54 da aula 13

--- curso_python/aula13/desafio_54_aula13.py
+++ curso_python/aula13/desafio_54_aula13.py
@@ -7,6 +7,6 @@ for pessoas in range(1, 8):
     idade = anoAtual - nascimento
     if idade < 21:
         conteMenorDeIdade += 1
-    elif idade <= 110:
+    elif idade >= 21:
         conteMaiorDeIdade += 1
 print (f'No total, temos {conteMenorDeIdade} menores de idade e {conteMaiorDeIdade} maiores de idade. ')
