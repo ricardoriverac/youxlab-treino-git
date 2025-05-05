bf223a020e6515745872c5ca737db98a433d7fca
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-05-05 14:50:10 -0300

    salvando desafio 57 da aula 14

--- curso_python/aula13/desafio_48_aula13.py
+++ curso_python/aula13/desafio_48_aula13.py
@@ -2,4 +2,4 @@ soma = 0
 for n in range (0, 501):
     if n % 2 != 0 and n % 3 == 0:
         soma = soma + n
-print (f'A soma dos números ímpares múltiplos de 3 é: {soma} ')
+print (f'A soma dos números ímpares múltiplos de 3 é: {soma} ') 
---
+++ curso_python/aula14/desafio_57_aula15.py
@@ -0,0 +1,4 @@
+sexo = str(input('Digite o seu sexo [F/M]: ')).upper()
+while sexo != 'F' and sexo != 'M':
+    sexo = str(input('Por favor, digite corretamente [F/M]: ')).upper()
+print ('Seja Bem-Vindo(a)! ')
