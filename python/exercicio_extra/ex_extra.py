69990485d3ad1ff49f09737d2fae74105dc556b7
alicefernandes7 <alice.juliao@youxlab.com.br>
2025-05-06 15:04:09 -0300

    Exercício extra

--- python/exercicio_extra/ex_extra.py
+++ python/exercicio_extra/ex_extra.py
@@ -1,1 +1,10 @@
 #jokenpô com 3 pessoas
+jogo= []
+while True:
+    print('''Suas opções: 
+      [1] PEDRA
+      [2] PAPEL
+      [3] TESOURA ''')
+    for jogo in range(1, 4):
+        escolha= input('Qual é sua escolha? ')
+    print(jogo)
