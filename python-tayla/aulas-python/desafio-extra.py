733c9fffe2985c3f94aaa3c474c0b20d4c5834e8
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-05-07 13:20:45 -0300

    desafio extra

---
+++ python-tayla/aulas-python/desafio-extra.py
@@ -0,0 +1,39 @@
+jogadores = [
+    {"nome": "Ricardo", "gols": 5, "nacionalidade": "BR"},
+    {"nome": "Walter", "gols": 7, "nacionalidade": "BR"},
+    {"nome": "Japa", "gols": 12, "nacionalidade": "JP"},
+    {"nome": "Joao", "gols": 9, "nacionalidade": "JP"},
+    {"nome": "Augusto", "gols": 2, "nacionalidade": "USA"},
+    {"nome": "Cesar", "gols": 5, "nacionalidade": "USA"},
+    {"nome": "Kaio", "gols": 2, "nacionalidade": "JP"},
+    {"nome": "Lucas", "gols": 4, "nacionalidade": "BR"},
+    {"nome": "Felipe", "gols": 8, "nacionalidade": "BR"},
+    {"nome": "Takeshi", "gols": 6, "nacionalidade": "JP"},
+    {"nome": "Yuki", "gols": 10, "nacionalidade": "JP"},
+    {"nome": "Max", "gols": 3, "nacionalidade": "USA"},
+    {"nome": "Julien", "gols": 7, "nacionalidade": "FR"},
+    {"nome": "Pierre", "gols": 5, "nacionalidade": "FR"},
+    {"nome": "Emiliano", "gols": 2, "nacionalidade": "AR"},
+    {"nome": "Ricardo Jr.", "gols": 9, "nacionalidade": "BR"},
+    {"nome": "Santos", "gols": 1, "nacionalidade": "BR"},
+    {"nome": "Gabriel", "gols": 3, "nacionalidade": "USA"},
+    {"nome": "Mauricio", "gols": 6, "nacionalidade": "AR"},
+    {"nome": "Mauricio", "gols": 6, "nacionalidade": "ES"}
+]
+paises = []
+medias = {}
+contador = contadorGols = 0
+
+for jogador in jogadores:
+    if jogador['nacionalidade'] not in paises:
+        paises.append(jogador['nacionalidade'])
+
+for pais in paises:
+    for jogador in jogadores:
+        if jogador['nacionalidade'] == pais:
+            contador += 1
+            contadorGols += jogador['gols']
+    media = contadorGols / contador
+    medias[pais] = media
+    
+print(f'{medias}')
