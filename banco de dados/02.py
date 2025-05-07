66f06201f1e4cd77f5f49e6e551168fbddd64c8e
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-05-07 14:42:36 -0300

    Informações do Dataset

---
+++ banco de dados/02.py
@@ -0,0 +1,4 @@
+# pacotes usados neste notebook
+import pandas as pd
+data = pd.read_csv('/home/youx/curso python/youxlab-treino-git/banco de dados/datasets/GasPricesinBrazil_2004-2019.csv', sep=';')
+data.info() # informações
