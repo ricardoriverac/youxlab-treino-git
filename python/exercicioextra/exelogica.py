55bf5a5c04cb22752e457938b93f54440ac1d15b
juliocesar <juliogoiabinha060@gmail.com>
2025-05-07 20:02:04 -0300

    exercicio extra do dolly

---
+++ python/exercicioextra/exelogica.py
@@ -0,0 +1,43 @@
+paises = []
+jogadores = [
+    {"nome": "Ricardo", "gols": 5, "nacionalidade": "BR"},
+    {"nome": "Walter", "gols": 7, "nacionalidade": "BR"},
+    {"nome": "Japa", "gols": 12, "nacionalidade": "JP"},
+    {"nome": "Joao", "gols": 9, "nacionalidade": "JP"},
+    {"nome": "Augusto", "gols": 2, "nacionalidade": "USA"},
+    {"nome": "Cesar", "gols": 5, "nacionalidade": "USA"},
+    {"nome": "Kaio", "gols": 2, "nacionalidade": "JP"},
+    {"nome": "Lucas", "gols": 4, "nacionalidade": "BR"},
+    {"nome": "Felipe", "gols": 8, "nacionalidade": "BR"},
+    {"nome": "Takeshi", "gols": 6, "nacionalidade": "JP"},
+    {"nome": "Yuki", "gols": 10, "nacionalidade": "JP"},
+    {"nome": "Max", "gols": 3, "nacionalidade": "USA"},
+    {"nome": "Julien", "gols": 7, "nacionalidade": "FR"},
+    {"nome": "Pierre", "gols": 5, "nacionalidade": "FR"},
+    {"nome": "Emiliano", "gols": 2, "nacionalidade": "AR"},
+    {"nome": "Ricardo Jr.", "gols": 9, "nacionalidade": "BR"},
+    {"nome": "Santos", "gols": 1, "nacionalidade": "BR"},
+    {"nome": "Gabriel", "gols": 3, "nacionalidade": "USA"},
+    {"nome": "Mauricio", "gols": 6, "nacionalidade": "AR"},
+    {"nome": "Fabricio", "gols": 1, "nacionalidade": "ES"},
+    {"nome": "paulo", "gols": 4, "nacionalidade": "ES"}
+
+]
+
+for jogador in jogadores:
+    if jogador['nacionalidade'] not in paises:
+        paises.append(jogador["nacionalidade"])
+
+for pais in paises:
+    jogadorpais = []
+    contador = 0
+    for na in jogadores:
+        if na["nacionalidade"] == pais:
+            jogadorpais.append(na)
+    soma = 0
+
+    for gol in jogadorpais:
+        soma += gol["gols"]
+        calculamedia = soma / len(jogadorpais)
+    print(f'{pais} a média desse país é {calculamedia}')
+
