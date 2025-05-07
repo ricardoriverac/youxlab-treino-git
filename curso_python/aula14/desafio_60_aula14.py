c6820bf0942463d157798d5ddd4f51ec20bda0b9
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-05-07 15:35:14 -0300

    salvando desafio 60 da aula 14

--- curso_python/aula14/desafio_60_aula14.py
+++ curso_python/aula14/desafio_60_aula14.py
@@ -1,6 +1,16 @@
 ### While ###
-import math
+from math import factorial
 numero = int(input('Digite um número para calcular o seu fatorial: '))
+comecoFatorial = numero
 fatorial = 1
-
-#print (f'O fatorial de {numero} é igual a {resultado} ')
+while comecoFatorial > 0:
+    print (f'{comecoFatorial}' ,end='')
+    if comecoFatorial > 1:
+        print (' x ', end='')
+    else:
+        print (' = ', end='')
+# ou apenas
+# print (' x ' if comecoFatorial > 1 else ' = ' ,end='')
+    fatorial *= comecoFatorial #ou fatorial = fatorial * comecoFatorial
+    comecoFatorial -= 1 #ou comecoFatorial = comecoFatorial -1
+print (fatorial)
