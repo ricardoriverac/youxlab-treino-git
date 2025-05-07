0acf82297c65c2c747a912e475273a4f20a3b431
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-05-07 17:01:07 -0300

    salvando desafio 61 da aula 14

---
+++ curso_python/aula14/desafio_61_aula14.py
@@ -0,0 +1,9 @@
+primeiroTermo = int(input('Digite o primeiro termo: '))
+razao = int(input('Digite a razão: '))
+termo = primeiroTermo
+quantidadeDeTermos = 0
+while quantidadeDeTermos < 10:
+    print (f'{termo} -> ' , end='')
+    termo += razao
+    quantidadeDeTermos += 1
+print ('FIM')
