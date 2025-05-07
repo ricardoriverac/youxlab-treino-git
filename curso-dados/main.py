710e82026f38a09bb2df94c27a93b4dc403a45c0
João Pedro <joao.pedro@youxlab.com.br>
2025-05-07 15:18:40 -0300

    Informações do dataset

--- curso-dados/main.py
+++ curso-dados/main.py
@@ -1,8 +1,12 @@
 import pandas as pd
 
-# # carregando o dataset corretamente ==> neste caso, usa o separador ";"
+# carregando o dataset corretamente ==> neste caso, usa o separador ";"
+# endereço ==> ./datasets/GasPricesinBrazil_2004-2019.csv
 data = pd.read_csv("C:/Users/jaump/Desktop/youxlab-treino-git/curso-dados/datasets/GasPricesinBrazil_2004-2019.csv", sep=";")
 print(data)
 
 # exibe as 10 primeiras linhas do dataframe
-data.head(10)
+#data.head(10)
+
+# exibe as informações do dataset
+data.info()
