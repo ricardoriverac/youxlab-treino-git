a2199a6182e1de4595b1f4fef07d5ac5cda25271
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-05-07 17:50:42 -0300

    curso dados video 6

--- curso_dados/datasets/videos/video6.py
+++ curso_dados/datasets/videos/video6.py
@@ -1,5 +1,5 @@
 import pandas as pd
 data = pd.read_csv('./curso_dados/datasets/GasPricesinBrazil_2004-2019 (1).csv')
 print(data.head())
-idades = pd.Series([45, 23, 17, 59])
+idades = pd.Series([45, 23, 17, 59], index = ['Gilmar', 'Larissa', 'Ana Clara', 'José Maria'])
 print(idades)
