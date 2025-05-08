8a9e013f6fd90751df9efea1ff5feadaa2ccfec2
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-05-08 14:43:52 -0300

    curso dados video 8

--- curso_dados/datasets/videos/video3.py
+++ curso_dados/datasets/videos/video3.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-data = pd.read_csv('./curso_dados/datasets/GasPricesinBrazil_2004-2019 (1).csv')
+data = pd.read_csv('python -m site
+python -m site
+')
 
 print(data)
 print = (data.head(10))
