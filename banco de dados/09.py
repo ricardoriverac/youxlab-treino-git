2c6ca8176302f062e5e7a102903108719cab0005
kawannehellen <kawanne.pereira@youxlab.com.br>
2025-05-08 14:13:29 -0300

    nova coluna multiplicando os valores de outra

--- banco de dados/09.py
+++ banco de dados/09.py
@@ -1,10 +1,10 @@
 import pandas as pd
-dados = pd.read_csv('/home/youx/curso python/youxlab-treino-git/banco de dados/vendas de carro/venda-de-carros.csv', sep=',')
+dados = pd.read_csv('/home/youx/curso python/youxlab-treino-git/banco de dados/vendas de carro/venda-de-carros.csv', sep=';')
 print ('-=' * 4, f'\033[35mTABELA COMPLETA\033[m','-=' * 4 )
 print (dados.head())
 print ('-=' * 4, f'\033[35m nova coluna \033[m','-=' * 4 )
 dados['Nova coluna'] = 'testando' 
 print(dados)
 print ('-=' * 4, f'\033[35m multiplicação da coluna \033[m','-=' * 4 )
-dados['Quilometragem2'] = dados['Quilometragem'] * 2
+dados['Quilometragem2'] = dados['quilometragem'] * 2
 print(dados)
