218e505fb86882672fe2b12ae727a7780e3c70cd
laber-Vinicius Farias de Souza791 <vinicius.souza@youxlab.com.br>
2025-05-12 21:12:35 -0300

    salvando desafio 69 da aula 15

---
+++ curso_python/aula15/desafio_69_aula15.py
@@ -0,0 +1,25 @@
+maisDezoito = 0
+homens = 0
+mulheresMenosVinte = 0
+
+while True:
+    print ('CADASTRO DE PESSOAS')
+    idade = int(input('Idade: '))
+    sexo = ' '
+    while sexo not in 'FM':
+        sexo = str(input('Sexo [F/M]:')).strip().upper()
+    if idade > 18:
+        maisDezoito += 1
+    if sexo == 'F' and idade < 20:
+        mulheresMenosVinte += 1
+    if sexo == 'M':
+        homens += 1
+    continuar = ' '
+    while continuar not in 'SN':
+        continuar = str(input('Deseja prosseguir? [S/N]: ')).strip().upper()
+    if continuar == 'N':
+        break
+
+print(f'Foram cadastradas {maisDezoito} pessoas com mais de 18 anos... ')
+print (f'{homens} delas são homens... ')
+print (f'E {mulheresMenosVinte} têm menos de 20 anos. ')
