ff18e48f67c941b709bad9dbd16abde51019190d
Keverson Teodoro <keverson.teodoro@youxlab.com.br>
2025-05-14 18:09:55 -0300

    java

---
+++ primeiro.java
@@ -0,0 +1,17 @@
+// public class Main {
+//     public static void main(String [] args){
+//         System.out.println("Olá mundo!");
+//     }
+// }
+
+import java.util.Scanner;
+
+public class primeiro{
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 6;
+        int soma = num1 * num2;
+        System.out.println("A soma é: " + soma);
+    }
+}
+
