40ae56d4568448ff1f12728e994b6bc81f7b4933
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-05-21 15:30:12 -0300

    quinta aula javascript

---
+++ curso.JavaScript/aula05.JavaScript/aula05.js
@@ -0,0 +1,15 @@
+let num1 = 8
+let num2 = 4
+let num3 = 8
+
+console.log(num1 > num2)
+console.log(num1 < num2)
+console.log(num1>=num3)
+console.log(num1==num2)
+console.log(num2<=num3)
+console.log('-------------------')
+console.log(!(num1 > num2))
+console.log(!(num1 < num2))
+console.log(!(num1>=num3))
+console.log(!(num1==num2))
+console.log(!(num2<=num3))
