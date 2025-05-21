59bf2607738c21bb268ef357a054f571476ae5e9
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-05-21 17:11:37 -0300

    oitava aula javascript

---
+++ curso.JavaScript/aula08.JavaScript/aula08.js
@@ -0,0 +1,18 @@
+let n1 = 15
+let n2 = 22
+let res = n1 & n2
+
+console.log(res)
+
+let res2 = n1 & n2
+
+console.log(res2)
+
+let res3 = n1 | n2
+
+console.log(res3)
+
+let res4 = n1 ^ n2
+
+console.log(res4)
+
