f1d302f7041a1db78e66e29ad6322507bb4659ed
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-05-21 15:16:53 -0300

    operadores matemáticos

---
+++ python-tayla/aulas-JavaScript/aula04/aula04.js
@@ -0,0 +1,22 @@
+/*
++ adição
+- subtração
+/ divisão
+* multiplicação
+% resto da divisão
+++ incremento (+=)
+-- decremento (-=)
+*/
+
+let numero1 = 0, numero2 = 0, resultado = 0 // pra declarar variavel tem q fazer isso
+
+numero1 = 10
+numero2 = 15
+
+resultado = (numero1 + numero2)*2
+
+numero1+= 5 // msm logica do python
+
+console.log(resultado)
+
+console.log(numero1)
