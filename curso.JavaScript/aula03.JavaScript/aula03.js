1f14ddeb65eda23dbbf757a3ef6dbea885d11532
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-05-21 14:55:27 -0300

    terceira aula javascript

---
+++ curso.JavaScript/aula03.JavaScript/aula03.js
@@ -0,0 +1,18 @@
+if (true){ 
+    var nome = 'Ricardinho'
+}
+console.log(nome)
+
+if (true){ 
+    let nome1 = 'Ricardão'
+}
+console.log(nome1)
+
+// var e let são usadas para criar variáveis
+// A diferença é que:
+// - var funciona em toda a função (ou fora, se estiver no topo do código)
+// - let só funciona dentro do bloco onde foi criada (como dentro de if ou for)
+// - var pode ser criada mais de uma vez, let não deixa
+// - var pode ser usada antes de ser declarada (mas fica undefined)
+// - let dá erro se tentar usar antes de declarar
+// const não pode ser alterada ao longo do meu programa
