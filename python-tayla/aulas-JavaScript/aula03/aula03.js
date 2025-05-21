2368e537af2f39a4a11574bcdfe8c8fbaaa22e33
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-05-21 14:27:45 -0300

    modo estrridente

--- python-tayla/aulas-JavaScript/aula03/aula03.js
+++ python-tayla/aulas-JavaScript/aula03/aula03.js
@@ -1,25 +1,25 @@
 "use strict"
 
-// function teste(){
-//     let nome="Bruno"
-//     if(true){
-//         console.log(`dentro do IF do teste: ${nome}`)
-//     }
-//     console.log("dentro do teste: " + nome)
-// }
+function teste(){
+    let nome="Bruno"
+    if(true){
+        console.log(`dentro do IF do teste: ${nome}`)
+    }
+    console.log("dentro do teste: " + nome)
+}
 
-// teste()
+teste()
 
-// console.log("fora do teste: " + nome)
+console.log("fora do teste: " + nome) // erro pq não pega a variavel nome pq ela ta declarada dentro da função
 
-// let nome='Bruno'
-// nome='CFB Cursos'
-// nome=10
+let nome='Bruno'
+nome='CFB Cursos'
+nome=10
 
-// console.log(nome)
+console.log(nome)
 
 const curso='Javascrispt'
 
-curso='HTML'
+curso='HTML' // erro pq não pode mudar quando declara a constante
 
-console.log(curso)
+console.log(curso) 
