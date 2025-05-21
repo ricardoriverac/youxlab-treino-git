186d103cd1896358ecce8347bd7bff1b4de496f6
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-05-21 15:22:07 -0300

    quarta aula javascript

---
+++ curso.JavaScript/aula04.JavaScript/aula04.js
@@ -0,0 +1,17 @@
+let num1 = 0, num2 = 0, res = 0
+
+num1 = 7
+num2 = 9
+res = num1 + num2
+console.log(res)
+console.log(num1 - num2)
+res = (num1 + num2)*2
+console.log(res)
+res = num1/num2
+console.log(res)
+res = num1%num2
+console.log(res)
+num1++
+console.log(num1)
+num1--
+console.log(num1)
