40a186349b3fe85bbd8ecc23d62a37e65da306a1
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-05-21 14:16:39 -0300

    dfiferença de LET, VAR e COSNT

---
+++ python-tayla/aulas-JavaScript/aula03/aula03.js
@@ -0,0 +1,25 @@
+"use strict"
+
+// function teste(){
+//     let nome="Bruno"
+//     if(true){
+//         console.log(`dentro do IF do teste: ${nome}`)
+//     }
+//     console.log("dentro do teste: " + nome)
+// }
+
+// teste()
+
+// console.log("fora do teste: " + nome)
+
+// let nome='Bruno'
+// nome='CFB Cursos'
+// nome=10
+
+// console.log(nome)
+
+const curso='Javascrispt'
+
+curso='HTML'
+
+console.log(curso)
