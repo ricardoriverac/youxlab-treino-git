8145aa3e16d2e265c28f2cf11bc955d51295bf68
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-05-21 15:29:20 -0300

    operadores relacionais

---
+++ python-tayla/aulas-JavaScript/aula05/aula05.js
@@ -0,0 +1,12 @@
+/*
+> maior
+>= maior igual
+< menor
+<= menor igual
+== igual
+!= diferente
+*/
+
+let numero1=10, numero2=5, numero3=10
+
+console.log(!(numero1 >= numero2)) // o !(not) só troca a saida
