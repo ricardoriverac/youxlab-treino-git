a027aa0e8555b1e0d2b072cd9a30d8b8f3442c6c
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-05-21 17:24:32 -0300

    nona aula javascript

---
+++ curso.JavaScript/aula09.JavaScript/aula09.js
@@ -0,0 +1,14 @@
+let num = 10
+
+
+console.log(++num)
+
+console.log(num++)
+console.log(num)
+console.log(num--)
+let x = -num
+console.log(x)
+
+n1 = 10
+n2 = 20
+console.log(n1+' + '+n2, 'é', n1+n2)
