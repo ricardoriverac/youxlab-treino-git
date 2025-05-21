4e4de445a32c86adcb13142b2ee8baf91abb1918
Maria Luiza <maria.silva@youxlab.com.br>
2025-05-21 17:09:00 -0300

    aula 10 javascript

---
+++ javascript/aula10.js
@@ -0,0 +1,16 @@
+let num = 10
+//res = num%2
+
+//if(res==0){
+// console.log('Par')
+//}else{
+//    console.log('Ímpar')
+//}
+
+res = (!(num%2)? "Par": 'Ímpar')
+console.log(res)
+// Teste lógico ? se verdadeiro : se falso
+
+let sr = "A"
+resp = (sr == "A"? "Ativo" : "Inativo" )
+console.log(resp)
