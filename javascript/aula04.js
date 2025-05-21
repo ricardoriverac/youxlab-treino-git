3804a05d67cf8f043481073e434a008064959092
Maria Luiza <maria.silva@youxlab.com.br>
2025-05-21 15:11:24 -0300

    aula 04 javascript

---
+++ javascript/aula04.js
@@ -0,0 +1,11 @@
+let num1 = 0,num2 = 0, res = 0
+num1 = 10
+num2 = 15
+res = (num1 + num2)*2
+console.log(res)
+num1++
+console.log(num1)
+num1--
+console.log(num1)
+num1*=2 //num1 = num1 * 2
+console.log(num1)
