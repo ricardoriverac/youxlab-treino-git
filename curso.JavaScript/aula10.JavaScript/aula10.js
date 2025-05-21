b873cf123e11fa33944846d15b407c52190874fd
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-05-21 17:54:19 -0300

    decima aula javascript

---
+++ curso.JavaScript/aula10.JavaScript/aula10.js
@@ -0,0 +1,20 @@
+let n = 12
+
+let res = n%2
+
+if(res == 0){
+    console.log('O número', n, 'é par')
+}else{
+    console.log('O número', n, 'é ímpar')
+}
+
+let n2 = 10
+let res2 = (!(n2%2) ? 'Par': 'Ímpar')
+console.log(res2)
+
+// teste lógico ? se verdadeiro : se salso
+
+let num1 = 10
+let num2 = 20
+res=(num1>num2 ? "Verdadeiro":"Falso")
+console.log(res)
