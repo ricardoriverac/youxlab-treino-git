3edc8189ccc01d8642995940205310162f453a9e
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-05-21 16:57:53 -0300

    diferença de pré incremento e pós incremento

---
+++ python-tayla/aulas-JavaScript/aula07/aula07.js
@@ -0,0 +1,6 @@
+let numero1=10, numero2=20
+let x=-numero1
+
+console.log(++numero1) // o sistema sempre lê da esquerda pra direita
+console.log(x)
+console.log(numero1 + '' + numero2) // quando tiver string no meio ele junta os valores e não faz a operação
