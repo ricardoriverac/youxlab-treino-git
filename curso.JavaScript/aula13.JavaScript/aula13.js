c8db2bf261c8be8333b89aad3499ea7d548cdf97
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-05-22 15:13:19 -0300

    décima terceira aula de JavaScript

---
+++ curso.JavaScript/aula13.JavaScript/aula13.js
@@ -0,0 +1,30 @@
+let clima = 23
+let tempo = "sol"
+
+
+if(clima>30 && tempo == "sol"){
+    console.log("Está quente, vou à praia!!")
+
+}else{
+    console.log("Está frio, não vou à praia. Vou ao cinema.")
+}
+
+console.log("----------------------------------------------------------------")
+
+let num = 80
+
+if(num > 10){
+    console.log('Número maior que 10')
+    console.log("Comando 2")
+    if (num > 50){
+        console.log("Esse número ta maior que 50")
+    }
+}else if (num > 5){
+    console.log('Número está entre 6 e 10')
+    console.log('Comando 2')
+}else{
+    console.log("Número menor que 6")
+}
+
+
+console.log("Fim do programa")
