e1c7db13077a5051bcb1e21bb07d3a7533a372da
Youx <sophia.carvalho@youxlab.com.br>
2025-05-22 14:36:28 -0300

    javascript

---
+++ Ciclo2/aula01/aula01.js
@@ -0,0 +1,6 @@
+console.log("Olá Mundo!") //comado de saída (aparecerá a mensagem na tela)
+//para aparecer a mensagem no terminal:
+// entre na pasta: cd (nome da pasta)
+// comando: node (nome do arquivo) 
+console.log("Oi!")
+console.log('Meu nome é Sophia')
---
+++ Ciclo2/aula02/aula02.js
@@ -0,0 +1,7 @@
+"use strict" // modo estrito
+ let nome='Sophia' // declara uma variável
+
+// 3 modos de utilizção do console.log()
+console.log('Meu nome é:') // só string
+console.log(nome) // só variável
+console.log('Nome:' + nome) // os dois juntos 
