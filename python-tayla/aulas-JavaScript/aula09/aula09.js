0fb6ad0cfd84e9dcbb587851815bcb4655ae3380
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-05-22 00:28:37 -0300

    operador typeof

---
+++ python-tayla/aulas-JavaScript/aula09/aula09.js
@@ -0,0 +1,9 @@
+let valor1=10
+let valor2='10'
+let valor3=valor1===valor2
+let valor4={nome: 'Bruno'}
+
+console.log('Valor: ' + valor1 + ' - Tipo: ' + typeof(valor1))
+console.log('Valor: ' + valor2 + ' - Tipo: ' + typeof(valor2))
+console.log('Valor: ' + valor3 + ' - Tipo: ' + typeof(valor3))
+console.log('Valor: ' + valor4 + ' - Tipo: ' + typeof(valor4))
