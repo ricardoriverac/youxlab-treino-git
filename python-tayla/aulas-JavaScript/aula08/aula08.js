bb6f2c5679e5ab281889aacaad0caf4d0ed2701d
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-05-21 23:38:59 -0300

    operador ternário

---
+++ python-tayla/aulas-JavaScript/aula08/aula08.js
@@ -0,0 +1,7 @@
+let numero1=10
+resultado = (!(numero1%2) ? 'Par' : 'Impar') // quando nega ele inverte o resultado da operação fazendo ele pegar o 'par'
+console.log(resultado)
+
+let status = 'A'
+resposta = (status == 'A' ? 'Ativo' : 'se Falso')
+// teste lógico ? se verdadeiro : se falso
