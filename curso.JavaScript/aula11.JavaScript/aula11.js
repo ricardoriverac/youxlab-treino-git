86f1dcf9941f656e94a90e7e0613d8911b62533e
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-05-22 13:18:10 -0300

    decima primeira aula javascript

---
+++ curso.JavaScript/aula11.JavaScript/aula11.js
@@ -0,0 +1,9 @@
+let v1 =32
+let v2 = "32"
+let v3=v1===v2 //vai retornar False pq o v1 é um numero e o v2 é uma str
+let v4 = {nome: "Ana Clara"}
+
+console.log("Valor:", + v1 + "; Tipo: " +(typeof(v1)))
+console.log("Valor:", + v2 + "; Tipo: " +(typeof(v2)))
+console.log("Valor:", + v3 + "; Tipo: " +(typeof(v3)))
+console.log("Valor:", + v4 + "; Tipo: " +(typeof(v4)))
