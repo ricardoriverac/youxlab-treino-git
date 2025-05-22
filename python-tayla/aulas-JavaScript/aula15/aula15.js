8678a952c7e85aa14b0b5033581f370da7af193d
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-05-22 17:05:19 -0300

    loop while

--- python-tayla/aulas-JavaScript/aula15/aula15.js
+++ python-tayla/aulas-JavaScript/aula15/aula15.js
@@ -1,9 +1,9 @@
-let numero1=0
+// let numero1=0
 
-while(numero1<10){
-    console.log(numero1)
-    numero1++
-}
+// while(numero1<10){
+//     console.log(numero1)
+//     numero1++
+// }
 
 let numero2=5
 let fatorial=1
@@ -12,3 +12,5 @@ while(numero2 >= 1){
     fatorial *= numero2
     numero2--
 }
+
+console.log(fatorial)
