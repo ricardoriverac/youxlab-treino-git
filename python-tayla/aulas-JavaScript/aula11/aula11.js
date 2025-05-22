cf81b8c8f59ee870293ad2f02ba395ac70732a5f
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-05-22 14:28:03 -0300

    if e else

---
+++ python-tayla/aulas-JavaScript/aula11/aula11.js
@@ -0,0 +1,25 @@
+// let numero=40
+
+// if(numero > 10){
+//     console.log('Numeral maior que 10')
+//     if(numero > 50){
+//         console.log('Numeral maior que 50')
+//     }
+// }else if(numero > 5){
+//     console.log('Numeral esta entre 6 e 10')
+// }else{
+//     console.log('Numeral menor ou igual a 5')
+// }
+
+// console.log('Fim do programa')
+
+let energia=100
+let clima='chuva'
+
+if(energia > 70  && clima=='sol'){
+    console.log('Vou a praia')
+}else{
+    console.log('Vou ao cinema')
+}
+
+console.log('Fim do programa')
