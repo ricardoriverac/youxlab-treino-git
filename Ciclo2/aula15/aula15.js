efd7da9d7cfa3feb2985d44696beaef63d2600a5
Youx <sophia.carvalho@youxlab.com.br>
2025-05-24 20:33:10 -0300

    Javascript

---
+++ Ciclo2/aula15/aula15.js
@@ -0,0 +1,23 @@
+// COMANDO DE LOOP FOR
+
+console.log('Início do programa')
+
+//  declarou var; enquanto for menor que 10; soma 1
+for(let indice=0; indice<10; indice++) {
+    console.log('Sophia - valor do índice: '+indice) // repetiu 10x
+}
+console.log('Fim do programa')
+
+
+//IMPRIMINDO VALORES PARES E ÍMPARES
+console.log('Início do programa')
+
+//  declarou var; enquanto for menor que 10; soma 1
+for(let indice=0; indice<100; indice++) {
+    if(indice%2==0){
+        console.log(indice+' é par')
+    }else{
+        console.log(indice+' é ímpar')
+    }
+}
+console.log('Fim do programa')
