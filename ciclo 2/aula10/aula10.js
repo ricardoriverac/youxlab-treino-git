761684f5be3afbcc175be847e04fca15ba035b6e
alexia-assis <alexiaassis.youxlab.com.br>
2025-05-24 18:03:20 -0300

    FIM DA AULA10, operador ternario  CICLO 2

---
+++ ciclo 2/aula10/aula10.js
@@ -0,0 +1,7 @@
+let num = 10, num2 = 5
+console.log("Rodando Par ou Impar")
+res = (!(num%2) ? "Par" : "Impar")//operador ternario
+//0 seria falso, 1 seria verdadeiro 
+rees = (num > num2 ? "Verdadeiro" : "Falso")// verificando se é verdadeiro ou falso
+console.log(rees)
+console.log(res)
