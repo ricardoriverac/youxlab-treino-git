51872825616d7c6311e44da82d3c61f446f2a7dd
alexia-assis <alexiaassis.youxlab.com.br>
2025-05-24 18:20:43 -0300

    FIM DA AULA11, usando typeof  CICLO 2

---
+++ ciclo 2/aula11/aula11.js
@@ -0,0 +1,10 @@
+//usando o typeof, ele retorna o tipo da variavel ou do elemento que está ultilizando
+let v1 = 10 //declarando um valor
+let v2 = "10" // em forma de texto
+let v3 = v1===v2 // mostrando se são valores iguais 
+let v4 ={nome:"alexia"} // retorna o valor do objeto
+console.log("MOstrando que tipo é com o typeof: ")
+console.log("Valor: " + v1 + "--Tipo:" + typeof(v1))
+console.log("Valor: " + v2 + "--Tipo:" + typeof(v2))
+console.log("Valor: " + v3 + "--Tipo:" + typeof(v3))
+console.log("Valor: " + v4 + "--Tipo:" + typeof(v4))
