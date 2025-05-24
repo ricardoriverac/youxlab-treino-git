32e0700da390cc4f2f6438ad9d304f9c528fbb53
Youx <sophia.carvalho@youxlab.com.br>
2025-05-24 07:54:43 -0300

    Javascript

---
+++ Ciclo2/aula11/aula11.js
@@ -0,0 +1,11 @@
+// OPERADOR TYPEOF (retorna o tipo ed variável que está sendo usada)
+
+let valor1=10 // número
+let valor2='10' // texto
+let valor3=valor1===valor2 // se valor das variáveis valor1 é IGUAL o da valor2
+let valor4={nome:'Sophia'} // Dicionário
+
+console.log('Valor: '+valor1+'  Tipo: '+typeof(valor1))
+console.log('Valor: '+valor2+'  Tipo: '+typeof(valor2))
+console.log('Valor: '+valor3+'  Tipo: '+typeof(valor3))
+console.log('Valor: '+valor4+'  Tipo: '+typeof(valor4))
