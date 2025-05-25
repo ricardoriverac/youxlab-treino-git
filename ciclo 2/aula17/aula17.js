04756982534801a8b5cf1c97d04a99a0c5abe228
alexia-assis <alexiaassis.youxlab.com.br>
2025-05-25 00:33:45 -0300

    FIM DA AULA17,loop usando while  CICLO 2

---
+++ ciclo 2/aula17/aula17.js
@@ -0,0 +1,18 @@
+let numero = 0
+
+while (numero < 10) {// O loop vai rodar enquanto o número for menor que 10
+    console.log(numero) 
+    numero++ // aui soma 1 em "numero" a cada volta do loop
+}
+
+// Fatorando um número:
+let numero2 = 5
+let fatorial = 1  // começando com 1 porque qualquer número multiplicado por 1 é ele mesmo
+
+// O loop vai continuar enquanto "numero2" for maior ou igual a 1
+while (numero2 >= 1) {
+    fatorial *= numero2  // Multiplicando
+    numero2--  // diminui 1 em "numero2" a cada loop
+}
+
+console.log(fatorial)  // No final, imprime o resultado do fatorial de 5 (120)
