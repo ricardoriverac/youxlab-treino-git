020bf38eb049ab8d112276561d4d16b26110022e
Youx <sophia.carvalho@youxlab.com.br>
2025-05-24 21:47:40 -0300

    Javascript

---
+++ Ciclo2/aula17/aula17.js
@@ -0,0 +1,20 @@
+// LOOP WHILE
+
+let numero=0
+
+while(numero<10){
+    console.log(numero)
+    numero++ // soma 1
+}
+
+
+//fatorando um número:
+//5!= 5*4*3*2*1 = 120
+let numero2=5
+let fatorial=1
+while(numero2>=1){
+    fatorial*=numero2
+    numero2--
+}
+
+console.log(fatorial)
