649b9b4dbcef9cdb6b1f670c2dbadab1ba96d51d
Youx <sophia.carvalho@youxlab.com.br>
2025-05-24 22:01:57 -0300

    Javascript

---
+++ Ciclo2/aula18/aula18.js
@@ -0,0 +1,19 @@
+// DIFERENÇA DE WHILE E DO WHILE
+
+let numero=10
+
+while(numero<10){ // false 
+    console.log('Curso de Javascript')
+    numero++
+}
+console.log('Fim do programa \n')// imprimirá está frase diretamente 
+
+
+// DO WHILE
+// FAÇA comandos ENQUANTO número menor que 10
+// Faz com que o loop fosse interado pelo menos 1 vez
+do{ 
+    console.log('Curso de Javascript')
+    numero++
+}while(numero<10)
+console.log('Fim do programa')
