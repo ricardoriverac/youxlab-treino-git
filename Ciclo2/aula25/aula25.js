340fe2fe4cba92ce491f8b1134db0735ecbf4425
Youx <sophia.carvalho@youxlab.com.br>
2025-05-25 22:45:50 -0300

    Javascript

---
+++ Ciclo2/aula25/aula25.js
@@ -0,0 +1,25 @@
+// ARROW FUNCTION -->tipo de declaração de função anônima
+
+// let soma=function(valor1,valor2){return valor1+valor2}
+
+let soma=(valor1,valor2)=>{return valor1+valor2}
+// troca a palavra 'function' por '=>' 
+console.log(soma(10,5))
+console.log('\n')
+
+
+//REGRAS:
+
+let apelido=sophia=>{return sophia}
+//não precisa de parênteses caso for 1 PARÂMETRO DE ENTRADA
+
+console.log(apelido('Sosô'))
+console.log('\n')
+
+//Sem RETURN
+let adiciona=numero=>numero+10
+//só é necessário as chaves se houver RETURN
+//SOMENTE SE TIVER UMA LINHA NO CORPO DA FUNÇÃO
+
+console.log(adiciona(10))
+console.log('\n')
