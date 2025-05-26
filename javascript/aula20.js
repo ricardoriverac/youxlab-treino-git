b151fbf21742db47990d9f817cee44b865717907
Maria Luiza <maria.silva@youxlab.com.br>
2025-05-26 13:29:52 -0300

    aula 20 javascript

---
+++ javascript/aula20.js
@@ -0,0 +1,25 @@
+// function nome(){ // Declaração da função
+//     console.log("CBF Cursos")
+// }
+// nome() // Chamada da função
+
+// for(i=0; i<10;i++){
+//     nome()
+// }
+
+// function soma2_10(){
+//     let n1 = 2
+//     let n2 = 10
+//     let soma = n1 + n2
+//     console.log(soma)
+// }
+// soma2_10()
+
+function mudarTexto(){
+    let d1=document.getElementById("d1")
+    let d2=document.getElementById("d2")
+    let d3=document.getElementById("d3")
+    d1.innerHTML = "CFB Cursos"
+    d2.innerHTML = "CFB Cursos"
+    d3.innerHTML = "CFB Cursos"
+}
