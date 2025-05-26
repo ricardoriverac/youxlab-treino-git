73ba7f03c4c1f9e0cb3567b62b79b32ff5d1c30d
Marcos <marcos.garcia@youxlab.com.br>
2025-05-26 17:58:33 -0300

    Atualizações

--- Ciclo_2/Back-End/Banco_De_Dados/sql_arquivo.sql
+++ Ciclo_2/Back-End/Banco_De_Dados/sql_arquivo.sql
@@ -277,7 +277,364 @@ update cliente set idprofissao = 5 where idcliente in (6, 13);
 update cliente set idprofissao = 5 where idcliente in (11, 14);
 
 select * from profissao;
-delete from profissao where idprofissao = 1;
+delete from profissao where idprofissao = 10;
 insert into profissao (idprofissao, nome) values (10, 'Teste');
+
+alter table cliente drop nacionalidade;
+-- BR - 1, 17, 2, 3, 4, 11
+-- IT - 5
+-- NA - 
+-- AA - 9
+alter table cliente add idnacionalidade integer;
+alter table cliente add constraint fk_cln_idnacionalidade foreign key (idnacionalidade) references nacionalidade (idnacionalidade);
+update cliente set idnacionalidade = 1 where idcliente in (1, 17, 2, 3, 4, 11);
+update cliente set idnacionalidade = 2 where idcliente = 5;
+update cliente set idnacionalidade = 3 where idcliente = 9;
+
+select * from cliente
+alter table cliente drop complemento;
+-- Apartamento - 1, 10, 11, 12 
+-- Casa - 7, 6, 9, 13, 16
+alter table cliente add idcomplemento integer;
+alter table cliente add constraint fk_cln_idcomplemento foreign key (idcomplemento) references complemento (idcomplemento);
+update cliente set idcomplemento = 1 where idcliente in (7, 6, 9, 13, 16);
+update cliente set idcomplemento = 2 where idcliente in (1, 10, 11, 12);
+
+alter table cliente drop bairro;
+alter table cliente add idbairro integer;
+alter table cliente add constraint fk_cln_idbairro foreign key (idbairro) references bairro (idbairro);
+
+select * from bairro
+update cliente set idbairro = 1 where idcliente in (1, 12, 13);
+update cliente set idbairro = 2 where idcliente in (2, 3, 6, 8, 9);
+update cliente set idbairro = 3 where idcliente in (4, 5)
+update cliente set idbairro = 4 where idcliente = 7;
+
+select * from cliente
+create table uf (
+	iduf integer not null,
+	nome  varchar(30) not null,
+	sigla char(2) not null,
+
+	constraint pk_ufd_idunidade_federacao primary key (iduf), 
+	constraint un_ufd_nome unique (nome),
+	constraint un_ufd_sigla unique (sigla)
+);
+
+insert into uf (iduf, nome, sigla) values (1, 'Santa Catarina', 'SC');
+insert into uf (iduf, nome, sigla) values (2, 'Paraná', 'PR');
+insert into uf (iduf, nome, sigla) values (3, 'São Paulo', 'SP');
+insert into uf (iduf, nome, sigla) values (4, 'Minas Gerais', 'MG');
+insert into uf (iduf, nome, sigla) values (5, 'Rio  Grande do Sul', 'RS');
+insert into uf (iduf, nome, sigla) values (6, 'Rio de Janeiro', 'RJ');
+
+select * from uf
+
+create table municipio(
+	idmunicipio integer not null,
+	nome varchar(30) not null,
+	iduf integer not null,
+
+	constraint pk_mnc_idmunicipio primary key (idmunicipio),
+	constraint un_mnc_nome unique (nome),
+	constraint fk_mnc_iduf foreign key (iduf) references uf (iduf)
+);
+
+insert into municipio (idmunicipio, nome, iduf) values (1, 'Porto União', 1);
+insert into municipio (idmunicipio, nome, iduf) values (2, 'Canoninhas', 1);
+insert into municipio (idmunicipio, nome, iduf) values (3, 'Porto Vitória', 2);
+insert into municipio (idmunicipio, nome, iduf) values (4, 'General Carneiro', 2);
+insert into municipio (idmunicipio, nome, iduf) values (5, 'São Paulo', 3);
+insert into municipio (idmunicipio, nome, iduf) values (6, 'Rio de Janeiro', 6);
+insert into municipio (idmunicipio, nome, iduf) values (7, 'Uberlândia', 4);
+insert into municipio (idmunicipio, nome, iduf) values (8, 'Porto Alegre', 5);
+insert into municipio (idmunicipio, nome, iduf) values (9, 'União da Vitória', 2);
+select * from municipio
+
+select * from cliente
+alter table cliente drop municipio;
+alter table cliente drop uf;
+alter table cliente add idmunicipio integer;
+alter table cliente add constraint fk_cliente_idmunicipio foreign key (idmunicipio) references municipio (idmunicipio);
+
+update cliente set idmunicipio = 1 where idcliente in (1, 2, 10, 11);
+update cliente set idmunicipio = 2 where idcliente in (3, 12);
+update cliente set idmunicipio = 3 where idcliente = 4;
+update cliente set idmunicipio = 4 where idcliente = 5;
+update cliente set idmunicipio = 5 where idcliente in (6, 13);
+update cliente set idmunicipio = 6 where idcliente = 7;
+update cliente set idmunicipio = 7 where idcliente = 8;
+update cliente set idmunicipio = 8 where idcliente = 9;
+update cliente set idmunicipio = 9 where idcliente in (14, 15);
+
+select * from cliente
+
+create table fornecedor(
+	id_fornecedor integer not null,
+	Nome varchar(50) not null,
+
+	constraint pk_fnr_fornecedor primary key (id_fornecedor), 
+	constraint un_fnr_nome unique (Nome)
+);
+
+insert into fornecedor (id_fornecedor, Nome) values (1, 'Cap. Computadores');
+insert into fornecedor (id_fornecedor, Nome) values (2, 'AA. Computadores');
+insert into fornecedor (id_fornecedor, Nome) values (3, 'BB. Maquinas');
+
+select * from fornecedor
+
+create table vendedor(
+	id_vendedor integer not null,
+	Nome varchar(50) not null,
+
+	constraint pk_vnd_vendedor primary key (id_vendedor), 
+	constraint un_vnd_nome unique (Nome)
+);
+
+insert into vendedor (id_vendedor, Nome) values (1, 'André');
+insert into vendedor (id_vendedor, Nome) values (2, 'Alisson');
+insert into vendedor (id_vendedor, Nome) values (3, 'José');
+insert into vendedor (id_vendedor, Nome) values (4, 'Ailton');
+insert into vendedor (id_vendedor, Nome) values (5, 'Maria');
+insert into vendedor (id_vendedor, Nome) values (6, 'Suelem');
+insert into vendedor (id_vendedor, Nome) values (7, 'Aline');
+insert into vendedor (id_vendedor, Nome) values (8, 'Silvania');
+
+select * from vendedor
+
+create table transportadora(
+	id_transportadora integer not null,
+	id_municipio integer,
+	nome varchar(50) not null,
+	logradouro varchar(50),
+	numero varchar(10),
+
+	constraint pk_tpd_transportadora primary key (id_transportadora), 
+	constraint fk_mcp_municipio foreign key (id_municipio) references municipio (idmunicipio),
+	constraint un_cln_nome unique (nome)
+);
+
+insert into transportadora(id_transportadora, id_municipio, nome, logradouro, numero) values (1, 9, 'BS. Transportes', 'Rua das Limas', '01');
+insert into transportadora(id_transportadora, id_municipio, nome, logradouro, numero) values (2, 5, 'União Transportes', null, null);
+
+drop table transportadora
+select * from transportadora
+
+create table produto(
+	id_produto integer not null,
+	id_fornecedor integer not null,
+	nome varchar(50) not null,
+	valor numeric(10,2) not null,
+
+	constraint pk_pdt_produto primary key (id_produto),
+	constraint fk_fnc_fornecedor foreign key (id_fornecedor) references fornecedor (id_fornecedor)	
+);
+
+insert into produto (id_produto, id_fornecedor, nome, valor) values (1, 1, 'Microcomputador', 800);
+insert into produto (id_produto, id_fornecedor, nome, valor) values (2, 1, 'Monitor', 500);
+insert into produto (id_produto, id_fornecedor, nome, valor) values (3, 2, 'Placa mãe', 200);
+insert into produto (id_produto, id_fornecedor, nome, valor) values (4, 2, 'HD', 150);
+insert into produto (id_produto, id_fornecedor, nome, valor) values (5, 2, 'Placa de vídeo', 200);
+insert into produto (id_produto, id_fornecedor, nome, valor) values (6, 1, 'Memória RAM', 100);
+insert into produto (id_produto, id_fornecedor, nome, valor) values (7, 2, 'Gabinete', 35);
+
+select * from produto
+
+create table pedido(
+	id_pedido integer not null,
+	idcliente integer not null,
+	id_transportadora integer,
+	id_vendedor integer not null,
+	date_pedido date not null, 
+	valor float not null,
+
+	constraint pk_pdd_idpedido primary key (id_pedido),
+	constraint fk_pdd_idcliente foreign key (idcliente) references cliente (idcliente),
+	constraint fk_pdd_idtransportadora foreign key (id_transportadora) references transportadora (id_transportadora),
+	constraint fk_pdd_idvendedor foreign key (id_vendedor) references vendedor (id_vendedor) 
+);
+
+select * from pedido
+
+insert into pedido (id_pedido, date_pedido, valor, idcliente, id_transportadora, id_vendedor) values (1, '2008-04-01', 1300, 1, 1, 1);
+insert into pedido (id_pedido, date_pedido, valor, idcliente, id_transportadora, id_vendedor) values (2, '2008-04-01', 500, 1, 1, 1);
+insert into pedido (id_pedido, date_pedido, valor, idcliente, id_transportadora, id_vendedor) values (3, '2008-04-02', 300, 11, 2, 5);
+insert into pedido (id_pedido, date_pedido, valor, idcliente, id_transportadora, id_vendedor) values (4, '2008-04-05', 1000, 8, 1, 7);
+insert into pedido (id_pedido, date_pedido, valor, idcliente, id_transportadora, id_vendedor) values (5, '2008-04-06', 200, 9, 2, 6);
+insert into pedido (id_pedido, date_pedido, valor, idcliente, id_transportadora, id_vendedor) values (6, '2008-04-06', 1985, 10, 1, 6);
+insert into pedido (id_pedido, date_pedido, valor, idcliente, id_transportadora, id_vendedor) values (7, '2008-04-06', 800, 3, 1, 7);
+insert into pedido (id_pedido, date_pedido, valor, idcliente, id_transportadora, id_vendedor) values (8, '2008-04-06', 175, 3, null, 7);
+insert into pedido (id_pedido, date_pedido, valor, idcliente, id_transportadora, id_vendedor) values (9, '2008-04-07', 1300, 12, null, 8);
+insert into pedido (id_pedido, date_pedido, valor, idcliente, id_transportadora, id_vendedor) values (10, '2008-04-10', 200, 6, 1, 8);
+insert into pedido (id_pedido, date_pedido, valor, idcliente, id_transportadora, id_vendedor) values (11, '2008-04-15', 300, 15, 2, 1);
+insert into pedido (id_pedido, date_pedido, valor, idcliente, id_transportadora, id_vendedor) values (12, '2008-04-15', 300, 15, 2, 5);
+insert into pedido (id_pedido, date_pedido, valor, idcliente, id_transportadora, id_vendedor) values (13, '2008-04-20', 350, 9, 1, 7);
+insert into pedido (id_pedido, date_pedido, valor, idcliente, id_transportadora, id_vendedor) values (14, '2008-04-23', 300, 2, 1, 5);
+insert into pedido (id_pedido, date_pedido, valor, idcliente, id_transportadora, id_vendedor) values (15, '2008-04-25', 200, 11, null, 5);
+select * from pedido
+
+create table pedido_produto (
+	id_pedido integer not null, 
+	id_produto integer not null,
+	quantidade integer not null,
+	valor_unitario float not null, 
+
+	constraint pk_pdp_idpedidoproduto primary key (id_pedido, id_produto), 
+	constraint fk_pdp_idpedido foreign key (id_pedido) references pedido (id_pedido),
+	constraint fk_pdp_idproduto foreign key (id_produto) references  produto (id_produto)
+);
+
+insert into pedido_produto (id_pedido, id_produto, quantidade,  valor_unitario) values (1, 1, 1, 800);
+insert into pedido_produto (id_pedido, id_produto, quantidade,  valor_unitario) values (1, 2, 1, 500);
+insert into pedido_produto (id_pedido, id_produto, quantidade,  valor_unitario) values (2, 2, 1, 500);
+insert into pedido_produto (id_pedido, id_produto, quantidade,  valor_unitario) values (3, 4, 2, 150);
+insert into pedido_produto (id_pedido, id_produto, quantidade,  valor_unitario) values (4, 1, 1, 800);
+insert into pedido_produto (id_pedido, id_produto, quantidade,  valor_unitario) values (4, 3, 1, 200);
+insert into pedido_produto (id_pedido, id_produto, quantidade,  valor_unitario) values (5, 3, 1, 200);
+insert into pedido_produto (id_pedido, id_produto, quantidade,  valor_unitario) values (6, 1, 2, 800);
+insert into pedido_produto (id_pedido, id_produto, quantidade,  valor_unitario) values (6, 7, 1, 35);
+insert into pedido_produto (id_pedido, id_produto, quantidade,  valor_unitario) values (6, 5, 1, 200);
+insert into pedido_produto (id_pedido, id_produto, quantidade,  valor_unitario) values (6, 4, 1, 150);
+insert into pedido_produto (id_pedido, id_produto, quantidade,  valor_unitario) values (7, 1, 1, 800);
+insert into pedido_produto (id_pedido, id_produto, quantidade,  valor_unitario) values (8, 1, 5, 35);
+insert into pedido_produto (id_pedido, id_produto, quantidade,  valor_unitario) values (9, 1, 1, 800);
+insert into pedido_produto (id_pedido, id_produto, quantidade,  valor_unitario) values (9, 2, 1, 500);
+insert into pedido_produto (id_pedido, id_produto, quantidade,  valor_unitario) values (10, 5, 1, 200);
+insert into pedido_produto (id_pedido, id_produto, quantidade,  valor_unitario) values (11, 5, 1, 200);
+insert into pedido_produto (id_pedido, id_produto, quantidade,  valor_unitario) values (11, 6, 1, 100);
+insert into pedido_produto (id_pedido, id_produto, quantidade,  valor_unitario) values (12, 2, 1, 500);
+insert into pedido_produto (id_pedido, id_produto, quantidade,  valor_unitario) values (13, 3, 1, 200);
+insert into pedido_produto (id_pedido, id_produto, quantidade,  valor_unitario) values (13, 4, 1, 150);
+insert into pedido_produto (id_pedido, id_produto, quantidade,  valor_unitario) values (14, 6, 3, 100);
+insert into pedido_produto (id_pedido, id_produto, quantidade,  valor_unitario) values (15, 3, 1, 200);
+
+--------------------------------------------------------------------------------------
+-- exercicios pt 3
+
+-- 1
+select * from vendedor order by nome;
+
+-- 2
+select * from pedido where valor > 200 order by valor;
+
+-- 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
