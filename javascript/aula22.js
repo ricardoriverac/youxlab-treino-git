ade67374696c8724e6bd99695d26ec5a27a315d3
Maria Luiza <maria.silva@youxlab.com.br>
2025-05-26 16:41:47 -0300

    aula 22 javascript

---
+++ javascript/aula22.js
@@ -0,0 +1,19 @@
+function soma(n1=0, n2=0){
+    let res = n1 + n2
+    return res
+}
+
+console.log(soma(5, 5))
+
+function add (v){
+    valor += v
+}
+
+let valor = 0
+console.log(valor)
+
+add(10)
+console.log(valor)
+
+add(5)
+console.log(valor)
