c75fa2b50cdd25524da6d043d1b9b07955c34b26
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-05-28 15:10:06 -0300

    aula 22 de JavaScript

---
+++ curso.JavaScript/aula22.JavaScript/aula22.js
@@ -0,0 +1,18 @@
+function soma (n1 = 0, n2 = 0){
+    console.log(n1 +n2)
+}
+
+soma(9, 3)
+soma(7)
+
+function add (v){
+    valor+=v
+}
+let valor = 0
+console.log(valor)
+
+add(10)
+console.log(valor)
+
+add(5)
+console.log(valor)
