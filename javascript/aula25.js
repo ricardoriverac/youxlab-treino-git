da5f766ec1cdbf949afbe5aed04b262b224ea6ac
Maria Luiza <maria.silva@youxlab.com.br>
2025-05-28 13:30:18 -0300

    aula 25 javascript

---
+++ javascript/aula25.js
@@ -0,0 +1,10 @@
+//const soma = function(v1, v2)(return v1 + v2)
+
+const soma = (v1, v2) => {return v1 + v2}
+console.log(soma(10, 5))
+
+const nome = n => {return n}
+console.log(nome("Bruno"))
+
+const add = n => n + 10
+console.log(add(10))
