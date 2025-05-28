3b20dbbd88003c0e9f293a7642a8c6911e6e638d
Maria Luiza <maria.silva@youxlab.com.br>
2025-05-28 16:25:12 -0300

    aula 02 strict

--- javascript/aula29.js
+++ javascript/aula29.js
@@ -2,10 +2,20 @@ function aluno(nome, nota){
     this.nome = nome // Uma variável "nome" que recebe o valor do parâmetro nome
     this.nota = nota // This sempre faz referência á própria função
 
-    this.dado_anonimo = function(){
+    this.dado_anonimo = function(){ // O this não funciona
         setTimeout(function(){
             console.log(this.nome)
-        })
+            console.log(this.nota)
+        },2000)
     }
 
-aluno("Bruno", 100)
+    this.dado_arrow = function(){ // O this funciona
+        setTimeout(() => {
+            console.log(this.nome)
+            console.log(this.nota)
+        })
+    },2000
+}
+const al1 = new aluno("Bruno", 100)
+al1.dado_anonimo() // Após 2 segundos → imprime undefined, undefined
+al1.dado_arrow() // Após 2 segundos → imprime Bruno, 100
