6b688f2ee9cd704d4da8fc3e6b3f696880098921
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-05-29 17:30:24 -0300

    tentei fazer html

--- curso.JavaScript/desafios/loops_while/desafio01.js
+++ curso.JavaScript/desafios/loops_while/desafio01.js
@@ -1,18 +1,13 @@
-let senha = []
-if (senha.length >= 8){
-    console.log('SENHA COM 8 CARACTERES.')
-}else{
-    console.log('SENHA NÃO CONTÉM A QUANTIDADE MINÍMA DE CARACTERES.')
-}if (senha.match(/[A-Z]/g)){
-    console.log('A SENHA CONTÉM LETRA MAIÚSCULA.')
-}else{
-    console.log('A SENHA NÃO CONTÉM LETRA MAIÚSCULA.')
-}if (senha.match(/[0-9]/g)){
-    console.log('SUA SENHA POSSUI NÚMEROS.')
-}else{
-    console.log('SUA SENHA NÃO CONTÉM NÚMEROS.')
-}if(senha.match(/[\W|_]/g)){
-    console.log("A SUA SENHA CONTÉM SÍMBOLOS.")
-}else{
-    console.log('A SUA SENHA NÃO CONTÉM SÍMBOLOS.')
-}
+let senha = 'youxLab98765'
+do {if (!senha.length >= 8){
+        console.log('SENHA NÃO CONTÉM A QUANTIDADE MINÍMA DE CARACTERES.')
+    }if (!senha.match(/[A-Z]/g)){
+        console.log('A SENHA NÃO CONTÉM LETRA MAIÚSCULA.')
+    }if (!senha.match(/[0-9]/g)){
+        console.log('SUA SENHA NÃO CONTÉM NÚMEROS.')
+        continue
+    }if(!senha.match(/[\W|_]/g)){
+        console.log('A SUA SENHA NÃO CONTÉM SÍMBOLOS.')
+    }}while(!senha.match(/[A-Z]/g) && !senha.match(/[\W|_]/g) && !senha.match(/[0-9]/g) && !senha.length >= 8){
+
+    }console.log('Tudo certo, fim do programa')
