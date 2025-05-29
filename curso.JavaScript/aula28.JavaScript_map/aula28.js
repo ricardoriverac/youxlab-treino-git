d960291ca2274c1fedf9e56f2f5a2bfafdd3b33e
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-05-29 14:19:23 -0300

    aula de javascript 28 map

--- curso.JavaScript/aula28.JavaScript_map/aula28.js
+++ curso.JavaScript/aula28.JavaScript_map/aula28.js
@@ -1,4 +1,17 @@
-const cursos = ['JavaScript', 'Python', 'HTML', 'CSS', 'PHP']
-cursos.map((el, i)=>{
-    console.log('CURSO: ' + el + ' | POSIÇÃO: ' + i)
-})
+//const cursos = ['JavaScript', 'Python', 'HTML', 'CSS', 'PHP']
+//cursos.map((ell, i)=>{
+ //   console.log('CURSO: ' + ell + ' | POSIÇÃO: ' + i)
+//})
+
+//let el =document.getElementsByTagName("div")
+//const valores = Array.prototype.map.call(el, ({innerHTML})=>{innerHTML})
+//console.log(valores)
+
+//el = [...el]
+//console.log(el)
+//el.map((e, i)=>{
+ //   e.innerHTML="CFB Cursos"})
+
+const converterint =(e)=>parseInt(e)
+let num = ['1', '2', '3', '4'].map(converterint)
+console.log(num)
