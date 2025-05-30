9b60b0e68f53b23229c747834683a1c70aa06c1e
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-05-30 16:51:07 -0300

    arrow function

--- aulas-JavaScript/aulas/aula23-arrow-function/aula23.js
+++ aulas-JavaScript/aulas/aula23-arrow-function/aula23.js
@@ -1,9 +1,8 @@
 const soma = (valor1, valor2)=>{return valor1 + valor2}
+console.log(soma(10, 5))
 
 const nomes = nome=>{return nome}
+console.log(nomes('Tayla'))
 
-const adicionar = n => n+10
-
+const adicionar = n => n+10 // não precisa do return e das chaves pra funcionar se for uma operação simples
 console.log(adicionar(10))
-// console.log(soma(10, 5))
-// console.log(nomes('Tayla'))
