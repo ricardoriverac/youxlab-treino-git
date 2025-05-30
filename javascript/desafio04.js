1a1c4aac35ca3aa2715b5e5dd23eb01008824ab8
Maria Luiza <maria.silva@youxlab.com.br>
2025-05-30 13:14:26 -0300

    desafio do gerador de senha

--- javascript/desafio04.js
+++ javascript/desafio04.js
@@ -1,14 +1,22 @@
-let senha = prompt("Digite uma senha: ")
-while (senha.length < 8 || !/[A-Z]/.test(senha)){
-    alert("A senha deve conter pelo menos 8 caracteres")
-    senha = prompt("Digite uma senha: ")
-}
 
-while(senha == /[A-Z]/.test(senha)){
-    alert("A senha deve ter pelo menos 1 letra maiúscula")
-}
-
-
-if (senha.length >= 8 || /[A-Z]/.test(senha)){
-    alert("Senha aceita")
+function alterandoValor (){
+    let senha = "123@#$DSE"
+    const senha2 = document.getElementById("password")
+    // console.log(senha2.value)
+    if (senha.length < 8){
+        console.log("A senha deve conter pelo menos 8 caracteres")
+    }
+    
+    if (!/[A-Z]/.test(senha)){
+        console.log("A senha deve ter pelo menos 1 letra maiúscula")
+    }
+    
+    if (!/[0-9]/.test(senha)){
+        console.log("A senha deve ter pelo menos 1 número")
+    }
+    
+    if (!/[!@#$%¨&*]/.test(senha)){
+        console.log("A senha deve ter pelo menos 1 caractere especial")
+    }
+    console.log(senha2)
 }
