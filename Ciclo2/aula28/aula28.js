1d26909cceb3f255e0b4605dc3395c84bdf0ea29
Youx <sophia.carvalho@youxlab.com.br>
2025-05-30 15:30:36 -0300

    Javascript

--- Ciclo2/aula28/aula28.js
+++ Ciclo2/aula28/aula28.js
@@ -1,38 +1,62 @@
-let senha = 'sophia21'
+//MAP-->percorre arrays
 
-while(true){
-    if (senha.length < 8) {
-        console.log(senha)
-        console.log('ERRO!\nSua senha tem menos que 8 caracteres ')
-        break
-    }else{
-        console.log(senha)
-        console.log('Sua senha TEM 8 caracteres')
-        break
-    }
-}
+const cursos=['html','css','javascript','php','react']
+
+cursos.map((elemento, indice)=>{
+    console.log('Curso: '+elemento+' -Posição no array: '+indice)
+})
+// percorre por todos elementos e índices da array
+console.log('\n')
+
+let aulas=cursos.map((elemento)=>{
+    return elemento
+})
+// percorre todos os elementos da array
+// return de maneira simples (RETORNA UMA ARRAY)
+console.log(aulas)
+console.log('\n')
+
+let aula=cursos.map((elemento)=>{
+    return '<div> '+elemento+' </div>'
+})
+// percorre todos os elementos da array
+// return de maneira separada (RETORNA OS ELEMENTOS SEPARADAMENTE COM COMANDO DIV)
+console.log(aula)
+console.log('\n')
 
 
-let TemMaicuscula = false;
-do {
-    for (let indice = 0; indice < senha.length; indice++) {
-        let letra = senha[indice];
-        if (
-            letra === letra.toLocaleUpperCase() &&
-            letra !== letra.toLocaleLowerCase()
-        ) {
-            TemMaicuscula = true;
-            break;
-        }
-    }
-    if (!TemMaicuscula) {
-        console.log('ERRO!\nA senha precisa conter pelo menos uma letra MAIÚSCULA');
-        break;
-    }
-} while (!TemMaicuscula);
 
-console.log('Senha válida!');
 
-if (TemMaicuscula) {
-    console.log('Sua senha tem letra maiúscula');
-}
+
+// USANDO MAP PARA OPERAR A COLEÇÃO(id) NO HTML
+
+let elementos=document.getElementsByTagName('div')// pegou todo os elementos que são do tipo 'div'
+elementos=[...elementos] // separou os elementos
+elementos.map((elemento,indice)=>{
+    console.log(elemento.innerHTML) // retorna os conteúdos das divs com o .innerHTML 
+    elemento.innerHTML='Curso de Javascript'// modifica os textos das divs
+})
+console.log('\n')
+
+// De outra forma:
+const element=document.getElementsByTagName('div')
+const valores= Array.prototype.map.call(element,({innerHTML})=>innerHTML)
+console.log(valores)
+console.log('\n')
+
+
+//CONVERTENDO COLEÇÃO COM TEXTOS PARA COLEÇÃO COM N° INTEIRO
+
+const converterInt=(elemento)=>parseInt(elemento) // recebe um valor e retorna esse valor convertido para inteiro 
+let numeros=['1','2','3','4','5'].map(converterInt) // coleção com textos convertida para n° inteiros
+console.log(numeros) // retornou a coleção já convertida
+console.log('\n')
+
+//Dobrando os valores da lista
+const dobrar=(elemento)=>elemento*2
+numeros=['1','2','3','4','5'].map(dobrar) // converte os valores da coleção
+console.log(numeros) // retornou a coleção com valores dobradas
+console.log('\n')
+
+
+
