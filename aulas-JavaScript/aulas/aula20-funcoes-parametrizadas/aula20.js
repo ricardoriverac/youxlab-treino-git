6481a67bc0030eec9dd07c4debb4b86b5c694706
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-05-30 14:41:27 -0300

    funções parametrizadas

--- aulas-JavaScript/aulas/aula20-funcoes-parametrizadas/aula20.js
+++ aulas-JavaScript/aulas/aula20-funcoes-parametrizadas/aula20.js
@@ -1,31 +1,31 @@
-// const valorPadrao = 0
+const valorPadrao = 0
 
-// function soma(numero1, numero2){
-//     console.log(numero1 + numero2)
-// }
+function soma(numero1, numero2){
+    console.log(numero1 + numero2)
+}
 
-// soma(10,5)
+soma(10,5)
 
-// // passando valores padroes pros parametros
-// function soma(numero1=0, numero2=0){
-//     console.log(numero1 + numero2)
-// }
+// passando valores padroes pros parametros
+function soma(numero1=0, numero2=0){
+    console.log(numero1 + numero2)
+}
 
-// soma(10)
+soma(10)
 
-// // retornando o valor
-// function soma(numero1, numero2){
-//     return numero1 + numero2
-// }
+// retornando o valor
+function soma(numero1, numero2){
+    return numero1 + numero2
+}
 
-// console.log(soma(10,10))
+console.log(soma(10,10))
 
-// // dando o valor de uma variavel pro parametro
-// function soma(numero1 = valorPadrao, numero2 = valorPadrao){
-//     return numero1 + numero2
-// }
+// dando o valor de uma variavel pro parametro
+function soma(numero1 = valorPadrao, numero2 = valorPadrao){
+    return numero1 + numero2
+}
 
-// console.log(soma(10,32))
+console.log(soma(10,32))
 
 let valor = 0
 
