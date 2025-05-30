6402b77bf9e414b61fe4e2cbd39e6037be8ee500
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-05-30 16:50:42 -0300

    arrow function

---
+++ aulas-JavaScript/aulas/aula23-arrow-function/aula23.js
@@ -0,0 +1,9 @@
+const soma = (valor1, valor2)=>{return valor1 + valor2}
+
+const nomes = nome=>{return nome}
+
+const adicionar = n => n+10
+
+console.log(adicionar(10))
+// console.log(soma(10, 5))
+// console.log(nomes('Tayla'))
