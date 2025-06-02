538ea89238dde080ccaa61dbb7d89e14c0ebfcd4
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-06-02 13:16:03 -0300

    aula 39 de javascript relaçoẽs entre elementos

--- curso.JavaScript/aula39.JavaScript_relacao/aula39.js
+++ curso.JavaScript/aula39.JavaScript_relacao/aula39.js
@@ -1,2 +1,14 @@
 const caixa1 = document.querySelector('#caixa1')
 const btn_c= [...document.querySelectorAll('.curso')]
+
+console.log(caixa1.hasChildNodes())
+console.log(btn_c[0].hasChildNodes())
+console.log(btn_c[0].childNodes)
+
+if(btn_c[0].children.length > 0){
+  console.log('Possui filhos')
+}else{
+  console.log('Não possui filhos')
+}
+
+console.log(caixa1.firstElementChild.innerHTML='TESTE')
