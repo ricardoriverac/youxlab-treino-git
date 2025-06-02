4269dbae79c3ae13e5931304a84473aa3778aadb
Maria Luiza <maria.silva@youxlab.com.br>
2025-06-02 09:41:39 -0300

    aula 39 javascript relação dos elementos no DOM

---
+++ javascript/39-relação_dos_elementos/aula39.js
@@ -0,0 +1,17 @@
+const caixa1 = document.querySelector("#caixa1")
+const btn_c = [...document.querySelectorAll(".curso")]
+const c1_2 = document.querySelector("#c1_2")
+
+console.log(c1_2.parentNode.parentNode.children[4])
+
+// console.log(caixa1.hasChildNodes()) // Retorna true se o elemento tiver filhos
+
+// if(caixa1.children.length > 0){
+//     console.log("Possui filhos")
+// }else{
+//     console.log("Não possui filhos")
+// }
+
+// console.log(btn_c[0].children.length > 0 ? "Possui filhos" : "NÃO possui filhos")
+
+// console.log(caixa1.firstElementChild.innerHTML = "TESTE")
