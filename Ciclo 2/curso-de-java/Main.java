9a5fd8c825a755eeb6dcb117951f682536dc58ad
davisales07 <davisales260810@gmail.com>
2025-06-05 17:14:38 -0300

    JAVA

---
+++ Ciclo 2/curso-de-java/Main.java
@@ -0,0 +1,44 @@
+
+public class Main {
+
+        public static void main(String[] args) {
+            int y = 32;
+            double x = 10.35784;
+            System.out.println(y);
+            System.out.println(x);
+            System.out.println("Bom Dia!");
+            System.out.println("Olá mundo!");
+
+
+
+
+            String product1 = "Computer";
+            String product2 = "Office Desk";
+            int age = 30;
+            int code = 5290;
+            char gender = 'F';
+            double price1 = 2100.0;
+            double price2 = 650.50;
+            double measure = 53.234567;
+
+            System.out.println("Products:");
+             System.out.print(product1);
+              System.out.println(", Which price is $ 2100,00");
+               System.out.print(product2);
+                System.out.println(", Which price is $650,50");
+             System.out.print(age);
+             System.out.print(" years old, code ");
+             System.out.print(code);
+             System.out.print(" and gender:");
+             System.out.println( gender);
+             System.out.print("Measue with eight decimal places: ");
+             System.out.println(measure);
+             System.out.print("Rouded (three decimal places): ");
+             System.out.printf("%.3f\n", measure);
+             System.out.print("US decimal point: ");
+             System.out.printf("%.3f\n", measure);
+             System.out.println(price1);
+             System.out.println(price2);
+        }
+
+    }
