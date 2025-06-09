b737749c9f2dd07755015441126e55406f1d7650
davisales07 <davisales260810@gmail.com>
2025-06-09 17:28:35 -0300

    java

--- Ciclo 2/curso-de-java/Main.java
+++ Ciclo 2/curso-de-java/Main.java
@@ -1,44 +1,263 @@
-
+import java.util.Locale;
+import java.util.Scanner;
 public class Main {
 
-        public static void main(String[] args) {
-            int y = 32;
-            double x = 10.35784;
-            System.out.println(y);
-            System.out.println(x);
-            System.out.println("Bom Dia!");
-            System.out.println("Olá mundo!");
+    public static void main(String[] args) {
+        // int y = 32;
+        // double x = 10.53784;
+        // System.out.println(y);
+        // System.out.println(x);
+        // System.out.println("Bom Dia!");
+        // System.out.println("Olá mundo!");
 
 
 
 
-            String product1 = "Computer";
-            String product2 = "Office Desk";
-            int age = 30;
-            int code = 5290;
-            char gender = 'F';
-            double price1 = 2100.0;
-            double price2 = 650.50;
-            double measure = 53.234567;
+        // String product1 = "Computer";
+        // String product2 = "Office Desk";
+        // int age = 30;
+        // int code = 5290;
+        // char gender = 'F';
+        // double price1 = 2100.0;
+        // double price2 = 650.50;
+        // double measure = 53.234567;
 
-            System.out.println("Products:");
-             System.out.print(product1);
-              System.out.println(", Which price is $ 2100,00");
-               System.out.print(product2);
-                System.out.println(", Which price is $650,50");
-             System.out.print(age);
-             System.out.print(" years old, code ");
-             System.out.print(code);
-             System.out.print(" and gender:");
-             System.out.println( gender);
-             System.out.print("Measue with eight decimal places: ");
-             System.out.println(measure);
-             System.out.print("Rouded (three decimal places): ");
-             System.out.printf("%.3f\n", measure);
-             System.out.print("US decimal point: ");
-             System.out.printf("%.3f\n", measure);
-             System.out.println(price1);
-             System.out.println(price2);
-        }
+        // System.out.println("Products:");
+
+        //  System.out.print(product1);
+
+        //   System.out.println(", Which price is $ 2100,00");
+
+        //    System.out.print(product2);
+
+
+        //  System.out.print(age);
+
+        //  System.out.print(" years old, code ");
+
+        //  System.out.print(code);
+
+        //  System.out.print(" and gender:");
+
+        //  System.out.println( gender);
+
+        //  System.out.print("Measue with eight decimal places: ");
+
+        //  System.out.println(measure);
+
+        //  System.out.print("Rouded (three decimal places): ");
+
+        //  System.out.printf("%.3f\n", measure);
+
+        //  System.out.print("US decimal point: ");
+
+        //  System.out.printf("%.3f\n", measure);
+
+        //  System.out.println(price1);
+
+        //  System.out.println(price2);
+
+        //  double b, B, h, area;
+        //  b = 6.0;
+
+        //  B = 8.0;
+
+        //  h = 5.0;
+
+        //  area = (b + B) / 2.0 * h;
+
+        //  System.out.println(area);
+
+        // int a, b;
+        // double resultado;
+
+        // a = 5;
+        // b = 2;
+
+        // resultado = (double) a / b;
+
+        // System.out.println(resultado);
+
+//        double a;
+//
+//        int b;
+//
+//        a = 5.0;
+//        b = (int) a;
+//
+//        System.out.println(b);
+//
+//        Scanner sc = new Scanner(System.in);
+//        String x;
+//        x = sc.next();
+//        System.out.println("Voce digitou: " + x);
+//
+//
+//        sc.close();
+
+//        Scanner sc = new Scanner(System.in);
+//        int x;
+//        x = sc.nextInt();
+//        System.out.println("Voce digitou: " + x);
+//
+//
+//        sc.close();
+
+//        Locale.setDefault(Locale.US);
+//        Scanner sc = new Scanner(System.in);
+//        double x;
+//        x = sc.nextDouble();
+//        System.out.println("Voce digitou: " + x);
+//        sc.close();
+
+//        Scanner sc = new Scanner(System.in);
+//        char x;
+//        x = sc.next().charAt(0);
+//        System.out.println("Voce digitou: " + x);
+//
+//
+//        sc.close();
+
+//
+//
+//        Scanner sc = new Scanner(System.in);
+//        String x;
+//        int y;
+//        float z;
+//
+//        x = sc.next();
+//        y = sc.nextInt();
+//        z = sc.nextFloat();
+//
+//        System.out.println("Seu nome: " + x );
+//        System.out.println("Seu número: " + y );
+//        System.out.println("Sua idade (pode incluir meses): " + z );
+//
+//
+//        sc.close();
+
+
+
+
+//        Scanner sc = new Scanner(System.in);
+//        int x;
+//        String s1,s2,s3;
+//        x = sc.nextInt();
+//        sc.nextLine();
+//        s1 = sc.nextLine();
+//        s2 = sc.nextLine();
+//        s3 = sc.nextLine();
+//
+//
+//
+//        System.out.println("Dados digitados:");
+//        System.out.println(x);
+//        System.out.println(s1);
+//        System.out.println(s2);
+//        System.out.println(s3);
+//
+//        sc.close();
+//
+//        double x = 3.0;
+//        double y = 4.0;
+//        double z = -5.0;
+//        double A, B, C;
+//        A = Math.sqrt(x);
+//        B = Math.sqrt(y);
+//        C = Math.sqrt(25.0);
+//        System.out.println("Raiz quadrada de " + x + " = " + A);
+//        System.out.println("Raiz quadrada de " + y + " = " + B);
+//        System.out.println("Raiz quadrada de 25 = " + C);
+//        A = Math.pow(x, y);
+//        B = Math.pow(x, 2.0);
+//        C = Math.pow(5.0, 2.0);
+//        System.out.println(x + " elevado a " + y + " = " + A);
+//        System.out.println(x + " elevado ao quadrado = " + B);
+//        System.out.println("5 elevado ao quadrado = " + C);
+//        A = Math.abs(y);
+//        B = Math.abs(z);
+//        System.out.println("Valor absoluto de " + y + " = " + A);
+//        System.out.println("Valor absoluto de " + z + " = " + B);
+
+        //Exercícios:
+
+        //01:
+//        Scanner sc = new Scanner(System.in);
+//        int x;
+//        int y;
+//
+//        x = sc.nextInt();
+//        y = sc.nextInt();
+//
+//        System.out.print("Soma = ");
+//        System.out.println(x+y);
+
+        //02:
+//        Locale.setDefault(Locale.US); // para usar ponto como separador decimal
+//        Scanner sc = new Scanner(System.in);
+//
+//        double raio = sc.nextDouble();
+//        double area = 3.14159 * raio * raio;
+//
+//        System.out.printf("A=%.4f%n", area);
+//
+//        sc.close();
+
+
+        //03:
+//        Scanner sc = new Scanner(System.in);
+//        int A = sc.nextInt();
+//        int B = sc.nextInt();
+//        int C = sc.nextInt();
+//        int D = sc.nextInt();
+//
+//        int diferenca = (A * B) - (C * D);
+//
+//        System.out.println("DIFERENCA = " + diferenca);
+//
+//        sc.close();
+
+        //4:
+//        Locale.setDefault(Locale.US);
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("fale o seu número, o seu número de horas trabalhadas e o valor recebido por horas");
+//        int A = sc.nextInt();
+//        int B = sc.nextInt();
+//        double C = sc.nextDouble();
+//        System.out.print("Number = ");
+//        System.out.println(A);
+//        System.out.print("SALARY = U$ ");
+//        System.out.println(B * A / 4.51);
+
+        //5:
+//        Locale.setDefault(Locale.US);
+//        Scanner sc = new Scanner(System.in);
+//        int A = sc.nextInt();
+//        int B = sc.nextInt();
+//        double C = sc.nextDouble();
+//        int D = sc.nextInt();
+//        int E = sc.nextInt();
+//        double F = sc.nextDouble();
+//        double somado1 = ((A + B + C) + (D + E +F));
+//        System.out.println("Valor a pagar: " + somado1);
+
+
+        //6:
+//        Scanner sc = new Scanner(System.in);
+//        float A = sc.nextFloat();
+//        float B = sc.nextFloat();
+//        float C = sc.nextFloat();
+//        float triangulo = ( A + C );
+//        double pi = (3.14159);
+//
+//
+//        System.out.println("Triângulo: "+triangulo);
+//        System.out.println("Circulo "+C * pi);
+//        System.out.println("Trapézio: "+(A + B) * C);
+//        System.out.println("Quadrado: "+B);
+//        System.out.println("Retângulo: "+ A + B);
+        //Fim dos desafios.
 
     }
+
+}
