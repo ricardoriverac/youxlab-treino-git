e00224eafce37110fd2f82ca4de040456b9b2a3a
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-06-10 14:41:02 -0300

    plus Ricardin

--- aulas-JavaScript/aulas/aula48-reduce/aula48.js
+++ aulas-JavaScript/aulas/aula48-reduce/aula48.js
@@ -2,7 +2,6 @@ const p_array = document.querySelector('#array')
 const botaoReduzir = document.querySelector('#botaoReduzir')
 const resultado = document.querySelector('#resultado')
 
-
 const botaoAdicionar = document.querySelector('#botaoAdicionar')
 
 let elementosArray = []
@@ -22,9 +21,11 @@ const adicionarNumeros = (evento)=>{
 botaoReduzir.addEventListener('click',(evento)=>{
     dobro.push(elementosArray[0]*2)
     resultado.innerHTML=elementosArray.reduce((anterior,atual,posicao)=>{
+
         ant.push(anterior)
         atu.push(atual)
         dobro.push(atual*2)
+
         return atual+anterior
     })
     resultado.innerHTML+=`<br/>V.anterior: ${ant} <br/>V.atual: ${atu} <br/>Dobro: ${dobro}`
