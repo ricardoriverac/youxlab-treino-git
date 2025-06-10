d1194ebd8da4117207ffeeb7c39690a64c5a3e3a
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-06-10 15:14:46 -0300

    aula 48 e 49 de Javascript some e every

---
+++ curso.JavaScript/aula50.JavaScript_reduce/aula50.js
@@ -0,0 +1,12 @@
+const p_array = document.querySelector('#array')
+const btnReduzir = document.querySelector('#btnReduzir')
+const resultado = document.querySelector('#Resultado')
+
+const elementos = [1, 2, 3, 4, 5]
+let aux = []
+
+p_array.innerHTML = elementos
+
+btnReduzir.addEventListener('click', (evt)=>{
+    
+})
