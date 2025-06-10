e921476496f88e73e60f954d9d7aa332c235e7c1
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-06-10 15:09:16 -0300

    aprendendo sobre ARRAYS

--- aulas-JavaScript/aulas/aula50-array/aula50.js
+++ aulas-JavaScript/aulas/aula50-array/aula50.js
@@ -1,14 +1,25 @@
 const caixa = document.querySelector('#caixa')
-let cursos = ['HTML', 'CSS', 'Javascript']
+let cores = ['azul', 'verde', 'vermelho', ['claro', 'escuro', 'médio']]
+let cursos = ['HTML', 'CSS', 'Javascript',cores]
 
 //cursos[0] = 'C++'
 
 cursos.push
 
+// adicionando elementos
+cursos.push('C++')
+cursos.unshift('Python') // adiciona no inicio do array
+
+// apagando elementos
+cursos.pop() // apaga o último item
+cursos.shift() // apaga o primeiro item 
+
+console.log(cursos[3][3][2]) // indice pra localizar um elemento dentro de um array q ta dentro de outro array
+
 console.log(cursos[0])
 
 cursos.map((elemento)=>{
-    let p = document.createElement('p')
-    p.innerHTML = elemento
-    caixa.appendChild(p)
+    let paragrafo = document.createElement('p')
+    paragrafo.innerHTML = elemento
+    caixa.appendChild(paragrafo)
 })
