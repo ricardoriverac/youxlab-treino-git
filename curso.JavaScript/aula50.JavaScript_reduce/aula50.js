5b456a25797f9997e9903a914335be003a1e65a6
anaclarapedrozo <ana.silva@youxlab.com.br>
2025-06-10 16:51:18 -0300

    aula 50 de Javascript reduce

--- curso.JavaScript/aula50.JavaScript_reduce/aula50.js
+++ curso.JavaScript/aula50.JavaScript_reduce/aula50.js
@@ -1,12 +1,20 @@
 const p_array = document.querySelector('#array')
 const btnReduzir = document.querySelector('#btnReduzir')
-const resultado = document.querySelector('#Resultado')
+const resultado = document.querySelector('#resultado')
 
 const elementos = [1, 2, 3, 4, 5]
-let aux = []
+let ant = []
+let atu = []
+let dobro = []
 
 p_array.innerHTML = elementos
 
 btnReduzir.addEventListener('click', (evt)=>{
-    
+    resultado.innerHTML = elementos.reduce((anterior, atual, pos)=>{
+        ant.push(anterior)
+        atu.push(atual)
+        dobro.push(atual*2)
+        return atual + anterior
+    })
+    resultado.innerHTML += '<br/> Valor anterior: '+ant+'<br/> Valor atual: '+ atu + '<br/> Dobro:' + dobro
 })
