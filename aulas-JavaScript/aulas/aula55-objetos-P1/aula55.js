ead083bfc91789a0c2c7247185cb2db47c42a991
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-06-12 16:30:01 -0300

    entendendo sobre objetos P1

---
+++ aulas-JavaScript/aulas/aula55-objetos-P1/aula55.js
@@ -0,0 +1,13 @@
+class Pessoa{
+    constructor(parametroNome){
+        this.nome = parametroNome
+    }
+}
+
+let pessoa1 = new Pessoa('Bruno')
+let pessoa2 = new Pessoa('Asdervardson')
+let pessoa3 = new Pessoa('Brenasclartel')
+
+console.log(pessoa1.nome)
+console.log(pessoa2.nome)
+console.log(pessoa3.nome)
