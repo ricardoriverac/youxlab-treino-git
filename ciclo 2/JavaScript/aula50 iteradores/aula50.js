eb4b825cf48297daf0d25a3492c7e0d851e8d344
alexia-assis <alexiaassis.youxlab.com.br>
2025-06-12 15:04:24 -0300

    fim da aula iteradores, ciclo 2

---
+++ ciclo 2/JavaScript/aula50 iteradores/aula50.js
@@ -0,0 +1,22 @@
+const p_array=document.querySelector("#array")
+const btnVerificar=document.querySelector("#btnVerificar")
+const resultado=document.querySelector("#resultado")
+
+const elementos_array=[1,2,3,4,5]
+let ant=[]
+let atu=[]
+let dobro=[]
+p_array.innerHTML="["+elementos_array+"]"
+
+btnReduzir.addEventListener("click",(evt)=>{
+     dobro.push=(elementos_array[0]*2)
+    resultado.innerHTML=elementos_array.reduce((anterior,atual,pos)=>{
+       // aux=atual*2
+        //aux=anterior
+        ant.push(anterior)
+        atu.push(atual)
+        dobro.push(atual*2)
+        return atual+anterior
+    })
+    resultado.innerHTML+="<br/> V anterior:"+ant+"<br/> V.atual:"+atu+"<br/> Dobro:"+dobro
+})
