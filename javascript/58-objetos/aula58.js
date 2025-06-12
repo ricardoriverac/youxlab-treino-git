c9e88bf560a8b80cec1d2d65bef3b75fb00fe78d
MariaLuiza78 <maria.silva@youx.com.br>
2025-06-12 16:54:14 -0300

    aula 58 objetos

--- javascript/58-objetos/aula58.js
+++ javascript/58-objetos/aula58.js
@@ -1,26 +1,64 @@
-class pessoa{
+class Carro{
+    canal = "CBF Cursos"
     constructor(pnome, ptipo){ // Constructor é um método que é automaticamente chamado quando instancia um novo objeto dessa classe
         this.nome = pnome
         if(ptipo==1){
-            this.ptipo = "Esportivo"
-            this.valmax = 300
+            this.tipo = "Esportivo"
+            this.velmax = 300
         }else if(ptipo == 2){
             this.tipo = "Utilitário"
-            this.valmax = 100
+            this.velmax = 100
         }else if(ptipo == 3){
             this.tipo = "Passeio"
-            this.valmax = 160
+            this.velmax = 160
         }else{
             this.tipo = "Militar"
-            this.valmax = 180
+            this.velmax = 180
         }
+    }
+    getNome(){
+    return this.nome
+    }
+    getTipo(){
+    return this.tipo
+    }
+    getVelmax(){
+    return this.velmax
+    }
+    getInfo(){
+        return[this.nome, this.tipo, this.velmax]
+    }
+    setNome(nome){
+        this.nome = nome
+    }
+    setTipo(tipo){
+        this.tipo = tipo
+    }
+    setVelmax(velmax){
+        this.velmax = velmax
+    }
+    info(){
+        console.log(`Nome.: ${this.nome}`)
+        console.log(`Tipo.: ${this.tipo}`)
+        console.log(`V.Max.:${this.velmax}`)
+        console.log(`Canal.:${this.canal}`)
+        console.log("---------------------")
     }
 }
 
-let c1 = new Carro("Nagi") // O new executa o método constructor
-let c2 = new Carro("Isagi")
-let c3 = new Carro("Bachira")
+let c1 = new Carro("Rapidão", 1) // O new executa o método constructor
+let c2 = new Carro("Super Luxo", 2)
+let c3 = new Carro("Bombadão", 4)
+let c4 = new Carro("Carrego tudo", 3)
 
 console.log(c1.nome)
-console.log(c2.nome)
-console.log(c3.nome)
+console.log(c2.tipo)
+console.log(c3.velmax)
+
+c1.setNome("Super veloz")
+c1.setVelmax(500)
+
+c1.info()
+c2.info()
+
+// console.log(c1.getInfo())
