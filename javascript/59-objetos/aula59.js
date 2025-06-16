ab8b812c123460b41a6f9664ba69d352094a212d
MariaLuiza78 <maria.silva@youx.com.br>
2025-06-16 14:25:53 -0300

    aula 59 javascript objetos

---
+++ javascript/59-objetos/aula59.js
@@ -0,0 +1,56 @@
+class Pessoa{
+    constructor(pnome,pidade){
+        this.nome=pnome
+        this.idade=pidade
+    }
+    getNome(){
+        return this.nome
+    }
+    getidade(){
+        return this.idade
+    }
+    setNome(nome){
+        this.nome=nome
+    }
+    setidade(idade){
+        this.idade=idade
+    }
+    info(){
+        console.log(`Nome..: ${this.nome}`)
+        console.log(`Idade.: ${this.idade}`)
+        console.log("---------------------")
+    }
+}
+
+let pessoas=[]
+
+const btn_add=document.querySelector("#btn_add")
+const res=document.querySelector(".res")
+const btn_limpar = document.querySelector("#btn_limpar")
+
+const addPessoa=()=>{
+    res.innerHTML="" // Limpa o elemento antes de adicionar uma nova pessoa
+    pessoas.map((p)=>{
+        const div=document.createElement("div")
+        div.setAttribute("class","pessoa")
+        div.innerHTML=`Nome:${p.getNome()}<br/>Idade:${p.getidade()}`
+        res.appendChild(div)
+    })
+}
+
+btn_add.addEventListener("click",(evt)=>{
+    const nome=document.querySelector("#f_nome")
+    const idade=document.querySelector("#f_idade")
+    const p=new Pessoa(nome.value,idade.value)
+    pessoas.push(p)
+    nome.value=""
+    idade.value=""
+    nome.focus()
+    addPessoa()
+})
+
+btn_limpar.addEventListener("click", (evt) => {
+        res.innerHTML=""
+        pessoas.splice(0, pessoas.length) // Esvazia o array
+
+})
