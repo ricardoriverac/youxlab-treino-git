3f08ece1d9a36b0822f2d9a2c02c3f3d568996cf
juliocesar <juliogoiabinha060@gmail.com>
2025-06-22 09:27:30 -0300

    Exercicio de poliformismo e heranca

---
+++ java/curso-programacao/src/introducao/poo/herance_poliformismo/exercicies/entities/Moto.java
@@ -0,0 +1,22 @@
+package introducao.poo.herance_poliformismo.exercicies.entities;
+
+public class Moto extends Veiculo{
+
+    public Moto() {
+    }
+
+    public Moto(String modelo, String placa, Double precoPorDia) {
+        super(modelo, placa, precoPorDia);
+    }
+
+    @Override
+    public void mover(int x, int y) {
+        System.out.println("RAMMDADAAADADAMM");
+        System.out.printf("Sua moto está na coordenada %d  %d%n", x , y);
+    }
+
+    @Override
+    public double calcularPreco(int dias) {
+        return getPrecoPorDia() * dias * 00.5;
+    }
+}
