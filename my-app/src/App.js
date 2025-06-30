392ee34394c53d574a8885d0f75b1a147183d466
MariaLuiza78 <maria.silva@youx.com.br>
2025-06-30 15:03:02 -0300

    react

---
+++ my-app/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import HelloWorld from './components/HelloWorld'
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
+import Frase from './components/Frase';
+
+function App () {
+  const nome = "Maria"
+
+  return(
+    <div className='App'>
+      <h1>Testando CSS</h1>
+      <Frase/>
+      <Frase/>
+      <HelloWorld/>
+      <SayMyName nome="Matheus"/>
+      <SayMyName nome="João"/>
+      <SayMyName nome={nome}/>
+      <Pessoa
+        nome = "Kleber"
+        idade = "10"
+        profissao = "Chefão"
+        foto = "/home/youx/youxlab-treino-git/my-app/public/foto01_coalas.jpg"
+      />
+    </div>
+  )
+}
+
+export default App;
---
+++ my-app/src/components/Pessoa.js
@@ -0,0 +1,10 @@
+function Pessoa({nome, idade, profissao, foto}) {
+    return <div>
+        <img scr={foto} alt={nome}/>
+        <h2>Nome: {nome}</h2>
+        <p>Idade: {idade}</p>
+        <p>Profissão: {profissao}</p>
+    </div>
+}
+
+export default Pessoa
