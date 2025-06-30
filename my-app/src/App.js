6869cc8cde3297cb06d0b5dc5d1f695b4142d14a
MariaLuiza78 <maria.silva@youx.com.br>
2025-06-30 17:56:52 -0300

    aula 9 eventos no React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,16 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld'
-import SayMyName from './components/SayMyName'
-import Pessoa from './components/Pessoa'
-import Frase from './components/Frase';
-import List from './components/List'
+import Evento from './components/Evento';
+import Form from './components/Form';
 
 function App () {
-  const nome = "Maria"
-
   return(
     <div className='App'>
-      <h1>Testando CSS</h1>
-      <Frase/>
-      <Frase/>
-      <HelloWorld/>
-      <SayMyName nome="Matheus"/>
-      <SayMyName nome="João"/>
-      <SayMyName nome={nome}/>
-      <Pessoa
-        nome = "Kleber"
-        idade = "10"
-        profissao = "Chefão"
-        foto = "/home/youx/youxlab-treino-git/my-app/public/foto01_coalas.jpg"
-      />
-     <List/>
+      <h1>Testando Eventos</h1>
+      <Evento numero = "1"/>
+      <Evento numero = "2"/>
+      <Form />
     </div>
   )
 }
 
-export default App;
+export default App
