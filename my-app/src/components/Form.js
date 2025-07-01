8cb6da31df0f346e0f1b72a6d6a73c891e44a0f4
MariaLuiza78 <maria.silva@youx.com.br>
2025-07-01 13:43:05 -0300

    aula 10 react useState

--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -1,17 +1,41 @@
+import { useState } from "react"
+
 function Form () {
 
     function cadastrarUsuario (e){
-        e.preventDefault()
-        console.log("Cadastrou o usuário!")
+        e.preventDefault() // Impede o comportamento padrão de um evento, permitindo ações personalizadas em vez da ações padrões do navegador
+        console.log(`Usuário ${name} foi cadastrado com a senha ${password}`)
     }
+
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
 
     return (
         <div>
             <h1>Meu cadastro</h1>
             <form onSubmit = {cadastrarUsuario}>
                 <div>
-                    <input type = "text" placeholder = "Digite o seu nome"/>
+                    <label htmlFor="name">Nome:</label>
+                    <input 
+                        type = "text" 
+                        id="name" 
+                        name="name" 
+                        placeholder = "Digite o seu nome"
+                        onChange={(e) => setName(e.target.value)} // Cada letra que digita dentro desse campo modifica o valor do UseState
+                        />
                 </div>
+
+                 <div>
+                    <label htmlFor="password">Senha:</label>
+                    <input 
+                        type = "password" 
+                        id="password" 
+                        name="password" 
+                        placeholder = "Digite a sua senha"
+                        onChange={(e) => setPassword(e.target.value)} // Cada letra que digita dentro desse campo modifica o valor do UseState
+                    />
+                </div>
+
                 <div>
                     <input type = "submit" value = "Cadastrar"/>
                 </div>
