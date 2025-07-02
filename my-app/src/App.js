c0a89cdd4fb55d5d34e9b71fa1c248e53c8942ed
MariaLuiza78 <maria.silva@youx.com.br>
2025-07-02 14:33:12 -0300

    aula 15 implementando o React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,23 @@
-import './App.css';
-import { useState } from 'react';
-import SeuNome from './components/SeuNome';
-import Saudacao from './components/saudacao';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './pages/Home'
+import Empresa from './pages/Empresa'
+import Contato from './pages/Contato'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App () {
 
-  const [nome, setNome] = useState()
   return(
-    <div className='App'>
-      <h1>State Lift</h1>
-      <SeuNome setNome = {setNome} />
-      <Saudacao nome={nome}/>
-    </div>
+    // Router ativa o sistema de rotas
+    <Router> 
+      <Navbar/>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route path='/empresa' element={<Empresa/>}/>
+        <Route path='/contato' element={<Contato/>}/>
+      </Routes>
+      <Footer/>
+    </Router>
   )
 }
 
