4c9a24d5d676332c939715cfc7bcc596451365b3
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-07-02 17:12:49 -0300

    desafio jogo da forca

--- aulas-JavaScript/atividade-avancada-js/jogo-da-velha/desafio.js
+++ aulas-JavaScript/atividade-avancada-js/jogo-da-velha/desafio.js
@@ -1,108 +1,118 @@
 const tabuleiro = document.querySelector(".tabuleiro");
 const botaoJogar = document.querySelector("#btnJogar");
-const botaoReiniciar = document.querySelector('#btnReiniciar')
+const botaoReiniciar = document.querySelector("#btnReiniciar");
 const input1 = document.getElementById("jogador1");
 const input2 = document.getElementById("jogador2");
 const nomeJogadores = document.getElementById("nomeJogadores");
-let jogada = 'X'
-let marcacoesTabuleiro = ['', '', '', '', '', '', '', '', '']
-let vitoriaJogador1 = 0
-let vitoriaJogador2 = 0
-let statusJogos = true
+let jogada = "X";
+let marcacoesTabuleiro = ["", "", "", "", "", "", "", "", ""];
+let vitoriaJogador1 = 0;
+let vitoriaJogador2 = 0;
+let statusJogos = true;
+
 
 botaoJogar.addEventListener("click", (evento) => {
-  if (input1.value === "" || input2.value === ""){
-    alert("Preencha o nome dos jogadores")
-    return
+  if (input1.value === "" || input2.value === "") {
+    alert("Preencha o nome dos jogadores");
+    return;
   }
 
   for (let i = 0; i < 9; i++) {
     const quadrado = document.createElement("div");
     quadrado.setAttribute("class", "coluna");
-    quadrado.setAttribute("id", i)
-    quadrado.addEventListener('click', marcarQuadrado)
+    quadrado.setAttribute("id", i);
+    quadrado.addEventListener("click", marcarQuadrado);
     tabuleiro.appendChild(quadrado);
   }
 
-  atualizarPlacar()
+  atualizarPlacar();
 
   botaoJogar.disabled = true;
 });
 
-function alternarJogador(){
-  if (jogada == "X"){
-    jogada = 'O'
+function alternarJogador() {
+  if (jogada == "X") {
+    jogada = "O";
   } else {
-    jogada = 'X'
+    jogada = "X";
   }
 }
 
-function verificarVitoria(){
-  const combinacoesVitorias = [[0,1,2], [3,4,5], [6,7,8],   // linhas
-                              [0,3,6], [1,4,7], [2,5,8],  // colunas
-                              [0,4,8], [2,4,6]]          // diagonais
-                              
-  const vitoria = combinacoesVitorias.some(([a,b,c])=>{
-    return marcacoesTabuleiro[a] === jogada &&
-    marcacoesTabuleiro[b] === jogada &&
-    marcacoesTabuleiro[c] === jogada
-  })
-  
-  if (vitoria){
-    alert(`${jogada} ganhou`)
+function verificarVitoria() {
+  const combinacoesVitorias = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], // linhas
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8], // colunas
+    [0, 4, 8],
+    [2, 4, 6],
+  ]; // diagonais
+
+  const vitoria = combinacoesVitorias.some(([a, b, c]) => {
+    return (
+      marcacoesTabuleiro[a] === jogada &&
+      marcacoesTabuleiro[b] === jogada &&
+      marcacoesTabuleiro[c] === jogada
+    );
+  });
+
+  if (vitoria) {
+    alert(`${jogada} ganhou`);
   }
-  
-  return vitoria
+
+  return vitoria;
 }
 
-botaoReiniciar.addEventListener('click', (evento)=>{
-  marcacoesTabuleiro = ['', '', '', '', '', '', '', '', '']
-  const quadrados = document.querySelectorAll('.coluna ')
-  quadrados.forEach((quadrado)=>{
-    quadrado.textContent = ''
-  })
-  statusJogos = true
-})
+botaoReiniciar.addEventListener("click", (evento) => {
+  marcacoesTabuleiro = ["", "", "", "", "", "", "", "", ""];
+  const quadrados = document.querySelectorAll(".coluna ");
+  quadrados.forEach((quadrado) => {
+    quadrado.textContent = "";
+  });
+  statusJogos = true;
+});
 
-function atualizarPlacar(){
+function atualizarPlacar() {
   nomeJogadores.innerHTML = `<p>${input1.value} X: ${vitoriaJogador1}</p>
                              <p>${input2.value} O: ${vitoriaJogador2}</p>
-                            `
+                            `;
 }
 
-function marcarQuadrado(evento){
-  if (!statusJogos){
-    return
+function marcarQuadrado(evento) {
+  if (!statusJogos) {
+    return;
   }
-  const quadrado = evento.target
-  if (marcacoesTabuleiro[quadrado.id] !== ''){
-    return
+  const quadrado = evento.target;
+  if (marcacoesTabuleiro[quadrado.id] !== "") {
+    return;
   }
-  quadrado.textContent = jogada
-  marcacoesTabuleiro[quadrado.id] = jogada
-  
-  if (verificarVitoria()){
-    if (jogada == 'X'){
-      vitoriaJogador1 += 1
+  quadrado.textContent = jogada;
+  marcacoesTabuleiro[quadrado.id] = jogada;
+
+  if (verificarVitoria()) {
+    if (jogada == "X") {
+      vitoriaJogador1 += 1;
     } else {
-      vitoriaJogador2 += 1
+      vitoriaJogador2 += 1;
     }
 
-    atualizarPlacar() 
-    statusJogos = false
-    
-    return
+    atualizarPlacar();
+    statusJogos = false;
+
+    return;
   }
 
   // verificando se deu velha
-  const velha = marcacoesTabuleiro.every((resultado)=>{
-    return resultado !== ''
-  })
-  if (velha){
-    alert('velha')
-    zerarTabuleiro()
-    return
+  const velha = marcacoesTabuleiro.every((resultado) => {
+    return resultado !== "";
+  });
+  if (velha) {
+    alert("velha");
+    zerarTabuleiro();
+    return;
   }
-  
-  alternarJogador()
+
+  alternarJogador();
 }
--- aulas-JavaScript/atividade-avancada-js/jogo-forca/desafio.js
+++ aulas-JavaScript/atividade-avancada-js/jogo-forca/desafio.js
@@ -1,102 +1,137 @@
 const palavras = [
-    {
-        palavra: "banana",
-        dica: "alimento"
-    },
-    {
-        palavra: "sabonete",
-        dica: "objeto de limpeza"
-    },
-    {
-        palavra: "computador",
-        dica: "tecnologia"
-    },
-    {
-        palavra: "avião",
-        dica: "meio de transporte"
-    },
-    {
-        palavra: "violão",
-        dica: "instrumento musical"
-    },
-    {
-        palavra: "girassol",
-        dica: "planta"
-    },
-    {
-        palavra: "astronauta",
-        dica: "profissão"
-    },
-    {
-        palavra: "pinguim",
-        dica: "animal"
-    },
-    {
-        palavra: "caneta",
-        dica: "material escolar"
-    },
-    {
-        palavra: "montanha",
-        dica: "formação geográfica"
-    },
-    {
-        palavra: "telescópio",
-        dica: "equipamento de observação"
-    },
-    {
-        palavra: "bicicleta",
-        dica: "meio de transporte"
-    },
-    {
-        palavra: "microfone",
-        dica: "equipamento de áudio"
-    },
-    {
-        palavra: "pipoca",
-        dica: "lanche"
-    },
-    {
-        palavra: "jacaré",
-        dica: "animal"
-    },
-    {
-        palavra: "hospital",
-        dica: "lugar"
-    },
-    {
-        palavra: "escola",
-        dica: "lugar"
-    },
-    {
-        palavra: "livro",
-        dica: "objeto"
-    },
-    {
-        palavra: "mochila",
-        dica: "acessório"
-    },
-    {
-        palavra: "chave",
-        dica: "objeto"
-    },
-    {
-        palavra: "tartaruga",
-        dica: "animal"
-    },
-    {
-        palavra: "planeta",
-        dica: "corpo celeste"
-    },
-    {
-        palavra: "janela",
-        dica: "parte da casa"
-    },
-    {
-        palavra: "teclado",
-        dica: "acessório de computador"
-    },
-    {
-        palavra: "relógio",
-        dica: "marca o tempo"
-    }
+  { id: 1, palavra: "banana", dica: "alimento" },
+  { id: 2, palavra: "sabonete", dica: "objeto" },
+  { id: 3, palavra: "computador", dica: "tecnologia" },
+  { id: 4, palavra: "aviao", dica: "meio de transporte" },
+  { id: 5, palavra: "violao", dica: "instrumento musical" },
+  { id: 6, palavra: "girassol", dica: "planta" },
+  { id: 7, palavra: "astronauta", dica: "profissão" },
+  { id: 8, palavra: "pinguim", dica: "animal" },
+  { id: 9, palavra: "caneta", dica: "material escolar" },
+  { id: 10, palavra: "montanha", dica: "formação geográfica" },
+  { id: 11, palavra: "telescopio", dica: "equipamento de observação" },
+  { id: 12, palavra: "bicicleta", dica: "meio de transporte" },
+  { id: 13, palavra: "microfone", dica: "equipamento de áudio" },
+  { id: 14, palavra: "pipoca", dica: "lanche" },
+  { id: 15, palavra: "jacare", dica: "animal" },
+  { id: 16, palavra: "hospital", dica: "lugar" },
+  { id: 17, palavra: "escola", dica: "lugar" },
+  { id: 18, palavra: "livro", dica: "objeto" },
+  { id: 19, palavra: "mochila", dica: "acessório" },
+  { id: 20, palavra: "chave", dica: "objeto" },
+  { id: 21, palavra: "tartaruga", dica: "animal" },
+  { id: 22, palavra: "planeta", dica: "corpo celeste" },
+  { id: 23, palavra: "janela", dica: "parte da casa" },
+  { id: 24, palavra: "teclado", dica: "acessório de computador" },
+  { id: 25, palavra: "relogio", dica: "marca o tempo" },
 ];
+const alfabeto = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Ç","Z","X","C","V","B","N","M",];
+
+let letrasUsadas = "";
+let tracinhos = [];
+
+const teclado = document.getElementById("teclado");
+const botaoJogar = document.querySelector(".btnJogar");
+const linha = document.getElementById("linha");
+const dica = document.getElementById("id_dica");
+const imagem = document.getElementById('imagem_coracao')
+
+function montarTeclado() {
+  botaoJogar.style.display = "none";
+
+  alfabeto.forEach((letra) => {
+    const botoesLetras = document.createElement("button");
+    botoesLetras.setAttribute("class", "letra");
+    botoesLetras.textContent = letra;
+    teclado.appendChild(botoesLetras);
+    // console.log(letra)
+  });
+}
+
+for(let i=0; i<6; i++){
+  var imagemCoracao = document.createElement('img')
+  imagemCoracao.src = 'coracao-mine-removebg-preview.png'
+  imagemCoracao.alt = 'imagem coração para contar vidas'
+  imagemCoracao.style.width = '5em'
+  imagemCoracao.setAttribute('id', i)
+  imagem.appendChild(imagemCoracao)
+}
+
+function iniciarJogo() {
+  montarTeclado();
+  mostrarDica();
+  montarTraco();
+
+  const letraTeclado = document.querySelectorAll(".letra");
+  letraTeclado.forEach((letra) => {
+    // console.log(letra.textContent)
+    letra.addEventListener("click", (evento) => {
+      if (imagem.childNodes.length - 3 == 0){
+        alert('já perdeu troxa')
+        return
+      }
+      letra.disabled = true;
+      letrasUsadas = letra.textContent;
+
+      let verificacaoLetra = palavraSorteadaDividida.includes(letrasUsadas);
+
+      if (verificacaoLetra) {
+        evento.target.classList.toggle("letraCerta");
+        marcarTracinho(letrasUsadas);
+      } else {
+        evento.target.classList.toggle("letraErrada");
+        imagem.removeChild(imagem.lastElementChild);
+
+      }
+
+      if (imagem.childNodes.length - 3 == 0){
+        alert('perdeu otário')
+      }
+    });
+  });
+}
+
+function sortearPalavra(max) {
+  return Math.floor(Math.random() * max);
+}
+const palavraSorteada = palavras[sortearPalavra(palavras.length)];
+// console.log(palavraSorteada.palavra)
+
+let palavraSorteadaDividida = palavraSorteada.palavra.toUpperCase().split("");
+console.log(palavraSorteadaDividida);
+
+function montarTraco() {
+  let traco = "_";
+  tracinhos = [];
+  let novoArrayTraco = palavraSorteadaDividida.map((elemento) => {
+    return traco;
+  });
+  // console.log(palavraSorteadaDividida)
+  // console.log(novoArrayTraco)
+
+  for (let linhas of novoArrayTraco) {
+    linhas = document.createElement("div");
+    linhas.setAttribute("class", "linha");
+    linhas.textContent = "_";
+    linha.appendChild(linhas);
+    tracinhos.push(linhas);
+    // console.log(linhas)
+  }
+  console.log(tracinhos);
+}
+
+function mostrarDica() {
+  let dicaSorteada = palavraSorteada.dica;
+  let dicaNaTela = document.createElement("h2");
+  dicaNaTela.setAttribute("class", "dicaNaTela");
+  dicaNaTela.textContent = `Dica: ${dicaSorteada}`;
+  dica.appendChild(dicaNaTela);
+}
+
+function marcarTracinho(letra) {
+  palavraSorteadaDividida.map((escolha, i) => {
+    if (escolha === letra) {
+      tracinhos[i].textContent = letra;
+    }
+  });
+}
