d9abb05a6b6c67b462560adca08301418370014e
Kauan-LS <kauan.silva@youxlab.com.br>
2025-07-03 16:49:13 -0300

    fin da aula-3

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -8,12 +8,14 @@ function App() {
     return a + b
   }
 
+   const url = "https://via.placeholder.com/150"
+
   return (
     <div className="App">
        <h2>Ola mundo</h2>
        <p>Ola {newName}</p>
        <p>Soma: {sum(1, 2)}</p>
-
+       <img src={url} alt="Minha imagem" />
     </div>
   )
 }
