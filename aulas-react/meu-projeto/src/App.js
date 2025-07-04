1a9ea0c669697927bfe2e6c62fda8212a4c91365
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-07-04 16:31:48 -0300

    utilizando React fragments

--- aulas-react/meu-projeto/src/App.js
+++ aulas-react/meu-projeto/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import SayMyName from "./components/SayMyName";
 import Pessoa from "./components/Pessoa";
 import Frase from "./components/Frase";
+import List from "./components/List";
 
 function App() {
   const nome = "Maria";
@@ -21,6 +22,7 @@ function App() {
         profissao="Programador"
         foto="https://picsum.photos/150"
       />
+      <List/>
     </div>
   );
 }
