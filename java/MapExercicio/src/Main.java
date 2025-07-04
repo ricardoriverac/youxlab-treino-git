98049317b3fe1f0aeaa2ed97cc7f6849ffbeb72d
julincesar02 <juliogoiabinha060@gmail.com>
2025-07-04 14:12:46 -0300

    Curso de java Udemy

--- java/MapExercicio/src/Main.java
+++ java/MapExercicio/src/Main.java
@@ -1,9 +1,6 @@
-import entities.Candidato;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
