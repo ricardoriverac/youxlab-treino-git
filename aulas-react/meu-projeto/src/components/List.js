fd5d8bb267d4839721fe560fb30790f4bb0fd00a
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-07-04 18:09:58 -0300

    avançando em props (PropTypes e defaultProps)

--- aulas-react/meu-projeto/src/components/List.js
+++ aulas-react/meu-projeto/src/components/List.js
@@ -2,14 +2,16 @@ import Item from "./Item"
 
 function List(){
     return(
-        <>
+        <div>
             <h1>Minha Lista</h1>
             <ul>
-                <Item marca='Ferrari'/>
-                <Item marca='Fiat'/>
+                <Item marca='Ferrari' ano_lancamento={1985}/>
+                <Item marca='Fiat' ano_lancamento={1964}/>
                 <Item marca='Renault'/>
+                <Item marca='Chevrolet' ano_lancamento="1999"/>
+                <Item/>
             </ul>
-        </>
+        </div>
     )
 }
 
