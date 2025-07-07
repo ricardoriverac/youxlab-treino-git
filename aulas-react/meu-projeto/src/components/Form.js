5284752fcdd9f4046738fd7e64a7de214e72152a
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-07-07 16:29:03 -0300

    useState na prática

--- aulas-react/meu-projeto/src/components/Form.js
+++ aulas-react/meu-projeto/src/components/Form.js
@@ -1,15 +1,38 @@
+import { useState } from "react";
+
 function Form() {
   function cadastrarUsuario(evento) {
-    evento.preventDefault()
-    console.log("Cadastrou o usuário!");
+    evento.preventDefault();
+    console.log(`Usuário ${name} foi cadastrado com a senha: ${password}`)
   }
+
+  const [name, setName] = useState("Tayla");
+  const [password, setPassword] = useState()
 
   return (
     <div>
       <h1>Meu cadastro:</h1>
       <form onSubmit={cadastrarUsuario}>
         <div>
-          <input type="text" placeholder="Digite o seu nome" />
+          <label htmlFor="name">Nome:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Digite o seu nome"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Senha:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Digite a sua senha"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <div>
           <input type="submit" value="Cadastrar" />
