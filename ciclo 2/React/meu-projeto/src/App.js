7ae47351255eb9d702511aab69c6b44d506288e5
alexia-assis <alexiaassis.youxlab.com.br>
2025-07-07 13:36:34 -0300

    Aula 02 React

---
+++ ciclo 2/React/meu-projeto/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  const name = 'Aléxia'
+  return (
+    <div className="App">
+     <h1>Olá React</h1>
+     <p>Meu primeiro React</p>
+     <p> Eu sou a {name}</p>
+    </div>
+
+  );
+}
+
+export default App;
