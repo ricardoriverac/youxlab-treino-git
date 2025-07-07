9c5619b960bc23fd7ebbfed2a878260e2ac4dc78
MariaLuiza78 <maria.silva@youx.com.br>
2025-07-07 17:34:46 -0300

    projeto costs react

---
+++ biblioteca/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TelaLogin from './componentes/TelaLogin';
+
+function App() {
+  return (
+    <div className="App">
+      <TelaLogin/>
+    </div>
+  );
+}
+
+export default App;
---
+++ biblioteca/src/componentes/TelaLogin.js
@@ -0,0 +1,13 @@
+import styles from './TelaLogin.module.css'
+
+function TelaLogin(){
+    return(
+        <div className={styles.telaLogin}>
+            <h2>Biblioteca Virtual</h2>
+            <label>Faça login para acessar o sistema</label>
+        </div>
+    )
+
+}
+
+export default TelaLogin
