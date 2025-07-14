8ad1dd1f9b56a444e4b9f4a3d648f68cae69de55
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-07-14 15:04:01 -0300

    react icons

--- aulas-react/meu-projeto/src/layout/Footer.js
+++ aulas-react/meu-projeto/src/layout/Footer.js
@@ -1,4 +1,17 @@
-function Footer(){
-    return <footer>Rodapé</footer>
+import styles from './Footer.module.css'
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <footer>
+        <ul className={styles.social_list}>
+            <li><FaFacebook /></li>
+            <li><FaInstagram /></li>
+            <li><FaLinkedin /></li>
+        </ul>
+        <p>Nosso rodapé</p>
+    </footer>
+  )
 }
+
 export default Footer
