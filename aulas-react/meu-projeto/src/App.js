e49f53c566c7fa2cd7743eaf955f6b76280bd770
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-07-14 13:54:20 -0300

    implementando o react router

--- aulas-react/meu-projeto/src/App.js
+++ aulas-react/meu-projeto/src/App.js
@@ -1,17 +1,22 @@
 import "./App.css";
-import Saudacao from "./components/Saudacao";
-import SeuNome from "./components/SeuNome";
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Empresa from "./pages/Empresa";
+import Contato from "./pages/Contato";
+import Navbar from "./layout/Navbar";
+import Footer from "./layout/Footer";
 
 function App() {
-  const [nome, setNome] = useState()
-
   return (
-    <div className="App">
-      <h1>State Lift</h1>
-      <SeuNome setNome={setNome}/>
-      <Saudacao nome={nome}/>
-    </div>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}></Route>
+        <Route path="/Empresa" element={<Empresa/>}></Route>
+        <Route path="/Contato" element={<Contato/>}></Route>
+      </Routes>
+      <Footer/>
+    </Router>
   );
 }
 
---
+++ aulas-react/meu-projeto/src/layout/Footer.js
@@ -0,0 +1,4 @@
+function Footer(){
+    return <footer>Rodapé</footer>
+}
+export default Footer
