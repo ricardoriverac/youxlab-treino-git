db746cc1903b40f342610a2cd723d7e712ecdaa8
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-07-16 17:19:00 -0300

    página para o formulário do projeto

--- aulas-react/costs/src/components/pages/NewProject.js
+++ aulas-react/costs/src/components/pages/NewProject.js
@@ -7,24 +7,6 @@ function NewProject() {
       <h1>Criar Projeto</h1>
       <p>Crie seu projeto para depois adicionar os serviços</p>
       <ProjectForm />
-      <form>
-        <div>
-          <input type="text" placeholder="Insira o nome do projeto" />
-        </div>
-        <div>
-          <input type="number" placeholder="Insira o orçamento total" />
-        </div>
-        <div>
-          <select name="category_id">
-            <option disabled selected>
-              Selecione a categoria
-            </option>
-          </select>
-        </div>
-        <div>
-          <input type="submit" value="Criar projeto" />
-        </div>
-      </form>
     </div>
   );
 }
