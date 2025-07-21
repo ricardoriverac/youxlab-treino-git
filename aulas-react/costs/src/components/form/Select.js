72aed2aaab96b56cb482edb1f33c96c800964f53
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-07-21 12:37:14 -0300

    conectando com API pelo react

--- aulas-react/costs/src/components/form/Select.js
+++ aulas-react/costs/src/components/form/Select.js
@@ -1,4 +1,4 @@
-import styles from './Select.module.css'
+import styles from "./Select.module.css";
 
 function Select({ text, name, options, handleOnChange, value }) {
   return (
@@ -6,6 +6,13 @@ function Select({ text, name, options, handleOnChange, value }) {
       <label htmlFor={name}>{text}:</label>
       <select name={name} id={name}>
         <option>Selecione uma opção</option>
+
+        {options.map((option) => (
+          <option value={option.id} key={option.id}>
+            {option.name}
+          </option>
+        ))}
+        
       </select>
     </div>
   );
