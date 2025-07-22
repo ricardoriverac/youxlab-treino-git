fb2a8c0634f22c1609f095ea13d9d3cee9561196
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-07-22 13:19:40 -0300

    resgatando projetos do banco de dados

--- aulas-react/costs/src/components/pages/Projects.js
+++ aulas-react/costs/src/components/pages/Projects.js
@@ -1,27 +1,50 @@
-import { useLocation } from "react-router-dom";
+import { data, useLocation } from "react-router-dom";
+
+import { useState, useEffect } from "react";
 
 import Message from "../layout/Message";
 import Container from "../layout/Container";
 import LinkButton from "../layout/LinkButton";
+import ProjectCard from "../project/ProjectCard";
 
-import styles from './Projects.module.css'
+import styles from "./Projects.module.css";
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+
   const location = useLocation();
   let message = "";
   if (location.state) {
     message = location.state.message;
   }
 
+  useEffect(() => {
+    fetch("http://localhost:5000/projects", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        setProjects(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className={styles.project_container}>
       <div className={styles.title_container}>
         <h1>Meus Projetos</h1>
-        <LinkButton to='/newproject' text='Criar Projeto'/>
+        <LinkButton to="/newproject" text="Criar Projeto" />
       </div>
-      {message && <Message type='sucess' msg={message} />}
-      <Container customClass='start'>
-        <p>Projetos...</p>
+      {message && <Message type="sucess" msg={message} />}
+      <Container customClass="start">
+        {projects.length > 0 &&
+          projects.map((project) => {
+            <ProjectCard name={project.name} />;
+          })}
       </Container>
     </div>
   );
---
+++ aulas-react/costs/src/components/project/ProjectCard.js
@@ -0,0 +1,9 @@
+import styles from "./ProjectCard.module.css";
+
+import { BsPencil, BsFillTrashFill } from "react-icons/bs";
+
+function ProjectCard({ id, name, budget, category, handleRemove }) {
+  return <p>{name}</p>;
+}
+
+export default ProjectCard;
