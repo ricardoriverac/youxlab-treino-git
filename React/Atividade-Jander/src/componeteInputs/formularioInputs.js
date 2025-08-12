4f245886686b75e5008d4f28f97134ae10c2773b
Kauan-LS <kauan.silva@youxlab.com.br>
2025-08-12 13:10:14 -0300

    Reinstalando-Vscode

---
+++ React/Atividade-Jander/src/componeteInputs/formularioInputs.js
@@ -0,0 +1,42 @@
+import styles from "./inputs.module.css";
+
+function FormularioInputs({
+  nome,
+  cpf,
+  data,
+  setNome,
+  setCpf,
+  setData,
+  adicionar,
+}) {
+  return (
+    <div>
+      <input
+        className={styles.dados}
+        type="text"
+        value={nome}
+        onChange={(e) => setNome(e.target.value)}
+        placeholder="Seu nome"
+      />
+      <input
+        className={styles.dados}
+        type="text"
+        value={cpf}
+        onChange={(e) => setCpf(e.target.value)}
+        placeholder="Seu CPF"
+      />
+      <input
+        className={styles.dados}
+        type="date"
+        value={data}
+        onChange={(e) => setData(e.target.value)}
+        placeholder="Sua data de nascimento"
+      />
+      <button className={styles.butonA} onClick={adicionar}>
+        Adicionar
+      </button>
+    </div>
+  );
+}
+
+export default FormularioInputs;
