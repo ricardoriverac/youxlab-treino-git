95d311401d3eddbb9b053083d9215ef9249fecae
MariaLuiza78 <maria.silva@youx.com.br>
2025-08-12 14:53:30 -0300

    tabela axios

--- tabela-db-json/src/App.jsx
+++ tabela-db-json/src/App.jsx
@@ -2,8 +2,7 @@ import { useState, useEffect } from "react";
 import Form from "./componentes/Form";
 import Tabela from "./componentes/Tabela";
 import "./App.module.css";
-import { getUser, getResp, cadastrarUser, getUsers } from "../src/service/api";
-import { all } from "axios";
+import { cadastrarUser, getUsers } from "../src/service/api";
 
 function App() {
   const [nome, setNome] = useState("");
@@ -11,6 +10,7 @@ function App() {
   const [estadoCivil, setEstadoCivil] = useState("");
   const [cpf, setCpf] = useState("");
   const [dados, setDados] = useState([]);
+  const [msgErro, setMsgErro] = useState(false);
 
   useEffect(() => {
     getUsers().then((resp) => {
@@ -35,78 +35,66 @@ function App() {
   };
 
   function salvarDados() {
-    cadastrarUser(nome, idade, estadoCivil, cpf, dados, setDados);
-    // let pessoa = {
-    //   nome: nome,
-    //   idade: idade,
-    //   estadoCivil: estadoCivil,
-    //   cpf: cpf,
-    //   id: 1
-    // };
+    if (nome === "" || idade === "" || estadoCivil === "" || cpf === "") {
+      alert("Preencha todos os campos antes de salvar!");
+    } else {
+      cadastrarUser(nome, idade, estadoCivil, cpf, dados, setDados);
+      // let pessoa = {
+      //   nome: nome,
+      //   idade: idade,
+      //   estadoCivil: estadoCivil,
+      //   cpf: cpf,
+      //   id: 1
+      // };
 
-    // setDados([...dados, pessoa]);
+      // setDados([...dados, pessoa]);
 
-    console.log(dados);
-    setNome("");
-    setIdade("");
-    setEstadoCivil("");
-    setCpf("");
+      console.log(dados);
+      setNome("");
+      setIdade("");
+      setEstadoCivil("");
+      setCpf("");
+    }
   }
 
-  function cpfMask(cpf) {
-    const listaCpf = [];
-    let cpfForm = cpf.split("");
-    if (cpfForm[3]) {
-      const listaUm = [cpfForm[0], cpfForm[1], cpfForm[2]];
-      listaCpf.push(listaUm);
-      listaCpf.push(".");
-    }
-    if (cpfForm[6]) {
-      const listaDois = [cpfForm[3], cpfForm[4], cpfForm[5]];
-      listaCpf.push(listaDois);
-      listaCpf.push(".");
-    }
-    if (cpfForm[9]) {
-      const listaTres = [cpfForm[6], cpfForm[7], cpfForm[8]];
-      listaCpf.push(listaTres);
-      listaCpf.push("-");
-    }
-    if (cpfForm[10]) {
-      const listaQuatro = [cpfForm[9], cpfForm[10]]
-      listaCpf.push(listaQuatro);
-    }
-    return listaCpf.toString().replaceAll(",", "");
+  const listaCpf = [];
+  let cpfForm = cpf.split("");
+  const listaUm = [cpfForm[0], cpfForm[1], cpfForm[2]];
+  listaCpf.push(listaUm);
+
+  if (cpfForm[2]) {
+    listaCpf.push(".");
+  }
+  const listaDois = [cpfForm[3], cpfForm[4], cpfForm[5]];
+  listaCpf.push(listaDois);
+
+  if (cpfForm[5]) {
+    listaCpf.push(".");
+  }
+  const listaTres = [cpfForm[6], cpfForm[7], cpfForm[8]];
+  listaCpf.push(listaTres);
+
+  if (cpfForm[8]) {
+    listaCpf.push("-");
   }
 
-  // const cpfMask = () => {
-  //   let tamanhoCpf = cpf.length;
-  //   let cpfFormatado = cpf;
-  //   let arrayCpf = cpf.split("");
-  //   console.log(tamanhoCpf);
-  //   console.log(arrayCpf);
-  //   if (tamanhoCpf === 4) {
-  //     arrayCpf[4] = arrayCpf[3];
-  //     arrayCpf[3] = ".";
-  //     console.log(arrayCpf);
-  //     cpfFormatado = arrayCpf.toString().replaceAll(",", "");
-  //     tamanhoCpf = cpfFormatado.length;
-  //     console.log(arrayCpf.toString().replaceAll(",", ""));
-  //   } else if (tamanhoCpf === 8) {
-  //     arrayCpf[4] = arrayCpf[3];
-  //     arrayCpf[3] = ".";
-  //     arrayCpf[8] = arrayCpf[7];
-  //     arrayCpf[7] = ".";
-  //     console.log(arrayCpf.toString().replaceAll(",", ""));
-  //   } else if (tamanhoCpf === 12) {
-  //     arrayCpf[4] = arrayCpf[3];
-  //     arrayCpf[3] = ".";
-  //     arrayCpf[8] = arrayCpf[7];
-  //     arrayCpf[7] = ".";
-  //     arrayCpf[12] = arrayCpf[11];
-  //     arrayCpf[11] = ".";
-  //     console.log(arrayCpf.toString().replaceAll(",", ""));
-  //   }
-  // };
+  if (cpfForm[9]) {
+    const listaQuatro = [cpfForm[9], cpfForm[10]];
+    listaCpf.push(listaQuatro);
+  }
+
+  let listaCpfForm = listaCpf.toString().replaceAll(",", "");
+  let listaCpfFormArray = [...listaCpfForm];
+  listaCpfFormArray.forEach((elem) => {
+    setCpf(elem)
+  })
+  console.log(listaCpfFormArray)
+
+  // listaCpfFormArray.map((elem) => {
+  //   setCpf(elem)
+  // });
+
+  // listaCpf.toString().replaceAll(",", "");
 
   return (
     <>
@@ -143,7 +131,7 @@ function App() {
         onChange={handleChangeCpf}
       />
 
-      <h1>{cpfMask(cpf)}</h1>
+      {/* <h1>{cpfMask(cpf)}</h1> */}
 
       <button onClick={salvarDados}>Salvar</button>
 
@@ -153,3 +141,33 @@ function App() {
 }
 
 export default App;
+
+// const cpfMask = () => {
+//   let tamanhoCpf = cpf.length;
+//   let cpfFormatado = cpf;
+//   let arrayCpf = cpf.split("");
+//   console.log(tamanhoCpf);
+//   console.log(arrayCpf);
+//   if (tamanhoCpf === 4) {
+//     arrayCpf[4] = arrayCpf[3];
+//     arrayCpf[3] = ".";
+//     console.log(arrayCpf);
+//     cpfFormatado = arrayCpf.toString().replaceAll(",", "");
+//     tamanhoCpf = cpfFormatado.length;
+//     console.log(arrayCpf.toString().replaceAll(",", ""));
+//   } else if (tamanhoCpf === 8) {
+//     arrayCpf[4] = arrayCpf[3];
+//     arrayCpf[3] = ".";
+//     arrayCpf[8] = arrayCpf[7];
+//     arrayCpf[7] = ".";
+//     console.log(arrayCpf.toString().replaceAll(",", ""));
+//   } else if (tamanhoCpf === 12) {
+//     arrayCpf[4] = arrayCpf[3];
+//     arrayCpf[3] = ".";
+//     arrayCpf[8] = arrayCpf[7];
+//     arrayCpf[7] = ".";
+//     arrayCpf[12] = arrayCpf[11];
+//     arrayCpf[11] = ".";
+//     console.log(arrayCpf.toString().replaceAll(",", ""));
+//   }
+// };
