1c382f3b7ea7830759189c6a7b235ce057289f75
Youx <sophia.carvalho@youxlab.com.br>
2025-08-27 17:23:40 -0300

    React-aula5

--- React_JS/meu-projeto/src/App.js
+++ React_JS/meu-projeto/src/App.js
@@ -1,28 +1,23 @@
-import './App.css';
-import HelloWorld from './Components/HelloWorld';
-import Frase from './Components/Frase';
+import "./App.css";
+import HelloWorld from "./Components/HelloWorld";
+import SayMyName from "./Components/SayMyName";
+import Pessoa from "./Components/Pessoa";
 
 function App() {
-
-  const nome = "Sophia"
-
-  const novoNome= nome.toLocaleUpperCase()
-
-  const url = 'https://via.placeholder.com/150'
-
-  function soma(x,y){
-    return x + y
-  }
+  const nome = "Yago";
 
   return (
     <div className="App">
-      <h2>Alterando o JSX</h2>
-      <p>Olá,{novoNome}!</p>
-      <p>Soma = {soma(3,5 )}</p>
-      <img src={url} alt='img'/>
-
-      <HelloWorld/>
-      <Frase/>
+      <HelloWorld />
+      <SayMyName nome="Sophia" />
+      <SayMyName nome="Sarah" />
+      <SayMyName nome={nome} />
+      <Pessoa
+        nome="Elena"
+        idade="17"
+        profissao="Product"
+        foto="https://via.placeholder.com/150"
+      />
     </div>
   );
 }
