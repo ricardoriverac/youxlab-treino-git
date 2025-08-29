6f75d80e2071f737ee6e4c99eceece0ea747813a
Youx <sophia.carvalho@youxlab.com.br>
2025-08-28 22:31:21 -0300

    React-aula11

--- React_JS/meu-projeto/src/Components/Evento.js
+++ React_JS/meu-projeto/src/Components/Evento.js
@@ -1,13 +1,23 @@
-function Evento({numero}){
+import Button from "./eventComponents/Button";
+
+function Evento({}){
 
     function meuEvento(){
-        console.log(`Foi Ativado! ${numero}`);
+        console.log(`Ativando o primeiro evento!`);
+    }
+
+    function segundoEvento(){
+        console.log('Ativando o segundo evento!');
     }
 
     return(
         <div>
             <p>Clique para disparar um evento:</p>
-            <button onClick={meuEvento}>Ativar!</button>
+
+            {/* pode reaproveitar componentes!! basta mudar os dados  */}
+            <Button evento={meuEvento} text="Primeiro Evento" />    
+            <Button evento={segundoEvento} text="Segundo Evento" />  
+
         </div>
     )
 }
