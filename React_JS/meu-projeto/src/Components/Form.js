9c1d91fe66c3ffa3cb67e99379e53e56f77d29fc
Youx <sophia.carvalho@youxlab.com.br>
2025-08-28 22:12:37 -0300

    React-aula10

--- React_JS/meu-projeto/src/Components/Form.js
+++ React_JS/meu-projeto/src/Components/Form.js
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 function Form() {
+
+    const [name, setName] = useState() 
+    const [password, setPassword] = useState() 
+
+
   function cadastrarUsuario(event) {
-    event.preventDefault(); 
-    console.log("Usuário Cadastrado!");
+    event.preventDefault();
+    console.log(`Usuário: ${name}`);
+    console.log(`Senha: ${password}`);
   }
 
   return (
@@ -9,7 +17,24 @@ function Form() {
       <h1>Meu Cadastro:</h1>
       <form onSubmit={cadastrarUsuario}>
         <div>
-          <input type="text" placeholder="Digite seu nome" />
+          <label htmlFor="name">Nome:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Digite seu nome"
+            onChange={(e) => setName(e.target.value)}   // a cada letra digitada, modifica o valor do state 
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Senha:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Digite sua senha"
+            onChange={(e) => setPassword(e.target.value)}   // a cada letra digitada, modifica o valor do state 
+          />
         </div>
         <div>
           <input type="submit" value="Cadastrar" />
