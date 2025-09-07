bc185c90c492d02e146def4355f1b3eeb872b519
marcos-garcia-lab <marcos.garcia@youxlab.com.br>
2025-09-07 13:34:52 -0300

    Atualizações curso springboot

---
+++ Ciclo_2/SpringBoot/seção 3/libraryapi/src/main/java/io/githubGarcia/libraryapi/LibraryapiApplication.java
@@ -0,0 +1,14 @@
+package io.githubGarcia.libraryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryapiApplication.class, args);
+        System.out.println("Rodando");
+	}
+
+}
---
+++ Ciclo_2/SpringBoot/seção 3/libraryapi/src/main/java/io/githubGarcia/libraryapi/Model/Autor.java
@@ -0,0 +1,32 @@
+package io.githubGarcia.libraryapi.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "autor")
+@Getter
+@Setter
+@AllArgsConstructor
+public class Autor {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "nacionalidade", length = 50, nullable = false)
+    private String nacionalidade;
+
+}
