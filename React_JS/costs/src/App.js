0f9c408d421a4730bf49069b1eb6829cd2afcd1d
Youx <sophia.carvalho@youxlab.com.br>
2025-09-08 15:25:47 -0300

    React-aula19

--- React_JS/costs/src/App.js
+++ React_JS/costs/src/App.js
@@ -3,29 +3,28 @@ import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
+import Projects from "./components/pages/Projects";
 
 import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <ul>
-          <Link to="/">Home</Link>
-          <Link to="/company">Company</Link>
-          <Link to="/contact">Contact</Link>
-          <Link to="/newproject">New Project</Link>
-        </ul>
+        <Navbar />
         <Container customClass="min-height">
           <Routes>
             <Route exact path="/" element={<Home />} />
+            <Route exact path="/projects" element={<Projects />} />
             <Route exact path="/company" element={<Company />} />
             <Route exact path="/contact" element={<Contact />} />
             <Route exact path="/newproject" element={<NewProject />} />
           </Routes>
         </Container>
-        <p>footer</p>
-      </div>  
+        <Footer />
+      </div>
     </Router>
   );
 }
