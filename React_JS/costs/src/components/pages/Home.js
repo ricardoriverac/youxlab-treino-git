1fb159c929eaf05b77663ae748167ea1fe6f13d5
Youx <sophia.carvalho@youxlab.com.br>
2025-09-08 17:07:04 -0300

    React-aula20

--- React_JS/costs/src/components/pages/Home.js
+++ React_JS/costs/src/components/pages/Home.js
@@ -1,7 +1,18 @@
-function Home(){
-    return(
-        <h1>Home</h1>
-    )
+import styles from "./Home.module.css";
+import savings from "../../img/savings.svg";
+import LinkButton from "../layout/LinkButton";
+
+function Home() {
+  return (
+    <section className={styles.home_container   }>
+      <h1>
+        Bem-vindo ao <span>Costs</span>
+      </h1>
+      <p>Começe a gerenciar os seus projetos agora mesmo!</p>
+      <LinkButton to="/newproject" text="Criar Projeto"/> 
+      <img src={savings} alt="Costs" />
+    </section>
+  );
 }
 
-export default Home
+export default Home;
