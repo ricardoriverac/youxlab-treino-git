b64dd8d846097a37a0ef298d6a6b3c9e50a21c94
taylalisboa <tayla.bianchini@youxlab.com.br>
2025-09-16 19:51:34 -0300

    criando área de serviços

--- aulas-react/costs/src/components/pages/Project.js
+++ aulas-react/costs/src/components/pages/Project.js
@@ -12,6 +12,7 @@ function Project() {
 
   const [project, setProject] = useState([]);
   const [showProjectForm, setShowProjectForm] = useState(false);
+  const [showServiceForm, setShowServiceForm] = useState(false);
   const [message, setMessage] = useState();
   const [type, setType] = useState();
 
@@ -32,6 +33,8 @@ function Project() {
   }, [id]);
 
   function editPost(project) {
+    setMessage("");
+
     // budget validation
     if (project.budget < project.cost) {
       setMessage("O orçamento não pode ser menor que o custo do projeto!");
@@ -58,6 +61,10 @@ function Project() {
 
   function toggleProjectForm() {
     setShowProjectForm(!showProjectForm);
+  }
+
+  function toggleServiceForm() {
+    setShowServiceForm(!showServiceForm);
   }
 
   return (
@@ -93,6 +100,19 @@ function Project() {
                 </div>
               )}
             </div>
+            <div className={styles.service_form_container}>
+              <h2>Adicione um serviço:</h2>
+              <button className={styles.btn} onClick={toggleServiceForm}>
+                {!showServiceForm ? "Adicionar serviço" : "Fechar"}
+              </button>
+              <div className={styles.project_info}>
+                {showServiceForm && <div>formulário do serviço</div>}
+              </div>
+            </div>
+            <h2>Serviços</h2>
+            <Container customClass="start">
+              <p>Itens de serviço</p>
+            </Container>
           </Container>
         </div>
       ) : (
