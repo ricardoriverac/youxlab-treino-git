8576d44e0284709ad35bf0399eb35574224a88d0
Youx <sophia.carvalho@youxlab.com.br>
2025-09-15 22:34:44 -0300

    React-aula33

--- React_JS/costs/src/components/pages/Project.js
+++ React_JS/costs/src/components/pages/Project.js
@@ -13,6 +13,7 @@ function Project() {
 
   const [project, setProject] = useState([]);
   const [showProjectForm, setShowProjectForm] = useState(false);
+  const [showServiceForm, setShowServiceForm] = useState(false);
   const [message, setMessage] = useState();
   const [type, setType] = useState();
 
@@ -33,8 +34,9 @@ function Project() {
   }, [id]);
 
   function editPost(project) {
-    //budget validation
+    setMessage("");
 
+    //budget validation
     if (project.budget < project.cost) {
       setMessage("O orçamento não pode ser menor que o custo do projeto!");
       setType("error");
@@ -60,6 +62,10 @@ function Project() {
 
   function toggleProjectForm() {
     setShowProjectForm(!showProjectForm); // recebe o valor contrário do inicial
+  }
+
+  function toggleServiceForm() {
+    setShowServiceForm(!showServiceForm); // recebe o valor contrário do inicial
   }
 
   return (
@@ -98,6 +104,22 @@ function Project() {
                 </div>
               )}
             </div>
+            <div className={styles.service_form_container}>
+              <h2>Adicione um serviço:</h2>
+
+              <button className={styles.btn} onClick={toggleServiceForm}>
+                {!showServiceForm ? "Adicionar serviço" : "Fechar"}
+              </button>
+
+              <div className={styles.project_info}>
+                {showServiceForm && <div>formulário do serviço</div>}
+              </div>
+            </div>
+
+            <h2>serviços</h2>
+            <Container customClass="start">
+              <p> Itens de serviços</p>
+            </Container>
           </Container>
         </div>
       ) : (
