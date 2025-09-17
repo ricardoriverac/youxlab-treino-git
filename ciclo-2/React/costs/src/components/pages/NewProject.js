6a4019578590612655c3384e092d457fe2def2e2
alexia-assis <alexia.assis@youxlab.com.br>
2025-09-17 15:19:40 -0300

    Finalizando e iniciando backend dbjson

--- ciclo-2/React/costs/src/components/pages/NewProject.js
+++ ciclo-2/React/costs/src/components/pages/NewProject.js
@@ -1,29 +1,37 @@
-import ProjectForm from '../project/ProjectForms'
+import { useNavigate } from 'react-router-dom'
+
+import ProjectForms from '../project/ProjectForms'
+
 import styles from './NewProject.module.css'
-import {useNavigate}  from 'react-router-dom'
 
-function NewProject () {
+function NewProject() {
+  const navigate = useNavigate()
 
-    const navigate= useNavigate()
-
-function createPost(project){
-    //intialize cost and services
-
+  function createPost(project) {
+    // initialize cost and services
     project.cost = 0
     project.services = []
 
-    fetch("")
-}
+    fetch('http://localhost:5000/projects', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        navigate('/projects', { message: 'Projeto criado com sucesso!' })
+      })
+  }
 
-
-
-    return (
+  return (
     <div className={styles.newproject_container}>
-        <h1>Novo projeto</h1>
-        <p> Crie seu projeto para depois adicionar serviços</p>
-        <ProjectForm btnText = "Criar Projeto" />
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForms handleSubmit={createPost} btnText="Criar Projeto" />
     </div>
-    )
+  )
 }
 
 export default NewProject
